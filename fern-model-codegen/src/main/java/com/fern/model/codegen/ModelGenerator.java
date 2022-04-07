efcbb686b4785367774fbffeda3f455848b90d5f
Deeptanshu Singhvi <dsinghvi@umich.edu>
2022-04-07 11:24:54 -0700

    improvement: init library (#1)
    * init library
    
    * retry compile
    
    * add check and unit test

---
+++ fern-model-codegen/src/main/java/com/fern/model/codegen/ModelGenerator.java
@@ -0,0 +1,11 @@
+package com.fern.model.codegen;
+
+import com.fern.TypeDefinition;
+
+import java.util.List;
+
+public final class ModelGenerator {
+
+    public static void generate(List<TypeDefinition> typeDefinitions) {
+    }
+}
