d0d7c18dec59fef20d1756f9bbadad42a2ead5e2
Zach Kirsch <zachkirsch@gmail.com>
2022-04-11 16:44:01 -0700

    Add immutable flag to yarn install

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
     "**/*.{ts,tsx}": "eslint --fix --max-warnings 0",
     "**/*.{css,scss}": "stylelint --fix --max-warnings 0",
     "**/{*,_}": "prettier --write --ignore-unknown",
-    "**/{*,__}": () => "yarn install",
+    "**/{*,__}": () => "yarn install --immutable",
 };
