13026eea306344f1146199c01adeaf736f5e54c7
Deep Singhvi <dsinghvi@umich.edu>
2022-04-15 14:44:55 -0700

    fix codegen

--- packages/fern-java/codegen.sh
+++ packages/fern-java/codegen.sh
@@ -1,10 +1,11 @@
 # Generate fern ir
-cd ../../ && ./scripts/codegen.sh
+sh ../../scripts/codegen.sh
 
 # Download java codegen plugin and invoke
 version=$1
+rm -rf fern-model-codegen-"$version"-"$version"
 wget --user "$JFROG_USERNAME" --password "$JFROG_API_KEY" \
   https://usebirch.jfrog.io/artifactory/default-maven-local/com/fern/java/fern-model-codegen/"$version"/fern-model-codegen-"$version".zip
 unzip fern-model-codegen-"$version".zip
 cd fern-model-codegen-"$version"-"$version"
-java -cp fern-model-codegen-"$version".jar:lib/* com.fern.model.codegen.ModelGeneratorCli ../../api/src/fern.yml
+java -cp fern-model-codegen-"$version".jar:lib/* com.fern.model.codegen.ModelGeneratorCli ../../api/generated/ir.json
