37aad551a5356b908fad9a3ab36dff4d90613671
Zach Kirsch <zachkirsch@gmail.com>
2022-04-14 18:50:18 -0700

    Revert "Remove codegen yarn package"
    This reverts commit fb46e46f7d8af891e9ee983c5c8d12b50ca6494c.

--- packages/api/codegen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-/bin/rm -r generated
-mkdir generated
-
-npx --ignore-existing @fernapi/fern generate src generated/ir.json
-npx --ignore-existing @fernapi/fern-typescript model generated/ir.json generated/typescript
