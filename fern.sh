36cbf30c1db5056bab24457494d3f04e099cff8a
Zach Kirsch <zachkirsch@gmail.com>
2022-04-15 15:46:03 -0700

    Fix fern.sh cleanup

--- fern.sh
+++ fern.sh
@@ -2,8 +2,5 @@
 
 set -e
 
-/bin/rm -rf packages/api/generated
-mkdir packages/api/generated
-
 npx --yes @fernapi/fern generate packages/api/src packages/api/generated/ir.json
 npx --yes @fernapi/fern-typescript model packages/api/generated/ir.json packages/api/generated/typescript
