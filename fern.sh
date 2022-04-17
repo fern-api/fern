edf0e3a6fbf2dbe20169b0799051cebdfd89c17f
Zach Kirsch <zachkirsch@gmail.com>
2022-04-17 02:34:03 -0700

    Fix script

--- fern.sh
+++ fern.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-npx --yes @fernapi/fern@1.1.12 generate packages/api/src packages/api/generated/ir.json
+npx --yes fern@0.0.1 generate packages/api/src packages/api/generated/ir.json
 npx --yes fern-typescript model packages/api/generated/ir.json packages/api/generated/typescript
