bb910453f86adfb8cd17721a1384019c7945bd3b
Zach Kirsch <zachkirsch@gmail.com>
2022-04-18 11:34:36 -0700

    Include java in codegen script

--- fern.sh
+++ fern.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-npx --yes fern-api@0.0.1 generate packages/api/src packages/api/generated/ir.json
+npx --yes fern-api@0.0.2 generate packages/api/src packages/api/generated/ir.json
 npx --yes fern-typescript model packages/api/generated/ir.json packages/api/generated/typescript
