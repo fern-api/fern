cdb7d3aaa8e1a251ede514eca024280c077f3907
Zach Kirsch <zachkirsch@gmail.com>
2022-04-20 20:29:00 -0700

    Add error args

--- packages/compiler/syntax-analysis/src/schemas/ErrorDefinitionSchema.ts
+++ packages/compiler/syntax-analysis/src/schemas/ErrorDefinitionSchema.ts
@@ -3,10 +3,10 @@ import { ErrorArgumentSchema } from "./ErrorArgumentSchema";
 import { HttpErrorConfigurationSchema } from "./HttpErrorConfigurationSchema";
 import { WithDocsSchema } from "./utils/WithDocsSchema";
 
-export const ErrorSchema = WithDocsSchema.extend({
+export const ErrorDefinitionSchema = WithDocsSchema.extend({
     http: z.optional(HttpErrorConfigurationSchema),
     bodyType: z.optional(z.string()),
     args: z.optional(z.record(ErrorArgumentSchema)),
 });
 
-export type ErrorSchema = z.infer<typeof ErrorSchema>;
+export type ErrorDefinitionSchema = z.infer<typeof ErrorDefinitionSchema>;
--- packages/compiler/syntax-analysis/src/schemas/RawFernConfigurationSchema.ts
+++ packages/compiler/syntax-analysis/src/schemas/RawFernConfigurationSchema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { ErrorSchema } from "./ErrorSchema";
+import { ErrorDefinitionSchema } from "./ErrorDefinitionSchema";
 import { IdSchema } from "./IdSchema";
 import { ServicesSchema } from "./ServicesSchema";
 import { TypeDefinitionSchema } from "./TypeDefinitionSchema";
@@ -9,7 +9,7 @@ export const RawFernConfigurationSchema = z.strictObject({
     ids: z.optional(z.array(IdSchema)),
     types: z.optional(z.record(TypeDefinitionSchema)),
     services: z.optional(ServicesSchema),
-    errors: z.optional(z.record(ErrorSchema)),
+    errors: z.optional(z.record(ErrorDefinitionSchema)),
 });
 
 export type RawFernConfigurationSchema = z.infer<typeof RawFernConfigurationSchema>;
