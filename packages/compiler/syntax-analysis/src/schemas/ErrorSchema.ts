5ae66c7356f9bfe26b505efc785b24afa2ecd13d
Zach Kirsch <zachkirsch@gmail.com>
2022-04-20 20:17:47 -0700

    Fix bugs in zod error schemas

--- packages/compiler/syntax-analysis/src/schemas/ErrorSchema.ts
+++ packages/compiler/syntax-analysis/src/schemas/ErrorSchema.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
+import { ErrorArgumentSchema } from "./ErrorArgumentSchema";
 import { HttpErrorConfigurationSchema } from "./HttpErrorConfigurationSchema";
 import { WithDocsSchema } from "./utils/WithDocsSchema";
 
 export const ErrorSchema = WithDocsSchema.extend({
     http: z.optional(HttpErrorConfigurationSchema),
     bodyType: z.optional(z.string()),
+    args: z.optional(z.record(ErrorArgumentSchema)),
 });
 
 export type ErrorSchema = z.infer<typeof ErrorSchema>;
