d60f6f9030fa9dbeff6c1bc98e5cd3e699f9f9e4
Zach Kirsch <zachkirsch@gmail.com>
2022-04-29 17:44:00 -0700

    Add logging

--- fern.sh
+++ fern.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-npx --yes fern-api@0.0.23 generate packages/api/src packages/api/generated/ir.json
-npx --yes fern-typescript@0.0.23 model packages/api/generated/ir.json packages/api/generated/ir
+npx --yes fern-api@0.0.24 generate packages/api/src packages/api/generated/ir.json
+npx --yes fern-typescript@0.0.24 model packages/api/generated/ir.json packages/api/generated/ir
 
 cat > packages/api/generated/ir/package.json <<EOL
 {
