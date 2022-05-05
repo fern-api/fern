49b3fce0f5fa9cf14c6de1c04700d49cc723492a
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-05-05 00:21:00 -0700

    Use webpack for Dockerizing (#128)
    * Use webpack
    
    * Update versioning
    
    * Fix depcheck maybe
    
    * Add terser-webpack-plugin
    
    * Fix depcheck
    
    * Lint monorepo
    
    * Rearrange vars

--- cli/docker/create_docker.sh
+++ cli/docker/create_docker.sh
@@ -1,30 +1,17 @@
 #!/usr/bin/env bash
-tag=$1
+
 set -e
 
-DOCKER_NAME=fern-typescript:"$tag"
+TAG="$1"
+DOCKER_NAME=fern-typescript:"$TAG"
 
 PACKAGE_DIR="$(pwd)"
 DOCKER_DIR="$PACKAGE_DIR/docker"
-MONOREPO_SETUP_DIR="$DOCKER_DIR/monorepo"
-ARTIFACTS_DIR="$MONOREPO_SETUP_DIR/artifacts"
-MONOREPO_ROOT="$PACKAGE_DIR/../../.."
+WEBPACK_CONFIG="$DOCKER_DIR/webpack.config.js"
 
 yarn
 yarn workspaces foreach --all --verbose --parallel --topological run compile
-
-rm -rf "$MONOREPO_SETUP_DIR"
-mkdir "$MONOREPO_SETUP_DIR"
-
-mkdir "$ARTIFACTS_DIR"
-yarn workspaces foreach --all --no-private pack --out "$ARTIFACTS_DIR/%s-%v.tgz"
-
-cd "$MONOREPO_ROOT"
-cp -R yarn.lock .yarnrc.yml "$MONOREPO_SETUP_DIR"
-mkdir "$MONOREPO_SETUP_DIR/.yarn"
-cp -R .yarn/plugins .yarn/releases "$MONOREPO_SETUP_DIR/.yarn"
-
-cd "$DOCKER_DIR"
+yarn webpack --config "$WEBPACK_CONFIG"
 docker build -f Dockerfile -t "$DOCKER_NAME" "$DOCKER_DIR"
 
 echo
