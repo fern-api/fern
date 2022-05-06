0c19de995dee55664c96678ca0f4dfb799bf1c87
Zach Kirsch <zachkirsch@gmail.com>
2022-05-06 14:02:24 -0700

    Never minify

--- packages/fern-typescript/cli/docker/create_docker.sh
+++ packages/fern-typescript/cli/docker/create_docker.sh
@@ -9,9 +9,14 @@ PACKAGE_DIR="$(pwd)"
 DOCKER_DIR="$PACKAGE_DIR/docker"
 WEBPACK_CONFIG="$DOCKER_DIR/webpack.config.js"
 
+webpack_mode="production"
+if [[ "$TAG" == "local" ]]; then
+	webpack_mode="development"
+fi
+
 yarn
 yarn workspaces foreach --all --verbose --parallel --topological run compile
-yarn webpack --config "$WEBPACK_CONFIG"
+yarn webpack --config "$WEBPACK_CONFIG" --mode "$webpack_mode"
 docker build -f "$DOCKER_DIR/Dockerfile" -t "$DOCKER_NAME" "$DOCKER_DIR"
 
 echo
--- packages/fern-typescript/cli/docker/webpack.config.js
+++ packages/fern-typescript/cli/docker/webpack.config.js
@@ -1,23 +1,28 @@
 const path = require("path");
 const SimpleProgressWebpackPlugin = require("simple-progress-webpack-plugin");
 
-module.exports = {
-    mode: process.env.NODE_ENV,
-    target: "node",
-    entry: path.join(__dirname, "../lib/cli.js"),
-    output: {
-        path: __dirname,
-        filename: "bundle.js",
-    },
-    // https://github.com/dsherret/ts-morph/issues/171#issuecomment-1107867732
-    module: {
-        rules: [
-            {
-                test: /node_modules[\\|/]code-block-writer[\\|/]umd[\\|/]/,
-                use: { loader: "umd-compat-loader" },
-            },
-        ],
-        noParse: [require.resolve("@ts-morph/common/dist/typescript.js")],
-    },
-    plugins: [new SimpleProgressWebpackPlugin()],
+module.exports = (_env, { mode = "production" }) => {
+    return {
+        mode,
+        target: "node",
+        entry: path.join(__dirname, "../lib/cli.js"),
+        output: {
+            path: __dirname,
+            filename: "bundle.js",
+        },
+        // https://github.com/dsherret/ts-morph/issues/171#issuecomment-1107867732
+        module: {
+            rules: [
+                {
+                    test: /node_modules[\\|/]code-block-writer[\\|/]umd[\\|/]/,
+                    use: { loader: "umd-compat-loader" },
+                },
+            ],
+            noParse: [require.resolve("@ts-morph/common/dist/typescript.js")],
+        },
+        plugins: [new SimpleProgressWebpackPlugin()],
+        optimization: {
+            minimize: false,
+        },
+    };
 };
