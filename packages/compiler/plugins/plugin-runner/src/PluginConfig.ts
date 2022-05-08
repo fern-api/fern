6b1c414d7e071a6da6c25010d17ca8f446d1d212
Zach Kirsch <zachkirsch@gmail.com>
2022-05-07 19:34:02 -0700

    Fix config.json validation in TS plugin

--- packages/compiler/plugins/plugin-runner/src/PluginConfig.ts
+++ packages/compiler/plugins/plugin-runner/src/PluginConfig.ts
@@ -1,13 +1,16 @@
+// we're using null here instead of undefined so we can validate
+// the zod validator in fern-typescript at compiler time
+
 export interface PluginConfig {
     irFilepath: string;
-    output: PluginOutputConfig | undefined;
+    output: PluginOutputConfig | null;
     helpers: PluginHelpers;
     customConfig: unknown;
 }
 
 export interface PluginOutputConfig {
     path: string;
-    pathRelativeToRootOnHost: string | undefined;
+    pathRelativeToRootOnHost: string | null;
 }
 
 export interface PluginHelpers {
