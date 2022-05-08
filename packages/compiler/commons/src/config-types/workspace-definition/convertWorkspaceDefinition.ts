8e63807b2d3c0f6b323dee58191d6b1393733922
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-05-07 18:54:35 -0700

    Add convertWorkspaceDefinition w/ tests (#146)
    * Add convertWorkspaceDefinition w/ tests
    
    * Split version from name
    
    * Use earlier version of fern for codegen

---
+++ packages/compiler/commons/src/config-types/workspace-definition/convertWorkspaceDefinition.ts
@@ -0,0 +1,32 @@
+import path from "path";
+import { WorkspaceDefinitionSchema } from "./schemas/WorkspaceDefinitionSchema";
+import { WorkspaceDefinition } from "./WorkspaceDefinition";
+
+export function convertWorkspaceDefinition({
+    definition,
+    absolutePathToDefinition,
+}: {
+    definition: WorkspaceDefinitionSchema;
+    absolutePathToDefinition: string;
+}): WorkspaceDefinition {
+    const absolutePathToWorkspaceDir = path.dirname(absolutePathToDefinition);
+    return {
+        _absolutePath: absolutePathToWorkspaceDir,
+        name: definition.name,
+        absolutePathToInput: path.resolve(absolutePathToWorkspaceDir, definition.input),
+        plugins: definition.plugins.map((plugin) => ({
+            name: plugin.name,
+            version: plugin.version,
+            absolutePathToOutput:
+                plugin.output != null ? path.resolve(absolutePathToWorkspaceDir, plugin.output) : undefined,
+            config: plugin.config,
+            helpers:
+                plugin.helpers != null
+                    ? plugin.helpers.map((helper) => ({
+                          name: helper.name,
+                          version: helper.version,
+                      }))
+                    : [],
+        })),
+    };
+}
--- packages/compiler/commons/src/config-types/workspace-definition/loadWorkspaceDefinition.ts
+++ packages/compiler/commons/src/config-types/workspace-definition/loadWorkspaceDefinition.ts
@@ -1,29 +1,15 @@
 import { readFile } from "fs/promises";
 import yaml from "js-yaml";
-import path from "path";
+import { convertWorkspaceDefinition } from "./convertWorkspaceDefinition";
 import { WorkspaceDefinitionSchema } from "./schemas/WorkspaceDefinitionSchema";
 import { WorkspaceDefinition } from "./WorkspaceDefinition";
 
-export async function loadWorkspaceDefinition(absolutePath: string): Promise<WorkspaceDefinition> {
-    const contentsStr = await readFile(absolutePath);
+export async function loadWorkspaceDefinition(absolutePathToDefinition: string): Promise<WorkspaceDefinition> {
+    const contentsStr = await readFile(absolutePathToDefinition);
     const contentsParsed = yaml.load(contentsStr.toString());
     const validated = await WorkspaceDefinitionSchema.parseAsync(contentsParsed);
-    return {
-        _absolutePath: path.dirname(absolutePath),
-        name: validated.name,
-        absolutePathToInput: path.resolve(validated.input),
-        plugins: validated.plugins.map((plugin) => ({
-            name: plugin.name,
-            version: plugin.version,
-            absolutePathToOutput: plugin.output != null ? path.resolve(plugin.output) : undefined,
-            config: plugin.config,
-            helpers:
-                plugin.helpers != null
-                    ? plugin.helpers.map((helper) => ({
-                          name: helper.name,
-                          version: helper.version,
-                      }))
-                    : [],
-        })),
-    };
+    return convertWorkspaceDefinition({
+        definition: validated,
+        absolutePathToDefinition,
+    });
 }
