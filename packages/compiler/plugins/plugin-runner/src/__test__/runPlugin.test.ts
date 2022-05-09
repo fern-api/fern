aee0c16b8bafefffd85738a8d408af90d4a58f4b
Zach Kirsch <zachkirsch@gmail.com>
2022-05-09 16:38:27 -0700

    Fix test

--- packages/compiler/plugins/plugin-runner/src/__test__/runPlugin.test.ts
+++ packages/compiler/plugins/plugin-runner/src/__test__/runPlugin.test.ts
@@ -3,7 +3,7 @@ import { getPluginOutputConfig } from "../runPlugin";
 describe("getPluginOutputConfig", () => {
     it("path relative to root", () => {
         const pluginOutputConfig = getPluginOutputConfig({
-            absolutePathToProject: "/path/to/root/fern.config.json",
+            absolutePathToProject: "/path/to/root",
             absolutePathToOutput: "/path/to/root/path/to/output",
         });
         expect(pluginOutputConfig.pathRelativeToRootOnHost).toEqual("path/to/output");
