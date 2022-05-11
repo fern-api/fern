f3c0562adf2b64fc8b3e0d1d78f03d56a1361043
Deep Singhvi <dsinghvi@umich.edu>
2022-05-10 21:50:05 -0700

    compile works

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -28,31 +28,25 @@ yargs(hideBin(process.argv))
             initialize();
         },
     })
-    .command({
-        command: ["$0 add [workspaces...] <plugin>"],
-        describe: "Add a plugin to .fernrc.yml.",
-        builder: (yargs) => {
-            return yargs
+    .command(
+        ["$0 add [workspaces...] <plugin>"],
+        "Add a plugin to .fernrc.yml.",
+        (yargs) =>
+            yargs
                 .positional("workspaces", {
                     array: true,
                     type: "string",
                     description:
                         "If omitted, every workspace specified in the project-level configuration (fern.config.json) will be processed.",
                 })
-                .positional("plugin", {
-                    description: 'Plugin to install. Supported values are "java" or "typescript"',
-                    type: "string",
-                });
-        },
-        handler: (argv): void => {
-            const workspaces = argv.workspaces ?? [];
-            if (argv.plugin === "java" || argv.plugin === "typescript") {
-                addPluginToWorkspaces(workspaces, argv.plugin);
-            } else {
-                console.error("Plugin %s does not exist", argv.plugin);
-            }
-        },
-    })
+                .option("plugin", {
+                    choices: ["typescript", "java"] as const,
+                    demandOption: true,
+                }),
+        (argv) => {
+            addPluginToWorkspaces(argv.workspaces ?? [], argv.plugin);
+        }
+    )
     .demandCommand()
     .showHelpOnFail(true)
     .parse();
