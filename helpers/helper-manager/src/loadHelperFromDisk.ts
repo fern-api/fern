b5aa731a867403fb85854155ca72b6fd0ba5b028
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-05-11 20:45:14 -0700

    Download plugin helpers from NPM (#165)
    * Download plugin helpers from NPM
    
    * Lint monorepo
    
    * Fix snapshot

--- helpers/helper-manager/src/loadHelperFromDisk.ts
+++ helpers/helper-manager/src/loadHelperFromDisk.ts
@@ -1,7 +1,11 @@
 import { validateSchema } from "@fern-typescript/commons";
 import { FernTypescriptHelper, FernTypescriptHelperSchema } from "@fern-typescript/helper-commons";
+import execa from "execa";
 
 export async function loadHelperFromDisk(pathToPlugin: string): Promise<FernTypescriptHelper> {
+    await execa("npm", ["install"], {
+        cwd: pathToPlugin,
+    });
     const plugin = await import(/* webpackIgnore: true */ pathToPlugin);
     return validateSchema<FernTypescriptHelper>(FernTypescriptHelperSchema, plugin);
 }
