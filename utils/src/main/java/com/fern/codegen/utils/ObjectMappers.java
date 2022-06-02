2b547fad0b95b53dd2cb35b3f0bbcf5e37e24317
Deep Singhvi <dsinghvi@umich.edu>
2022-06-02 15:55:47 -0700

    accept case insensitive mode (#51)

--- utils/src/main/java/com/fern/codegen/utils/ObjectMappers.java
+++ utils/src/main/java/com/fern/codegen/utils/ObjectMappers.java
@@ -1,6 +1,7 @@
 package com.fern.codegen.utils;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
@@ -8,7 +9,8 @@ public final class ObjectMappers {
 
     public static final ObjectMapper CLIENT_OBJECT_MAPPER = new ObjectMapper()
             .registerModule(new Jdk8Module())
-            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
 
     private ObjectMappers() {}
 }
