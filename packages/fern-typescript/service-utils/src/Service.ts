ce06bedc9241bf358ac99fbd11e8ef7e98e62441
Deep Singhvi <dsinghvi@umich.edu>
2022-06-08 14:00:14 -0400

    update token type

--- packages/fern-typescript/service-utils/src/Service.ts
+++ packages/fern-typescript/service-utils/src/Service.ts
@@ -1,9 +1,10 @@
 import { Fetcher } from "./Fetcher";
+import { Token } from "./Token";
 
 export declare namespace Service {
     interface Init {
         origin: string;
-        token?: Fetcher.Args["token"];
+        token?: Token;
         fetcher?: Fetcher;
     }
 }
--- packages/fern-typescript/service-utils/src/index.ts
+++ packages/fern-typescript/service-utils/src/index.ts
@@ -2,3 +2,4 @@ export { defaultFetcher } from "./defaultFetcher";
 export type { Fetcher } from "./Fetcher";
 export { isResponseOk } from "./isResponseOk";
 export type { Service } from "./Service";
+export type { Token } from "./Token";
