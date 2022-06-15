99d1ae43f273173badb704c695b364a6e1734705
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-06-15 16:45:20 -0700

    Fix entrypoints in generated package.json (#225)
    * Fix entrypoints in generated package.json
    
    * Fix snapshots
    
    * Fix config.yml to publish java snapshot
    
    * Delete generate ts
    
    * Don't generate src/ in lib/

--- commons/src/generate-ts-project/utils.ts
+++ commons/src/generate-ts-project/utils.ts
@@ -8,3 +8,22 @@ export function getPathToProjectFile(relativePath: string): string {
     // the project lives at the root of the Volume
     return path.join(path.sep, relativePath);
 }
+
+// not including src/ since we're using rootDir=src in the tsconfig.json
+const RELATIVE_ENTRYPOINT_WITHOUT_EXTENSION = "index";
+export const RELATIVE_CJS_ENTRYPOINT = path.join(
+    RELATIVE_CJS_OUT_DIR_PATH,
+    `${RELATIVE_ENTRYPOINT_WITHOUT_EXTENSION}.js`
+);
+export const RELATIVE_CJS_TYPES_ENTRYPOINT = path.join(
+    RELATIVE_CJS_OUT_DIR_PATH,
+    `${RELATIVE_ENTRYPOINT_WITHOUT_EXTENSION}.d.ts`
+);
+export const RELATIVE_ESM_ENTRYPOINT = path.join(
+    RELATIVE_ESM_OUT_DIR_PATH,
+    `${RELATIVE_ENTRYPOINT_WITHOUT_EXTENSION}.js`
+);
+export const RELATIVE_ESM_TYPES_ENTRYPOINT = path.join(
+    RELATIVE_CJS_OUT_DIR_PATH,
+    `${RELATIVE_ENTRYPOINT_WITHOUT_EXTENSION}.d.ts`
+);
