b87f3cebf1a63f9fc17b75eed08df97b762c1341
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-06-15 17:35:37 -0700

    Change perms on java cli in docker to 777 (#53)
    * Change perms on java cli in docker to 777
    
    * Remove comment

--- cli/init.sh
+++ cli/init.sh
@@ -1,7 +1,8 @@
 configPath=$1
+
+cd cli
 tar -xvf cli.tar
 rm -rf cli.tar
-cd ../cli-"$VERSION"
-echo "$configPath"
+cd ./cli-"$VERSION"
 java -cp cli-"$VERSION".jar:lib/* \
   com.fern.java.client.cli.ClientGeneratorCli "$configPath"
