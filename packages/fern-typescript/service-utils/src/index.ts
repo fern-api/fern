0852ad1535c340221aafcd102550b9cb02488e6b
Zach Kirsch <zachkirsch@gmail.com>
2022-06-24 14:39:09 -0700

    Only add underscores to types if there's a conflict

--- packages/fern-typescript/service-utils/src/index.ts
+++ packages/fern-typescript/service-utils/src/index.ts
@@ -1,5 +1,6 @@
 export { defaultFetcher } from "./defaultFetcher";
 export { type Fetcher } from "./Fetcher";
 export { isResponseOk } from "./isResponseOk";
+export { type MaybePromise } from "./MaybePromise";
 export { type Service } from "./Service";
 export { type Token } from "./Token";
