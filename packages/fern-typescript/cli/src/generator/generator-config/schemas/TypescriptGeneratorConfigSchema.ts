4031dcb964512bb0f0a15c6f9500867da3335501
Deep Singhvi <dsinghvi@umich.edu>
2022-06-24 20:15:27 -0700

    fern-typescript-updated-types

--- packages/fern-typescript/cli/src/generator/generator-config/schemas/TypescriptGeneratorConfigSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const TypescriptGeneratorConfigSchema = z.strictObject({
-    packageName: z.string(),
-    mode: z.enum(["client", "server", "model", "client_and_server"]),
-});
-
-export type TypescriptGeneratorConfigSchema = z.infer<typeof TypescriptGeneratorConfigSchema>;
