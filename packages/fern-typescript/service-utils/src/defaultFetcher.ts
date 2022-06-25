7a1577f06e6e711c4cb089f0aa4099db844954b4
Zach Kirsch <zachkirsch@gmail.com>
2022-06-25 00:57:58 -0700

    Don't use Headers in fetcher

--- packages/fern-typescript/service-utils/src/defaultFetcher.ts
+++ packages/fern-typescript/service-utils/src/defaultFetcher.ts
@@ -1,14 +1,16 @@
 import { Fetcher } from "./Fetcher";
 
 export const defaultFetcher: Fetcher = async (args) => {
-    const headers = new Headers(args.headers);
+    const headers = {
+        ...args.headers,
+    };
     if (args.body != null) {
-        headers.append("Content-Type", args.body.contentType);
+        headers["Content-Type"] = args.body.contentType;
     }
 
     const token = typeof args.token === "function" ? await args.token() : args.token;
     if (token != null) {
-        headers.append("Authorization", `Bearer ${token}`);
+        headers["Authorization"] = `Bearer ${token}`;
     }
 
     const url = new URL(args.url);
