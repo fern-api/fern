fb55cf3f81305728300cab208b8daaf7f0eb60e8
Zach Kirsch <zachkirsch@gmail.com>
2022-06-29 18:39:25 -0700

    Fix testMatch

--- jest.config.ts
+++ jest.config.ts
@@ -20,6 +20,10 @@ export default async (): Promise<Config> => {
         ...defaultConfig,
         projects: packages.map((p) => ({
             ...defaultConfig,
+            // if there are no packages, then jest will run all tests by default.
+            // so in that case, change the test match to a dummy path that doesn't
+            // match anything.
+            testMatch: packages.length > 0 ? defaultConfig.testMatch : ["__path_that_does_not_exist"],
             displayName: p.name,
             rootDir: `${p.location}/src`,
         })),
