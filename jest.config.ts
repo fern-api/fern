af01781b880dae6ce82e43714eb12155b4d5c7e2
Zach Kirsch <zachkirsch@gmail.com>
2022-06-29 18:58:20 -0700

    Move testMatch out of projects

--- jest.config.ts
+++ jest.config.ts
@@ -18,17 +18,15 @@ export default async (): Promise<Config> => {
 
     return {
         ...defaultConfig,
+        // if there are no packages, then jest will run all tests by default.
+        // so in that case, change the test match to a dummy path that doesn't
+        // match anything.
+        testMatch: packages.length > 0 ? defaultConfig.testMatch : ["__path_that_does_not_exist"],
         projects: packages.map((p) => {
-            const cwd = `${p.location}/src`;
             return {
                 ...defaultConfig,
-                // if there are no packages, then jest will run all tests by default.
-                // so in that case, change the test match to a dummy path that doesn't
-                // match anything.
-                testMatch: packages.length > 0 ? defaultConfig.testMatch : ["__path_that_does_not_exist"],
                 displayName: p.name,
-                cwd,
-                rootDir: cwd,
+                rootDir: `${p.location}/src`,
             };
         }),
     };
