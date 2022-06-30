4846922fa00ff7bfb43669422de5f1efd5b859ea
Zach Kirsch <zachkirsch@gmail.com>
2022-06-29 17:00:35 -0700

    Add trim

--- packages/fern-typescript/service-utils/src/Token.ts
+++ packages/fern-typescript/service-utils/src/Token.ts
@@ -2,12 +2,12 @@ export type Token = string & {
     __Token: void;
 };
 
-const BEARER_AUTH_HEADER_PREFIX = /^Bearer\s+/i;
+const BEARER_AUTH_HEADER_PREFIX = /^Bearer /i;
 
 export const Token = {
     of: (value: string): Token => value as Token,
 
     fromAuthorizationHeader: (header: string): Token => {
-        return header.replace(BEARER_AUTH_HEADER_PREFIX, "") as Token;
+        return header.replace(BEARER_AUTH_HEADER_PREFIX, "").trim() as Token;
     },
 };
