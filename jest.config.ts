147d7d9242f1990b0eecdc8139e924b82cce2913
Zach Kirsch <zachkirsch@gmail.com>
2022-06-29 18:54:36 -0700

    Add cwd

--- jest.config.ts
+++ jest.config.ts
@@ -18,15 +18,19 @@ export default async (): Promise<Config> => {
 
     return {
         ...defaultConfig,
-        projects: packages.map((p) => ({
-            ...defaultConfig,
-            // if there are no packages, then jest will run all tests by default.
-            // so in that case, change the test match to a dummy path that doesn't
-            // match anything.
-            testMatch: packages.length > 0 ? defaultConfig.testMatch : ["__path_that_does_not_exist"],
-            displayName: p.name,
-            rootDir: `${p.location}/src`,
-        })),
+        projects: packages.map((p) => {
+            const cwd = `${p.location}/src`;
+            return {
+                ...defaultConfig,
+                // if there are no packages, then jest will run all tests by default.
+                // so in that case, change the test match to a dummy path that doesn't
+                // match anything.
+                testMatch: packages.length > 0 ? defaultConfig.testMatch : ["__path_that_does_not_exist"],
+                displayName: p.name,
+                cwd,
+                rootDir: cwd,
+            };
+        }),
     };
 };
 
