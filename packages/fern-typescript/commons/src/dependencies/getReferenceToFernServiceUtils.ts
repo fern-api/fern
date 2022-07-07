7b3b0c8935928bcfc7a8c01f72786073fc5267ae
Zach Kirsch <zachkirsch@gmail.com>
2022-07-06 21:43:53 -0700

    Fix test

--- packages/fern-typescript/commons/src/dependencies/getReferenceToFernServiceUtils.ts
+++ packages/fern-typescript/commons/src/dependencies/getReferenceToFernServiceUtils.ts
@@ -2,7 +2,7 @@ import { SourceFile, ts } from "ts-morph";
 import { DependencyManager } from "./DependencyManager";
 
 const PACKAGE_NAME = "@fern-typescript/service-utils";
-const VERSION = "0.0.125";
+const VERSION = "0.0.124";
 
 type FernServiceUtilsExport = ExportedFernServiceUtilsType | ExportedFernServiceUtilsValue;
 
