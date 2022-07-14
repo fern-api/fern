142633dac9e69018fe4d09494d00468d1cfbd7ab
Deep Singhvi <dsinghvi@umich.edu>
2022-07-14 16:30:23 -0700

    implement OptionalAware Feign Contract (#107)

---
+++ runtime-utils/jersey-utils/src/main/java/com/fern/java/jersey/contracts/OptionalAwareContract.java
@@ -0,0 +1,56 @@
+/*
+ * (c) Copyright 2022 Birch Solutions Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fern.java.jersey.contracts;
+
+import com.fern.java.jersey.expanders.OptionalExpander;
+import feign.Contract;
+import feign.MethodMetadata;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import javax.ws.rs.QueryParam;
+
+public final class OptionalAwareContract extends AbstractDelegatingContract {
+
+    OptionalAwareContract(Contract delegate) {
+        super(delegate);
+    }
+
+    @Override
+    protected void processMetadata(Class<?> targetType, Method method, MethodMetadata metadata) {
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        Annotation[][] annotations = method.getParameterAnnotations();
+        for (int i = 0; i < parameterTypes.length; i++) {
+            Class<?> cls = parameterTypes[i];
+            if (cls.equals(Optional.class)) {
+                List<Class<?>> paramAnnotations = getAnnotations(annotations, i);
+                if (paramAnnotations.contains(QueryParam.class)) {
+                    metadata.indexToExpanderClass().put(i, OptionalExpander.class);
+                }
+            }
+        }
+    }
+
+    private List<Class<?>> getAnnotations(Annotation[][] annotations, int index) {
+        return List.of(annotations[index]).stream().map(EXTRACT_CLASS).collect(Collectors.toList());
+    }
+
+    private static final Function<Annotation, Class<?>> EXTRACT_CLASS = Annotation::annotationType;
+}
