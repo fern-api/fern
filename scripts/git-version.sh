ab560c45359ed14404f992685f3cc2f7f5614121
Zach Kirsch <zachkirsch@gmail.com>
2022-07-13 16:01:20 -0800

    Try to fix tag detection

--- scripts/git-version.sh
+++ scripts/git-version.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-commit_tag="$(git describe --exact-match --tags HEAD 2> /dev/null || :)"
-head_tag="$(git describe --exact-match HEAD 2> /dev/null || :)"
+tag="$(git describe --exact-match --tags HEAD 2> /dev/null || :)"
 
-# if the current commit is tagged but HEAD doesn't point to that tag, then
+# if the current commit is tagged but we're not on a tag in Circle, then
 # should then ignore the tag
-[[ -n "$commit_tag" && -z "$head_tag" ]] \
-	&& git describe --tags --always --first-parent --exclude "$commit_tag" \
+[[ -n "$tag" && -z "$CIRCLE_TAG" ]] \
+	&& git describe --tags --always --first-parent --exclude "$tag" \
 	|| git describe --tags --always --first-parent
