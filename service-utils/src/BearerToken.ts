fe496adfe88e78dee686c412fead813f248e0532
Deep Singhvi <dsinghvi@umich.edu>
2022-07-15 15:08:37 -0700

    improvement: add toAuthorizationheader methods to BasicAuth and BearerToken (#391)
    * add toAuthorizationheader methods
    
    * take auth as input
    
    * remove yarn install'

--- service-utils/src/BearerToken.ts
+++ service-utils/src/BearerToken.ts
@@ -3,6 +3,9 @@ export type BearerToken = string;
 const BEARER_AUTH_HEADER_PREFIX = /^Bearer /i;
 
 export const BearerToken = {
+    toAuthorizationHeader: (token: BearerToken): string => {
+        return `Bearer ${token}`;
+    },
     fromAuthorizationHeader: (header: string): BearerToken => {
         return header.replace(BEARER_AUTH_HEADER_PREFIX, "").trim() as BearerToken;
     },
