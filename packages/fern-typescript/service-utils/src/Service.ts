f5a7a1801cddcf748a2198026d75500bcc401bb3
Deep Singhvi <dsinghvi@umich.edu>
2022-07-15 12:50:47 -0700

    add Service.ts

--- packages/fern-typescript/service-utils/src/Service.ts
+++ packages/fern-typescript/service-utils/src/Service.ts
@@ -1,11 +1,10 @@
-import { Token } from "./BearerToken";
 import { Fetcher } from "./Fetcher";
 import { MaybeGetter } from "./MaybeGetter";
 
 export declare namespace Service {
     interface Init {
         origin: string;
-        token?: MaybeGetter<Token | undefined>;
+        authHeader?: MaybeGetter<string | undefined>;
         fetcher?: Fetcher;
     }
 }
