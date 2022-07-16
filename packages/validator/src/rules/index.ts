c38ca2acb417f9389babe4d5a8e541964ca13f9e
Deep Singhvi <dsinghvi@umich.edu>
2022-07-16 00:52:48 -0700

    fix: enable valid enum name rule

--- packages/validator/src/rules/index.ts
+++ packages/validator/src/rules/index.ts
@@ -3,10 +3,12 @@ import { NoDuplicateEnumValuesRule } from "./no-duplicate-enum-values/no-duplica
 import { NoUndefinedPathParametersRule } from "./no-undefined-path-parameters/no-undefined-path-parameters";
 import { NoUndefinedTypeReferenceRule } from "./no-undefined-type-reference/no-undefined-type-reference";
 import { ValidAuthInServiceRule } from "./valid-auth-in-service/valid-auth-in-service";
+import { ValidEnumNameRule } from "./valid-enum-name/valid-enum-name";
 
 export const rules: Rule[] = [
     NoUndefinedTypeReferenceRule,
     NoDuplicateEnumValuesRule,
     NoUndefinedPathParametersRule,
     ValidAuthInServiceRule,
+    ValidEnumNameRule,
 ];
