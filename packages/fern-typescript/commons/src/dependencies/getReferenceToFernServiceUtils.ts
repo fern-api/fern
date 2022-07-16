e7cc00e363247af90168b55a77e193d4cecb6806
Deep Singhvi <dsinghvi@umich.edu>
2022-07-15 21:25:44 -0700

    push to see diff of snapshots

--- packages/fern-typescript/commons/src/dependencies/getReferenceToFernServiceUtils.ts
+++ packages/fern-typescript/commons/src/dependencies/getReferenceToFernServiceUtils.ts
@@ -2,7 +2,7 @@ import { SourceFile, ts } from "ts-morph";
 import { DependencyManager } from "./DependencyManager";
 
 const PACKAGE_NAME = "@fern-typescript/service-utils";
-const VERSION = "0.0.124";
+const VERSION = "0.0.151";
 
 type FernServiceUtilsExport = ExportedFernServiceUtilsType | ExportedFernServiceUtilsValue;
 
