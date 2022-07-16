953a56b80f46a937714224c41ee577297a5cc700
Deep Singhvi <dsinghvi@umich.edu>
2022-07-15 22:37:49 -0700

    improvement: auth-utils has bearer and basic auth headers (#109)
    * auth-utils has bearer and basic auth headers
    
    * add back auth header class till we can consume new ir
    
    * fix snapshots

---
+++ runtime-utils/auth-utils/src/main/java/com/fern/java/auth/BearerAuthHeader.java
@@ -0,0 +1,37 @@
+/*
+ * (c) Copyright 2022 Birch Solutions Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fern.java.auth;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.immutables.value.Value;
+
+@Value.Immutable
+@Value.Style(visibility = Value.Style.ImplementationVisibility.PACKAGE, jdkOnly = true)
+public abstract class BearerAuthHeader {
+
+    @Value.Parameter
+    @JsonValue
+    public abstract String getToken();
+
+    @Override
+    public final String toString() {
+        return "Bearer " + getToken();
+    }
+
+    public static BearerAuthHeader of(String token) {
+        return ImmutableBearerAuthHeader.of(token.startsWith("Bearer ") ? token.substring(7) : token);
+    }
+}
