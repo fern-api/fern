e54b6104b4dbddfcd364fcb50abcf196cd885165
Zach Kirsch <zachkirsch@gmail.com>
2022-07-19 10:55:17 -0800

    Simplify getter

--- packages/fern-typescript/service-utils/src/Supplier.ts
+++ packages/fern-typescript/service-utils/src/Supplier.ts
@@ -3,6 +3,8 @@ type SuppliableType = string | number | boolean | undefined;
 export type Supplier<T extends SuppliableType> = T | Promise<T> | (() => T | Promise<T>);
 
 export const Supplier = {
-    get: async <T extends SuppliableType>(supplier: Supplier<T>): Promise<T> =>
-        typeof supplier === "function" ? supplier() : supplier,
+    get: async <T extends SuppliableType>(supplier: Supplier<T>): Promise<T> => {
+        const value: T = await (typeof supplier === "function" ? supplier() : supplier);
+        return value;
+    },
 };
