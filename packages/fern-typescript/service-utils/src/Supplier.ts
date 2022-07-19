1e733d8022c5ac7137fe7363a8b83dc89b48d774
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-07-19 10:58:00 -0800

    MaybeGetter -> Supplier (#404)
    * MaybeGetter -> Supplier
    
    * Fix export
    
    * Simplify getter
    
    * Simplify getter

---
+++ packages/fern-typescript/service-utils/src/Supplier.ts
@@ -0,0 +1,10 @@
+type SuppliableType = string | number | boolean | undefined;
+
+export type Supplier<T extends SuppliableType> = T | Promise<T> | (() => T | Promise<T>);
+
+export const Supplier = {
+    get: async <T extends SuppliableType>(supplier: Supplier<T>): Promise<T> => {
+        const value: T = await (typeof supplier === "function" ? supplier() : supplier);
+        return value;
+    },
+};
