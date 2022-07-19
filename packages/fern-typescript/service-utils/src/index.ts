a8798605407c4663edd761497d7316c77eb42cec
Zach Kirsch <zachkirsch@gmail.com>
2022-07-19 10:52:02 -0800

    Fix export

--- packages/fern-typescript/service-utils/src/index.ts
+++ packages/fern-typescript/service-utils/src/index.ts
@@ -4,4 +4,4 @@ export { defaultFetcher } from "./defaultFetcher";
 export { type Fetcher } from "./Fetcher";
 export { isResponseOk } from "./isResponseOk";
 export { type Service } from "./Service";
-export { type Supplier } from "./Supplier";
+export { Supplier } from "./Supplier";
