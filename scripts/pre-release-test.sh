4b1e970734ebe1c5a94802d1b96992c7cdfb7542
Deep Singhvi <dsinghvi@umich.edu>
2022-07-20 00:24:57 -0700

    pre-release testing

---
+++ scripts/pre-release-test.sh
@@ -0,0 +1,12 @@
+npm install -g fern-api@next
+mkdir pre-release-test
+cd pre-release-test
+fern init
+fern add java
+fern add typescript
+fern add postman
+fern generate > generate.out
+if grep -q ❌ "$File"; then
+  exit 1;
+fi
+rm -rf pre-release-test
