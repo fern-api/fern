2b79e896d02807fbd1e63d91621e35441209c093
Zach Kirsch <zachkirsch@gmail.com>
2022-07-23 00:05:31 -0800

    Actually use EnumValueSchema

--- packages/yaml-schema/src/schemas/EnumSchema.ts
+++ packages/yaml-schema/src/schemas/EnumSchema.ts
@@ -1,16 +1,9 @@
 import { z } from "zod";
+import { EnumValueSchema } from "./EnumValueSchema";
 import { WithDocsSchema } from "./WithDocsSchema";
 
 export const EnumSchema = WithDocsSchema.extend({
-    enum: z.array(
-        z.union([
-            z.string(),
-            WithDocsSchema.extend({
-                name: z.optional(z.string()),
-                value: z.string(),
-            }),
-        ])
-    ),
+    enum: z.array(EnumValueSchema),
 });
 
 export type EnumSchema = z.infer<typeof EnumSchema>;
