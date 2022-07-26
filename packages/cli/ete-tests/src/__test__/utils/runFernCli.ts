4bc5116c27beeb3d53ec554325222dd84009c969
Zach Kirsch <zachkirsch@gmail.com>
2022-07-25 17:48:33 -0700

    Remove ts-jest, use babel

--- packages/cli/ete-tests/src/__test__/utils/runFernCli.ts
+++ packages/cli/ete-tests/src/__test__/utils/runFernCli.ts
@@ -3,7 +3,9 @@ import path from "path";
 
 export async function runFernCli(args: string[], options?: Options): Promise<execa.ExecaChildProcess> {
     const cmd = execa("node", [path.join(__dirname, "../../../../cli/dist/bundle.cjs"), ...args], options);
-    cmd.stdout?.pipe(process.stdout);
-    cmd.stderr?.pipe(process.stderr);
+    if (options?.reject !== false) {
+        cmd.stdout?.pipe(process.stdout);
+        cmd.stderr?.pipe(process.stderr);
+    }
     return cmd;
 }
