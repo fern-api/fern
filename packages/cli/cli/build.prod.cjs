cce184e5f6ad202ee948cda3d49fae996fb9a777
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-07-26 18:24:10 -0800

    Add env var for fiddle origin (#450)

--- packages/cli/cli/build.prod.cjs
+++ packages/cli/cli/build.prod.cjs
@@ -19,6 +19,7 @@ async function main() {
             "process.env.PACKAGE_VERSION": getEnvironmentVariable("PACKAGE_VERSION"),
             "process.env.AUTH0_DOMAIN": getEnvironmentVariable("AUTH0_DOMAIN"),
             "process.env.AUTH0_CLIENT_ID": getEnvironmentVariable("AUTH0_CLIENT_ID"),
+            "process.env.DEFAULT_FIDDLE_ORIGIN": getEnvironmentVariable("DEFAULT_FIDDLE_ORIGIN"),
         },
     };
 
