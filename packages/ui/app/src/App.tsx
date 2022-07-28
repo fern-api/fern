5f0f32865dbb9da68e9c4ef7b3b4c0df735afb11
Zach Kirsch <zachkirsch@gmail.com>
2022-07-28 12:51:09 -0700

    Add logo

--- packages/ui/app/src/App.tsx
+++ packages/ui/app/src/App.tsx
@@ -2,11 +2,13 @@ import { H1, InputGroup } from "@blueprintjs/core";
 import { CenteredContent } from "@fern-ui/common-components";
 import { ApiCard } from "./ApiCard";
 import styles from "./App.module.scss";
+import { FernLogo } from "./FernLogo";
 
 export const App: React.FC = () => {
     return (
         <CenteredContent scrollable wrapperClassName={styles.wrapper} className={styles.app} fill>
             <H1>Fern</H1>
+            <FernLogo size={50} />
             <InputGroup large fill placeholder="Search..." />
             <div className={styles.apiCards}>
                 <ApiCard />
