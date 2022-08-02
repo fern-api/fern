f64560c666557b9f959b60aaa6fe200731fdd257
Deep Singhvi <dsinghvi@umich.edu>
2022-08-01 21:10:02 -0700

    fix

--- packages/cli/cli/src/upgradeNotifier.ts
+++ packages/cli/cli/src/upgradeNotifier.ts
@@ -7,33 +7,35 @@ import semverDiff from "semver-diff";
 const FERN_PACKAGE_NAME = "fern-api";
 
 export async function getUpgradeMessage(currentVersion: string): Promise<string | undefined> {
-    const latestPackageVersion = await latestVersion(FERN_PACKAGE_NAME);
-    const diff = semverDiff(currentVersion, latestPackageVersion);
-    if (diff != null) {
-        const template =
-            "Update available " +
-            chalk.dim("{currentVersion}") +
-            chalk.reset(" → ") +
-            chalk.green("{latestVersion}") +
-            " \nRun " +
-            chalk.cyan("{updateCommand}") +
-            " to update";
-        const message = boxen(
-            pupa(template, {
-                packageName: FERN_PACKAGE_NAME,
-                currentVersion,
-                latestVersion: latestPackageVersion,
-                updateCommand: `npm i -g ${FERN_PACKAGE_NAME}`,
-            }),
-            {
-                padding: 1,
-                margin: 1,
-                textAlignment: "center",
-                borderColor: "yellow",
-                borderStyle: "round",
-            }
-        );
-        return message;
-    }
+    try {
+        const latestPackageVersion = await latestVersion(FERN_PACKAGE_NAME);
+        const diff = semverDiff(currentVersion, latestPackageVersion);
+        if (diff != null) {
+            const template =
+                "Update available " +
+                chalk.dim("{currentVersion}") +
+                chalk.reset(" → ") +
+                chalk.green("{latestVersion}") +
+                " \nRun " +
+                chalk.cyan("{updateCommand}") +
+                " to update";
+            const message = boxen(
+                pupa(template, {
+                    packageName: FERN_PACKAGE_NAME,
+                    currentVersion,
+                    latestVersion: latestPackageVersion,
+                    updateCommand: `npm i -g ${FERN_PACKAGE_NAME}`,
+                }),
+                {
+                    padding: 1,
+                    margin: 1,
+                    textAlignment: "center",
+                    borderColor: "yellow",
+                    borderStyle: "round",
+                }
+            );
+            return message;
+        }
+    } catch (e) {}
     return undefined;
 }
