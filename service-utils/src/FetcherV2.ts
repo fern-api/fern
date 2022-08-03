d7950147b60f8221ed57ef9f8f2f76b7c30b09a3
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-08-03 00:50:14 -0700

    Add new types to service-utils for v2 client (#526)
    * Add new types
    
    * Remove Content-Type

---
+++ service-utils/src/FetcherV2.ts
@@ -0,0 +1,17 @@
+export type FetcherV2 = (args: FetcherV2.Args) => Promise<FetcherV2.Response>;
+
+export declare namespace FetcherV2 {
+    export interface Args {
+        url: string;
+        method: string;
+        headers?: Record<string, string | null | undefined>;
+        authHeader?: string | undefined;
+        queryParameters?: URLSearchParams;
+        body?: unknown;
+    }
+
+    export interface Response {
+        ok: boolean;
+        body: unknown;
+    }
+}
