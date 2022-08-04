bde370862892d385d25bcd613413bc009bd622f1
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-08-04 10:35:41 -0700

    Add client-v2 tests (#533)
    * Exports should be from api.ts, not index.ts, in root
    
    * Test all modes
    
    * Put tests in different directories

--- cli/src/__test__/generate.test.ts
+++ cli/src/__test__/generate.test.ts
@@ -10,63 +10,67 @@ import { runGenerator } from "../generator/runGenerator";
 const FIXTURES = ["trace"];
 const FIXTURES_PATH = path.join(__dirname, "fixtures");
 
+const MODES = ["client", "client-v2", "server", "client_and_server"];
+
 describe("runGenerator", () => {
     for (const fixture of FIXTURES) {
-        it(
-            // eslint-disable-next-line jest/valid-title
-            fixture,
-            async () => {
-                const fixturePath = path.join(FIXTURES_PATH, fixture);
-                const configPath = path.join(fixturePath, "config.json");
-                const irPath = path.join(fixturePath, "ir.json");
-                const apiPath = path.join(fixturePath, "api");
-                const relativeOutputPath = "generated";
-                const outputPath = path.join(fixturePath, relativeOutputPath);
+        for (const mode of MODES) {
+            it(
+                // eslint-disable-next-line jest/valid-title
+                fixture,
+                async () => {
+                    const fixturePath = path.join(FIXTURES_PATH, fixture);
+                    const configPath = path.join(fixturePath, "config.json");
+                    const irPath = path.join(fixturePath, "ir.json");
+                    const apiPath = path.join(fixturePath, "api");
+                    const relativeOutputPath = path.join("generated", mode);
+                    const outputPath = path.join(fixturePath, relativeOutputPath);
 
-                await rm(outputPath, { recursive: true, force: true });
-                await rm(configPath, { force: true });
+                    await rm(outputPath, { recursive: true, force: true });
+                    await rm(configPath, { force: true });
 
-                const parseWorkspaceResult = await loadWorkspace({
-                    name: undefined,
-                    absolutePathToDefinition: apiPath,
-                });
-                if (!parseWorkspaceResult.didSucceed) {
-                    throw new Error(JSON.stringify(parseWorkspaceResult.failures));
-                }
-                const intermediateRepresentation = await generateIntermediateRepresentation(
-                    parseWorkspaceResult.workspace
-                );
+                    const parseWorkspaceResult = await loadWorkspace({
+                        name: undefined,
+                        absolutePathToDefinition: apiPath,
+                    });
+                    if (!parseWorkspaceResult.didSucceed) {
+                        throw new Error(JSON.stringify(parseWorkspaceResult.failures));
+                    }
+                    const intermediateRepresentation = generateIntermediateRepresentation(
+                        parseWorkspaceResult.workspace
+                    );
 
-                await writeFile(irPath, JSON.stringify(intermediateRepresentation, undefined, 4));
+                    await writeFile(irPath, JSON.stringify(intermediateRepresentation, undefined, 4));
 
-                const config: GeneratorConfig = {
-                    irFilepath: irPath,
-                    output: {
-                        path: outputPath,
-                    },
-                    publish: null,
-                    helpers: {
-                        encodings: {},
-                    },
-                    customConfig: {
-                        mode: "client",
-                    },
-                    workspaceName: "my-api",
-                    organization: "fern-api",
-                    environment: { _type: "local" },
-                };
+                    const config: GeneratorConfig = {
+                        irFilepath: irPath,
+                        output: {
+                            path: outputPath,
+                        },
+                        publish: null,
+                        helpers: {
+                            encodings: {},
+                        },
+                        customConfig: {
+                            mode,
+                        },
+                        workspaceName: "my-api",
+                        organization: "fern-api",
+                        environment: { _type: "local" },
+                    };
 
-                await writeFile(configPath, JSON.stringify(config, undefined, 4));
-                await runGenerator(configPath);
+                    await writeFile(configPath, JSON.stringify(config, undefined, 4));
+                    await runGenerator(configPath);
 
-                const directoryContents = await getDirectoryContents(path.join(outputPath, "client"));
-                expect(directoryContents).toMatchSnapshot();
+                    const directoryContents = await getDirectoryContents(outputPath);
+                    expect(directoryContents).toMatchSnapshot();
 
-                // compile after getDirectoryContents(), so directoryContents
-                // doesn't include compiled files, node_modules
-                await installAndCompileGeneratedProjects(outputPath);
-            },
-            90_000
-        );
+                    // compile after snapshotting, so directoryContents doesn't
+                    // include compiled files, node_modules
+                    await installAndCompileGeneratedProjects(outputPath);
+                },
+                90_000
+            );
+        }
     }
 });
