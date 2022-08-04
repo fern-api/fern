f88707df3bf11ce2cd079aed0c51ff36b28650cd
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-08-04 10:24:04 -0700

    Exports should be from api.ts, not index.ts, in root (#532)

--- errors-v2/src/ErrorDeclarationHandler.ts
+++ errors-v2/src/ErrorDeclarationHandler.ts
@@ -1,21 +1,16 @@
-import { ErrorDeclaration } from "@fern-fern/ir-model";
-import { DeclarationHandler } from "@fern-typescript/declaration-handler";
+import { ErrorDeclaration, ObjectTypeDeclaration } from "@fern-fern/ir-model";
+import { DeclarationHandler, File } from "@fern-typescript/declaration-handler";
 import { generateObjectType } from "@fern-typescript/types-v2";
 import { ts } from "ts-morph";
 
 export const ErrorDeclarationHandler: DeclarationHandler<ErrorDeclaration> = {
     run: async (errorDeclaration, { withFile }) => {
-        if (errorDeclaration.type._type !== "object") {
-            throw new Error("Error declaration must be an object");
-        }
-        const shape = errorDeclaration.type;
-
         await withFile((file) => {
             generateObjectType({
                 typeName: file.sourceFile.getBaseNameWithoutExtension(),
                 docs: errorDeclaration.docs,
                 file,
-                shape,
+                shape: getErrorShapeWithoutAdditionalProperties(errorDeclaration, file),
                 additionalProperties: {
                     [file.fernConstants.errorDiscriminant]: ts.factory.createLiteralTypeNode(
                         ts.factory.createStringLiteral(errorDeclaration.discriminantValue)
@@ -25,3 +20,21 @@ export const ErrorDeclarationHandler: DeclarationHandler<ErrorDeclaration> = {
         });
     },
 };
+
+function getErrorShapeWithoutAdditionalProperties(
+    errorDeclaration: ErrorDeclaration,
+    file: File
+): ObjectTypeDeclaration {
+    if (errorDeclaration.type._type === "alias") {
+        const resolvedType = file.resolveTypeReference(errorDeclaration.type.aliasOf);
+        if (resolvedType._type === "void") {
+            return { extends: [], properties: [] };
+        }
+    }
+
+    if (errorDeclaration.type._type === "object") {
+        return errorDeclaration.type;
+    }
+
+    throw new Error("Error declaration type must be an object");
+}
