629c000bd9b7dc298646bbc61012d80143d17ecc
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-08-07 23:43:51 -0700

    Remove branded strings (#541)

--- types/src/index.ts
+++ types/src/index.ts
@@ -1,5 +1,5 @@
 export { ENUM_VALUES_PROPERTY_KEY } from "./enum/generateEnumType";
-export { ALIAS_UTILS_OF_KEY, shouldUseBrandedTypeForAlias } from "./generateAliasType";
+export { ALIAS_UTILS_OF_KEY } from "./generateAliasType";
 export { generateType } from "./generateType";
 export { generateTypeFiles } from "./generateTypeFiles";
 export { generateUnionType } from "./union/generateUnionType";
