c10569533f1959d3422174c3cf14b7aecf7e9a59
Zach Kirsch <zachkirsch@gmail.com>
2022-08-10 03:34:12 -0400

    Rename Client to _Client, except root wrapper

--- packages/fern-typescript/cli/src/v2/client/constructWrapperDeclarations.ts
+++ packages/fern-typescript/cli/src/v2/client/constructWrapperDeclarations.ts
@@ -3,8 +3,6 @@ import { WrapperDeclaration, WrapperName } from "@fern-typescript/commons-v2";
 import { isEqual } from "lodash-es";
 import { StringifiedFernFilepath, stringifyFernFilepath } from "./utils/stringifyFernFilepath";
 
-const WRAPPER_NAME = "Client";
-
 export function constructWrapperDeclarations(
     intermediateRepresentation: IntermediateRepresentation
 ): WrapperDeclaration[] {
@@ -12,26 +10,27 @@ export function constructWrapperDeclarations(
 
     for (const service of intermediateRepresentation.services.http) {
         for (let index = 0; index < service.name.fernFilepath.length; index++) {
-            const nameOfWrapped: WrapperName = {
-                name: WRAPPER_NAME,
+            const wrapped: WrapperName = {
+                name: "_Client",
                 fernFilepath: service.name.fernFilepath.slice(0, index + 1),
             };
 
-            const nameOfWrapper: WrapperName = {
-                name: WRAPPER_NAME,
-                fernFilepath: nameOfWrapped.fernFilepath.slice(0, -1),
+            const fernFilepathOfWrapper = wrapped.fernFilepath.slice(0, -1);
+            const wrapper: WrapperName = {
+                name: fernFilepathOfWrapper.length === 0 ? "Client" : wrapped.name,
+                fernFilepath: fernFilepathOfWrapper,
             };
 
-            const declarationOfWrapper = (declarations[stringifyFernFilepath(nameOfWrapper.fernFilepath)] ??= {
-                name: nameOfWrapper,
+            const declarationOfWrapper = (declarations[stringifyFernFilepath(wrapper.fernFilepath)] ??= {
+                name: wrapper,
                 wrappedServices: [],
                 wrappedWrappers: [],
             });
 
             if (index === service.name.fernFilepath.length - 1) {
-                declarationOfWrapper.wrappedServices.push(nameOfWrapped);
-            } else if (!declarationOfWrapper.wrappedWrappers.some((wrapper) => isEqual(wrapper, nameOfWrapped))) {
-                declarationOfWrapper.wrappedWrappers.push(nameOfWrapped);
+                declarationOfWrapper.wrappedServices.push(wrapped);
+            } else if (!declarationOfWrapper.wrappedWrappers.some((wrapper) => isEqual(wrapper, wrapped))) {
+                declarationOfWrapper.wrappedWrappers.push(wrapped);
             }
         }
     }
