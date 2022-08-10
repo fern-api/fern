2b87d96f8c788fba22025aae5f40b1ab407e1702
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-08-10 00:42:07 -0700

    Rename Client to _Client, except root wrapper (#561)
    * Rename Client to _Client, except root wrapper
    
    * Use constants
    
    * Simplify constructWrapperDeclarations

--- cli/src/v2/client/constructWrapperDeclarations.ts
+++ cli/src/v2/client/constructWrapperDeclarations.ts
@@ -1,9 +1,10 @@
 import { IntermediateRepresentation } from "@fern-fern/ir-model";
+import { ClientConstants } from "@fern-typescript/client-v2";
 import { WrapperDeclaration, WrapperName } from "@fern-typescript/commons-v2";
 import { isEqual } from "lodash-es";
 import { StringifiedFernFilepath, stringifyFernFilepath } from "./utils/stringifyFernFilepath";
 
-const WRAPPER_NAME = "Client";
+const ROOT_WRAPPER_NAME = "Client";
 
 export function constructWrapperDeclarations(
     intermediateRepresentation: IntermediateRepresentation
@@ -11,27 +12,28 @@ export function constructWrapperDeclarations(
     const declarations: Record<StringifiedFernFilepath, WrapperDeclaration> = {};
 
     for (const service of intermediateRepresentation.services.http) {
-        for (let index = 0; index < service.name.fernFilepath.length; index++) {
-            const nameOfWrapped: WrapperName = {
-                name: WRAPPER_NAME,
-                fernFilepath: service.name.fernFilepath.slice(0, index + 1),
+        for (const [index, fernFilepathPart] of service.name.fernFilepath.entries()) {
+            const wrapper: WrapperName = {
+                name: index === 0 ? ROOT_WRAPPER_NAME : ClientConstants.HttpService.SERVICE_NAME,
+                fernFilepath: service.name.fernFilepath.slice(0, index),
             };
 
-            const nameOfWrapper: WrapperName = {
-                name: WRAPPER_NAME,
-                fernFilepath: nameOfWrapped.fernFilepath.slice(0, -1),
-            };
-
-            const declarationOfWrapper = (declarations[stringifyFernFilepath(nameOfWrapper.fernFilepath)] ??= {
-                name: nameOfWrapper,
+            const declarationOfWrapper = (declarations[stringifyFernFilepath(wrapper.fernFilepath)] ??= {
+                name: wrapper,
                 wrappedServices: [],
                 wrappedWrappers: [],
             });
 
             if (index === service.name.fernFilepath.length - 1) {
-                declarationOfWrapper.wrappedServices.push(nameOfWrapped);
-            } else if (!declarationOfWrapper.wrappedWrappers.some((wrapper) => isEqual(wrapper, nameOfWrapped))) {
-                declarationOfWrapper.wrappedWrappers.push(nameOfWrapped);
+                declarationOfWrapper.wrappedServices.push(service.name);
+            } else {
+                const wrapped: WrapperName = {
+                    name: ClientConstants.HttpService.SERVICE_NAME,
+                    fernFilepath: [...wrapper.fernFilepath, fernFilepathPart],
+                };
+                if (!declarationOfWrapper.wrappedWrappers.some((wrapper) => isEqual(wrapper, wrapped))) {
+                    declarationOfWrapper.wrappedWrappers.push(wrapped);
+                }
             }
         }
     }
