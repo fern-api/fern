7867590c647f817e45f58040f4654f2e3f203621
Zach Kirsch <zachkirsch@gmail.com>
2022-08-17 22:58:56 -0400

    Fix eslint

--- packages/config-management/manage-generator/src/upgradeGenerators.ts
+++ packages/config-management/manage-generator/src/upgradeGenerators.ts
@@ -27,6 +27,7 @@ export function upgradeGenerators({
         return generatorConfig;
     });
 
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (!didUpgrade) {
         console.log("No upgrades found");
     }
