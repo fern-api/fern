454a7ec2b02f88297f0afaf50c44fdf6677eb204
Zach Kirsch <zachkirsch@gmail.com>
2022-08-21 17:21:24 -0700

    Fix depcheck

--- .pnp.cjs
+++ .pnp.cjs
@@ -6521,7 +6521,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/jest", "npm:28.1.7"],\
             ["@types/js-yaml", "npm:4.0.5"],\
             ["@types/node", "npm:18.7.7"],\
-            ["chalk", "npm:5.0.1"],\
             ["depcheck", "npm:1.4.3"],\
             ["eslint", "npm:8.22.0"],\
             ["jest", "virtual:f8e21c9fa0a798b21a87ac455600e39198fef65848556a3a2cd5c956b0460181f9e618173dcff6d9d4fb96fb870055dfe1a3ec3db5db0700e71c0184e4711659#npm:28.1.3"],\
