d0caec0b0ecd9873fc6665d8f9c8b42f52f1bf1f
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-08-30 14:28:31 -0400

    Simplify lint-staged (#50)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "**/*.ts{,x}": ["eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js", "yarn format"],
     "**/*.{js,json,yml,html,css,less,scss,md}": "yarn format",
-    "**/{*,__}": () => "yarn lint:monorepo",
-    "**/{package.json, __}": () => "yarn install --immutable",
+    "**": () => "yarn lint:monorepo",
+    "**/package.json": () => "yarn install --immutable",
 };
