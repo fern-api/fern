9a7cba0207df597a0bd800a5f14a8d822b3b39c8
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-09-15 18:23:48 -0700

    Move discriminant migration to 0.0.191 (#703)
    Move migration to 0.0.191

--- packages/cli/migrations/src/migrations/0.0.191/index.ts
+++ packages/cli/migrations/src/migrations/0.0.191/index.ts
@@ -2,7 +2,7 @@ import { VersionMigrations } from "../../types/VersionMigrations";
 import DiscriminantMigration from "./discriminant";
 
 const versionMigrations: VersionMigrations = {
-    version: "0.0.190",
+    version: "0.0.191",
     migrations: [DiscriminantMigration],
 };
 
--- packages/cli/migrations/src/migrations/index.ts
+++ packages/cli/migrations/src/migrations/index.ts
@@ -1,5 +1,5 @@
 import { VersionMigrations } from "../types/VersionMigrations";
 import migrations_0_0_188 from "./0.0.188";
-import migrations_0_0_190 from "./0.0.190";
+import migrations_0_0_191 from "./0.0.191";
 
-export const ALL_MIGRATIONS: VersionMigrations[] = [migrations_0_0_188, migrations_0_0_190];
+export const ALL_MIGRATIONS: VersionMigrations[] = [migrations_0_0_188, migrations_0_0_191];
