b7297d176a129750af29e494fe61a9b5073c07a4
Zach Kirsch <zachkirsch@gmail.com>
2022-09-15 20:08:44 -0700

    Clean up getIndexOfFirstMigrationGreaterThanOrEqualTo

--- packages/cli/migrations/src/migrations/getMigrationsToRun.ts
+++ packages/cli/migrations/src/migrations/getMigrationsToRun.ts
@@ -10,31 +10,22 @@ export function getMigrationsToRun({
     toVersion: string;
 }): VersionMigrations[] {
     return ALL_MIGRATIONS.slice(
-        getIndexOfFirstMigrationToNotRun(fromVersion),
-        getIndexOfFirstMigrationToNotRun(toVersion)
+        getIndexOfFirstMigrationGreaterThanOrEqualTo(fromVersion),
+        getIndexOfFirstMigrationGreaterThanOrEqualTo(toVersion)
     );
 }
 
 /**
- * returns the index of the first migration that _shouldn't_ be run for this version.
- * example:
- *   migrations are [0.0.188, 0.0.191, 0.0.200]
- *   getIndexOfFirstMigrationToNotRun("0.0.188") -> 0, since we don't run any migrations.
- *   getIndexOfFirstMigrationToNotRun("0.0.189") -> 1, since we run just the first migration.
- *   getIndexOfFirstMigrationToNotRun("0.0.191") -> 1, since we run just the first migration.
- *   getIndexOfFirstMigrationToNotRun("0.0.192") -> 2, since we run the first two migrations.
- *   getIndexOfFirstMigrationToNotRun("0.0.200") -> 2, since we run the first two migrations.
- *   getIndexOfFirstMigrationToNotRun("0.0.201") -> 3, since we run the all three migrations.
+ * returns the index of the first migration that is >= the provided version.
+ * returns undefined if no migrations are >= the provided version.
  */
-function getIndexOfFirstMigrationToNotRun(version: string): number {
-    let index;
-    for (index = 0; index < ALL_MIGRATIONS.length; index++) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const { version: versionOfMigration } = ALL_MIGRATIONS[index]!;
-
-        if (version === versionOfMigration || isVersionAhead(versionOfMigration, version)) {
-            return index;
-        }
+function getIndexOfFirstMigrationGreaterThanOrEqualTo(version: string): number | undefined {
+    const index = ALL_MIGRATIONS.findIndex(
+        ({ version: versionOfMigration }) =>
+            versionOfMigration === version || isVersionAhead(versionOfMigration, version)
+    );
+    if (index === -1) {
+        return undefined;
     }
     return index;
 }
