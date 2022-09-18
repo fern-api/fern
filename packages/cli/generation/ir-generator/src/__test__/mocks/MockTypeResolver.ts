873ed25ac83418e85a6cafc2b23dac53f8d02cf3
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-09-17 20:40:32 -0700

    Add referencedTypes to IR (#731)

--- packages/cli/generation/ir-generator/src/__test__/mocks/MockTypeResolver.ts
+++ packages/cli/generation/ir-generator/src/__test__/mocks/MockTypeResolver.ts
@@ -1,6 +1,9 @@
 import { TypeResolver } from "../../type-resolver/TypeResolver";
 
 export const MockTypeResolver: TypeResolver = {
+    getDeclarationOfNamedType: () => {
+        throw new Error("Not implemented");
+    },
     resolveType: () => {
         throw new Error("Not implemented");
     },
