b6d54e358513b8495b1796e733b8edaf1df31031
Danny Sheridan <83524670+dannysheridan@users.noreply.github.com>
2022-09-26 17:26:24 -0700

    Update generator invocations for all code generators (#758)
    * Add description to fern upgrade
    
    * Change file structure & revise CLI help message
    
    * Update intro blurb
    
    * Bump generator versions
    
    * Update test for upgrade
    
    * Change fern-java in docs to java-client

--- packages/cli/config-management/manage-generator/src/generatorInvocations.ts
+++ packages/cli/config-management/manage-generator/src/generatorInvocations.ts
@@ -2,17 +2,13 @@ import { RelativeFilePath } from "@fern-api/core-utils";
 import { DraftGeneratorInvocationSchema } from "@fern-api/generators-configuration";
 
 export const JAVA_GENERATOR_INVOCATION: DraftGeneratorInvocationSchema = {
-    name: "fernapi/fern-java",
-    version: "0.0.92",
-    config: {
-        packagePrefix: "com",
-        mode: "client",
-    },
+    name: "fernapi/java-client",
+    version: "0.0.109",
 };
 
 export const TYPESCRIPT_GENERATOR_INVOCATION: DraftGeneratorInvocationSchema = {
     name: "fernapi/fern-typescript",
-    version: "0.0.189",
+    version: "0.0.197",
     config: {
         mode: "client-v2",
     },
@@ -20,13 +16,13 @@ export const TYPESCRIPT_GENERATOR_INVOCATION: DraftGeneratorInvocationSchema = {
 
 export const POSTMAN_GENERATOR_INVOCATION: DraftGeneratorInvocationSchema = {
     name: "fernapi/fern-postman",
-    version: "0.0.22",
+    version: "0.0.23",
     "local-output": RelativeFilePath.of("./generated-postman.json"),
 };
 
 export const OPENAPI_GENERATOR_INVOCATION: DraftGeneratorInvocationSchema = {
     name: "fernapi/fern-openapi",
-    version: "0.0.5",
+    version: "0.0.6",
     "local-output": RelativeFilePath.of("./generated-openapi.json"),
     config: {
         format: "yaml",
