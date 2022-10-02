5dfed56b7b1b89ba5edfd0d1609bbdaf138e2102
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-10-02 15:11:06 -0700

    Cache heavy dependencies inside docker image (#89)

---
+++ packages/generators/sdk/cli/docker/cache_heavy_dependencies.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+project_name=heavy_deps_project
+
+mkdir $project_name
+cd $project_name
+
+# this exits with a non-zero exit code because git is not installed.
+# but it initializes the project correctly.
+yarn init -2 || :
+
+yarn add typescript@4.6.4
+yarn add esbuild-linux-64@0.14.47
+
+cd ..
+/bin/rm -rf $project_name
