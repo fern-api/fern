61beb126ddcac8b356f3ef5d293ad8bd64918e06
Zach Kirsch <zachkirsch@gmail.com>
2022-10-02 13:41:08 -0700

    Fix tests

--- packages/commons/core-utils/src/fs-utils/__test__/join.test.ts
+++ packages/commons/core-utils/src/fs-utils/__test__/join.test.ts
@@ -7,61 +7,61 @@ import { RelativeFilePath } from "../RelativeFilePath";
 
 describe("join", () => {
     it("should compile with all relative paths", () => {
-        join("a", "b/c", "d");
+        () => join("a", "b/c", "d");
     });
 
     it("should compile when only first path is absolute", () => {
-        join("/a", "b/c", "d");
+        () => join("/a", "b/c", "d");
     });
 
     it("should compile with some RelativeFilepath's", () => {
-        join("a", "b/c"), "d";
+        () => join("a", "b/c", "d");
     });
 
     it("should compile with all RelativeFilepath's", () => {
-        join("a"), RelativeFilePath.of("b/c");
+        () => join("a", RelativeFilePath.of("b/c"));
     });
 
     it("should compile when only first path is an AbsoluteFilePath", () => {
-        join(AbsoluteFilePath.of("a"), "b/c");
+        () => join(AbsoluteFilePath.of("a"), "b/c");
     });
 
     it("should not compile when first path is absolute and non-first path is absolute", () => {
         // @ts-expect-error
-        join("/a", "b/c", "/d");
+        () => join("/a", "b/c", "/d");
     });
 
     it("should not compile when first path is relative and non-first path is absolute", () => {
         // @ts-expect-error
-        join("a", "b/c", "/d");
+        () => join("a", "b/c", "/d");
     });
 
     it("should not compile when first path is relative and non-first path is just a string", () => {
         const justAString = ((): string => "hello")();
         // @ts-expect-error
-        join("a", justAString);
+        () => join("a", justAString);
     });
 
     it("should not compile when first path is absolute and non-first path is just a string", () => {
         const justAString = ((): string => "hello")();
         // @ts-expect-error
-        join("/a", justAString);
+        () => join("/a", justAString);
     });
 
     it("should not compile when first path is an AbsoluteFilePath and non-first path is an AbsoluteFilePath", () => {
         // @ts-expect-error
-        join(AbsoluteFilePath.of("/a"), "b/c", AbsoluteFilePath.of("/d"));
+        () => join(AbsoluteFilePath.of("/a"), "b/c", AbsoluteFilePath.of("/d"));
     });
 
     it("should not compile when first path is a RelativeFilePath and non-first path is an AbsoluteFilePath", () => {
         // @ts-expect-error
-        join("a", "b/c", AbsoluteFilePath.of("/d"));
+        () => join("a", "b/c", AbsoluteFilePath.of("/d"));
     });
 
     it("should not compile when first path is a RelativeFilePath and non-first path is just a string", () => {
         const justAString = ((): string => "hello")();
         // @ts-expect-error
-        join("a", justAString);
+        () => join("a", justAString);
     });
 
     it("should not compile when first path is an AbsoluteFilePath and non-first path is just a string", () => {
