1351937c0d75badb8a7bb76026d36d2200dc21d7
Deep Singhvi <deep@usebirch.com>
2022-10-10 17:57:38 -0700

    improvement: upgrade fiddle client to consume GitHub output mode (#809)
    upgrade fiddle to consume github output mode

--- packages/cli/config-management/generators-configuration/src/schemas/GithubRepositoryOutputSchema.ts
+++ packages/cli/config-management/generators-configuration/src/schemas/GithubRepositoryOutputSchema.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const GithubRepositoryOutputSchema = z.strictObject({
     repository: z.string(),
-    token: z.string(),
 });
 
 export type GithubRepositoryOutputSchema = z.infer<typeof GithubRepositoryOutputSchema>;
