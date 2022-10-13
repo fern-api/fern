1d1a924fb0bfa6e430ea0380061580c660c384bc
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-10-12 19:23:12 -0700

    Add error constants to IR (#815)
    * Add error constants to IR
    
    * string -> wire string
    
    * Add doc

--- packages/cli/generation/ir-generator/src/FernConstants.ts
+++ packages/cli/generation/ir-generator/src/FernConstants.ts
@@ -1,7 +1,17 @@
+import { Constants } from "@fern-fern/ir-model/constants";
 import { FernConstants } from "@fern-fern/ir-model/ir";
+import { generateWireStringWithAllCasings } from "./utils/generateCasings";
 
 export const FERN_CONSTANTS: FernConstants = {
     errorDiscriminant: "_error",
     errorInstanceIdKey: "_errorInstanceId",
     unknownErrorDiscriminantValue: "_unknown",
+};
+
+export const FERN_CONSTANTS_V2: Constants = {
+    errors: {
+        errorDiscriminant: generateWireStringWithAllCasings({ wireValue: "error", name: "error" }),
+        errorInstanceIdKey: generateWireStringWithAllCasings({ wireValue: "errorInstanceId", name: "errorInstanceId" }),
+        errorContentKey: generateWireStringWithAllCasings({ wireValue: "content", name: "content" }),
+    },
 };
