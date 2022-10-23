83834625cf35573a3e81684320aa8b81cc14ee1f
dsinghvi <deep@usebirch.com>
2022-10-22 21:46:48 -0700

    generate ir origin from yaml

---
+++ packages/cli/generation/ir-generator/src/converters/convertEnvironments.ts
@@ -0,0 +1,31 @@
+import { RawSchemas } from "@fern-api/yaml-schema";
+import { Environment } from "@fern-fern/ir-model/environment";
+import { CasingsGenerator } from "../casings/CasingsGenerator";
+
+export function convertEnvironments({
+    rawApiFileSchema,
+    casingsGenerator,
+}: {
+    rawApiFileSchema: RawSchemas.RootApiFileSchema;
+    casingsGenerator: CasingsGenerator;
+}): Environment[] {
+    const environments: Environment[] = [];
+    for (const [environmentId, environment] of rawApiFileSchema.environments) {
+        if (typeof environment === "string") {
+            environments.push({
+                id: environmentId,
+                name: casingsGenerator.generateName(environmentId),
+                url: environment,
+                docs: undefined,
+            });
+        } else {
+            environments.push({
+                id: environmentId,
+                name: casingsGenerator.generateName(environmentId),
+                url: environment.url,
+                docs: environment.docs,
+            });
+        }
+    }
+    return environments;
+}
