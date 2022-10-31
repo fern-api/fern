48460d2aab782f83455007b59fb97e40b47bc381
Danny Sheridan <danny@usebirch.com>
2022-10-31 15:05:04 -0700

    Update generators for invocation

--- packages/cli/config-management/manage-generator/src/generatorInvocations.ts
+++ packages/cli/config-management/manage-generator/src/generatorInvocations.ts
@@ -2,26 +2,26 @@ import { DraftGeneratorInvocationSchema } from "@fern-api/generators-configurati
 
 export const JAVA_GENERATOR_INVOCATION: DraftGeneratorInvocationSchema = {
     name: "fernapi/fern-java-sdk",
-    version: "0.0.120",
+    version: "0.0.122",
     mode: "publish",
 };
 
 export const TYPESCRIPT_GENERATOR_INVOCATION: DraftGeneratorInvocationSchema = {
     name: "fernapi/fern-typescript-sdk",
-    version: "0.0.224",
+    version: "0.0.227",
     mode: "publish",
 };
 
 export const POSTMAN_GENERATOR_INVOCATION: DraftGeneratorInvocationSchema = {
     name: "fernapi/fern-postman",
-    version: "0.0.23",
+    version: "0.0.25",
     mode: "download-files",
     "output-path": "./generated-postman",
 };
 
 export const OPENAPI_GENERATOR_INVOCATION: DraftGeneratorInvocationSchema = {
     name: "fernapi/fern-openapi",
-    version: "0.0.9",
+    version: "0.0.10",
     config: {
         format: "yaml",
     },
