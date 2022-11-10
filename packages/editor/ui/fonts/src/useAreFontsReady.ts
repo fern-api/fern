3d92c6772efe758a5185efd2f1d46a6c2f3a588a
Zach Kirsch <zachkirsch@gmail.com>
2022-11-10 13:14:41 -0800

    Simplify hook

--- packages/editor/ui/fonts/src/useAreFontsReady.ts
+++ packages/editor/ui/fonts/src/useAreFontsReady.ts
@@ -2,13 +2,15 @@ import { useEffect, useMemo, useState } from "react";
 
 export function useAreFontsReady(fonts: string[]): boolean {
     const [isReady, setIsReady] = useState(false);
-    useEffect(() => {
-        if (!isReady) {
+    useEffect(
+        () => {
             void Promise.all(fonts.map((font) => document.fonts.load(`12px ${font}`))).then(() => {
                 setIsReady(true);
             });
-        }
-    }, [fonts, isReady]);
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        fonts
+    );
     return isReady;
 }
 
