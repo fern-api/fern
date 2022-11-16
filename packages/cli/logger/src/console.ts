1e7b83f4b556a58e59b2480600caf28842f6c2a3
Zach Kirsch <zachkirsch@gmail.com>
2022-11-16 13:08:53 -0800

    Add createLogger

--- packages/cli/logger/src/console.ts
+++ packages/cli/logger/src/console.ts
@@ -1,20 +1,15 @@
+import { createLogger } from "./createLogger";
 import { Logger } from "./Logger";
 import { LogLevel } from "./LogLevel";
 
-export const CONSOLE_LOGGER: Logger = {
-    debug: (...args) => log(LogLevel.Debug, ...args),
-    info: (...args) => log(LogLevel.Info, ...args),
-    warn: (...args) => log(LogLevel.Warn, ...args),
-    error: (...args) => log(LogLevel.Error, ...args),
-    log,
-};
+export const CONSOLE_LOGGER: Logger = Object.freeze(createLogger(log));
 
-function log(level: LogLevel, ...args: unknown[]): void {
+function log(level: LogLevel, ...args: string[]): void {
     const consoleLogger = getConsoleLoggerForLevel(level);
     consoleLogger(...args);
 }
 
-function getConsoleLoggerForLevel(level: LogLevel): (...args: unknown[]) => void {
+function getConsoleLoggerForLevel(level: LogLevel): (...args: string[]) => void {
     switch (level) {
         case LogLevel.Debug:
             // eslint-disable-next-line no-console
