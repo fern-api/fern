69582a772a23b17dc00dfd14b8e3a8fbb3f5b450
Zach Kirsch <zachkirsch@gmail.com>
2022-12-02 00:50:43 -0800

    Redirect /docs to mintlify docs/intro

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -55,8 +55,7 @@ const config = {
                 },
                 items: [
                     {
-                        type: "doc",
-                        docId: "intro",
+                        href: "/docs",
                         position: "left",
                         label: "Docs",
                     },
