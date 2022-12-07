045d5e00b34332637c0c9431291a542fe4bc2c80
Deep Singhvi <deep@usebirch.com>
2022-12-06 16:42:19 -0800

    validate that `auth` is defined if auth is required on an endpoint or service (#958)
    if auth is required then it must be defined

---
+++ packages/cli/yaml/validator/src/rules/no-missing-auth/__test__/no-missing-auth.test.ts
@@ -0,0 +1,27 @@
+import { AbsoluteFilePath, join } from "@fern-api/fs-utils";
+import { getViolationsForRule } from "../../../testing-utils/getViolationsForRule";
+import { NoMissingAuthRule } from "../no-missing-auth";
+
+describe("no-missing-auth", () => {
+    it("simple", async () => {
+        const violations = await getViolationsForRule({
+            rule: NoMissingAuthRule,
+            absolutePathToWorkspace: join(AbsoluteFilePath.of(__dirname), "fixtures", "simple"),
+        });
+
+        expect(violations).toEqual([
+            {
+                severity: "error",
+                relativeFilepath: "a.yml",
+                nodePath: ["services", "http", "AService", "endpoints", "foo"],
+                message: "Endpoint requires auth, but no auth is defined.",
+            },
+            {
+                severity: "error",
+                relativeFilepath: "b.yml",
+                nodePath: ["services", "http", "BService"],
+                message: "Service requires auth, but no auth is defined.",
+            },
+        ]);
+    });
+});
