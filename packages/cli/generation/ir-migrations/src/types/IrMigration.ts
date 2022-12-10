d66856c86e17f0e02924b575096ce427d31ba21e
Zach Kirsch <zachkirsch@gmail.com>
2022-12-10 11:04:41 -0800

    Add Typescript SDK docker name for IR migrations

--- packages/cli/generation/ir-migrations/src/types/IrMigration.ts
+++ packages/cli/generation/ir-migrations/src/types/IrMigration.ts
@@ -11,9 +11,9 @@ export interface IrMigration<NextVersion, PreviousVersion> {
      * if the targeted generator's version is less than or equal to its value in
      * requiredForGeneratorVersions, then this migration is needed.
      *
-     * if the targeted generator's version is greater than its value in this map,
-     * or the targeted generator's name is not in this map, then this migration
-     * is needed.
+     * if the targeted generator's version is greater than its value in this
+     * map, or the value in this map is `undefined`, then this migration is
+     * needed.
      *
      * if the targeted generator's version is AlwaysRunMigration, then this
      * migration is needed
