a35f791b493083a94f3375617dbda97ab4bf4709
Zach Kirsch <zachkirsch@gmail.com>
2022-12-10 11:38:31 -0800

    Add missing generators to IR migrations

--- packages/cli/generation/ir-migrations/src/types/GeneratorName.ts
+++ packages/cli/generation/ir-migrations/src/types/GeneratorName.ts
@@ -2,8 +2,10 @@ export const GeneratorName = {
     TYPESCRIPT: "fernapi/fern-typescript",
     TYPESCRIPT_SDK: "fernapi/fern-typescript-sdk",
     JAVA: "fernapi/fern-java",
+    JAVA_MODEL: "fernapi/java-model",
     JAVA_SDK: "fernapi/fern-java-sdk",
-    PYTHON: "fernapi/fern-python",
+    PYTHON_FASTAPI: "fernapi/fern-fastapi-server",
+    PYTHON_PYDANTIC: "fernapi/fern-pydantic-model",
     OPENAPI: "fernapi/fern-openapi",
     POSTMAN: "fernapi/fern-postman",
 } as const;
