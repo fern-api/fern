90c936504412a9c9587d628a01ca2af5930a2016
Zach Kirsch <zachkirsch@gmail.com>
2022-12-10 10:38:26 -0800

    Add version for TS

--- packages/cli/generation/ir-migrations/src/migrations/ir-v1/migrateFromV2ToV1.ts
+++ packages/cli/generation/ir-migrations/src/migrations/ir-v1/migrateFromV2ToV1.ts
@@ -4,7 +4,7 @@ import { AlwaysRunMigration, IrMigration } from "../../types/IrMigration";
 
 export const V2_TO_V1_MIGRATION: IrMigration<V2.ir.IntermediateRepresentation, V1.ir.IntermediateRepresentation> = {
     requiredForGeneratorVersions: {
-        "fernapi/fern-typescript": AlwaysRunMigration,
+        "fernapi/fern-typescript": "0.0.245",
         "fernapi/fern-python": AlwaysRunMigration,
         "fernapi/fern-java": AlwaysRunMigration,
         "fernapi/fern-openapi": AlwaysRunMigration,
