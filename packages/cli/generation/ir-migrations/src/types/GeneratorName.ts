333a34a9386872b847ac9629d2586a75d0c10964
Zach Kirsch <zachkirsch@gmail.com>
2022-12-10 11:11:44 -0800

    Add version for Java SDK

--- packages/cli/generation/ir-migrations/src/types/GeneratorName.ts
+++ packages/cli/generation/ir-migrations/src/types/GeneratorName.ts
@@ -2,6 +2,7 @@ export const GeneratorName = {
     TYPESCRIPT: "fernapi/fern-typescript",
     TYPESCRIPT_SDK: "fernapi/fern-typescript-sdk",
     JAVA: "fernapi/fern-java",
+    JAVA_SDK: "fernapi/fern-java-sdk",
     PYTHON: "fernapi/fern-python",
     OPENAPI: "fernapi/fern-openapi",
     POSTMAN: "fernapi/fern-postman",
