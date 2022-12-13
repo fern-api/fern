7d04f1344e9b8fc6ffb0db1f56abd49c994ec498
Zach Kirsch <zachkirsch@gmail.com>
2022-12-12 17:19:22 -0800

    Remove dry-run flag

--- scripts/git-version.sh
+++ scripts/git-version.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-tag="$(git describe --exact-match --tags HEAD 2> /dev/null || :)"
-
 if [[ -n "$CIRCLE_TAG" ]]; then
 	echo "$CIRCLE_TAG"
 	exit 0
@@ -9,6 +7,7 @@ fi
 
 # if we're not on a tag in Circle but the current commit is tagged, then
 # should then ignore the tag
+tag="$(git describe --exact-match --tags HEAD 2> /dev/null || :)"
 if [[ -n "$tag" ]]; then
 	exclude_param="--exclude $tag"
 fi
