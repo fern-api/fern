23dc3c703640407f1c27cf2d3195e3a7cd88208c
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-12-13 23:31:57 -0800

    Upgrade to fiddle SDK 0.0.126 (#999)

--- .pnp.cjs
+++ .pnp.cjs
@@ -8142,7 +8142,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/fs-utils", "workspace:packages/commons/fs-utils"],\
             ["@fern-api/project-configuration", "workspace:packages/cli/config-management/project-configuration"],\
             ["@fern-api/task-context", "workspace:packages/cli/task-context"],\
-            ["@fern-fern/fiddle-sdk", "npm:0.0.89"],\
+            ["@fern-fern/fiddle-sdk", "npm:0.0.126"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/js-yaml", "npm:4.0.5"],\
             ["@types/node", "npm:18.7.18"],\
@@ -8469,7 +8469,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/logger", "workspace:packages/cli/logger"],\
             ["@fern-api/task-context", "workspace:packages/cli/task-context"],\
             ["@fern-api/workspace-loader", "workspace:packages/cli/workspace-loader"],\
-            ["@fern-fern/fiddle-sdk", "npm:0.0.89"],\
+            ["@fern-fern/fiddle-sdk", "npm:0.0.126"],\
             ["@fern-fern/ir-model", "npm:0.0.436"],\
             ["@types/decompress", "npm:4.2.4"],\
             ["@types/jest", "npm:29.0.3"],\
@@ -8745,10 +8745,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["@fern-fern/fiddle-sdk", [\
-        ["npm:0.0.89", {\
-          "packageLocation": "./.yarn/cache/@fern-fern-fiddle-sdk-npm-0.0.89-133c5ee421-82112bbae0.zip/node_modules/@fern-fern/fiddle-sdk/",\
+        ["npm:0.0.126", {\
+          "packageLocation": "./.yarn/cache/@fern-fern-fiddle-sdk-npm-0.0.126-ca99c8c958-1212052582.zip/node_modules/@fern-fern/fiddle-sdk/",\
           "packageDependencies": [\
-            ["@fern-fern/fiddle-sdk", "npm:0.0.89"],\
+            ["@fern-fern/fiddle-sdk", "npm:0.0.126"],\
             ["@types/url-join", "npm:4.0.1"],\
             ["axios", "npm:0.27.2"],\
             ["url-join", "npm:4.0.1"]\
