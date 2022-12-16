2cfad3af07bf40856151e38e6cc028cefed9e99a
Zach Kirsch <zachkirsch@users.noreply.github.com>
2022-12-16 12:50:50 -0800

    Allow dependencies to match same major.minor.x as CLI (#1016)
    * Allow dependencies to match same major.minor.x as CLI
    
    * Fix depcheck

--- .pnp.cjs
+++ .pnp.cjs
@@ -8724,6 +8724,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/fs-utils", "workspace:packages/commons/fs-utils"],\
             ["@fern-api/generators-configuration", "workspace:packages/cli/config-management/generators-configuration"],\
             ["@fern-api/project-configuration", "workspace:packages/cli/config-management/project-configuration"],\
+            ["@fern-api/semver-utils", "workspace:packages/cli/semver-utils"],\
             ["@fern-api/services", "workspace:packages/cli/services"],\
             ["@fern-api/task-context", "workspace:packages/cli/task-context"],\
             ["@fern-api/yaml-schema", "workspace:packages/cli/yaml/yaml-schema"],\
--- packages/cli/semver-utils/src/index.ts
+++ packages/cli/semver-utils/src/index.ts
@@ -1,1 +1,10 @@
 export { isVersionAhead } from "./isVersionAhead";
+export {
+    parseVersion,
+    type BaseVersion,
+    type ParsedVersion,
+    type PostReleaseCandidateCommit,
+    type PostReleaseCommit,
+    type Release,
+    type ReleaseCandidate,
+} from "./parseVersion";
