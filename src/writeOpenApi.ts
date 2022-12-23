a6697ab49ba52ccd352b74b39240d4dfea5fb03f
Deep Singhvi <deep@usebirch.com>
2022-12-23 16:07:13 -0500

    `console.log` errors (#33)
    console.log errors

--- src/writeOpenApi.ts
+++ src/writeOpenApi.ts
@@ -12,42 +12,48 @@ const OPENAPI_JSON_FILENAME = "openapi.json";
 const OPENAPI_YML_FILENAME = "openapi.yml";
 
 export async function writeOpenApi(pathToConfig: string): Promise<void> {
-    const configStr = await readFile(pathToConfig);
-    const config = JSON.parse(configStr.toString()) as GeneratorConfig;
-    const customConfig = getCustomConfig(config);
-
-    const generatorLoggingClient = new GeneratorLoggingWrapper(config);
-
     try {
-        await generatorLoggingClient.sendUpdate(
-            GeneratorUpdate.init({
-                packagesToPublish: [],
-            })
-        );
+        const configStr = await readFile(pathToConfig);
+        const config = JSON.parse(configStr.toString()) as GeneratorConfig;
+        const customConfig = getCustomConfig(config);
 
-        const ir = await loadIntermediateRepresentation(config.irFilepath);
-        const openApiDefinition = convertToOpenApi({
-            apiName: config.workspaceName,
-            apiVersion: config.publish?.version ?? "",
-            ir,
-        });
-        if (customConfig.format === "json") {
-            await writeFile(
-                path.join(config.output.path, OPENAPI_JSON_FILENAME),
-                JSON.stringify(openApiDefinition, undefined, 2)
-            );
-        } else {
-            await writeFile(path.join(config.output.path, OPENAPI_YML_FILENAME), yaml.dump(openApiDefinition));
-        }
-        await generatorLoggingClient.sendUpdate(GeneratorUpdate.exitStatusUpdate(ExitStatusUpdate.successful()));
-    } catch (e) {
-        await generatorLoggingClient.sendUpdate(
-            GeneratorUpdate.exitStatusUpdate(
-                ExitStatusUpdate.error({
-                    message: e instanceof Error ? e.message : "Encountered error",
+        const generatorLoggingClient = new GeneratorLoggingWrapper(config);
+
+        try {
+            await generatorLoggingClient.sendUpdate(
+                GeneratorUpdate.init({
+                    packagesToPublish: [],
                 })
-            )
-        );
+            );
+
+            const ir = await loadIntermediateRepresentation(config.irFilepath);
+            const openApiDefinition = convertToOpenApi({
+                apiName: config.workspaceName,
+                apiVersion: config.publish?.version ?? "",
+                ir,
+            });
+            if (customConfig.format === "json") {
+                await writeFile(
+                    path.join(config.output.path, OPENAPI_JSON_FILENAME),
+                    JSON.stringify(openApiDefinition, undefined, 2)
+                );
+            } else {
+                await writeFile(path.join(config.output.path, OPENAPI_YML_FILENAME), yaml.dump(openApiDefinition));
+            }
+            await generatorLoggingClient.sendUpdate(GeneratorUpdate.exitStatusUpdate(ExitStatusUpdate.successful()));
+        } catch (e) {
+            console.log("Encountered error", e);
+            await generatorLoggingClient.sendUpdate(
+                GeneratorUpdate.exitStatusUpdate(
+                    ExitStatusUpdate.error({
+                        message: e instanceof Error ? e.message : "Encountered error",
+                    })
+                )
+            );
+        }
+    } catch (e) {
+        console.log("Encountered error", e);
+        throw e;
     }
 }
 
