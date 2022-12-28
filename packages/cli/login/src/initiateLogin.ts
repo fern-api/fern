cbdf05416b3ede1ff348c9b9d64d4e5f96b5122f
dsinghvi <deep@usebirch.com>
2022-12-27 21:34:09 -0500

    directly reference env var

--- packages/cli/login/src/initiateLogin.ts
+++ packages/cli/login/src/initiateLogin.ts
@@ -1,9 +1,6 @@
 import { getToken } from "./getToken";
 import { storeToken } from "./storeToken";
 
-const AUTH0_DOMAIN_ENV_VAR = "AUTH0_DOMAIN";
-const AUTH0_CLIENT_ID_ENV_VAR = "AUTH0_CLIENT_ID";
-
 export declare namespace initiateLogin {
     export interface Args {
         auth0Domain?: string;
@@ -12,8 +9,8 @@ export declare namespace initiateLogin {
 }
 
 export async function initiateLogin({
-    auth0Domain = process.env[AUTH0_DOMAIN_ENV_VAR],
-    auth0ClientId = process.env[AUTH0_CLIENT_ID_ENV_VAR],
+    auth0Domain = process.env.AUTH0_DOMAIN,
+    auth0ClientId = process.env.AUTH0_CLIENT_ID,
 }: initiateLogin.Args = {}): Promise<void> {
     if (auth0Domain == null) {
         throw new Error("Cannot login because Auth0 domain is not defined.");
