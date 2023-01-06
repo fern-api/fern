ff597bd43bdf21b6a5e34d6de36545c8d75d2f5d
Deep Singhvi <deep@usebirch.com>
2023-01-06 13:57:11 -0800

    update prod auth0 domain and client id (#1092)

--- packages/cli/cli/.env-cmdrc.cjs
+++ packages/cli/cli/.env-cmdrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     prod: {
-        AUTH0_DOMAIN: "fern-dev.us.auth0.com",
-        AUTH0_CLIENT_ID: "4QiMvRvRUYpnycrVDK2M59hhJ6kcHYFQ",
+        AUTH0_DOMAIN: "fern-prod.us.auth0.com",
+        AUTH0_CLIENT_ID: "syaWnk6SjNoo5xBf1omfvziU3q7085lh",
         DEFAULT_FIDDLE_ORIGIN: "https://fiddle-coordinator.buildwithfern.com",
         VENUS_AUDIENCE: "venus-prod",
         LOCAL_STORAGE_FOLDER: ".fern",
