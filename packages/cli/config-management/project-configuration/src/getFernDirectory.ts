e1b571552dc2f057b7fe2e1f11805c7c8e8c5962
Zach Kirsch <zachkirsch@users.noreply.github.com>
2023-01-10 14:14:00 -0800

    Look for fern.config.json when finding nearest fern directory (#1118)

--- packages/cli/config-management/project-configuration/src/getFernDirectory.ts
+++ packages/cli/config-management/project-configuration/src/getFernDirectory.ts
@@ -1,11 +1,17 @@
-import { AbsoluteFilePath } from "@fern-api/fs-utils";
+import { AbsoluteFilePath, doesPathExist, join } from "@fern-api/fs-utils";
 import { findUp } from "find-up";
-import { FERN_DIRECTORY } from "./constants";
+import { FERN_DIRECTORY, PROJECT_CONFIG_FILENAME } from "./constants";
 
 export async function getFernDirectory(): Promise<AbsoluteFilePath | undefined> {
     const fernDirectoryStr = await findUp(FERN_DIRECTORY, { type: "directory" });
     if (fernDirectoryStr == null) {
         return undefined;
     }
-    return AbsoluteFilePath.of(fernDirectoryStr);
+    const absolutePathToFernDirectory = AbsoluteFilePath.of(fernDirectoryStr);
+
+    if (await doesPathExist(join(absolutePathToFernDirectory, PROJECT_CONFIG_FILENAME))) {
+        return absolutePathToFernDirectory;
+    } else {
+        return undefined;
+    }
 }
