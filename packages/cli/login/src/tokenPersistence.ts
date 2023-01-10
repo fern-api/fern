6629030d7b432fa6ed77a78d6806aca88db9deb6
Zach Kirsch <zachkirsch@gmail.com>
2023-01-09 20:56:30 -0800

    Look for FERN_TOKEN env var

--- packages/cli/login/src/tokenPersistence.ts
+++ packages/cli/login/src/tokenPersistence.ts
@@ -5,6 +5,8 @@ import path from "path";
 
 const TOKEN_FILENAME = "token";
 
+const FERN_TOKEN_ENV_VAR = "FERN_TOKEN";
+
 export async function storeToken(token: string): Promise<void> {
     if (process.env.LOCAL_STORAGE_FOLDER == null) {
         throw new Error("Failed to store token: path to local storage is undefined!");
@@ -15,14 +17,20 @@ export async function storeToken(token: string): Promise<void> {
 }
 
 export async function loadToken(): Promise<string | undefined> {
+    if (process.env[FERN_TOKEN_ENV_VAR] != null) {
+        return process.env[FERN_TOKEN_ENV_VAR];
+    }
+
     if (process.env.LOCAL_STORAGE_FOLDER == null) {
         throw new Error("Failed to load token: path to local storage is undefined!");
     }
     const pathToTokenFile = path.join(homedir(), process.env.LOCAL_STORAGE_FOLDER, TOKEN_FILENAME);
-    const tokenFileExists = await doesPathExist(AbsoluteFilePath.of(pathToTokenFile));
-    if (!tokenFileExists) {
+
+    const doesTokenFileExist = await doesPathExist(AbsoluteFilePath.of(pathToTokenFile));
+    if (!doesTokenFileExist) {
         return undefined;
     }
+
     const tokenFileContents = await readFile(pathToTokenFile);
     return tokenFileContents.toString();
 }
