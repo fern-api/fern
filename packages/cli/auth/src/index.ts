3a4b276b59ed2846a88bf1792c7b3870c469481f
Zach Kirsch <zachkirsch@gmail.com>
2023-01-11 13:38:36 -0800

    Validate organization name

--- packages/cli/auth/src/index.ts
+++ packages/cli/auth/src/index.ts
@@ -1,5 +1,6 @@
-export { createOrganizationIfDoesNotExist } from "./createOrganizationIfDoesNotExist";
 export { type FernToken } from "./FernToken";
 export { login } from "./login";
+export { createOrganizationIfDoesNotExist } from "./orgs/createOrganizationIfDoesNotExist";
+export { getOrganizationNameValidationError } from "./orgs/getOrganizationNameValidationError";
 export { getToken } from "./persistence/getToken";
 export { isLoggedIn } from "./verify/isLoggedIn";
