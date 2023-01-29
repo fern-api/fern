cedd03bfb60f0f5462aa215b65c32d1794b7150f
Deep Singhvi <deep@usebirch.com>
2023-01-29 09:32:06 -0800

    import-file-exists validation rule accounts for all files (#1240)
    * file exists validation should check against loaded workspace files
    
    * imports can come from on disk service files, imported service files, or package markers

--- packages/cli/yaml/validator/src/rules/import-file-exists/import-file-exists.ts
+++ packages/cli/yaml/validator/src/rules/import-file-exists/import-file-exists.ts
@@ -1,21 +1,33 @@
-import { dirname, doesPathExist, join, RelativeFilePath } from "@fern-api/fs-utils";
+import { dirname, join, RelativeFilePath } from "@fern-api/fs-utils";
 import chalk from "chalk";
 import { Rule, RuleViolation } from "../../Rule";
 
 export const ImportFileExistsRule: Rule = {
     name: "import-file-exists",
     create: ({ workspace }) => {
+        const relativePaths = [
+            ...Object.keys(workspace.serviceFiles),
+            ...Object.keys(workspace.importedServiceFiles),
+            ...Object.keys(workspace.packageMarkers),
+        ];
+
+        const absolutePaths = new Set<string>();
+        relativePaths.forEach((relativeFilepath) => {
+            const absolutePath = join(workspace.absolutePathToDefinition, RelativeFilePath.of(relativeFilepath));
+            absolutePaths.add(absolutePath);
+        });
+
         return {
             serviceFile: {
                 import: async ({ importedAs, importPath }, { relativeFilepath }) => {
                     const violations: RuleViolation[] = [];
-                    const importedFilePath = join(
+                    const importAbsoluteFilepath = join(
                         workspace.absolutePathToDefinition,
                         dirname(relativeFilepath),
                         RelativeFilePath.of(importPath)
                     );
-                    const fileExists = await doesPathExist(importedFilePath);
-                    if (!fileExists) {
+                    const serviceFilePresent = absolutePaths.has(importAbsoluteFilepath);
+                    if (!serviceFilePresent) {
                         violations.push({
                             severity: "error",
                             message: `Import ${chalk.bold(importedAs)} points to non-existent path ${chalk.bold(
