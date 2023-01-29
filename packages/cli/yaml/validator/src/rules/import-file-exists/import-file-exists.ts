70309aa0c312e4f0adc761df475ac51fc5227bcd
dsinghvi <deep@usebirch.com>
2023-01-29 09:06:09 -0800

    file exists validation should check against loaded workspace files

--- packages/cli/yaml/validator/src/rules/import-file-exists/import-file-exists.ts
+++ packages/cli/yaml/validator/src/rules/import-file-exists/import-file-exists.ts
@@ -1,21 +1,24 @@
-import { dirname, doesPathExist, join, RelativeFilePath } from "@fern-api/fs-utils";
+import { dirname, join, RelativeFilePath } from "@fern-api/fs-utils";
 import chalk from "chalk";
 import { Rule, RuleViolation } from "../../Rule";
 
 export const ImportFileExistsRule: Rule = {
     name: "import-file-exists",
     create: ({ workspace }) => {
+        const absoluteServiceFilePaths = Object.keys(workspace.serviceFiles).map((relativeFilepath) => {
+            return join(workspace.absolutePathToDefinition, RelativeFilePath.of(relativeFilepath));
+        });
         return {
             serviceFile: {
                 import: async ({ importedAs, importPath }, { relativeFilepath }) => {
                     const violations: RuleViolation[] = [];
-                    const importedFilePath = join(
+                    const importAbsoluteFilepath = join(
                         workspace.absolutePathToDefinition,
                         dirname(relativeFilepath),
                         RelativeFilePath.of(importPath)
                     );
-                    const fileExists = await doesPathExist(importedFilePath);
-                    if (!fileExists) {
+                    const serviceFilePresent = absoluteServiceFilePaths.includes(importAbsoluteFilepath);
+                    if (!serviceFilePresent) {
                         violations.push({
                             severity: "error",
                             message: `Import ${chalk.bold(importedAs)} points to non-existent path ${chalk.bold(
