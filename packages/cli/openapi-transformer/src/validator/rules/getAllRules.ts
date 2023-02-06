ec2b8ca68dcdea75cd9bdafb9dfd8b733e8c67bf
Zach Kirsch <zachkirsch@gmail.com>
2023-02-05 21:55:03 -0800

    Add query params -> request name rule

--- packages/cli/openapi-transformer/src/validator/rules/getAllRules.ts
+++ packages/cli/openapi-transformer/src/validator/rules/getAllRules.ts
@@ -2,11 +2,12 @@ import { NoInlineEnums } from "./no-inline-enums";
 import { NoInlineObjects } from "./no-inline-objects";
 import { NoInlineUnions } from "./no-inline-unions";
 import { OperationIdRequired } from "./operation-id-required";
+import { RequestName } from "./request-name";
 import { CustomRule, SpectralRule } from "./Rule";
 import { ValidEnumValue } from "./valid-enum-value";
 
 const ALL_RULES = {
-    spectralRules: [OperationIdRequired, NoInlineEnums, NoInlineObjects, NoInlineUnions, ValidEnumValue],
+    spectralRules: [OperationIdRequired, NoInlineEnums, NoInlineObjects, NoInlineUnions, ValidEnumValue, RequestName],
     customRules: [],
 };
 
---
+++ packages/cli/openapi-transformer/src/validator/rules/request-name/index.ts
@@ -0,0 +1,1 @@
+export { RequestName } from "./request-name";
