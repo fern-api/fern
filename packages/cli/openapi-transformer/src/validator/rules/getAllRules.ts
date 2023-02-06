673bb9734665fcedcc6eb4568751264cfb961bb6
Deep Singhvi <deep@usebirch.com>
2023-02-05 20:11:24 -0800

    [openapi validator] disallow inlined oneOf type (#1291)
    disallow inlined unions

--- packages/cli/openapi-transformer/src/validator/rules/getAllRules.ts
+++ packages/cli/openapi-transformer/src/validator/rules/getAllRules.ts
@@ -1,10 +1,11 @@
 import { NoInlineEnums } from "./no-inline-enums";
 import { NoInlineObjects } from "./no-inline-objects";
+import { NoInlineUnions } from "./no-inline-unions";
 import { OperationIdRequired } from "./operation-id-required";
 import { CustomRule, SpectralRule } from "./Rule";
 
 const ALL_RULES = {
-    spectralRules: [OperationIdRequired, NoInlineEnums, NoInlineObjects],
+    spectralRules: [OperationIdRequired, NoInlineEnums, NoInlineObjects, NoInlineUnions],
     customRules: [],
 };
 
