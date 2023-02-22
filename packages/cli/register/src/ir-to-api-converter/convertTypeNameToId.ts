0f256526da032671ac2689e17af70bbb4fc0d1b4
Zach Kirsch <zachkirsch@gmail.com>
2023-02-22 13:18:05 -0800

    Fix compile

--- packages/cli/register/src/ir-to-api-converter/convertTypeNameToId.ts
+++
@@ -1,8 +0,0 @@
-import * as ir from "@fern-fern/ir-model/types";
-import { FernRegistry } from "@fern-fern/registry";
-
-export function convertTypeNameToId(typeName: ir.DeclaredTypeName): FernRegistry.TypeId {
-    return FernRegistry.TypeId(
-        [...typeName.fernFilepath.allParts.map((part) => part.originalName), typeName.name.originalName].join(".")
-    );
-}
