acc7a755114b2ff5af910c0b6b44bcde7559a28b
dsinghvi <deep@usebirch.com>
2023-03-02 16:03:45 -0800

    check to to-be fern definition when converting from openapi

--- packages/cli/cli/src/commands/validate/validateWorkspaces.ts
+++ packages/cli/cli/src/commands/validate/validateWorkspaces.ts
@@ -1,5 +1,6 @@
 import { Project } from "@fern-api/project-loader";
 import { CliContext } from "../../cli-context/CliContext";
+import { convertOpenApiWorkspaceToFernWorkspace } from "../generate/generateWorkspaces";
 import { validateFernWorkspaceAndLogIssues } from "./validateFernWorkspaceAndLogIssues";
 import { validateOpenAPIWorkspaceAndLogIssues } from "./validateOpenAPIWorkspaceAndLogIssues";
 
@@ -15,6 +16,8 @@ export async function validateWorkspaces({
             await cliContext.runTaskForWorkspace(workspace, async (context) => {
                 if (workspace.type === "openapi") {
                     await validateOpenAPIWorkspaceAndLogIssues(workspace, context);
+                    const fernWorkspace = await convertOpenApiWorkspaceToFernWorkspace(workspace, context);
+                    await validateFernWorkspaceAndLogIssues(fernWorkspace, context);
                 } else {
                     await validateFernWorkspaceAndLogIssues(workspace, context);
                 }
