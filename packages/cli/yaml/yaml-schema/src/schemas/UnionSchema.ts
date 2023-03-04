0507ec1d525e401329310ebfd259cf4c9e1dfca0
Deep Singhvi <deep@usebirch.com>
2023-03-03 17:37:08 -0800

    add `base-properties` to union (#1353)
    * add base-properties to union
    
    * fix compile
    
    * feedback + fix

--- packages/cli/yaml/yaml-schema/src/schemas/UnionSchema.ts
+++ packages/cli/yaml/yaml-schema/src/schemas/UnionSchema.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 import { BaseTypeDeclarationSchema } from "./BaseTypeDeclarationSchema";
+import { ObjectPropertySchema } from "./ObjectPropertySchema";
 import { SingleUnionTypeSchema } from "./SingleUnionTypeSchema";
 import { UnionDiscriminantSchema } from "./UnionDiscriminantSchema";
 
 export const UnionSchema = BaseTypeDeclarationSchema.extend({
+    "base-properties": z.optional(z.record(ObjectPropertySchema)),
     union: z.record(SingleUnionTypeSchema),
     discriminant: z.optional(z.union([z.string(), UnionDiscriminantSchema])),
 });
