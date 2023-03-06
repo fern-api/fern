ecf2a7d0ce089e547f8b4661a23e985195916d89
Zach Kirsch <zachkirsch@gmail.com>
2023-03-05 19:21:38 -0800

    Add hasEndpointsInTree to IR

--- packages/cli/generation/ir-generator/src/PackageTreeGenerator.tsx
+++ packages/cli/generation/ir-generator/src/PackageTreeGenerator.tsx
@@ -1,13 +1,17 @@
-import { ErrorId, FernFilepath, ServiceId, TypeId } from "@fern-fern/ir-model/commons";
+import { ErrorId, FernFilepath, ServiceId, SubpackageId, TypeId } from "@fern-fern/ir-model/commons";
 import { ErrorDeclaration } from "@fern-fern/ir-model/errors";
 import { HttpService } from "@fern-fern/ir-model/http";
 import { IntermediateRepresentation, Package, Subpackage } from "@fern-fern/ir-model/ir";
 import { TypeDeclaration } from "@fern-fern/ir-model/types";
+import { mapValues } from "lodash-es";
 import { IdGenerator } from "./IdGenerator";
 
+type UnprocessedPackage = Omit<Package, "hasEndpointsInTree">;
+type UnprocessedSubpackage = Omit<Subpackage, "hasEndpointsInTree">;
+
 export class PackageTreeGenerator {
-    private subpackages: IntermediateRepresentation["subpackages"] = {};
-    private rootPackage: IntermediateRepresentation["rootPackage"] = {
+    private subpackages: Record<SubpackageId, UnprocessedSubpackage> = {};
+    private rootPackage: UnprocessedPackage = {
         docs: undefined,
         fernFilepath: {
             allParts: [],
@@ -45,13 +49,44 @@ export class PackageTreeGenerator {
     }
 
     public build(): Pick<IntermediateRepresentation, "subpackages" | "rootPackage"> {
+        const allSubpackagesWithEndpoints = new Set(this.getAllChildrenWithEndpoints(this.rootPackage));
         return {
-            subpackages: this.subpackages,
-            rootPackage: this.rootPackage,
+            subpackages: mapValues(this.subpackages, (subpackage, subpackageId) => ({
+                ...subpackage,
+                hasEndpointsInTree: allSubpackagesWithEndpoints.has(subpackageId),
+            })),
+            rootPackage: {
+                ...this.rootPackage,
+                hasEndpointsInTree: allSubpackagesWithEndpoints.size > 0 || this.rootPackage.service != null,
+            },
         };
     }
 
-    private getPackageForFernFilepath(fernFilepath: FernFilepath, index = 0, parent = this.rootPackage): Package {
+    private getAllSubpackagesWithEndpoints(root: SubpackageId): SubpackageId[] {
+        const subpackage = this.subpackages[root];
+        if (subpackage == null) {
+            throw new Error("Subpackage does not exist: " + root);
+        }
+
+        const subpackagesWithEndpoints = this.getAllChildrenWithEndpoints(subpackage);
+        if (subpackagesWithEndpoints.length > 0 || subpackage.service != null) {
+            subpackagesWithEndpoints.push(root);
+        }
+
+        return subpackagesWithEndpoints;
+    }
+
+    private getAllChildrenWithEndpoints(package_: UnprocessedPackage): SubpackageId[] {
+        return package_.subpackages.flatMap((child) => {
+            return [...this.getAllSubpackagesWithEndpoints(child)];
+        });
+    }
+
+    private getPackageForFernFilepath(
+        fernFilepath: FernFilepath,
+        index = 0,
+        parent = this.rootPackage
+    ): UnprocessedPackage {
         const nextPart = fernFilepath.allParts[index];
         if (nextPart == null) {
             return parent;
@@ -75,7 +110,7 @@ export class PackageTreeGenerator {
         );
         if (nextParent == null) {
             const newParentId = IdGenerator.generateSubpackageId(fernFilepathForNextParent);
-            const newParent: Subpackage = {
+            const newParent: UnprocessedSubpackage = {
                 docs: undefined,
                 fernFilepath: fernFilepathForNextParent,
                 name: nextPart,
