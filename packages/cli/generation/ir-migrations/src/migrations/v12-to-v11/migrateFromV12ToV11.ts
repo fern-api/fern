b5e600e15a7b4f641e84ec289fc8afe06ccd27a4
Deep Singhvi <deep@usebirch.com>
2023-03-08 00:00:13 -0800

    ts generator versions above `0.5.0-rc0-6` use IR V12 (#1373)
    * ts versions after 0.5.0-rc0-6-g80f89f98 dont need ir migration
    
    * ts versions after 0.5.0-rc0-6-g80f89f98 dont need ir migration

--- packages/cli/generation/ir-migrations/src/migrations/v12-to-v11/migrateFromV12ToV11.ts
+++ packages/cli/generation/ir-migrations/src/migrations/v12-to-v11/migrateFromV12ToV11.ts
@@ -12,8 +12,8 @@ export const V12_TO_V11_MIGRATION: IrMigration<
     earlierVersion: "v11",
     minGeneratorVersionsToExclude: {
         [GeneratorName.TYPESCRIPT]: AlwaysRunMigration,
-        [GeneratorName.TYPESCRIPT_SDK]: AlwaysRunMigration,
-        [GeneratorName.TYPESCRIPT_EXPRESS]: AlwaysRunMigration,
+        [GeneratorName.TYPESCRIPT_SDK]: "0.5.0-rc0-6-g80f89f98",
+        [GeneratorName.TYPESCRIPT_EXPRESS]: "0.5.0-rc0-6-g80f89f98",
         [GeneratorName.JAVA]: AlwaysRunMigration,
         [GeneratorName.JAVA_MODEL]: AlwaysRunMigration,
         [GeneratorName.JAVA_SDK]: AlwaysRunMigration,
