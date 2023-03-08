4dfc6c3f1e9d7cf57876dee6b21a75a174710a6a
Zach Kirsch <zachkirsch@gmail.com>
2023-03-08 01:52:44 -0800

    Add query-param stream condition

--- packages/cli/yaml/validator/src/rules/valid-stream-condition/valid-stream-condition.ts
+++ packages/cli/yaml/validator/src/rules/valid-stream-condition/valid-stream-condition.ts
@@ -1,6 +1,16 @@
-import { constructStreamCondition, ResolvedType } from "@fern-api/ir-generator";
+import { RelativeFilePath } from "@fern-api/fs-utils";
+import {
+    constructFernFileContext,
+    constructStreamCondition,
+    ResolvedType,
+    TypeResolverImpl,
+} from "@fern-api/ir-generator";
+import { FernWorkspace } from "@fern-api/workspace-loader";
+import { RawSchemas } from "@fern-api/yaml-schema";
+import { StreamCondition } from "@fern-fern/ir-model/http";
 import { PrimitiveType } from "@fern-fern/ir-model/types";
-import { Rule } from "../../Rule";
+import { Rule, RuleViolation } from "../../Rule";
+import { CASINGS_GENERATOR } from "../../utils/casingsGenerator";
 import { getAllPropertiesForRequest } from "../../utils/getAllPropertiesForRequest";
 
 export const ValidStreamConditionRule: Rule = {
@@ -8,9 +18,12 @@ export const ValidStreamConditionRule: Rule = {
     create: async ({ workspace }) => {
         return {
             serviceFile: {
-                streamCondition: ({ endpoint, streamCondition }, { relativeFilepath, contents }) => {
+                streamCondition: (
+                    { endpoint, streamCondition: rawStreamCondition },
+                    { relativeFilepath, contents }
+                ) => {
                     if (endpoint.response == null || endpoint["response-stream"] == null) {
-                        if (streamCondition == null) {
+                        if (rawStreamCondition == null) {
                             return [];
                         }
                         return [
@@ -22,7 +35,7 @@ export const ValidStreamConditionRule: Rule = {
                         ];
                     }
 
-                    if (streamCondition == null) {
+                    if (rawStreamCondition == null) {
                         return [
                             {
                                 severity: "error",
@@ -32,57 +45,40 @@ export const ValidStreamConditionRule: Rule = {
                         ];
                     }
 
-                    const parsedStreamCondition = constructStreamCondition(streamCondition);
-                    if (parsedStreamCondition == null) {
+                    const streamCondition = constructStreamCondition(rawStreamCondition);
+                    if (streamCondition == null) {
                         return [
                             {
                                 severity: "error",
                                 message:
-                                    "stream-condition is not valid. You should specify a selector for a boolean request body property, e.g. $request.yourProperty.",
+                                    "stream-condition is not valid. You should specify a selector for a boolean request property, e.g. $request.yourProperty or $query.yourQueryParameter",
                             },
                         ];
                     }
 
-                    const properties = getAllPropertiesForRequest({
-                        endpoint,
-                        filepath: relativeFilepath,
-                        serviceFile: contents,
-                        workspace,
+                    return StreamCondition._visit(streamCondition, {
+                        queryParameterKey: (queryParameterKey) => {
+                            return validateQueryParameterKeyStreamCondition(
+                                endpoint,
+                                relativeFilepath,
+                                contents,
+                                queryParameterKey,
+                                workspace
+                            );
+                        },
+                        requestPropertyKey: (requestPropertyKey) => {
+                            return validatePropertyKeyStreamCondition(
+                                endpoint,
+                                relativeFilepath,
+                                contents,
+                                workspace,
+                                requestPropertyKey
+                            );
+                        },
+                        _unknown: () => {
+                            throw new Error("Unknown stream condition: " + streamCondition.type);
+                        },
                     });
-
-                    if (properties == null) {
-                        return [
-                            {
-                                severity: "error",
-                                message:
-                                    "A request body is required when the response can be either streaming or non-streaming.",
-                            },
-                        ];
-                    }
-
-                    const streamConditionProperty = properties.find(
-                        (property) => property.wireKey === parsedStreamCondition.requestPropertyKey
-                    );
-
-                    if (streamConditionProperty == null) {
-                        return [
-                            {
-                                severity: "error",
-                                message: `Property "${parsedStreamCondition.requestPropertyKey}" does not exist on the request.`,
-                            },
-                        ];
-                    }
-
-                    if (!isBooleanOrOptionalBoolean(streamConditionProperty.resolvedPropertyType)) {
-                        return [
-                            {
-                                severity: "error",
-                                message: `Property "${parsedStreamCondition.requestPropertyKey}" is not a boolean.`,
-                            },
-                        ];
-                    }
-
-                    return [];
                 },
             },
         };
@@ -98,4 +94,109 @@ function isBooleanOrOptionalBoolean(resolvedType: ResolvedType): boolean {
         resolvedType.container._type === "optional" &&
         isBooleanOrOptionalBoolean(resolvedType.container.itemType)
     );
+}
+
+function validatePropertyKeyStreamCondition(
+    endpoint: RawSchemas.HttpEndpointSchema,
+    relativeFilepath: RelativeFilePath,
+    serviceFile: RawSchemas.ServiceFileSchema,
+    workspace: FernWorkspace,
+    requestPropertyKey: string
+): RuleViolation[] {
+    const properties = getAllPropertiesForRequest({
+        endpoint,
+        filepath: relativeFilepath,
+        serviceFile,
+        workspace,
+    });
+
+    if (properties == null) {
+        return [
+            {
+                severity: "error",
+                message: "A request body is required when the response can be either streaming or non-streaming.",
+            },
+        ];
+    }
+
+    const streamConditionProperty = properties.find((property) => property.wireKey === requestPropertyKey);
+
+    if (streamConditionProperty == null) {
+        return [
+            {
+                severity: "error",
+                message: `Property "${requestPropertyKey}" does not exist on the request.`,
+            },
+        ];
+    }
+
+    if (!isBooleanOrOptionalBoolean(streamConditionProperty.resolvedPropertyType)) {
+        return [
+            {
+                severity: "error",
+                message: `Property "${requestPropertyKey}" is not a boolean.`,
+            },
+        ];
+    }
+
+    return [];
+}
+
+function getQueryParameterType(endpoint: RawSchemas.HttpEndpointSchema, queryParameterKey: string): string | undefined {
+    if (endpoint.request == null || typeof endpoint.request === "string") {
+        return undefined;
+    }
+
+    const type = endpoint.request["query-parameters"]?.[queryParameterKey];
+    if (type == null || typeof type === "string") {
+        return type;
+    }
+
+    return type.type;
+}
+
+function validateQueryParameterKeyStreamCondition(
+    endpoint: RawSchemas.HttpEndpointSchema,
+    relativeFilepath: RelativeFilePath,
+    serviceFile: RawSchemas.ServiceFileSchema,
+    queryParameterKey: string,
+    workspace: FernWorkspace
+): RuleViolation[] {
+    const queryParamType = getQueryParameterType(endpoint, queryParameterKey);
+
+    if (queryParamType == null) {
+        return [
+            {
+                severity: "error",
+                message: `Query parameter "${queryParameterKey}" does not exist on this endpoint.`,
+            },
+        ];
+    }
+
+    const typeResolver = new TypeResolverImpl(workspace);
+    const resolvedType = typeResolver.resolveType({
+        type: queryParamType,
+        file: constructFernFileContext({
+            relativeFilepath,
+            serviceFile,
+            casingsGenerator: CASINGS_GENERATOR,
+        }),
+    });
+
+    // invalid type
+    // will be caught by another rule
+    if (resolvedType == null) {
+        return [];
+    }
+
+    if (!isBooleanOrOptionalBoolean(resolvedType)) {
+        return [
+            {
+                severity: "error",
+                message: `Query parameter "${queryParameterKey}" is not a boolean.`,
+            },
+        ];
+    }
+
+    return [];
 }
