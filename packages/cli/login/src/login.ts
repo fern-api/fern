98edf2f289cd5373ce088ef14c018727aa5a2e48
Zach Kirsch <zachkirsch@gmail.com>
2023-03-11 00:02:47 -0800

    Add --device-code option

--- packages/cli/login/src/login.ts
+++ packages/cli/login/src/login.ts
@@ -10,26 +10,15 @@ const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN ?? "fern-prod.us.auth0.com";
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID ?? "syaWnk6SjNoo5xBf1omfvziU3q7085lh";
 const VENUS_AUDIENCE = process.env.VENUS_AUDIENCE ?? "venus-prod";
 
-export async function login(context: TaskContext): Promise<FernUserToken> {
+export async function login(
+    context: TaskContext,
+    { useDeviceCodeFlow = false }: { useDeviceCodeFlow?: boolean } = {}
+): Promise<FernUserToken> {
     context.instrumentPostHogEvent({
         command: "Login initiated",
     });
 
-    let token: string;
-    try {
-        token = await doAuth0LoginFlow({
-            auth0Domain: AUTH0_DOMAIN,
-            auth0ClientId: AUTH0_CLIENT_ID,
-            audience: VENUS_AUDIENCE,
-        });
-    } catch {
-        token = await doAuth0DeviceAuthorizationFlow({
-            auth0Domain: AUTH0_DOMAIN,
-            auth0ClientId: AUTH0_CLIENT_ID,
-            audience: VENUS_AUDIENCE,
-            context,
-        });
-    }
+    const token = await getTokenFromAuth0(context, { useDeviceCodeFlow });
     await storeToken(token);
 
     context.logger.info(chalk.green("Logged in!"));
@@ -40,4 +29,33 @@ export async function login(context: TaskContext): Promise<FernUserToken> {
         type: "user",
         value: token,
     };
+}
+
+async function getTokenFromAuth0(
+    context: TaskContext,
+    { useDeviceCodeFlow }: { useDeviceCodeFlow: boolean }
+): Promise<string> {
+    if (useDeviceCodeFlow) {
+        return await doAuth0DeviceAuthorizationFlow({
+            auth0Domain: AUTH0_DOMAIN,
+            auth0ClientId: AUTH0_CLIENT_ID,
+            audience: VENUS_AUDIENCE,
+            context,
+        });
+    }
+
+    try {
+        return await doAuth0LoginFlow({
+            auth0Domain: AUTH0_DOMAIN,
+            auth0ClientId: AUTH0_CLIENT_ID,
+            audience: VENUS_AUDIENCE,
+        });
+    } catch {
+        return await doAuth0DeviceAuthorizationFlow({
+            auth0Domain: AUTH0_DOMAIN,
+            auth0ClientId: AUTH0_CLIENT_ID,
+            audience: VENUS_AUDIENCE,
+            context,
+        });
+    }
 }
