a52be2f47b249a799cb334329beb4c32f8579d67
Zach Kirsch <zachkirsch@gmail.com>
2023-03-10 23:57:24 -0800

    Fall back to device code auth flow if normal flow throws

--- packages/cli/login/src/login.ts
+++ packages/cli/login/src/login.ts
@@ -2,6 +2,7 @@ import { FernUserToken, storeToken } from "@fern-api/auth";
 import { getPosthogManager } from "@fern-api/posthog-manager";
 import { TaskContext } from "@fern-api/task-context";
 import chalk from "chalk";
+import { doAuth0DeviceAuthorizationFlow } from "./auth0-login/doAuth0DeviceAuthorizationFlow";
 import { doAuth0LoginFlow } from "./auth0-login/doAuth0LoginFlow";
 
 // these are client-side safe values
@@ -13,11 +14,22 @@ export async function login(context: TaskContext): Promise<FernUserToken> {
     context.instrumentPostHogEvent({
         command: "Login initiated",
     });
-    const token = await doAuth0LoginFlow({
-        auth0Domain: AUTH0_DOMAIN,
-        auth0ClientId: AUTH0_CLIENT_ID,
-        audience: VENUS_AUDIENCE,
-    });
+
+    let token: string;
+    try {
+        token = await doAuth0LoginFlow({
+            auth0Domain: AUTH0_DOMAIN,
+            auth0ClientId: AUTH0_CLIENT_ID,
+            audience: VENUS_AUDIENCE,
+        });
+    } catch {
+        token = await doAuth0DeviceAuthorizationFlow({
+            auth0Domain: AUTH0_DOMAIN,
+            auth0ClientId: AUTH0_CLIENT_ID,
+            audience: VENUS_AUDIENCE,
+            context,
+        });
+    }
     await storeToken(token);
 
     context.logger.info(chalk.green("Logged in!"));
