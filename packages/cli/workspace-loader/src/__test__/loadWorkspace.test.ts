1dd772c77e38233fb6843933d7e61822b55b8817
Zach Kirsch <zachkirsch@gmail.com>
2023-03-31 15:48:50 -0700

    Parse datetimes as string, not Date

--- packages/cli/workspace-loader/src/__test__/loadWorkspace.test.ts
+++ packages/cli/workspace-loader/src/__test__/loadWorkspace.test.ts
@@ -1,5 +1,7 @@
 import { AbsoluteFilePath, join, RelativeFilePath } from "@fern-api/fs-utils";
 import { createMockTaskContext } from "@fern-api/task-context";
+import { RawSchemas } from "@fern-api/yaml-schema";
+import assert from "assert";
 import { loadWorkspace } from "../loadWorkspace";
 
 describe("loadWorkspace", () => {
@@ -10,5 +12,12 @@ describe("loadWorkspace", () => {
             cliVersion: "0.0.0",
         });
         expect(workspace.didSucceed).toBe(true);
+        assert(workspace.didSucceed);
+        assert(workspace.workspace.type === "fern");
+
+        const simpleYaml = workspace.workspace.definition.namedDefinitionFiles[RelativeFilePath.of("simple.yml")];
+        const exampleDateTime = (simpleYaml?.contents.types?.MyDateTime as RawSchemas.BaseTypeDeclarationSchema)
+            .examples?.[0]?.value;
+        expect(typeof exampleDateTime).toBe("string");
     });
 });
