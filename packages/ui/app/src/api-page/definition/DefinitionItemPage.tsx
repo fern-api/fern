5b957d3d77a883092cca898bda87b1413e7d7bf9
Zach Kirsch <zachkirsch@gmail.com>
2023-04-03 08:59:38 -0700

    Remove padding

--- packages/ui/app/src/api-page/definition/DefinitionItemPage.tsx
+++ packages/ui/app/src/api-page/definition/DefinitionItemPage.tsx
@@ -10,7 +10,7 @@ export declare namespace DefinitionItemPage {
 
 export const DefinitionItemPage: React.FC<DefinitionItemPage.Props> = ({ title, subtitle, description, children }) => {
     return (
-        <div className="flex-1 flex flex-col px-[10%] min-h-0 overflow-y-auto">
+        <div className="flex-1 flex flex-col px-[10%] pb-10 min-h-0 overflow-y-auto">
             <div className="pt-10 text-3xl font-bold">{title}</div>
             {subtitle != null && <div className="mt-2">{subtitle}</div>}
             {description != null && (
