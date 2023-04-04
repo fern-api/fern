22ef2818dc23cd5aba2f7d3b316a182ca5ec6524
Deep Singhvi <deep@usebirch.com>
2023-04-03 17:48:50 -0700

    fix: remove org name validation error (#1459)
    remove org name validation error

--- packages/cli/auth/src/orgs/getOrganizationNameValidationError.ts
+++ packages/cli/auth/src/orgs/getOrganizationNameValidationError.ts
@@ -1,11 +1,6 @@
-const ORG_NAME_REGEX = /^[a-z_-]+$/;
-
 export function getOrganizationNameValidationError(organizationName: string): string | undefined {
     if (organizationName.length === 0) {
         return "Organization name cannot be empty";
-    }
-    if (!ORG_NAME_REGEX.test(organizationName)) {
-        return "Organization name can only container lowercase letters, hypens, and underscores.";
     }
     return undefined;
 }
