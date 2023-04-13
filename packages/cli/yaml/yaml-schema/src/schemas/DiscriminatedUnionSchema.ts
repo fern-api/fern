0873e271b883b756d8896365eea43ca4d54df1f7
dsinghvi <deep@usebirch.com>
2023-04-13 19:18:04 -0400

    add union extensions

--- packages/cli/yaml/yaml-schema/src/schemas/DiscriminatedUnionSchema.ts
+++ packages/cli/yaml/yaml-schema/src/schemas/DiscriminatedUnionSchema.ts
@@ -1,11 +1,13 @@
 import { z } from "zod";
 import { BaseTypeDeclarationSchema } from "./BaseTypeDeclarationSchema";
+import { ObjectExtendsSchema } from "./ObjectExtendsSchema";
 import { ObjectPropertySchema } from "./ObjectPropertySchema";
 import { SingleUnionTypeSchema } from "./SingleUnionTypeSchema";
 import { UnionDiscriminantSchema } from "./UnionDiscriminantSchema";
 
 export const DiscriminatedUnionSchema = BaseTypeDeclarationSchema.extend({
     "base-properties": z.optional(z.record(ObjectPropertySchema)),
+    extends: z.optional(ObjectExtendsSchema),
     union: z.record(SingleUnionTypeSchema),
     discriminant: z.optional(z.union([z.string(), UnionDiscriminantSchema])),
 });
