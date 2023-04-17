2fab86a8a2aa97ee1c344fbb86cdac85a5199a69
Deep Singhvi <deep@usebirch.com>
2023-04-16 23:54:30 -0700

    fix: add` __str__` method to core/api_error.py (#231)
    add __str__ method to core/api_error.py

--- tests/sdk/snapshots/snap_test_sdk/test_github_sdk src_fern_core_api_error.py
+++ tests/sdk/snapshots/snap_test_sdk/test_github_sdk src_fern_core_api_error.py
@@ -10,3 +10,6 @@ class ApiError(Exception):
     def __init__(self, *, status_code: typing.Optional[int] = None, body: typing.Any = None):
         self.status_code = status_code
         self.body = body
+
+    def __str__(self) -> str:
+        return f"status_code: {self.status_code}, body: {self.body}"
