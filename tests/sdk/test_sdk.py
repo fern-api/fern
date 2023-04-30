c8d0271e5d648c08e7bc69c5e1e8da1b24cd778d
dsinghvi <deep@usebirch.com>
2023-04-30 17:46:50 -0400

    upgrade generator exec client to 0.0.195

--- tests/sdk/test_sdk.py
+++ tests/sdk/test_sdk.py
@@ -96,8 +96,8 @@ def test_github_sdk(snapshot: SnapshotTest, tmpdir: Path) -> None:
                     config.PypiGithubPublishInfo(
                         registry_url="https://pypi.org/",
                         package_name="my-package-name",
-                        username_environment_variable=EnvironmentVariable.from_str("PYPI_USERNAME"),
-                        password_environment_variable=EnvironmentVariable.from_str("PYPI_PASSWORD"),
+                        username_environment_variable="PYPI_USERNAME",
+                        password_environment_variable="PYPI_PASSWORD",
                     )
                 ),
             ),
