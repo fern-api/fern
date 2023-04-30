b687f04dd7d91ed8500682bea8aa694040eba2a2
Deep Singhvi <deep@usebirch.com>
2023-04-30 12:38:15 -0400

    fix: prefix python model published package with api name (#239)
    prefix python model published package with api name

--- src/fern_python/generators/pydantic_model/pydantic_filepath_creator.py
+++ src/fern_python/generators/pydantic_model/pydantic_filepath_creator.py
@@ -1,5 +1,14 @@
+from typing import Tuple
+
+from fern_python.codegen import ExportStrategy, Filepath
 from fern_python.declaration_referencer import FernFilepathCreator
 
 
 class PydanticFilepathCreator(FernFilepathCreator):
-    pass
+    def _get_filepath_prefix_for_published_package(self) -> Tuple[Filepath.DirectoryFilepathPart, ...]:
+        return (
+            Filepath.DirectoryFilepathPart(
+                module_name=self._ir.api_name.snake_case.unsafe_name,
+                export_strategy=ExportStrategy(export_all=True),
+            ),
+        )
