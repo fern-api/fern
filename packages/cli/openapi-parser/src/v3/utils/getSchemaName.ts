2015698011bd5ef0e7cce6a4b44d028675298cfb
dsinghvi <deep@usebirch.com>
2023-04-30 20:27:10 -0400

    snapshot generated names

--- packages/cli/openapi-parser/src/v3/utils/getSchemaName.ts
+++ packages/cli/openapi-parser/src/v3/utils/getSchemaName.ts
@@ -1,9 +1,9 @@
 import { camelCase, upperFirst } from "lodash-es";
 
 export function getGeneratedTypeName(breadcrumbs: string[]): string {
-    if (breadcrumbs.length === 0) {
-        throw new Error("Cannot generate name from breadcrumbs");
-    }
+    // if (breadcrumbs.length === 0) {
+    //     throw new Error("Cannot generate name from breadcrumbs");
+    // }
     const underscoreDelimeted = breadcrumbs.join("_");
     return upperFirst(camelCase(underscoreDelimeted));
 }
