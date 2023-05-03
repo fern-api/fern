16e51b4d3fba3dbd736f8219ee400a8a99af11de
dsinghvi <deep@usebirch.com>
2023-05-03 19:14:31 -0400

    add extensions to objects

--- packages/cli/yaml/yaml-schema/src/schemas/ObjectSchema.ts
+++ packages/cli/yaml/yaml-schema/src/schemas/ObjectSchema.ts
@@ -6,10 +6,12 @@ import { ObjectPropertySchema } from "./ObjectPropertySchema";
 // for Object schemas, you need either extends/properties (or both).
 export const ObjectSchema = z.union([
     BaseTypeDeclarationSchema.extend({
+        extensions: z.optional(z.record(z.string(), z.any())),
         extends: ObjectExtendsSchema,
         properties: z.optional(z.record(ObjectPropertySchema)),
     }),
     BaseTypeDeclarationSchema.extend({
+        extensions: z.optional(z.record(z.string(), z.any())),
         extends: z.optional(ObjectExtendsSchema),
         properties: z.record(ObjectPropertySchema),
     }),
