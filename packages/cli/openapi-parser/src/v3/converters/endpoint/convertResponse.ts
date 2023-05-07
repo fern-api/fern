7c325381beaa2d5e4e42ab4d2466ff6db1c86328
dsinghvi <deep@usebirch.com>
2023-05-07 18:24:33 -0400

    handle parsing responses from application/json; charset=utf-8

--- packages/cli/openapi-parser/src/v3/converters/endpoint/convertResponse.ts
+++ packages/cli/openapi-parser/src/v3/converters/endpoint/convertResponse.ts
@@ -5,6 +5,7 @@ import { isReferenceObject } from "../../utils/isReferenceObject";
 import { convertSchema } from "../convertSchemas";
 
 const APPLICATION_JSON_CONTENT = "application/json";
+const APPLICATION_JSON_UTF_8_CONTENT = "application/json; charset=utf-8";
 const TEXT_PLAIN_CONTENT = "text/plain";
 
 // The converter will attempt to get response in priority order
@@ -30,7 +31,9 @@ export function convertResponse({
             throw new Error(`Converting referenced response is unsupported: ${JSON.stringify(response)}`);
         }
         const responseSchema =
-            response.content?.[APPLICATION_JSON_CONTENT]?.schema ?? response.content?.[TEXT_PLAIN_CONTENT]?.schema;
+            response.content?.[APPLICATION_JSON_CONTENT]?.schema ??
+            response.content?.[APPLICATION_JSON_UTF_8_CONTENT]?.schema ??
+            response.content?.[TEXT_PLAIN_CONTENT]?.schema;
 
         if (responseSchema == null) {
             continue;
