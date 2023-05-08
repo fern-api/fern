80d5b91ac3024a555955112e8bcc4f0db691a02b
dsinghvi <deep@usebirch.com>
2023-05-08 10:38:32 -0400

    read from x-fern-server-name

--- packages/cli/openapi-parser/src/v3/converters/convertServer.ts
+++ packages/cli/openapi-parser/src/v3/converters/convertServer.ts
@@ -3,10 +3,12 @@ import { OpenAPIV3 } from "openapi-types";
 
 export function convertServer(server: OpenAPIV3.ServerObject): Server {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const serverName = (server as any)["x-name"] as string | undefined;
+    const name = (server as any)["x-name"] as string | undefined;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const fernServerName = (server as any)["x-fern-server-name"] as string | undefined;
     return {
         url: server.url,
         description: server.description,
-        name: serverName,
+        name: name ?? fernServerName,
     };
 }
