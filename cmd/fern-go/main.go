c5e7d1d8309c635a030d0088fe1df548be284dfb
Alex McKinney <alexmckinney01@gmail.com>
2023-05-15 12:59:01 -0700

    Add basic command line flags to fern-go binary

--- cmd/fern-go/main.go
+++ cmd/fern-go/main.go
@@ -7,17 +7,34 @@ import (
 	"github.com/fern-api/fern-go"
 )
 
+const usage = `Generate Go models from your Fern API definition.
+
+Usage:
+  fern-go <config_file_path>
+
+Flags:
+  -h, --help     Print this help and exit.
+  -v, --version  Print the version and exit.`
+
 func main() {
 	if len(os.Args) == 2 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
 		fmt.Fprintln(os.Stdout, fern.Version)
 		os.Exit(0)
 	}
-	if err := run(); err != nil {
+	if len(os.Args) == 2 && (os.Args[1] == "--help" || os.Args[1] == "-h") {
+		fmt.Fprintln(os.Stdout, usage)
+		os.Exit(0)
+	}
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, usage)
+		os.Exit(1)
+	}
+	if err := run(os.Args[1]); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 }
 
-func run() error {
+func run(configFilename string) error {
 	return nil
 }
