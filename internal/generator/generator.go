ef3704773ae0b4c2d3be2183d0c1ecbdf882f9ff
Alex McKinney <alexmckinney01@gmail.com>
2023-05-16 17:20:11 -0700

    Add generated file writer abstraction

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -1,6 +1,13 @@
 package generator
 
-import "github.com/fern-api/fern-go/internal/types"
+import (
+	"bytes"
+	"fmt"
+	"go/format"
+	"strings"
+
+	"github.com/fern-api/fern-go/internal/types"
+)
 
 // Generator represents the Go code generator.
 type Generator struct {
@@ -30,11 +37,81 @@ func (g *Generator) Generate() ([]*File, error) {
 }
 
 func (g *Generator) generate(ir *types.IntermediateRepresentation) ([]*File, error) {
-	// TODO: This is just a placeholder for now.
-	return []*File{
-		{
-			Path:    "example.go",
-			Content: []byte("package example"),
-		},
+	writer := newFileWriter(fmt.Sprintf("%s.go", ir.APIName.SnakeCase))
+	if err := writer.WritePackage(ir.APIName); err != nil {
+		return nil, err
+	}
+	for _, irType := range ir.Types {
+		// TODO: How do we want to delineate types across files? Should
+		// a single file contain all of the types? It should probably
+		// correspond to the FernFilepath associated with every type.
+		if err := writer.WriteType(irType); err != nil {
+			return nil, err
+		}
+	}
+	file, err := writer.File()
+	if err != nil {
+		return nil, err
+	}
+	return []*File{file}, nil
+}
+
+func newFileWriter(filename string) *fileWriter {
+	return &fileWriter{
+		filename: filename,
+		buffer:   new(bytes.Buffer),
+		imports:  make(map[string]struct{}),
+	}
+}
+
+// fileWriter wries and formats Go files.
+type fileWriter struct {
+	filename string
+	buffer   *bytes.Buffer
+
+	// TODO: Placeholder for now - we'll want to use something a little more sophisticated.
+	imports map[string]struct{}
+}
+
+func (f *fileWriter) WritePackage(apiName *types.Name) error {
+	f.P("package ", strings.ToLower(apiName.CamelCase.SafeName))
+	return nil
+}
+
+func (f *fileWriter) WriteType(typeDeclaration *types.TypeDeclaration) error {
+	f.P("type ", typeDeclaration.Name.PascalCase.SafeName, " struct {")
+	switch shape := typeDeclaration.Shape.(type) {
+	case *types.ObjectTypeDeclaration:
+		for _, property := range shape.Properties {
+			var typeIdentifier string
+			switch valueType := property.ValueType.(type) {
+			case *types.TypeReferencePrimitive:
+				// TODO: This is a hack as-is; it only works for a couple of the primitive types.
+				typeIdentifier = strings.ToLower(valueType.Primitive.String())
+			}
+			f.P(property.Name.Name.PascalCase.SafeName, " ", typeIdentifier)
+		}
+	}
+	f.P("}")
+	return nil
+}
+
+// P writes the given element into a single line, concluding with a newline.
+func (f *fileWriter) P(elements ...any) {
+	for _, element := range elements {
+		fmt.Fprint(f.buffer, element)
+	}
+	fmt.Fprintln(f.buffer)
+}
+
+// Finish formats and writes the content stored in the writer's buffer into a *File.
+func (f *fileWriter) File() (*File, error) {
+	formatted, err := format.Source(f.buffer.Bytes())
+	if err != nil {
+		return nil, err
+	}
+	return &File{
+		Path:    f.filename,
+		Content: formatted,
 	}, nil
 }
