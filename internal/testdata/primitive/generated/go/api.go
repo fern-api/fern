d1895ed55a530872e4afbc447c891004e8ae2a71
Alex McKinney <alexmckinney01@gmail.com>
2023-05-17 10:34:22 -0700

    Exercise all of the primitive types

--- internal/testdata/primitive/generated/go/api.go
+++ internal/testdata/primitive/generated/go/api.go
@@ -1,6 +1,18 @@
 package api
 
-type Movie struct {
-	Id    string `json:"id"`
-	Title string `json:"title"`
+import (
+	uuid "github.com/gofrs/uuid"
+	time "time"
+)
+
+type Type struct {
+	One   int       `json:"one"`
+	Two   float64   `json:"two"`
+	Three string    `json:"three"`
+	Four  bool      `json:"four"`
+	Five  int64     `json:"five"`
+	Six   time.Time `json:"six"`
+	Seven time.Time `json:"seven"`
+	Eight uuid.UUID `json:"eight"`
+	Nine  []byte    `json:"nine"`
 }
