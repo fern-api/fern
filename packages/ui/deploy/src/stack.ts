a713cb2c555b3b5c4d52a17a804af9983d842274
dsinghvi <deep@usebirch.com>
2023-05-22 19:57:45 -0400

    deploy to imported bucket

--- packages/ui/deploy/src/stack.ts
+++ packages/ui/deploy/src/stack.ts
@@ -1,6 +1,7 @@
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3Deploy from "@aws-cdk/aws-s3-deployment";
 import * as cdk from "@aws-cdk/core";
+import { FernFernCloud } from "@fern-fern/fern-cloud-sdk";
 
 export class CdkStack extends cdk.Stack {
     constructor({
@@ -8,24 +9,20 @@ export class CdkStack extends cdk.Stack {
         id,
         stackProps,
         distDirectory,
-        bucketName,
+        environmentConfig,
     }: {
         scope: cdk.App;
         id: string;
         stackProps: cdk.StackProps;
         distDirectory: string;
-        bucketName: string;
+        environmentConfig: FernFernCloud.EnvironmentInfo;
     }) {
         super(scope, id, stackProps);
 
-        const bucket = new s3.Bucket(this, "bucket", {
-            websiteIndexDocument: "index.html",
-            bucketName,
-        });
-
+        const docsFeBucket = s3.Bucket.fromBucketName(this, "docs-fe", environmentConfig.docsS3BucketName);
         new s3Deploy.BucketDeployment(this, "deploy", {
             sources: [s3Deploy.Source.asset(distDirectory)],
-            destinationBucket: bucket,
+            destinationBucket: docsFeBucket,
         });
     }
 }
