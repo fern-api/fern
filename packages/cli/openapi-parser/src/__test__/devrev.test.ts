b1b9dc9ee541d0f83a01f4cdfcb2dbcd082e28cf
dsinghvi <deep@usebirch.com>
2023-05-23 17:08:56 -0400

    openapi snapshots are per file

---
+++ packages/cli/openapi-parser/src/__test__/devrev.test.ts
@@ -0,0 +1,5 @@
+import { testParseOpenAPI } from "./testParseOpenApi";
+
+describe("open api parser", () => {
+    testParseOpenAPI("devrev", "openapi.yml");
+});
---
+++ packages/cli/openapi-parser/src/__test__/testParseOpenApi.ts
@@ -0,0 +1,29 @@
+import { AbsoluteFilePath, join, RelativeFilePath } from "@fern-api/fs-utils";
+import { CONSOLE_LOGGER } from "@fern-api/logger";
+import { createMockTaskContext } from "@fern-api/task-context";
+import path from "path";
+import { parse } from "../parse";
+
+const FIXTURES_PATH = join(AbsoluteFilePath.of(__dirname), RelativeFilePath.of("fixtures"));
+
+// eslint-disable-next-line jest/no-export
+export function testParseOpenAPI(fixtureName: string, filename: string) {
+    // eslint-disable-next-line jest/valid-title
+    describe(fixtureName, () => {
+        it("parse open api", async () => {
+            const openApiPath = path.join(FIXTURES_PATH, fixtureName, filename);
+            const openApiIr = await parse({
+                root: {
+                    file: {
+                        absoluteFilepath: AbsoluteFilePath.of(openApiPath),
+                        contents: "",
+                        relativeFilepath: RelativeFilePath.of(filename),
+                    },
+                    subDirectories: [],
+                },
+                taskContext: createMockTaskContext({ logger: CONSOLE_LOGGER }),
+            });
+            expect(openApiIr).toMatchSnapshot();
+        });
+    });
+}
