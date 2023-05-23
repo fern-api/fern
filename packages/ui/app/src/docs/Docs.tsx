988df28440cdff8b4e60ef4ff7755c716167089e
Zach Kirsch <zachkirsch@users.noreply.github.com>
2023-05-23 11:54:44 -0400

    [docs] Read domain from URL (#1625)

--- packages/ui/app/src/docs/Docs.tsx
+++ packages/ui/app/src/docs/Docs.tsx
@@ -1,7 +1,12 @@
+import { NonIdealState } from "@blueprintjs/core";
 import { MaybeLoadedDocs } from "./MaybeLoadedDocs";
 
-export const Docs: React.FC = () => {
-    const domain = import.meta.env.VITE_DOCS_DOMAIN ?? window.location.hostname;
+const docsDomainRegex = /^(.*)\.buildwithfern.com$/;
+const domain = import.meta.env.VITE_DOCS_DOMAIN ?? window.location.hostname.match(docsDomainRegex)?.[1];
 
+export const Docs: React.FC = () => {
+    if (domain == null) {
+        return <NonIdealState title="Domain not found" />;
+    }
     return <MaybeLoadedDocs domain={domain} />;
 };
