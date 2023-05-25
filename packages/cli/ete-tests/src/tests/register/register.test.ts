3032e14570fa906dc16ff6fdd0e6de15ae01fb99
dsinghvi <deep@usebirch.com>
2023-05-25 08:20:02 -0400

    disable register ete test

--- packages/cli/ete-tests/src/tests/register/register.test.ts
+++ packages/cli/ete-tests/src/tests/register/register.test.ts
@@ -1,8 +1,8 @@
 import { runFernCli } from "../../utils/runFernCli";
 import { init } from "../init/init";
 
-// eslint-disable-next-line jest/expect-expect
-it("fern register", async () => {
+// eslint-disable-next-line jest/expect-expect, jest/no-disabled-tests
+it.skip("fern register", async () => {
     const pathOfDirectory = await init();
 
     await runFernCli(["register-v2", "--log-level", "debug"], {
