2a3b03fdca126bf28be518991f2ce417ae263144
dsinghvi <deep@usebirch.com>
2023-06-05 10:08:04 -0400

    update generator versions to the latest

--- packages/cli/config-management/manage-generator/src/generatorInvocations.ts
+++ packages/cli/config-management/manage-generator/src/generatorInvocations.ts
@@ -12,40 +12,40 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
         },
     },
     [GeneratorName.JAVA_MODEL]: {
-        version: "0.0.132",
+        version: "0.3.6",
     },
     [GeneratorName.JAVA_SDK]: {
-        version: "0.0.132",
+        version: "0.3.6",
     },
     [GeneratorName.JAVA_SPRING]: {
-        version: "0.0.133",
+        version: "0.3.6",
     },
     [GeneratorName.TYPESCRIPT]: {
         version: "0.0.247",
     },
     [GeneratorName.TYPESCRIPT_SDK]: {
-        version: "0.2.2",
+        version: "0.5.25",
     },
     [GeneratorName.TYPESCRIPT_NODE_SDK]: {
-        version: "0.6.0",
+        version: "0.6.4",
     },
     [GeneratorName.TYPESCRIPT_BROWSER_SDK]: {
-        version: "0.6.0",
+        version: "0.6.4",
     },
     [GeneratorName.PYTHON_PYDANTIC]: {
-        version: "0.1.0",
+        version: "0.3.7",
     },
     [GeneratorName.PYTHON_FASTAPI]: {
-        version: "0.1.0",
+        version: "0.3.7",
     },
     [GeneratorName.PYTHON_SDK]: {
-        version: "0.1.0",
+        version: "0.3.7",
     },
     [GeneratorName.GO_MODEL]: {
         version: "0.0.1",
     },
     [GeneratorName.OPENAPI]: {
-        version: "0.0.11",
+        version: "0.0.27",
         config: {
             format: "yaml",
         },
