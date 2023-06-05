a05e073f20485206fb4395a3d0d2b813f0b3ba49
Alex McKinney <alexmckinney01@gmail.com>
2023-06-05 13:01:38 -0700

    Remove stale, unnecessary debugging

--- internal/goexec/goexec.go
+++ internal/goexec/goexec.go
@@ -1,23 +1,12 @@
 package goexec
 
 import (
-	"bytes"
-	"fmt"
 	"os/exec"
 )
 
 // RunTidy runs the 'go mod tidy' command from the given path.
 func RunTidy(path string) error {
 	cmd := exec.Command("go", "mod", "tidy")
-	stdout := bytes.NewBuffer(nil)
-	stderr := bytes.NewBuffer(nil)
 	cmd.Dir = path
-	cmd.Stdout = stdout
-	cmd.Stderr = stderr
-	fmt.Println("About to run tidy")
-	defer func() {
-		fmt.Println("stdout", stdout.String())
-		fmt.Println("stderr", stderr.String())
-	}()
 	return cmd.Run()
 }
