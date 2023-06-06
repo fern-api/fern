f880ed943e0cde9629cd082ac389c6d68dd4f968
Zach Kirsch <zachkirsch@users.noreply.github.com>
2023-06-06 00:47:39 -0400

    Remove copy curl button (#1713)

--- packages/ui/app/src/api-page/examples/useCopyToClipboard.ts
+++ packages/ui/app/src/api-page/examples/useCopyToClipboard.ts
@@ -1,19 +1,24 @@
 import { useTimeout } from "@fern-api/react-commons";
-import { useCallback, useState } from "react";
+import { useMemo, useState } from "react";
 
 export declare namespace useCopyToClipboard {
     export interface Return {
-        copyToClipboard: () => void;
+        copyToClipboard: (() => void) | undefined;
         wasJustCopied: boolean;
     }
 }
 
-export function useCopyToClipboard(content: string): useCopyToClipboard.Return {
+export function useCopyToClipboard(content: string | undefined): useCopyToClipboard.Return {
     const [wasJustCopied, setWasJustCopied] = useState(false);
 
-    const copyToClipboard = useCallback(async () => {
-        setWasJustCopied(true);
-        await navigator.clipboard.writeText(content);
+    const copyToClipboard = useMemo(() => {
+        if (content == null) {
+            return undefined;
+        }
+        return async () => {
+            setWasJustCopied(true);
+            await navigator.clipboard.writeText(content);
+        };
     }, [content]);
 
     useTimeout(
