1a979ad4f2077576ed6f4df5573145af2dd51247
Zach Kirsch <zachkirsch@gmail.com>
2023-06-05 23:14:18 -0400

    Self cr

--- packages/ui/app/src/api-page/examples/curl-example/CurlExampleLine.tsx
+++ packages/ui/app/src/api-page/examples/curl-example/CurlExampleLine.tsx
@@ -5,15 +5,15 @@ import { CurlExamplePart } from "./CurlExamplePart";
 export declare namespace CurlExampleLine {
     export interface Props {
         part: CurlExamplePart;
-        indent: number;
+        indentInSpaces: number;
         isLastPart: boolean;
     }
 }
 
-export const CurlExampleLine: React.FC<CurlExampleLine.Props> = ({ part, indent, isLastPart }) => {
+export const CurlExampleLine: React.FC<CurlExampleLine.Props> = ({ part, indentInSpaces, isLastPart }) => {
     const { excludeTrailingBackslash = false, excludeIndent = false } = part;
 
-    const indentToRender = excludeIndent ? 0 : indent;
+    const indentToRender = excludeIndent ? 0 : indentInSpaces;
     const shouldRenderTrailingSlash = !excludeTrailingBackslash && !isLastPart;
 
     if (indentToRender === 0 && !shouldRenderTrailingSlash && typeof part.value !== "string") {
--- packages/ui/app/src/utils/repeat.ts
+++
@@ -1,7 +0,0 @@
-export function repeat<T>(generate: (index: number) => T, count: number): T[] {
-    const elements = [];
-    for (let i = 0; i < count; i++) {
-        elements.push(generate(i));
-    }
-    return elements;
-}
