72ed2f5554867f4f31d5a347b12b8d1666719435
Zach Kirsch <zachkirsch@users.noreply.github.com>
2023-06-07 18:05:24 -0400

    Follow extensions in getAllObjectProperties (#1726)

--- packages/ui/app/src/api-page/utils/getAllObjectProperties.ts
+++ packages/ui/app/src/api-page/utils/getAllObjectProperties.ts
@@ -7,11 +7,22 @@ export function getAllObjectProperties(
     return [
         ...object.properties,
         ...object.extends.flatMap((typeId) => {
-            const type = resolveTypeById(typeId);
+            const type = resolveTypeByIdRecursive(typeId, resolveTypeById);
             if (type.shape.type !== "object") {
                 throw new Error("Object extends non-object " + typeId);
             }
             return getAllObjectProperties(type.shape, resolveTypeById);
         }),
     ];
+}
+
+function resolveTypeByIdRecursive(
+    typeId: FernRegistryApiRead.TypeId,
+    resolveTypeById: (typeId: FernRegistryApiRead.TypeId) => FernRegistryApiRead.TypeDefinition
+): FernRegistryApiRead.TypeDefinition {
+    const type = resolveTypeById(typeId);
+    if (type.shape.type === "alias" && type.shape.value.type === "id") {
+        return resolveTypeByIdRecursive(type.shape.value.value, resolveTypeById);
+    }
+    return type;
 }
