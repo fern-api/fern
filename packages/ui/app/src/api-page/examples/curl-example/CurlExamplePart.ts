244fa2ecb2bf992091a7bf0c503bc411571a851c
Zach Kirsch <zachkirsch@gmail.com>
2023-06-07 17:29:22 -0400

    Fix padding when curl is one line

--- packages/ui/app/src/api-page/examples/curl-example/CurlExamplePart.ts
+++ packages/ui/app/src/api-page/examples/curl-example/CurlExamplePart.ts
@@ -1,5 +1,15 @@
-export interface CurlExamplePart {
-    value: string | JSX.Element;
-    excludeTrailingBackslash?: boolean;
-    excludeIndent?: boolean;
+export type CurlExamplePart = CurlExamplePart.Jsx | CurlExamplePart.Line;
+
+export declare namespace CurlExamplePart {
+    export interface Jsx {
+        type: "jsx";
+        jsx: JSX.Element;
+    }
+
+    export interface Line {
+        type: "line";
+        value: string | JSX.Element;
+        excludeTrailingBackslash?: boolean;
+        excludeIndent?: boolean;
+    }
 }
