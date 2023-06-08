cf9af724e296208935f04fd269e95d4ef16aa065
Deep Singhvi <deep@usebirch.com>
2023-06-08 07:46:04 -0400

    [openapi parser] fix: set enum names for enum values that are entirely numeric (#1733)
    set enum names for enum values that are entirely numeric

--- packages/cli/openapi-parser/src/v3/converters/schema/convertEnum.ts
+++ packages/cli/openapi-parser/src/v3/converters/schema/convertEnum.ts
@@ -74,7 +74,18 @@ export function wrapEnum({
 }
 
 function generateEnumNameFromValue(value: string): string {
-    return upperFirst(camelCase(value));
+    const maybeParsedNumber = maybeParseNumber(value);
+    if (maybeParsedNumber != null) {
+        return convertNumberToSnakeCase(maybeParsedNumber) ?? value;
+    } else {
+        return upperFirst(camelCase(value));
+    }
+}
+
+function maybeParseNumber(input: string): number | null {
+    const parsedNumber = parseFloat(input);
+
+    return !isNaN(parsedNumber) && isFinite(parsedNumber) ? parsedNumber : null;
 }
 
 function stripCommonPrefix(names: string[]): string[] {
@@ -92,4 +103,66 @@ function stripCommonPrefix(names: string[]): string[] {
 
     // prefix is the substring from the beginning to the last successfully checked i
     return names.map((name) => name.substring(i));
+}
+
+function convertNumberToSnakeCase(number: number): string | undefined {
+    const singleDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    const teenNumbers = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const tensNumbers = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    if (number < 0 || number > 9999) {
+        return undefined;
+    }
+
+    if (number < 10) {
+        return singleDigits[number];
+    }
+
+    if (number < 20) {
+        return teenNumbers[number - 10];
+    }
+
+    if (number < 100) {
+        const tens = Math.floor(number / 10);
+        const remainder = number % 10;
+
+        if (remainder === 0) {
+            return tensNumbers[tens];
+        } else {
+            return `${tensNumbers[tens]}_${singleDigits[remainder]}`;
+        }
+    }
+
+    if (number < 1000) {
+        const hundreds = Math.floor(number / 100);
+        const remainder = number % 100;
+
+        if (remainder === 0) {
+            return `${singleDigits[hundreds]}_hundred`;
+        } else {
+            return `${singleDigits[hundreds]}_hundred_${convertNumberToSnakeCase(remainder)}`;
+        }
+    }
+
+    const thousands = Math.floor(number / 1000);
+    const remainder = number % 1000;
+
+    if (remainder === 0) {
+        return `${singleDigits[thousands]}_thousand`;
+    } else {
+        return `${singleDigits[thousands]}_thousand_${convertNumberToSnakeCase(remainder)}`;
+    }
 }
