a19890d183e73d646d2d1d2df7a0c34e514c0788
Zach Kirsch <zachkirsch@gmail.com>
2023-06-08 17:40:09 -0400

    Add BrowserRouter

--- packages/ui/app/src/contexts.tsx
+++ packages/ui/app/src/contexts.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "@fern-api/theme";
 import { PropsWithChildren } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 export const CONTEXTS = [
     ({ children }: PropsWithChildren): JSX.Element => <ThemeProvider theme="dark">{children}</ThemeProvider>,
+    BrowserRouter,
 ];
