aa14766f1bcf61d955be0d958a3e574e044bda08
Zach Kirsch <zachkirsch@gmail.com>
2023-06-08 17:17:04 -0400

    Upgrade mrlint

--- .pnp.cjs
+++ .pnp.cjs
@@ -323,7 +323,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jsonc-parser", "npm:2.2.1"],\
             ["lint-staged", "npm:13.0.3"],\
             ["lodash-es", "npm:4.17.21"],\
-            ["mrlint", "npm:0.0.116"],\
+            ["mrlint", "npm:0.0.117"],\
             ["organize-imports-cli", "npm:0.10.0"],\
             ["prettier", "npm:2.7.1"],\
             ["react", "npm:18.2.0"],\
@@ -14278,7 +14278,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jsonc-parser", "npm:2.2.1"],\
             ["lint-staged", "npm:13.0.3"],\
             ["lodash-es", "npm:4.17.21"],\
-            ["mrlint", "npm:0.0.116"],\
+            ["mrlint", "npm:0.0.117"],\
             ["organize-imports-cli", "npm:0.10.0"],\
             ["prettier", "npm:2.7.1"],\
             ["react", "npm:18.2.0"],\
@@ -18550,10 +18550,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["mrlint", [\
-        ["npm:0.0.116", {\
-          "packageLocation": "./.yarn/cache/mrlint-npm-0.0.116-c76f5f4138-46ff0d29e4.zip/node_modules/mrlint/",\
+        ["npm:0.0.117", {\
+          "packageLocation": "./.yarn/cache/mrlint-npm-0.0.117-bafe352c9f-d82afa6bef.zip/node_modules/mrlint/",\
           "packageDependencies": [\
-            ["mrlint", "npm:0.0.116"]\
+            ["mrlint", "npm:0.0.117"]\
           ],\
           "linkType": "HARD"\
         }]\
