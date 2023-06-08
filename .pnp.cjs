34d5e2989ff370df1d91239123d6ff82b77acb08
Zach Kirsch <zachkirsch@gmail.com>
2023-06-08 17:01:58 -0400

    Upgrade mrlint

--- .pnp.cjs
+++ .pnp.cjs
@@ -323,7 +323,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jsonc-parser", "npm:2.2.1"],\
             ["lint-staged", "npm:13.0.3"],\
             ["lodash-es", "npm:4.17.21"],\
-            ["mrlint", "npm:0.0.115"],\
+            ["mrlint", "npm:0.0.116"],\
             ["organize-imports-cli", "npm:0.10.0"],\
             ["prettier", "npm:2.7.1"],\
             ["react", "npm:18.2.0"],\
@@ -14278,7 +14278,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jsonc-parser", "npm:2.2.1"],\
             ["lint-staged", "npm:13.0.3"],\
             ["lodash-es", "npm:4.17.21"],\
-            ["mrlint", "npm:0.0.115"],\
+            ["mrlint", "npm:0.0.116"],\
             ["organize-imports-cli", "npm:0.10.0"],\
             ["prettier", "npm:2.7.1"],\
             ["react", "npm:18.2.0"],\
@@ -18550,10 +18550,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["mrlint", [\
-        ["npm:0.0.115", {\
-          "packageLocation": "./.yarn/cache/mrlint-npm-0.0.115-1805db41bb-d724446a0a.zip/node_modules/mrlint/",\
+        ["npm:0.0.116", {\
+          "packageLocation": "./.yarn/cache/mrlint-npm-0.0.116-c76f5f4138-46ff0d29e4.zip/node_modules/mrlint/",\
           "packageDependencies": [\
-            ["mrlint", "npm:0.0.115"]\
+            ["mrlint", "npm:0.0.116"]\
           ],\
           "linkType": "HARD"\
         }]\
--- packages/commons/react/toaster/src/declarations.d.ts
+++ packages/commons/react/toaster/src/declarations.d.ts
@@ -1,99 +1,14 @@
 // CSS modules
-type CSSModuleClasses = { readonly [key: string]: string };
+type CSSModuleClasses = Readonly<Record<string, string>>;
 
-declare module "*.module.css" {
-    const classes: CSSModuleClasses;
-    export default classes;
-}
 declare module "*.module.scss" {
-    const classes: CSSModuleClasses;
-    export default classes;
-}
-declare module "*.module.sass" {
-    const classes: CSSModuleClasses;
-    export default classes;
-}
-declare module "*.module.less" {
-    const classes: CSSModuleClasses;
-    export default classes;
-}
-declare module "*.module.styl" {
-    const classes: CSSModuleClasses;
-    export default classes;
-}
-declare module "*.module.stylus" {
-    const classes: CSSModuleClasses;
-    export default classes;
-}
-declare module "*.module.pcss" {
-    const classes: CSSModuleClasses;
-    export default classes;
-}
-declare module "*.module.sss" {
     const classes: CSSModuleClasses;
     export default classes;
 }
 
 // CSS
-declare module "*.css" {
-    /**
-     * @deprecated Use `` instead.
-     */
-    const css: string;
-    export default css;
-}
-declare module "*.scss" {
-    /**
-     * @deprecated Use `` instead.
-     */
-    const css: string;
-    export default css;
-}
-declare module "*.sass" {
-    /**
-     * @deprecated Use `` instead.
-     */
-    const css: string;
-    export default css;
-}
-declare module "*.less" {
-    /**
-     * @deprecated Use `` instead.
-     */
-    const css: string;
-    export default css;
-}
-declare module "*.styl" {
-    /**
-     * @deprecated Use `` instead.
-     */
-    const css: string;
-    export default css;
-}
-declare module "*.stylus" {
-    /**
-     * @deprecated Use `` instead.
-     */
-    const css: string;
-    export default css;
-}
-declare module "*.pcss" {
-    /**
-     * @deprecated Use `` instead.
-     */
-    const css: string;
-    export default css;
-}
-declare module "*.sss" {
-    /**
-     * @deprecated Use `` instead.
-     */
-    const css: string;
-    export default css;
-}
-
-// Built-in asset types
-// see `src/node/constants.ts`
+declare module "*.css" {}
+declare module "*.scss" {}
 
 // images
 declare module "*.png" {
@@ -108,18 +23,6 @@ declare module "*.jpeg" {
     const src: string;
     export default src;
 }
-declare module "*.jfif" {
-    const src: string;
-    export default src;
-}
-declare module "*.pjpeg" {
-    const src: string;
-    export default src;
-}
-declare module "*.pjp" {
-    const src: string;
-    export default src;
-}
 declare module "*.gif" {
     const src: string;
     export default src;
@@ -129,145 +32,6 @@ declare module "*.svg" {
     export default src;
 }
 declare module "*.ico" {
-    const src: string;
-    export default src;
-}
-declare module "*.webp" {
-    const src: string;
-    export default src;
-}
-declare module "*.avif" {
-    const src: string;
-    export default src;
-}
-
-// media
-declare module "*.mp4" {
-    const src: string;
-    export default src;
-}
-declare module "*.webm" {
-    const src: string;
-    export default src;
-}
-declare module "*.ogg" {
-    const src: string;
-    export default src;
-}
-declare module "*.mp3" {
-    const src: string;
-    export default src;
-}
-declare module "*.wav" {
-    const src: string;
-    export default src;
-}
-declare module "*.flac" {
-    const src: string;
-    export default src;
-}
-declare module "*.aac" {
-    const src: string;
-    export default src;
-}
-
-declare module "*.opus" {
-    const src: string;
-    export default src;
-}
-
-// fonts
-declare module "*.woff" {
-    const src: string;
-    export default src;
-}
-declare module "*.woff2" {
-    const src: string;
-    export default src;
-}
-declare module "*.eot" {
-    const src: string;
-    export default src;
-}
-declare module "*.ttf" {
-    const src: string;
-    export default src;
-}
-declare module "*.otf" {
-    const src: string;
-    export default src;
-}
-
-// other
-declare module "*.webmanifest" {
-    const src: string;
-    export default src;
-}
-declare module "*.pdf" {
-    const src: string;
-    export default src;
-}
-declare module "*.txt" {
-    const src: string;
-    export default src;
-}
-
-// wasm?init
-declare module "*.wasm?init" {
-    const initWasm: (options: WebAssembly.Imports) => Promise<WebAssembly.Instance>;
-    export default initWasm;
-}
-
-// web worker
-declare module "*?worker" {
-    const workerConstructor: {
-        new (): Worker;
-    };
-    export default workerConstructor;
-}
-
-declare module "*?worker&inline" {
-    const workerConstructor: {
-        new (): Worker;
-    };
-    export default workerConstructor;
-}
-
-declare module "*?worker&url" {
-    const src: string;
-    export default src;
-}
-
-declare module "*?sharedworker" {
-    const sharedWorkerConstructor: {
-        new (): SharedWorker;
-    };
-    export default sharedWorkerConstructor;
-}
-
-declare module "*?sharedworker&inline" {
-    const sharedWorkerConstructor: {
-        new (): SharedWorker;
-    };
-    export default sharedWorkerConstructor;
-}
-
-declare module "*?sharedworker&url" {
-    const src: string;
-    export default src;
-}
-
-declare module "*?raw" {
-    const src: string;
-    export default src;
-}
-
-declare module "*?url" {
-    const src: string;
-    export default src;
-}
-
-declare module "*?inline" {
     const src: string;
     export default src;
 }
