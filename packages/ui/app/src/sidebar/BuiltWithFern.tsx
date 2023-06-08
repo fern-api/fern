cf70898c680862bc2d3e1bc6d35046ff5082235a
Zach Kirsch <zachkirsch@gmail.com>
2023-06-07 21:42:05 -0400

    Center 'built with fern'

--- packages/ui/app/src/sidebar/BuiltWithFern.tsx
+++ packages/ui/app/src/sidebar/BuiltWithFern.tsx
@@ -1,32 +1,23 @@
-import { useIsHovering } from "@fern-api/react-commons";
 import classNames from "classnames";
 import { useCallback } from "react";
 import { FernLogo } from "./FernLogo";
 
 export const BuiltWithFern: React.FC = () => {
-    const { isHovering, ...containerCallbacks } = useIsHovering();
-
     const onClick = useCallback(() => {
         window.open("https://buildwithfern.com", "_blank", "noopener noreferrer");
     }, []);
 
     return (
         <div
-            className="flex cursor-pointer items-center gap-2 p-3 shadow-[0_-5px_10px_0px_rgba(18,20,24,1)]"
-            {...containerCallbacks}
+            className={classNames(
+                "flex cursor-pointer items-center justify-center gap-2 p-3 shadow-[0_-5px_10px_0px_rgba(18,20,24,1)] opacity-75 hover:opacity-100 transition"
+            )}
             onClick={onClick}
         >
             <div className="h-4 w-4">
                 <FernLogo />
             </div>
-            <div
-                className={classNames(
-                    "whitespace-nowrap text-xs transition",
-                    isHovering ? "text-white" : "text-text-muted"
-                )}
-            >
-                Built with Fern
-            </div>
+            <div className="whitespace-nowrap text-xs">Built with Fern</div>
         </div>
     );
 };
