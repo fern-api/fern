efa8db4433bb20a0acc54979da025ed91a5347d7
Zach Kirsch <zachkirsch@users.noreply.github.com>
2023-06-11 15:34:51 -0400

    Remove vite fe-bundle (#1758)
    * Remove vite fe-bundle
    
    * Add empty commit

--- packages/ui/fe-bundle/deploy/deploy.js
+++
@@ -1,9 +0,0 @@
-import { deploy } from "@fern-api/docs-deploy";
-import path from "path";
-import * as url from "url";
-
-const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
-
-void deploy({
-    distDirectory: path.join(__dirname, "../dist"),
-});
