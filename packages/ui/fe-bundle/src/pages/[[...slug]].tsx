a6dab721f65a45623e8715a6a69e1b515ed6e1bc
Zach Kirsch <zachkirsch@gmail.com>
2023-06-12 23:07:50 -0400

    Revert "Move data fetching to _app.tsx"
    This reverts commit dacc145987f8bbe09bff40c555a945ee7a619677.

--- packages/ui/fe-bundle/src/pages/[[...slug]].tsx
+++ packages/ui/fe-bundle/src/pages/[[...slug]].tsx
@@ -1,9 +1,15 @@
 import { App } from "@fern-api/ui";
+import { FernRegistryClient } from "@fern-fern/registry-browser";
 import * as FernRegistryDocsRead from "@fern-fern/registry-browser/serialization/resources/docs/resources/v2/resources/read";
+import { GetServerSideProps } from "next";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const REGISTRY_SERVICE = new FernRegistryClient({
+    environment: process.env.NEXT_PUBLIC_FDR_ORIGIN ?? "https://registry.buildwithfern.com",
+});
 
 export declare namespace Docs {
     export interface Props {
@@ -25,3 +31,31 @@ export default function Docs({ docs, pathname }: Docs.Props): JSX.Element {
         </main>
     );
 }
+
+export const getServerSideProps: GetServerSideProps<Docs.Props> = async (context) => {
+    const host = context.req.headers["x-fern-host"] ?? context.req.headers.host;
+    if (host == null) {
+        throw new Error("Host header is not defined");
+    }
+
+    const docs = await REGISTRY_SERVICE.docs.v2.read.getDocsForUrl({
+        url: process.env.NEXT_PUBLIC_DOCS_DOMAIN ?? `${host}${context.resolvedUrl}`,
+    });
+
+    if (!docs.ok) {
+        // eslint-disable-next-line no-console
+        console.error(docs.error);
+        return { notFound: true };
+    }
+
+    return {
+        props: {
+            docs: await FernRegistryDocsRead.LoadDocsForUrlResponse.jsonOrThrow(docs.body, {
+                unrecognizedObjectKeys: "passthrough",
+                allowUnrecognizedEnumValues: true,
+                allowUnrecognizedUnionMembers: true,
+            }),
+            pathname: context.query.slug != null ? (context.query.slug as string[]).join("/") : "",
+        },
+    };
+};
