2d174b75dc0bb17802a681391292b71efb8aff4e
Zach Kirsch <zachkirsch@gmail.com>
2023-06-13 11:20:45 -0400

    Revert "Hardcode docs response"
    This reverts commit fbf84e479341b39d95c77008bf81e5b021f55ac2.

--- packages/ui/fe-bundle/src/pages/[[...slug]].tsx
+++ packages/ui/fe-bundle/src/pages/[[...slug]].tsx
@@ -1,11 +1,16 @@
 import { App } from "@fern-api/ui";
+import { FernRegistryClient } from "@fern-fern/registry-browser";
 import * as FernRegistryDocsRead from "@fern-fern/registry-browser/serialization/resources/docs/resources/v2/resources/read";
 import { GetServerSideProps } from "next";
 import { Inter } from "next/font/google";
 import Head from "next/head";
-import { HARDCODED_DOCS } from "../components/hardcodedDocs";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const REGISTRY_SERVICE = new FernRegistryClient({
+    environment: process.env.NEXT_PUBLIC_FDR_ORIGIN ?? "https://registry.buildwithfern.com",
+});
+
 export declare namespace Docs {
     export interface Props {
         docs: FernRegistryDocsRead.LoadDocsForUrlResponse.Raw;
@@ -28,9 +33,28 @@ export default function Docs({ docs, pathname }: Docs.Props): JSX.Element {
 }
 
 export const getServerSideProps: GetServerSideProps<Docs.Props> = async (context) => {
+    const host = context.req.headers["x-fern-host"] ?? context.req.headers.host;
+    if (host == null) {
+        throw new Error("Host header is not defined");
+    }
+
+    const docs = await REGISTRY_SERVICE.docs.v2.read.getDocsForUrl({
+        url: process.env.NEXT_PUBLIC_DOCS_DOMAIN ?? `${host}${context.resolvedUrl}`,
+    });
+
+    if (!docs.ok) {
+        // eslint-disable-next-line no-console
+        console.error(docs.error);
+        return { notFound: true };
+    }
+
     return {
         props: {
-            docs: HARDCODED_DOCS,
+            docs: await FernRegistryDocsRead.LoadDocsForUrlResponse.jsonOrThrow(docs.body, {
+                unrecognizedObjectKeys: "passthrough",
+                allowUnrecognizedEnumValues: true,
+                allowUnrecognizedUnionMembers: true,
+            }),
             pathname: context.query.slug != null ? (context.query.slug as string[]).join("/") : "",
         },
     };
