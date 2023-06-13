edbc4c75b9399c37315958fe5e22f0ff6af98203
Alex McKinney <alexmckinney01@gmail.com>
2023-06-13 10:37:08 -0700

    Setup placeholder for client/server error generation

--- internal/generator/sdk.go
+++ internal/generator/sdk.go
@@ -7,3 +7,22 @@ func (f *fileWriter) WriteClient(service *ir.HttpService) error {
 	f.P("type ", service.Name.FernFilepath.File.PascalCase.UnsafeName, "Client interface {}")
 	return nil
 }
+
+func (f *fileWriter) WriteError(errorDeclaration *ir.ErrorDeclaration) error {
+	f.P("type ", errorDeclaration.Name.Name.PascalCase.UnsafeName, " struct {")
+	f.P("StatusCode int `json:\"-\"`")
+	if errorDeclaration.Type == nil {
+		// This error doesn't have a body, so we only need to include the status code.
+		f.P("}")
+		f.P()
+		return nil
+	}
+	if errorDeclaration.Type.Named != nil {
+		// TODO: Write out the properties specified on the given type.
+	}
+	// TODO: Otherwise, need to use a [de]serialization strategy similar to undiscriminated unions
+	// for the built-in types.
+	f.P("}")
+	f.P()
+	return nil
+}
