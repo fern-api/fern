2feadc9a4b4470b8379636e064da4f0a93a8b90a
Alex McKinney <alexmckinney01@gmail.com>
2023-06-13 14:15:23 -0700

    Generate the error json.Unmarshaler

--- internal/generator/sdk.go
+++ internal/generator/sdk.go
@@ -1,6 +1,11 @@
 package generator
 
-import "github.com/fern-api/fern-go/internal/fern/ir"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fern-api/fern-go/internal/fern/ir"
+)
 
 // WriteClient writes a client for interacting with the given service.
 func (f *fileWriter) WriteClient(service *ir.HttpService) error {
@@ -9,7 +14,20 @@ func (f *fileWriter) WriteClient(service *ir.HttpService) error {
 }
 
 func (f *fileWriter) WriteError(errorDeclaration *ir.ErrorDeclaration) error {
-	f.P("type ", errorDeclaration.Name.Name.PascalCase.UnsafeName, " struct {")
+	var (
+		typeName   = errorDeclaration.Name.Name.PascalCase.UnsafeName
+		receiver   = typeNameToReceiver(typeName)
+		importPath = fernFilepathToImportPath(f.baseImportPath, errorDeclaration.Name.FernFilepath)
+		value      = typeReferenceToGoType(errorDeclaration.Type, f.types, f.imports, f.baseImportPath, importPath)
+	)
+	var literal string
+	isLiteral := errorDeclaration.Type.Container != nil && errorDeclaration.Type.Container.Literal != nil
+	if isLiteral {
+		literal = literalToValue(errorDeclaration.Type.Container.Literal)
+	}
+
+	// Generate the error type declaration.
+	f.P("type ", typeName, " struct {")
 	f.P("StatusCode int")
 	if errorDeclaration.Type == nil {
 		// This error doesn't have a body, so we only need to include the status code.
@@ -17,13 +35,38 @@ func (f *fileWriter) WriteError(errorDeclaration *ir.ErrorDeclaration) error {
 		f.P()
 		return nil
 	}
-	importPath := fernFilepathToImportPath(f.baseImportPath, errorDeclaration.Name.FernFilepath)
-	value := typeReferenceToGoType(errorDeclaration.Type, f.types, f.imports, f.baseImportPath, importPath)
 	f.P("Body ", value)
 	f.P("}")
 	f.P()
 
-	// TODO: Generate the [un]marshaler logic.
-	// literal = literalToValue(unionMember.Type.Container.Literal)
+	// Implement the error interface.
+	f.P("func (", receiver, "*", typeName, ") Error() string {")
+	f.P(`return fmt.Sprintf("`, errorDeclaration.StatusCode, `: %+v", *`, receiver, ")")
+	f.P("}")
+	f.P()
+
+	// Implement the json.Unmarshaler.
+	format := "var body %s"
+	if errorDeclaration.Type.Named != nil && isPointer(f.types[errorDeclaration.Type.Named.TypeId]) {
+		format = "body := new(%s)"
+		value = strings.TrimLeft(value, "*")
+	}
+	f.P("func (", receiver, "*", typeName, ") UnmarshalJSON(data []byte) error {")
+	f.P(fmt.Sprintf(format, value))
+	f.P("if err := json.Unmarshal(data, &body); err != nil {")
+	f.P("return err")
+	f.P("}")
+	if isLiteral {
+		// If the error specifies a literal, it will only succeed if the literal matches exactly.
+		f.P("if body != ", literal, " {")
+		f.P(`return fmt.Errorf("expected literal %q, but found %q", `, literal, ", body)")
+		f.P("}")
+	}
+	f.P(receiver, ".StatusCode = ", errorDeclaration.StatusCode)
+	f.P(receiver, ".Body = body")
+	f.P("return nil")
+	f.P("}")
+	f.P()
+
 	return nil
 }
