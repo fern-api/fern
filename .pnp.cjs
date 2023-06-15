2f76ac64d158189a6e2b8d6d1998c3e93e1752bd
Deep Singhvi <deep@usebirch.com>
2023-06-15 11:12:09 -0700

    [openapi parser] fix: handle allOf property conflicts (#1782)
    * handle parent extends conflicts
    
    * update generated fern definition
    
    * fix bug

--- .pnp.cjs
+++ .pnp.cjs
@@ -6257,7 +6257,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/project-configuration", "workspace:packages/cli/config-management/project-configuration"],\
             ["@fern-api/task-context", "workspace:packages/cli/task-context"],\
             ["@fern-api/yaml-schema", "workspace:packages/cli/yaml/yaml-schema"],\
-            ["@fern-fern/openapi-ir-model", "npm:0.0.74"],\
+            ["@fern-fern/openapi-ir-model", "npm:0.0.81"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/lodash-es", "npm:4.17.7"],\
             ["@types/node", "npm:18.7.18"],\
@@ -6313,7 +6313,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/fs-utils", "workspace:packages/commons/fs-utils"],\
             ["@fern-api/logger", "workspace:packages/cli/logger"],\
             ["@fern-api/task-context", "workspace:packages/cli/task-context"],\
-            ["@fern-fern/openapi-ir-model", "npm:0.0.74"],\
+            ["@fern-fern/openapi-ir-model", "npm:0.0.81"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/lodash-es", "npm:4.17.7"],\
             ["@types/node", "npm:18.7.18"],\
@@ -7214,10 +7214,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["@fern-fern/openapi-ir-model", [\
-        ["npm:0.0.74", {\
-          "packageLocation": "./.yarn/cache/@fern-fern-openapi-ir-model-npm-0.0.74-a64aeb4d2b-eefa7cf33f.zip/node_modules/@fern-fern/openapi-ir-model/",\
+        ["npm:0.0.81", {\
+          "packageLocation": "./.yarn/cache/@fern-fern-openapi-ir-model-npm-0.0.81-86e06cdbd2-dd978b285a.zip/node_modules/@fern-fern/openapi-ir-model/",\
           "packageDependencies": [\
-            ["@fern-fern/openapi-ir-model", "npm:0.0.74"]\
+            ["@fern-fern/openapi-ir-model", "npm:0.0.81"]\
           ],\
           "linkType": "HARD"\
         }]\
