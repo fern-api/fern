f28e2de080d2bd3e70d257de7282f51cc787b541
shashank tiwari <shashanktiwari40@gmail.com>
2023-06-21 17:00:53 +0530

    formatted indentation

--- packages/cli/auth/src/orgs/getOrganizationNameValidationError.ts
+++ packages/cli/auth/src/orgs/getOrganizationNameValidationError.ts
@@ -2,8 +2,7 @@ export function getOrganizationNameValidationError(organizationName: string): st
     const regex = new RegExp("^[a-z][a-z0-9-]*$");
     if (organizationName.length === 0) {
         return "Organization name cannot be empty.\nUse --organization flag to specify organization name.\nUse command: fern init --organization <org name>";
-    }
-    else if (!regex.test(organizationName)) {
+    } else if (!regex.test(organizationName)) {
         return "Organization name must start with a Small Letter & contain only Lowercase Letters, Numbers, and Dashes.\nUse --organization flag to specify organization name.\nUse command: fern init --organization <org name>";
     }
 
