c2365161adbb5c5275f207ec46b6e023847ee3f2
shashank tiwari <shashanktiwari40@gmail.com>
2023-06-21 16:53:00 +0530

    Issue #1655: Improving error message for incorrect organization name

--- packages/cli/auth/src/orgs/getOrganizationNameValidationError.ts
+++ packages/cli/auth/src/orgs/getOrganizationNameValidationError.ts
@@ -1,6 +1,11 @@
 export function getOrganizationNameValidationError(organizationName: string): string | undefined {
+    const regex = new RegExp("^[a-z][a-z0-9-]*$");
     if (organizationName.length === 0) {
-        return "Organization name cannot be empty";
+        return "Organization name cannot be empty.\nUse --organization flag to specify organization name.\nUse command: fern init --organization <org name>";
     }
+    else if (!regex.test(organizationName)) {
+        return "Organization name must start with a Small Letter & contain only Lowercase Letters, Numbers, and Dashes.\nUse --organization flag to specify organization name.\nUse command: fern init --organization <org name>";
+    }
+
     return undefined;
 }
