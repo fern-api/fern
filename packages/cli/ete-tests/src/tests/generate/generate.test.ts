25fae35653af7956014eaa20c3942e467cf8267f
slamchillz <mendyslam@gmail.com>
2023-06-23 12:10:40 +0100

    tests for .fernignore implementation when generating locally

--- packages/cli/ete-tests/src/tests/generate/generate.test.ts
+++ packages/cli/ete-tests/src/tests/generate/generate.test.ts
@@ -1,6 +1,7 @@
 import { AbsoluteFilePath, doesPathExist, join, RelativeFilePath } from "@fern-api/fs-utils";
 import { runFernCli } from "../../utils/runFernCli";
 import { init } from "../init/init";
+import { writeFile, mkdir } from "fs/promises";
 
 const FIXTURES = ["docs"];
 
@@ -29,4 +30,31 @@ describe("fern generate", () => {
             180_000
         );
     }
+});
+
+
+describe("fern generate --local", () => {
+    it("Keep files listed in .fernignore from unmodified", async () => {
+        const pathOfDirectory = await init();
+
+        await runFernCli(["generate", "--local", "--keepDocker"], {
+            cwd: pathOfDirectory,
+        });
+
+        const absolutePathToLocalOutput = join(pathOfDirectory, RelativeFilePath.of("generated/typescript"));
+
+        await writeFile(`${pathOfDirectory}/generated/typescript/.fernignore`, "fern.js\n**/*.txt");
+        await writeFile(`${pathOfDirectory}/generated/typescript/fern.js`, "#!/usr/bin/env node\nconsole.log('Fern >>> OpenAPI')");
+        await mkdir(`${pathOfDirectory}/generated/typescript/slam`);
+        await writeFile(`${pathOfDirectory}/generated/typescript/slam/slam.txt`, "Practice schema-first API design with Fern");
+
+        await runFernCli(["generate", "--local", "--keepDocker"], {
+            cwd: pathOfDirectory,
+        });
+
+        expect(await doesPathExist(join(absolutePathToLocalOutput, RelativeFilePath.of("fern.js")))).toBe(true);
+        expect(await doesPathExist(join(absolutePathToLocalOutput, RelativeFilePath.of(".fernignore")))).toBe(true);
+        expect(await doesPathExist(join(absolutePathToLocalOutput, RelativeFilePath.of("slam")))).toBe(true);
+        expect(await doesPathExist(join(absolutePathToLocalOutput, RelativeFilePath.of("slam/slam.txt")))).toBe(true);
+    }, 180_000);
 });
