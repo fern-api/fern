c21a0d0306077a3a501c5fb8b934b251e844b50f
Zach Kirsch <zachkirsch@gmail.com>
2023-06-24 19:26:50 -0400

    Fix double link underline in mdx pages

--- packages/ui/app/src/mdx/MdxContent.tsx
+++ packages/ui/app/src/mdx/MdxContent.tsx
@@ -20,6 +20,15 @@ const COMPONENTS: MDXRemoteProps["components"] = {
     h5: (props) => <h5 {...props} className={classNames("text-xl mt-10 mb-3", props.className)} />,
     h6: (props) => <h6 {...props} className={classNames("text-xl mt-10 mb-3", props.className)} />,
     code: (props) => <code {...props} className={classNames("px-2 py-1 bg-white/10 rounded", props.className)} />,
+    a: (props) => (
+        <a
+            {...props}
+            className={classNames(
+                "transition pb-[2px] !text-white hover:!text-accentPrimary no-underline border-b border-b-border hover:border-b-accentPrimary hover:no-underline font-medium",
+                props.className
+            )}
+        />
+    ),
     Cards,
     Card,
 };
