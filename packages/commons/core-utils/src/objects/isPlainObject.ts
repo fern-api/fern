96181fa461bddfed65b18a27f8bfe60e9d41610a
Zach Kirsch <zachkirsch@gmail.com>
2023-06-27 14:26:15 -0400

    Remove lodash from core-utils

--- packages/commons/core-utils/src/objects/isPlainObject.ts
+++ packages/commons/core-utils/src/objects/isPlainObject.ts
@@ -1,5 +1,18 @@
-import { isPlainObject as isPlainObjectLodash } from "lodash-es";
-
+// https://github.com/lodash/lodash/blob/master/isPlainObject.js
 export function isPlainObject(value: unknown): value is Record<string, unknown> {
-    return isPlainObjectLodash(value);
+    if (!isObjectLike(value) || toString.call(value) !== "[object Object]") {
+        return false;
+    }
+    if (Object.getPrototypeOf(value) == null) {
+        return true;
+    }
+    let proto = value;
+    while (Object.getPrototypeOf(proto) != null) {
+        proto = Object.getPrototypeOf(proto);
+    }
+    return Object.getPrototypeOf(value) === proto;
+}
+
+function isObjectLike(value: unknown) {
+    return typeof value === "object" && value != null;
 }
