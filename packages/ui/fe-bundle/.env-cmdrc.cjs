b545e822d4b47a6d314dd5b9e420916f84e9032e
Anar Kafkas <anarkafkas@gmail.com>
2023-06-28 17:44:50 +0300

    Add new env vars for algolia

--- packages/ui/fe-bundle/.env-cmdrc.cjs
+++ packages/ui/fe-bundle/.env-cmdrc.cjs
@@ -2,6 +2,9 @@ module.exports = {
     "fern-dev": {
         NEXT_PUBLIC_FDR_ORIGIN: "https://registry-dev.buildwithfern.com",
         NEXT_PUBLIC_POSTHOG_API_KEY: "",
+        NEXT_PUBLIC_ALGOLIA_APP_ID: "HV2RJ3GNCN",
+        NEXT_PUBLIC_ALGOLIA_API_KEY: "0fdb81b4d20f6cb46d51e9f007bffda0",
+        NEXT_PUBLIC_ALGOLIA_SEARCH_INDEX: "dev-permit",
     },
     "fern-prod": {
         NEXT_PUBLIC_FDR_ORIGIN: "https://registry.buildwithfern.com",
