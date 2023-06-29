f47d461350b878e8a65d56ed319991daedd6961f
Anar Kafkas <anarkafkas@gmail.com>
2023-06-29 20:03:24 +0300

    Navigate to path from SearchHit

--- packages/ui/app/src/search/SearchHit.tsx
+++ packages/ui/app/src/search/SearchHit.tsx
@@ -1,4 +1,6 @@
 import { Highlight } from "react-instantsearch-hooks-web";
+import { useDocsContext } from "../docs-context/useDocsContext";
+import { useSearchContext } from "../search-context/useSearchContext";
 
 export declare namespace SearchHit {
     export interface Props {
@@ -6,29 +8,37 @@ export declare namespace SearchHit {
         hit: {
             __position: number;
             objectID: string;
-            property_name: string;
-            description: string;
-            location: string;
+            title: string;
+            subtitle: string;
+            path: string;
         };
     }
 }
 
 export const SearchHit: React.FC<SearchHit.Props> = (providedProps) => {
+    const { navigateToPath } = useDocsContext();
+    const { closeSearchDialog } = useSearchContext();
     const { hit } = providedProps;
 
     return (
-        <button className="hover:bg-accentPrimary group flex w-full flex-col space-y-1 rounded-md p-2">
+        <button
+            className="hover:bg-accentPrimary group flex w-full flex-col space-y-1 rounded-md p-2"
+            onClick={() => {
+                closeSearchDialog();
+                navigateToPath(hit.path);
+            }}
+        >
             <div className="flex flex-col items-start">
                 <Highlight
                     className="text-text-default text-start group-hover:text-white"
-                    attribute="property_name"
+                    attribute="title"
                     hit={hit}
                 />
             </div>
             <div className="flex flex-col items-start">
                 <Highlight
                     className="text-text-default text-start group-hover:text-white"
-                    attribute="description"
+                    attribute="subtitle"
                     hit={hit}
                 />
             </div>
