fb420cc6d1bfa55ceda60489e10023736bbe459d
Anar Kafkas <anarkafkas@gmail.com>
2023-06-30 14:13:52 +0300

    Compute platform once

--- packages/ui/app/src/utils/platform.ts
+++ packages/ui/app/src/utils/platform.ts
@@ -4,11 +4,11 @@ const uaParser = new UAParser();
 
 type Platform = "mac" | "windows" | "other";
 
-export function getPlatform(): Platform {
+export const platform = (function getPlatform(): Platform {
     const { name } = uaParser.getOS();
     if (typeof name === "string") {
         if (name.startsWith("Mac OS")) return "mac";
         if (name.startsWith("Windows")) return "windows";
     }
     return "other";
-}
+})();
