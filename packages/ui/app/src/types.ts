91810d8a47e5a22f70e40b35bff5df475f7c828d
Anar Kafkas <anarkafkas@gmail.com>
2023-06-30 16:10:48 +0300

    Show the type of the record in SearchHit

--- packages/ui/app/src/types.ts
+++ packages/ui/app/src/types.ts
@@ -2,6 +2,7 @@ import { Hit as AlgoliaHit } from "instantsearch.js";
 
 // TODO: Import the type from our sdk
 export type SearchRecord = AlgoliaHit<{
+    type: "endpoint";
     title: string;
     subtitle: string;
     path: string;
