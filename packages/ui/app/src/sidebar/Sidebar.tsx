33cabf44fe703950dc9f6cd26e375f54cfd34e18
Anar Kafkas <anarkafkas@gmail.com>
2023-07-01 23:23:12 +0300

    Hide sidebar search bar if search is not available

--- packages/ui/app/src/sidebar/Sidebar.tsx
+++ packages/ui/app/src/sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import { useCallback } from "react";
 import { useDocsContext } from "../docs-context/useDocsContext";
 import { useSearchContext } from "../search-context/useSearchContext";
+import { useSearchService } from "../services/useSearchService";
 import { BuiltWithFern } from "./BuiltWithFern";
 import { SidebarContext, SidebarContextValue } from "./context/SidebarContext";
 import styles from "./Sidebar.module.scss";
@@ -17,6 +18,7 @@ export declare namespace Sidebar {
 export const Sidebar: React.FC<Sidebar.Props> = ({ expandAllSections = false }) => {
     const { docsDefinition } = useDocsContext();
     const { openSearchDialog } = useSearchContext();
+    const searchService = useSearchService();
 
     const contextValue = useCallback((): SidebarContextValue => ({ expandAllSections }), [expandAllSections]);
 
@@ -24,7 +26,7 @@ export const Sidebar: React.FC<Sidebar.Props> = ({ expandAllSections = false }) 
         <SidebarContext.Provider value={contextValue}>
             <div className="border-border bg-background flex min-w-0 flex-1 flex-col justify-between overflow-hidden border-r">
                 <div className="z-10 flex flex-col px-2.5 pt-2.5 shadow-[0_-5px_10px_10px_rgba(18,20,24,1)]">
-                    <SidebarSearchBar onClick={openSearchDialog} />
+                    {searchService.isAvailable && <SidebarSearchBar onClick={openSearchDialog} />}
                 </div>
                 <div className={classNames("flex flex-1 flex-col overflow-y-auto pb-6", styles.scrollingContainer)}>
                     <SidebarItems navigationItems={docsDefinition.config.navigation.items} slug="" />
