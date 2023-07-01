213f8ae4ec6ef90e96681d1960c28c1ce6e9291e
Anar Kafkas <anarkafkas@gmail.com>
2023-07-01 23:22:26 +0300

    Implement hook to access search service from multiple locations

--- packages/ui/app/src/docs/Docs.tsx
+++ packages/ui/app/src/docs/Docs.tsx
@@ -4,6 +4,7 @@ import { useKeyboardCommand } from "../hooks/useKeyboardCommand";
 import { MobileSidebar } from "../mobile-sidebar/MobileSidebar";
 import { useSearchContext } from "../search-context/useSearchContext";
 import { SearchDialog } from "../search/SearchDialog";
+import { useSearchService } from "../services/useSearchService";
 import { Sidebar } from "../sidebar/Sidebar";
 import { DocsMainContent } from "./DocsMainContent";
 import { Header } from "./Header";
@@ -13,24 +14,16 @@ import { useCustomTheme } from "./useCustomTheme";
 export const Docs: React.FC = () => {
     const { docsDefinition } = useDocsContext();
     const { isSearchDialogOpen, openSearchDialog, closeSearchDialog } = useSearchContext();
+    const searchService = useSearchService();
     useCustomTheme(docsDefinition);
     useKeyboardCommand("K", openSearchDialog);
-
-    const { algoliaSearchIndex } = docsDefinition;
-    const isSearchAvailable = !!algoliaSearchIndex;
 
     return (
         <div
             className="relative flex min-h-0 flex-1 flex-col bg-cover"
             style={{ backgroundImage: `url('${nebulaImage.src}')` }}
         >
-            {isSearchAvailable && (
-                <SearchDialog
-                    isOpen={isSearchDialogOpen}
-                    onClose={closeSearchDialog}
-                    searchIndexName={algoliaSearchIndex}
-                />
-            )}
+            {searchService.isAvailable && <SearchDialog isOpen={isSearchDialogOpen} onClose={closeSearchDialog} />}
             <div className="border-border bg-background sticky inset-x-0 top-0 border-b">
                 <Header />
                 <div className="flex md:hidden">
---
+++ packages/ui/app/src/services/useSearchService.ts
@@ -0,0 +1,34 @@
+import algolia, { type SearchClient } from "algoliasearch/lite";
+import { useDocsContext } from "../docs-context/useDocsContext";
+
+export type SearchService =
+    | {
+          isAvailable: true;
+          client: SearchClient;
+          index: string;
+      }
+    | {
+          isAvailable: false;
+      };
+
+export function useSearchService(): SearchService {
+    const { docsDefinition } = useDocsContext();
+    const { algoliaSearchIndex } = docsDefinition;
+
+    if (!algoliaSearchIndex) {
+        return { isAvailable: false };
+    }
+
+    if (!process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || !process.env.NEXT_PUBLIC_ALGOLIA_API_KEY) {
+        // TODO: Move this validation elsewhere
+        throw new Error("Missing Algolia variables.");
+    }
+
+    const client = algolia(process.env.NEXT_PUBLIC_ALGOLIA_APP_ID, process.env.NEXT_PUBLIC_ALGOLIA_API_KEY);
+
+    return {
+        isAvailable: true,
+        index: algoliaSearchIndex,
+        client,
+    };
+}
