5d34a0875c647ed4475859e1ca2dd4d3b6a966d4
Anar Kafkas <anarkafkas@gmail.com>
2023-07-04 15:17:46 +0300

    Update algolia app id and api key

--- packages/ui/fe-bundle/.env-cmdrc.cjs
+++ packages/ui/fe-bundle/.env-cmdrc.cjs
@@ -2,8 +2,8 @@ module.exports = {
     "fern-dev": {
         NEXT_PUBLIC_FDR_ORIGIN: "https://registry-dev.buildwithfern.com",
         NEXT_PUBLIC_POSTHOG_API_KEY: "",
-        NEXT_PUBLIC_ALGOLIA_APP_ID: "HV2RJ3GNCN",
-        NEXT_PUBLIC_ALGOLIA_API_KEY: "78675e03a1ed32c8eba51e8abc97c7da",
+        NEXT_PUBLIC_ALGOLIA_APP_ID: "CQINPZSKS3",
+        NEXT_PUBLIC_ALGOLIA_API_KEY: "9515d5b15764da73b5cfad85772779fa",
     },
     "fern-prod": {
         NEXT_PUBLIC_FDR_ORIGIN: "https://registry.buildwithfern.com",
