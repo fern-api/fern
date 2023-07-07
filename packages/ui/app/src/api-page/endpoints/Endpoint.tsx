59c262cc1165059fbe73c68889b57905a4186451
Zach Kirsch <zachkirsch@users.noreply.github.com>
2023-07-07 17:19:20 -0400

    Delete unnecessary scroll listeners on endpoints (#1876)

--- packages/ui/app/src/api-page/endpoints/Endpoint.tsx
+++ packages/ui/app/src/api-page/endpoints/Endpoint.tsx
@@ -1,7 +1,4 @@
 import * as FernRegistryApiRead from "@fern-fern/registry-browser/api/resources/api/resources/v1/resources/read";
-import { useEffect } from "react";
-import { useDocsContext } from "../../docs-context/useDocsContext";
-import { useApiPageContext } from "../api-page-context/useApiPageContext";
 import { useApiPageCenterElement } from "../useApiPageCenterElement";
 import { EndpointContextProvider } from "./endpoint-context/EndpointContextProvider";
 import { EndpointContent } from "./EndpointContent";
@@ -14,22 +11,7 @@ export declare namespace Endpoint {
 }
 
 export const Endpoint: React.FC<Endpoint.Props> = ({ endpoint, slug }) => {
-    const { isInVerticalCenter, setTargetRef } = useApiPageCenterElement({ slug });
-    const { onScrollToPath } = useDocsContext();
-    const { containerRef: apiPageContainerRef } = useApiPageContext();
-    useEffect(() => {
-        if (!isInVerticalCenter) {
-            return;
-        }
-
-        const handler = () => {
-            onScrollToPath(slug);
-        };
-        apiPageContainerRef?.addEventListener("scroll", handler, false);
-        return () => {
-            apiPageContainerRef?.removeEventListener("scroll", handler);
-        };
-    }, [apiPageContainerRef, isInVerticalCenter, onScrollToPath, slug]);
+    const { setTargetRef } = useApiPageCenterElement({ slug });
 
     return (
         <EndpointContextProvider>
