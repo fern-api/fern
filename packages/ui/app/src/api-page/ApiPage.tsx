f2d3bf92edc9b0ac3174778e9272f0a864b8f2d8
Zach Kirsch <zachkirsch@gmail.com>
2023-07-07 17:14:06 -0400

    Delete unnecessary scroll listeners on endpoints

--- packages/ui/app/src/api-page/ApiPage.tsx
+++ packages/ui/app/src/api-page/ApiPage.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import { useApiDefinitionContext } from "../api-context/useApiDefinitionContext";
 import { BottomNavigationButtons } from "../bottom-navigation-buttons/BottomNavigationButtons";
-import { ApiPageContextProvider } from "./api-page-context/ApiPageContextProvider";
 import { ApiPackageContents } from "./ApiPackageContents";
 import { ApiArtifacts } from "./artifacts/ApiArtifacts";
 import { areApiArtifactsNonEmpty } from "./artifacts/areApiArtifactsNonEmpty";
@@ -10,22 +8,18 @@ import { ApiPageMargins } from "./page-margins/ApiPageMargins";
 export const ApiPage: React.FC = () => {
     const { apiDefinition, apiSlug, apiSection } = useApiDefinitionContext();
 
-    const [containerRef, setContainerRef] = useState<HTMLDivElement | null>(null);
-
     return (
-        <ApiPageContextProvider containerRef={containerRef ?? undefined}>
-            <div ref={setContainerRef} className="min-h-0 overflow-y-auto pb-36">
-                <ApiPageMargins>
-                    <div className="mt-20 pb-2 text-4xl font-medium">{apiSection.title}</div>
-                </ApiPageMargins>
-                {apiSection.artifacts != null && areApiArtifactsNonEmpty(apiSection.artifacts) && (
-                    <ApiArtifacts apiArtifacts={apiSection.artifacts} />
-                )}
-                <ApiPackageContents package={apiDefinition.rootPackage} slug={apiSlug} />
-                <ApiPageMargins>
-                    <BottomNavigationButtons />
-                </ApiPageMargins>
-            </div>
-        </ApiPageContextProvider>
+        <div className="min-h-0 overflow-y-auto pb-36">
+            <ApiPageMargins>
+                <div className="mt-20 pb-2 text-4xl font-medium">{apiSection.title}</div>
+            </ApiPageMargins>
+            {apiSection.artifacts != null && areApiArtifactsNonEmpty(apiSection.artifacts) && (
+                <ApiArtifacts apiArtifacts={apiSection.artifacts} />
+            )}
+            <ApiPackageContents package={apiDefinition.rootPackage} slug={apiSlug} />
+            <ApiPageMargins>
+                <BottomNavigationButtons />
+            </ApiPageMargins>
+        </div>
     );
 };
