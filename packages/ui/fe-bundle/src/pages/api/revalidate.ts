4a966c2500498c2d310aed6cec31ad4357035bf9
Zach Kirsch <zachkirsch@gmail.com>
2023-07-12 14:25:56 -0400

    Add trailing slash to revalidate

--- packages/ui/fe-bundle/src/pages/api/revalidate.ts
+++ packages/ui/fe-bundle/src/pages/api/revalidate.ts
@@ -4,7 +4,7 @@ const handler: NextApiHandler = async (_req, res) => {
     try {
         // this should be the actual path not a rewritten path
         // e.g. for "/blog/[slug]" this should be "/blog/post-1"
-        await res.revalidate(`/${encodeURIComponent("devtest.buildwithfern.com")}`);
+        await res.revalidate("/devtest.buildwithfern.com/");
         return res.json({ revalidated: true });
     } catch (err) {
         // If there was an error, Next.js will continue
