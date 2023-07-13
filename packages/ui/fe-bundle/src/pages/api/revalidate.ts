b1c0f26239c15b2eb261cc0199d9f1929462337b
Zach Kirsch <zachkirsch@gmail.com>
2023-07-13 00:14:34 -0400

    Remove obsolete comments

--- packages/ui/fe-bundle/src/pages/api/revalidate.ts
+++ packages/ui/fe-bundle/src/pages/api/revalidate.ts
@@ -42,15 +42,11 @@ const handler: NextApiHandler = async (req, res) => {
 
 async function tryRevalidate(res: NextApiResponse, path: string): Promise<boolean> {
     try {
-        // this should be the actual path not a rewritten path
-        // e.g. for "/blog/[slug]" this should be "/blog/post-1"
         await res.revalidate(path);
         return true;
     } catch (err) {
         // eslint-disable-next-line no-console
         console.error(err);
-        // If there was an error, Next.js will continue
-        // to show the last successfully generated page
         return false;
     }
 }
