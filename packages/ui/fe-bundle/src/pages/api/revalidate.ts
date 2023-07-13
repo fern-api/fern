e87b35ee90fb946bf997dbbc56d7f26e4b8dab95
Zach Kirsch <zachkirsch@gmail.com>
2023-07-13 14:17:09 -0400

    Add url to request

--- packages/ui/fe-bundle/src/pages/api/revalidate.ts
+++ packages/ui/fe-bundle/src/pages/api/revalidate.ts
@@ -2,32 +2,39 @@ import { NextApiHandler, NextApiResponse } from "next";
 import { REGISTRY_SERVICE } from "../../service";
 import { UrlSlugTree } from "../../url-path-resolver/UrlSlugTree";
 
-// TODO support URLs that have a path component, e.g. buildwithfern.com/docs
+export interface Request {
+    url: string;
+}
+
 const handler: NextApiHandler = async (req, res) => {
+    const url = req.body.url;
+
+    if (url == null) {
+        return res.status(400).send("Property 'url' is missing from request.");
+    }
+    if (typeof url !== "string") {
+        return res.status(400).send("Property 'url' is not a string.");
+    }
+
     // when we call res.revalidate() nextjs uses
     // req.headers.host to make the network request
     if (typeof req.headers["x-fern-host"] === "string") {
         req.headers.host = req.headers["x-fern-host"];
     }
-    const host = req.headers.host;
-    if (host == null) {
-        return res.status(400).send("No host header");
-    }
-
-    const revalidated: string[] = [];
-    const failures: string[] = [];
 
     const docs = await REGISTRY_SERVICE.docs.v2.read.getDocsForUrl({
-        url: host,
+        url,
     });
     if (!docs.ok) {
         // eslint-disable-next-line no-console
         console.error("Failed to fetch docs", docs.error);
-        return res.status(500).send("Faild to load docs for: " + host);
+        return res.status(500).send("Failed to load docs for: " + url);
     }
     const urlSlugTree = new UrlSlugTree(docs.body.definition);
     const paths = ["/", ...urlSlugTree.getAllSlugs().map((slug) => `/${slug}`)];
 
+    const revalidated: string[] = [];
+    const failures: string[] = [];
     await Promise.all(
         paths.map(async (path) => {
             const didSucceed = await tryRevalidate(res, path);
