e9a9ab0fbe7d573cb9191ae38219bad78918ab87
Anar Kafkas <anarkafkas@gmail.com>
2023-07-17 19:53:46 +0300

    Implement a new 'fdr' cli command

---
+++ packages/cli/cli/src/commands/generate-fdr/generateFdrForWorkspaces.ts
@@ -0,0 +1,48 @@
+import { Audiences } from "@fern-api/config-management-commons";
+import { AbsoluteFilePath, stringifyLargeObject } from "@fern-api/fs-utils";
+import { GenerationLanguage } from "@fern-api/generators-configuration";
+import { Project } from "@fern-api/project-loader";
+import { convertIrToFdrApi } from "@fern-api/register";
+import { convertOpenApiWorkspaceToFernWorkspace } from "@fern-api/workspace-loader";
+import { writeFile } from "fs/promises";
+import path from "path";
+import { CliContext } from "../../cli-context/CliContext";
+import { generateIrForFernWorkspace } from "../generate-ir/generateIrForFernWorkspace";
+
+export async function generateFdrForWorkspaces({
+    project,
+    outputFilepath,
+    cliContext,
+    generationLanguage,
+    audiences,
+}: {
+    project: Project;
+    outputFilepath: AbsoluteFilePath;
+    cliContext: CliContext;
+    generationLanguage: GenerationLanguage | undefined;
+    audiences: Audiences;
+}): Promise<void> {
+    await Promise.all(
+        project.workspaces.map(async (workspace) => {
+            await cliContext.runTaskForWorkspace(workspace, async (context) => {
+                const fernWorkspace =
+                    workspace.type === "openapi"
+                        ? await convertOpenApiWorkspaceToFernWorkspace(workspace, context)
+                        : workspace;
+
+                const intermediateRepresentation = await generateIrForFernWorkspace({
+                    workspace: fernWorkspace,
+                    context,
+                    generationLanguage,
+                    audiences,
+                });
+
+                const apiDefinition = convertIrToFdrApi(intermediateRepresentation);
+
+                const resolvedOutputFilePath = path.resolve(outputFilepath);
+                await writeFile(resolvedOutputFilePath, await stringifyLargeObject(apiDefinition, { pretty: true }));
+                context.logger.info(`Wrote IR to ${resolvedOutputFilePath}`);
+            });
+        })
+    );
+}
