0242af0b54b84b3934964c22c962722041c5d479
codebender828 <codebender828@gmail.com>
2023-07-20 13:23:37 +0800

    chore: apply review feedback

--- packages/ui/fe-bundle/src/utils/theme/loadDocsTypography.ts
+++ packages/ui/fe-bundle/src/utils/theme/loadDocsTypography.ts
@@ -1,9 +1,9 @@
 import type * as FernRegistryDocsRead from "@fern-fern/registry-browser/api/resources/docs/resources/v1/resources/read";
 
-export function getFontExtension(url: URL): string {
+function getFontExtension(url: URL): string {
     const ext = url.pathname.split(".").pop();
     if (ext == null) {
-        throw new Error("No extension found");
+        throw new Error("No extension found for font: " + url.pathname);
     }
     return ext;
 }
@@ -14,7 +14,13 @@ export interface GenerationFontConfig {
     fontType: FontType;
     fontName: string;
     fontExtension: string;
-    fontUrl: FernRegistryDocsRead.Url | undefined;
+    fontUrl: FernRegistryDocsRead.Url;
+}
+
+export interface GenerationFontConfigs {
+    headingsFont: GenerationFontConfig | undefined;
+    bodyFont: GenerationFontConfig | undefined;
+    codeFont: GenerationFontConfig | undefined;
 }
 
 export function getFontConfig(
@@ -27,7 +33,10 @@ export function getFontConfig(
     }
     const fontName = typographyFontType.name;
     const fontUrl = docsDefinition.files[typographyFontType.fontFile];
-    const fontExtension = getFontExtension(new URL(fontUrl ?? ""));
+    if (fontUrl == null) {
+        throw new Error(`Could not resolve font file for type ${fontType}.`);
+    }
+    const fontExtension = getFontExtension(new URL(fontUrl));
 
     const fontConfig: GenerationFontConfig = {
         fontType,
@@ -39,102 +48,104 @@ export function getFontConfig(
     return fontConfig;
 }
 
-/**
- * Load Documentation Typography configuration
- * @param docsDefinition
- * @returns
- */
-export function loadDocTypography(docsDefinition: FernRegistryDocsRead.DocsDefinition): GenerationFontConfig[] {
-    const generationConfiguration: GenerationFontConfig[] = [];
+export function loadDocTypography(docsDefinition: FernRegistryDocsRead.DocsDefinition): GenerationFontConfigs {
+    const generationConfiguration: Record<keyof GenerationFontConfigs, GenerationFontConfig | undefined> = {
+        headingsFont: undefined,
+        bodyFont: undefined,
+        codeFont: undefined,
+    };
 
     const headingsFontConfig = getFontConfig(docsDefinition, "headingsFont");
     if (headingsFontConfig != null) {
-        generationConfiguration.push(headingsFontConfig);
+        generationConfiguration.headingsFont = headingsFontConfig;
     }
 
     const bodyFontConfig = getFontConfig(docsDefinition, "bodyFont");
     if (bodyFontConfig != null) {
-        generationConfiguration.push(bodyFontConfig);
+        generationConfiguration.bodyFont = bodyFontConfig;
     }
 
     const codeFontConfig = getFontConfig(docsDefinition, "codeFont");
     if (codeFontConfig != null) {
-        generationConfiguration.push(codeFontConfig);
+        generationConfiguration.codeFont = codeFontConfig;
     }
 
     return generationConfiguration;
 }
 
-export function generateFontFaces(generationConfiguration: GenerationFontConfig[]): string {
+export function generateFontFaces(generationConfiguration: GenerationFontConfigs): string {
     const fontFaces: string[] = [];
 
-    for (const fontConfig of generationConfiguration) {
-        if (fontConfig.fontType === "headingsFont") {
-            const fontFace = `
-            @font-face {
-                font-family: '${fontConfig.fontName}';
-                src: url('${fontConfig.fontUrl?.toString()}') format('${fontConfig.fontExtension}');
-                font-weight: 700;
-                font-style: normal;
+    for (const fontType in generationConfiguration) {
+        const fontConfig = generationConfiguration[fontType as keyof GenerationFontConfigs];
+        if (fontConfig != null) {
+            if (fontConfig.fontType === "headingsFont") {
+                const fontFace = `
+                @font-face {
+                    font-family: '${fontConfig.fontName}';
+                    src: url('${fontConfig.fontUrl.toString()}') format('${fontConfig.fontExtension}');
+                    font-weight: 700;
+                    font-style: normal;
+                }
+    
+                h1, h2, h3, h4, h5, h6 {
+                    font-family: '${fontConfig.fontName}', sans-serif;
+                }
+    
+                :root {
+                  --typography-heading-font-family: '${fontConfig.fontName}', sans-serif;
+                }
+    
+                .typography-font-heading {
+                  font-family: var(--typography-heading-font-family);
+                }
+              `;
+                fontFaces.push(fontFace);
             }
 
-            h1, h2, h3, h4, h5, h6 {
-                font-family: '${fontConfig.fontName}', sans-serif;
+            if (fontConfig.fontType === "bodyFont") {
+                const fontFace = `
+                @font-face {
+                    font-family: '${fontConfig.fontName}';
+                    src: url('${fontConfig.fontUrl.toString()}') format('${fontConfig.fontExtension}');
+                    font-weight: 400;
+                    font-style: normal;
+                }
+    
+                :root {
+                  --typography-body-font-family: '${fontConfig.fontName}', sans-serif;
+                }
+    
+                .typography-font-body {
+                  font-family: var(--typography-body-font-family);
+                }
+              `;
+                fontFaces.push(fontFace);
             }
 
-            :root {
-              --typography-heading-font-family: '${fontConfig.fontName}', sans-serif;
+            if (fontConfig.fontType === "codeFont") {
+                const fontFace = `
+                @font-face {
+                    font-family: '${fontConfig.fontName}';
+                    src: url('${fontConfig.fontUrl.toString()}') format('${fontConfig.fontExtension}');
+                    font-weight: 400;
+                    font-style: normal;
+                }
+    
+                code, pre {
+                    font-family: '${fontConfig.fontName}', monospace;
+                }
+    
+                :root {
+                  --typography-code-font-family: '${fontConfig.fontName}', sans-serif;
+                }
+    
+                .typography-font-code {
+                  font-family: var(--typography-code-font-family), Monospace;
+                }
+              `;
+                fontFaces.push(fontFace);
             }
-
-            .typography-font-heading {
-              font-family: var(--typography-heading-font-family);
-            }
-          `;
-            fontFaces.push(fontFace);
-        }
-
-        if (fontConfig.fontType === "bodyFont") {
-            const fontFace = `
-            @font-face {
-                font-family: '${fontConfig.fontName}';
-                src: url('${fontConfig.fontUrl?.toString()}') format('${fontConfig.fontExtension}');
-                font-weight: 400;
-                font-style: normal;
-            }
-
-            :root {
-              --typography-body-font-family: '${fontConfig.fontName}', sans-serif;
-            }
-
-            .typography-font-body {
-              font-family: var(--typography-body-font-family);
-            }
-          `;
-            fontFaces.push(fontFace);
-        }
-
-        if (fontConfig.fontType === "codeFont") {
-            const fontFace = `
-            @font-face {
-                font-family: '${fontConfig.fontName}';
-                src: url('${fontConfig.fontUrl?.toString()}') format('${fontConfig.fontExtension}');
-                font-weight: 400;
-                font-style: normal;
-            }
-
-            code, pre {
-                font-family: '${fontConfig.fontName}', monospace;
-            }
-
-            :root {
-              --typography-code-font-family: '${fontConfig.fontName}', sans-serif;
-            }
-
-            .typography-font-code {
-              font-family: var(--typography-code-font-family), Monospace;
-            }
-          `;
-            fontFaces.push(fontFace);
         }
     }
 
