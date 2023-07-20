425e56ef7f9ebc634211d10ffe59b85f9e0c6225
Deep Singhvi <deep@usebirch.com>
2023-07-20 21:57:45 +0300

    feature: introduce `pull-request` mode in github integration (#1929)
    * introduce github mode pull-request
    
    * upgrade sdk

--- packages/cli/config-management/generators-configuration/src/schemas/GithubConfigurationSchema.ts
+++ packages/cli/config-management/generators-configuration/src/schemas/GithubConfigurationSchema.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const GithubConfigurationSchema = z.strictObject({
     repository: z.string(),
+    mode: z.optional(z.enum(["pull-request", "commit"])),
 });
 
 export type GithubConfigurationSchema = z.infer<typeof GithubConfigurationSchema>;
