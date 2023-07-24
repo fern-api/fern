33044c3f04397097b7d0a720ce38bbe6eb90e5ff
Zach Kirsch <zachkirsch@gmail.com>
2023-07-24 16:39:39 -0700

    Validate that paths exist in docs.yml

--- packages/cli/ete-tests/src/tests/generate/generate.test.ts
+++ packages/cli/ete-tests/src/tests/generate/generate.test.ts
@@ -1,7 +1,6 @@
 /* eslint-disable jest/expect-expect */
 import { AbsoluteFilePath, doesPathExist, join, RelativeFilePath } from "@fern-api/fs-utils";
-import { FERNIGNORE_FILENAME } from "@fern-api/project-configuration";
-import { writeFile } from "fs/promises";
+import stripAnsi from "strip-ansi";
 import { runFernCli } from "../../utils/runFernCli";
 import { init } from "../init/init";
 
@@ -32,53 +31,17 @@ describe("fern generate", () => {
             180_000
         );
     }
-});
 
-const FERNIGNORE_FILECONTENTS = `
-fern.js
-**/*.txt
-`;
-
-const FERN_JS_FILENAME = "fern.js";
-const FERN_JS_FILECONTENTS = `
-#!/usr/bin/env node
-console.log('Water the plants')
-`;
-
-const DUMMY_TXT_FILENAME = "dummy.txt";
-const DUMMY_TXT_FILECONTENTS = `
-Practice schema-first API design with Fern
-`;
-
-describe("fern generate --local", () => {
-    it("Keep files listed in .fernignore from unmodified", async () => {
-        const pathOfDirectory = await init();
-        await runFernCli(["generate", "--local", "--keepDocker"], {
-            cwd: pathOfDirectory,
+    it("missing docs page", async () => {
+        const { stdout } = await runFernCli(["generate"], {
+            cwd: join(fixturesDir, RelativeFilePath.of("docs-missing-page")),
+            reject: false,
         });
 
-        // write custom files and override
-        const absolutePathToLocalOutput = join(pathOfDirectory, RelativeFilePath.of("generated/typescript"));
-
-        const absolutePathToFernignore = join(absolutePathToLocalOutput, RelativeFilePath.of(FERNIGNORE_FILENAME));
-        await writeFile(absolutePathToFernignore, FERNIGNORE_FILECONTENTS);
-
-        const absolutePathToFernJs = join(absolutePathToLocalOutput, RelativeFilePath.of(FERN_JS_FILENAME));
-        await writeFile(absolutePathToFernJs, FERN_JS_FILECONTENTS);
-
-        const absolutePathToDummyText = join(absolutePathToLocalOutput, RelativeFilePath.of(DUMMY_TXT_FILENAME));
-        await writeFile(absolutePathToDummyText, DUMMY_TXT_FILECONTENTS);
-
-        await runFernCli(["generate", "--local", "--keepDocker"], {
-            cwd: pathOfDirectory,
-        });
-
-        await expectPathExists(absolutePathToFernignore);
-        await expectPathExists(absolutePathToFernJs);
-        await expectPathExists(absolutePathToDummyText);
-    }, 180_000);
+        expect(
+            stripAnsi(stdout)
+                // for some reason, locally the output contains a newline that Circle doesn't
+                .trim()
+        ).toMatchSnapshot();
+    });
 });
-
-async function expectPathExists(absoluteFilePath: AbsoluteFilePath): Promise<void> {
-    expect(await doesPathExist(absoluteFilePath)).toBe(true);
-}
