97d53dd066e58d99ca04043847c684722d89aff1
Anar Kafkas <anarkafkas@gmail.com>
2023-07-25 18:51:46 +0300

    Upgrade @fern-fern/registry-node in cli

--- .pnp.cjs
+++ .pnp.cjs
@@ -5892,7 +5892,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/project-configuration", "workspace:packages/cli/config-management/project-configuration"],\
             ["@fern-api/task-context", "workspace:packages/cli/task-context"],\
             ["@fern-fern/docs-config", "npm:0.0.21"],\
-            ["@fern-fern/registry-node", "npm:0.11.1-1-g35623b3"],\
+            ["@fern-fern/registry-node", "npm:0.11.1-2-gd8ee803"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/js-yaml", "npm:4.0.5"],\
             ["@types/node", "npm:18.7.18"],\
@@ -6564,7 +6564,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/task-context", "workspace:packages/cli/task-context"],\
             ["@fern-api/workspace-loader", "workspace:packages/cli/workspace-loader"],\
             ["@fern-fern/ir-model", "npm:0.0.2653"],\
-            ["@fern-fern/registry-node", "npm:0.11.1-1-g35623b3"],\
+            ["@fern-fern/registry-node", "npm:0.11.1-2-gd8ee803"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/lodash-es", "npm:4.17.7"],\
             ["@types/node", "npm:18.7.18"],\
@@ -6599,7 +6599,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/workspace-loader", "workspace:packages/cli/workspace-loader"],\
             ["@fern-fern/fiddle-sdk", "npm:0.0.285"],\
             ["@fern-fern/ir-model", "npm:0.0.2653"],\
-            ["@fern-fern/registry-node", "npm:0.11.1-1-g35623b3"],\
+            ["@fern-fern/registry-node", "npm:0.11.1-2-gd8ee803"],\
             ["@types/decompress", "npm:4.2.4"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/lodash-es", "npm:4.17.7"],\
@@ -6703,7 +6703,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/services", "workspace:packages/cli/services"],\
             ["@fern-api/venus-api-sdk", "npm:0.0.20-7-g6ea8dc4"],\
             ["@fern-fern/fiddle-sdk", "npm:0.0.285"],\
-            ["@fern-fern/registry-node", "npm:0.11.1-1-g35623b3"],\
+            ["@fern-fern/registry-node", "npm:0.11.1-2-gd8ee803"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/node", "npm:18.7.18"],\
             ["depcheck", "npm:1.4.3"],\
@@ -7309,10 +7309,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["@fern-fern/registry-node", [\
-        ["npm:0.11.1-1-g35623b3", {\
-          "packageLocation": "./.yarn/cache/@fern-fern-registry-node-npm-0.11.1-1-g35623b3-4bd86adf86-1c86576012.zip/node_modules/@fern-fern/registry-node/",\
+        ["npm:0.11.1-2-gd8ee803", {\
+          "packageLocation": "./.yarn/cache/@fern-fern-registry-node-npm-0.11.1-2-gd8ee803-78569d2a02-b773d67740.zip/node_modules/@fern-fern/registry-node/",\
           "packageDependencies": [\
-            ["@fern-fern/registry-node", "npm:0.11.1-1-g35623b3"],\
+            ["@fern-fern/registry-node", "npm:0.11.1-2-gd8ee803"],\
             ["@types/url-join", "npm:4.0.1"],\
             ["@ungap/url-search-params", "npm:0.2.2"],\
             ["axios", "npm:0.27.2"],\
