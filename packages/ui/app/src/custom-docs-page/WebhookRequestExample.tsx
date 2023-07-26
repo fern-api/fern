124884d5e78760f9dae0d8555e445687afe08525
Anar Kafkas <anarkafkas@gmail.com>
2023-07-26 17:26:39 +0300

    Implement WebhookRequestExample

--- packages/ui/app/src/custom-docs-page/WebhookRequestExample.tsx
+++ packages/ui/app/src/custom-docs-page/WebhookRequestExample.tsx
@@ -9,11 +9,11 @@ export declare namespace WebhookRequestExample {
 
 export const WebhookRequestExample: React.FC<WebhookRequestExample.Props> = ({ requestExampleJson }) => {
     return (
-        <div className="flex min-h-0 flex-1 flex-col">
+        <div className="min-w-md flex max-h-[80vh] min-h-0 flex-1 flex-col">
             <div className="flex min-h-0 flex-1 flex-col">
                 <div className="grid min-h-0 flex-1 grid-rows-[repeat(auto-fit,_minmax(0,_min-content))] flex-col gap-6">
                     <TitledExample
-                        title="Request Example"
+                        title="Payload Example"
                         type="primary"
                         onClick={(e) => {
                             e.stopPropagation();
