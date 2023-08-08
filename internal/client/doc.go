aeb9fdc1b363a2e60b84c4482423f1e6b3f73202
Alex McKinney <alexmckinney01@gmail.com>
2023-08-08 13:06:51 -0700

    Remove unused internal/client package

--- internal/client/doc.go
+++
@@ -1,7 +0,0 @@
-// Package client defines basic utilities required to issue
-// HTTP requests.
-//
-// Note this package is temporary and is meant for in-development
-// purposes only. It will eventually be generated alongside the rest
-// of the client code (i.e. in the form of a template).
-package client
