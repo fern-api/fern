dcfd1ea6cdbc106851685a989a40191e7ca7c380
Zach Kirsch <zachkirsch@users.noreply.github.com>
2023-08-12 13:14:57 -0400

    Use manual snapshot for v23->v22 migration (#2033)

--- packages/cli/generation/ir-migrations/src/migrations/v23-to-v22/__test__/migrateFromV23ToV22.test.ts
+++ packages/cli/generation/ir-migrations/src/migrations/v23-to-v22/__test__/migrateFromV23ToV22.test.ts
@@ -1,6 +1,7 @@
 import { AbsoluteFilePath, join, RelativeFilePath } from "@fern-api/fs-utils";
 import { createLogger } from "@fern-api/logger";
 import { createMockTaskContext } from "@fern-api/task-context";
+import { readFile } from "fs/promises";
 import { createMigrationTester } from "../../../__test__/utils/runFixtureThroughMigration";
 import { V23_TO_V22_MIGRATION } from "../migrateFromV23ToV22";
 
@@ -8,10 +9,12 @@ const runMigration = createMigrationTester(V23_TO_V22_MIGRATION);
 
 describe("migrateFromV23ToV22", () => {
     it("migrates extensive", async () => {
+        const pathToFixture = join(AbsoluteFilePath.of(__dirname), RelativeFilePath.of("./fixtures/exhaustive"));
         const migrated = await runMigration({
-            pathToFixture: join(AbsoluteFilePath.of(__dirname), RelativeFilePath.of("./fixtures/exhaustive")),
+            pathToFixture,
         });
-        expect(migrated).toMatchSnapshot();
+        const expected = JSON.parse((await readFile(join(pathToFixture, RelativeFilePath.of("ir.json")))).toString());
+        expect(migrated).toMatchObject(expected);
     });
 
     it("throws when definition contains bytes", async () => {
