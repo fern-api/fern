b0874e706b357049f969552b4b0e25ff68b5c6a6
Anar Kafkas <anarkafkas@gmail.com>
2023-08-15 00:47:19 +0800

    Upgrade docs-config package

--- .pnp.cjs
+++ .pnp.cjs
@@ -3171,7 +3171,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/fs-utils", "workspace:packages/commons/fs-utils"],\
             ["@fern-api/project-configuration", "workspace:packages/cli/config-management/project-configuration"],\
             ["@fern-api/task-context", "workspace:packages/cli/task-context"],\
-            ["@fern-fern/docs-config", "npm:0.0.37"],\
+            ["@fern-fern/docs-config", "npm:0.0.39"],\
             ["@fern-fern/registry-node", "npm:0.12.1-1-g4f47174"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/js-yaml", "npm:4.0.5"],\
@@ -3523,7 +3523,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/project-configuration", "workspace:packages/cli/config-management/project-configuration"],\
             ["@fern-api/task-context", "workspace:packages/cli/task-context"],\
             ["@fern-api/workspace-loader", "workspace:packages/cli/workspace-loader"],\
-            ["@fern-fern/docs-config", "npm:0.0.37"],\
+            ["@fern-fern/docs-config", "npm:0.0.39"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/js-yaml", "npm:4.0.5"],\
             ["@types/node", "npm:18.7.18"],\
@@ -4063,10 +4063,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["@fern-fern/docs-config", [\
-        ["npm:0.0.37", {\
-          "packageLocation": "./.yarn/cache/@fern-fern-docs-config-npm-0.0.37-1cf8ab14d5-a5fbd02ce9.zip/node_modules/@fern-fern/docs-config/",\
+        ["npm:0.0.39", {\
+          "packageLocation": "./.yarn/cache/@fern-fern-docs-config-npm-0.0.39-7fad949e8d-ed1c822397.zip/node_modules/@fern-fern/docs-config/",\
           "packageDependencies": [\
-            ["@fern-fern/docs-config", "npm:0.0.37"]\
+            ["@fern-fern/docs-config", "npm:0.0.39"]\
           ],\
           "linkType": "HARD"\
         }]\
