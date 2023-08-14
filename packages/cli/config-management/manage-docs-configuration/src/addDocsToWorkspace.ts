11f51e6acf80aea2d8df874aab2cd7142a11f2a9
Anar Kafkas <anarkafkas@gmail.com>
2023-08-15 01:07:50 +0800

    Update cli to allow light mode logo

--- packages/cli/config-management/manage-docs-configuration/src/addDocsToWorkspace.ts
+++ packages/cli/config-management/manage-docs-configuration/src/addDocsToWorkspace.ts
@@ -58,6 +58,8 @@ function getDocsConfig(organization: string): RawDocs.DocsConfiguration {
         colors: {
             accentPrimary: "#ffffff",
         },
-        logo: "./logo.png",
+        logo: {
+            dark: "./logo.png",
+        },
     };
 }
