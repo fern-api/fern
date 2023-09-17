522b3af208602850fb767cd8af8544bd4d356312
dsinghvi <deep@usebirch.com>
2023-09-17 17:14:19 -0400

    delete oai 3.1 migrator

--- packages/cli/openapi-migrator/src/v3/EndpointNamer.ts
+++
@@ -1,9 +0,0 @@
-export class EndpointNamer {
-    private count = 0;
-
-    public getName(): string {
-        const name = `_unnamedOperation${this.count}`;
-        this.count += 1;
-        return name;
-    }
-}
--- packages/cli/openapi-migrator/src/v3/InlinedTypeNamer.ts
+++
@@ -1,9 +0,0 @@
-export class InlinedTypeNamer {
-    private count = 0;
-
-    public getName(): string {
-        const name = `_InlinedType${this.count}`;
-        this.count += 1;
-        return name;
-    }
-}
