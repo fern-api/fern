58e9f6af207a4bc8533fb4f9df3bdd4fd0c202bf
Deep Singhvi <deep@usebirch.com>
2023-10-05 04:27:00 -0400

    fix: async api parser (#2155)
    fix async api parser

--- packages/cli/openapi-parser/src/asyncapi/generateSchemasFromAsyncAPI.ts
+++ packages/cli/openapi-parser/src/asyncapi/generateSchemasFromAsyncAPI.ts
@@ -1,28 +1,21 @@
 import { TaskContext } from "@fern-api/task-context";
 import { Schema, SchemaId } from "@fern-fern/openapi-ir-model/ir";
+import { OpenAPIV3 } from "openapi-types";
 import { AsyncAPI } from "../asyncapi";
-import { convertSchema } from "../v3/converters/convertSchemas";
-import { DummyOpenAPIV3ParserContext } from "../v3/DummyOpenAPIV3ParserContext";
+import { generateIr } from "../v3/generateIr";
 
 export function generateSchemasFromAsyncAPI(asyncApi: AsyncAPI, taskContext: TaskContext): Record<SchemaId, Schema> {
-    const context = new DummyOpenAPIV3ParserContext({
-        document: {
-            openapi: "3.0.0",
-            info: {
-                title: "",
-                version: "",
-            },
-            paths: {},
-            components: {
-                schemas: asyncApi.components?.schemas ?? {},
-            },
+    const openApiV3: OpenAPIV3.Document = {
+        openapi: "3.0.0",
+        info: {
+            title: "",
+            version: "",
         },
-        taskContext,
-    });
-    return Object.fromEntries(
-        Object.entries(asyncApi.components?.schemas ?? {}).map(([key, schema]) => {
-            taskContext.logger.debug(`Converting schema ${key}`);
-            return [key, convertSchema(schema, false, context, [key])];
-        })
-    );
+        paths: {},
+        components: {
+            schemas: asyncApi.components?.schemas ?? {},
+        },
+    };
+    const openApiIr = generateIr(openApiV3, taskContext);
+    return openApiIr.schemas;
 }
