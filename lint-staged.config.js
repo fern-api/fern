1da7d530958e88096c45b5e3ee251439e38cc47c
dsinghvi <deep@buildwithfern.com>
2023-10-15 16:51:16 -0400

    fix husky commands

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    "**/*.ts{,x}": ["eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js", "yarn format"],
+    "**/*.ts{,x}": [
+        "eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js",
+        "yarn format:fix",
+        "yarn orgznize-imports",
+    ],
     "**/*.{js,json,yml,html,css,less,scss,md}": "yarn format",
     "**": () => "yarn lint:monorepo",
     "**/package.json": () => "yarn install --immutable",
