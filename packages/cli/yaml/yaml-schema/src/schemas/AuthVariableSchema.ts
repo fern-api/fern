6c53ca39dee7022e3d35a1e9fab345e710bb175b
dsinghvi <deep@buildwithfern.com>
2023-10-15 02:16:24 -0400

    fix compile

--- packages/cli/yaml/yaml-schema/src/schemas/AuthVariableSchema.ts
+++ packages/cli/yaml/yaml-schema/src/schemas/AuthVariableSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 import { WithNameSchema } from "./WithNameSchema";
 
 export const AuthVariableSchema = WithNameSchema.extend({
-    "sdk-environment-variable": z.string(),
+    env: z.optional(z.string()),
 });
 
 export type AuthVariableSchema = z.infer<typeof AuthVariableSchema>;
