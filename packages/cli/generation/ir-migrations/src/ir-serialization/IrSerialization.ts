a11d2f1e3bb7a4c853de37bd30403c64bc7f5954
Alex McKinney <alexmckinney01@gmail.com>
2023-10-17 17:47:45 -0700

    Update migrator flow

--- packages/cli/generation/ir-migrations/src/ir-serialization/IrSerialization.ts
+++ packages/cli/generation/ir-migrations/src/ir-serialization/IrSerialization.ts
@@ -1,3 +1,5 @@
-export * as V25 from "@fern-fern/ir-sdk/serialization";
+export * as V27 from "@fern-fern/ir-sdk/serialization";
 export * as V23 from "@fern-fern/ir-v23-sdk/serialization";
 export * as V24 from "@fern-fern/ir-v24-sdk/serialization";
+export * as V25 from "@fern-fern/ir-v25-sdk/serialization";
+export * as V26 from "@fern-fern/ir-v26-sdk/serialization";
