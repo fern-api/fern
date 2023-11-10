dc2490315a349b4161cc1035a91d6ec095fcb276
Deep Singhvi <deep@usebirch.com>
2023-11-10 11:56:58 -0500

    fix: update IRV28 import in `IrSerialization` and add more docs about IntermediateRepresentation (#2263)
    * fix IRSerializations and update ir docs
    
    * typo
    
    * proofread

--- packages/cli/generation/ir-migrations/src/ir-serialization/IrSerialization.ts
+++ packages/cli/generation/ir-migrations/src/ir-serialization/IrSerialization.ts
@@ -1,6 +1,7 @@
-export * as V28 from "@fern-fern/ir-sdk/serialization";
+export * as V29 from "@fern-fern/ir-sdk/serialization";
 export * as V23 from "@fern-fern/ir-v23-sdk/serialization";
 export * as V24 from "@fern-fern/ir-v24-sdk/serialization";
 export * as V25 from "@fern-fern/ir-v25-sdk/serialization";
 export * as V26 from "@fern-fern/ir-v26-sdk/serialization";
 export * as V27 from "@fern-fern/ir-v27-sdk/serialization";
+export * as V28 from "@fern-fern/ir-v28-sdk/serialization";
