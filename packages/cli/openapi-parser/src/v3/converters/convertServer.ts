c579efe8ef49b98602444df7c5224f2b1ca08b58
Deep Singhvi <deep@usebirch.com>
2023-11-14 17:26:50 -0500

    fix: OpenAPI importer handles parsing server variables (#2275)
    openapi importer handles parsing server variables

--- packages/cli/openapi-parser/src/v3/converters/convertServer.ts
+++ packages/cli/openapi-parser/src/v3/converters/convertServer.ts
@@ -4,30 +4,49 @@ import { FernOpenAPIExtension } from "../extensions/fernExtensions";
 import { getExtension } from "../extensions/getExtension";
 
 export function convertServer(server: OpenAPIV3.ServerObject): Server {
+    return {
+        url: getServerUrl({ url: server.url, variables: server.variables ?? {} }),
+        description: server.description,
+        name: getServerName(server),
+    };
+}
+
+function getServerUrl({
+    url,
+    variables,
+}: {
+    url: string;
+    variables: Record<string, OpenAPIV3.ServerVariableObject>;
+}): string {
+    const valuesToSubstitute = Object.fromEntries(
+        Object.entries(variables).map(([name, declaration]) => {
+            return [name, declaration.default];
+        })
+    );
+
+    for (const [variable, value] of Object.entries(valuesToSubstitute)) {
+        url = url.replace(`{${variable}}`, value);
+    }
+
+    return url;
+}
+
+function getServerName(server: OpenAPIV3.ServerObject): string | undefined {
     const name = getExtension<string>(server, [
         FernOpenAPIExtension.SERVER_NAME_V1,
         FernOpenAPIExtension.SERVER_NAME_V2,
     ]);
 
-    if (name == null) {
-        if (server.description?.toLowerCase() === "production") {
-            return {
-                url: server.url,
-                description: undefined,
-                name: "Production",
-            };
-        }
-        if (server.description?.toLowerCase() === "sandbox") {
-            return {
-                url: server.url,
-                description: undefined,
-                name: "Sandbox",
-            };
-        }
+    if (name != null) {
+        return name;
     }
-    return {
-        url: server.url,
-        description: server.description,
-        name,
-    };
+
+    if (server.description?.toLowerCase() === "production") {
+        return "Production";
+    }
+    if (server.description?.toLowerCase() === "sandbox") {
+        return "Sandbox";
+    }
+
+    return undefined;
 }
