1f992f44c0db6020c1422d90c0768761928b2aa9
dsinghvi <dsinghvi@umich.edu>
2023-11-24 09:10:17 -0500

    try again

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     "**/*.ts{,x}": [
         "eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js",
-        "yarn organize-imports",
         "yarn format:fix",
     ],
     "**/*.{js,json,yml,html,css,less,scss,md}": "yarn format",
