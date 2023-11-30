d6fdacaffc85f8b7934ef0cf92bb0eb337776559
Deep Singhvi <deep@usebirch.com>
2023-11-30 10:51:10 -0500

    fix: see docker logs when running `fern generate --local` (#2400)
    * if docker exits non zero then we write logs to tmp file
    
    * fix compile

--- .pnp.cjs
+++ .pnp.cjs
@@ -3801,6 +3801,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jest", "virtual:f8e21c9fa0a798b21a87ac455600e39198fef65848556a3a2cd5c956b0460181f9e618173dcff6d9d4fb96fb870055dfe1a3ec3db5db0700e71c0184e4711659#npm:29.7.0"],\
             ["organize-imports-cli", "npm:0.10.0"],\
             ["prettier", "npm:2.7.1"],\
+            ["tmp-promise", "npm:3.0.3"],\
             ["typescript", "patch:typescript@npm%3A4.6.4#~builtin<compat/typescript>::version=4.6.4&hash=5d3a66"]\
           ],\
           "linkType": "SOFT"\
