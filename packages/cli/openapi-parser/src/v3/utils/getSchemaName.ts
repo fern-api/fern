a37a8be0976186c5e44c66bd084503856e70c90e
Deep Singhvi <deep@usebirch.com>
2023-11-30 00:35:40 -0500

    fix: OpenAPI importer generates non-conflicting names for multipart file upload endpoints (#2399)
    set request breadcrumbs properly in file upload requests

--- packages/cli/openapi-parser/src/v3/utils/getSchemaName.ts
+++ packages/cli/openapi-parser/src/v3/utils/getSchemaName.ts
@@ -1,17 +1,11 @@
 import { camelCase, upperFirst } from "lodash-es";
 
 export function getGeneratedTypeName(breadcrumbs: string[]): string {
-    // if (breadcrumbs.length === 0) {
-    //     throw new Error("Cannot generate name from breadcrumbs");
-    // }
     const underscoreDelimeted = breadcrumbs.join("_");
     return upperFirst(camelCase(underscoreDelimeted));
 }
 
 export function getGeneratedPropertyName(breadcrumbs: string[]): string {
-    // if (breadcrumbs.length === 0) {
-    //     throw new Error("Cannot generate name from breadcrumbs");
-    // }
     const underscoreDelimeted = breadcrumbs.join("_");
     return camelCase(underscoreDelimeted);
 }
