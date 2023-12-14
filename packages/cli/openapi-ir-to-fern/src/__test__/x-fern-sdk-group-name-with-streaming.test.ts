868e19cac4038c114e08f15f6521babcd9a69201
Alex McKinney <alexmckinney01@gmail.com>
2023-12-13 20:37:44 -0800

    Add x-fern-sdk-group-name + x-fern-streaming test

---
+++ packages/cli/openapi-ir-to-fern/src/__test__/x-fern-sdk-group-name-with-streaming.test.ts
@@ -0,0 +1,5 @@
+import { testConvertOpenAPI } from "./testConvertOpenApi";
+
+describe("x-fern-sdk-group-name-with-streaming", () => {
+    testConvertOpenAPI("x-fern-sdk-group-name-with-streaming", "openapi.yml");
+});
