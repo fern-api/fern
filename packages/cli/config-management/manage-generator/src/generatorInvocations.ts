d94f0a77070bf2f6aae1a66a5580401506dcee64
Danny Sheridan <danny@buildwithfern.com>
2024-01-12 13:26:31 -0600

    improvement: upgrade generator invocations

--- packages/cli/config-management/manage-generator/src/generatorInvocations.ts
+++ packages/cli/config-management/manage-generator/src/generatorInvocations.ts
@@ -5,7 +5,7 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
         version: "0.0.122"
     },
     [GeneratorName.TYPESCRIPT_EXPRESS]: {
-        version: "0.7.2",
+        version: "0.9.0",
         output: {
             location: "local-file-system",
             path: "../src/fern"
@@ -34,42 +34,54 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
         version: "0.7.2"
     },
     [GeneratorName.TYPESCRIPT_NODE_SDK]: {
-        version: "0.7.2"
+        version: "0.9.0",
+        output: {
+            location: "local-file-system",
+            path: "../generated/sdk/node"
+        }
     },
     [GeneratorName.TYPESCRIPT_BROWSER_SDK]: {
-        version: "0.7.2"
+        version: "0.9.0",
+        output: {
+            location: "local-file-system",
+            path: "../generated/sdk/browser"
+        }
     },
     [GeneratorName.PYTHON_PYDANTIC]: {
-        version: "0.6.0"
+        version: "0.7.6"
     },
     [GeneratorName.PYTHON_FASTAPI]: {
-        version: "0.6.0",
+        version: "0.7.6",
         output: {
             location: "local-file-system",
             path: "../src/fern"
         }
     },
     [GeneratorName.PYTHON_SDK]: {
-        version: "0.6.0"
-    },
-    [GeneratorName.GO_FIBER]: {
-        version: "0.9.0"
-    },
-    [GeneratorName.GO_MODEL]: {
-        version: "0.9.0"
-    },
-    [GeneratorName.GO_SDK]: {
-        version: "0.9.0",
+        version: "0.7.6",
         output: {
             location: "local-file-system",
-            path: "../generated/go"
+            path: "../generated/sdk/python"
+        }
+    },
+    [GeneratorName.GO_FIBER]: {
+        version: "0.9.4"
+    },
+    [GeneratorName.GO_MODEL]: {
+        version: "0.9.4"
+    },
+    [GeneratorName.GO_SDK]: {
+        version: "0.9.4",
+        output: {
+            location: "local-file-system",
+            path: "../generated/sdk/go"
         },
         config: {
             importPath: "go-mod-path/generated/go"
         }
     },
     [GeneratorName.OPENAPI]: {
-        version: "0.0.28",
+        version: "0.0.30-rc0",
         config: {
             format: "yaml"
         },
@@ -85,7 +97,7 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
         }
     },
     [GeneratorName.POSTMAN]: {
-        version: "0.0.45",
+        version: "0.0.46",
         output: {
             location: "local-file-system",
             path: "../generated/postman"
@@ -93,7 +105,7 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
     },
     [GeneratorName.OPENAPI_PYTHON_CLIENT]: {
         version: "0.0.11",
-        output: {
+        output: {   
             location: "local-file-system",
             path: "../generated/python"
         },
