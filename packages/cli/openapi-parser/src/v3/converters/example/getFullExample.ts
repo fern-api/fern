ee9f94694a1e5d79485f3e4f0fb87fe26bd9f009
dsinghvi <dsinghvi@umich.edu>
2024-01-15 16:18:20 -0500

    rerun ci

--- packages/cli/openapi-parser/src/v3/converters/example/getFullExample.ts
+++ packages/cli/openapi-parser/src/v3/converters/example/getFullExample.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getFullExampleAsObject(value: unknown): Record<string, any> | undefined {
     if (value != null && typeof value === "object" && Object.keys(value).every((key) => typeof key === "string")) {
         return value;
