85743cbb50a5a0c3308d3292b40a909ab35674bc
Alex McKinney <alexmckinney01@gmail.com>
2024-01-19 14:48:30 -0800

    Go generators use IRv32 (#2672)

--- packages/cli/generation/ir-migrations/src/migrations/v32-to-v31/migrateFromV32ToV31.ts
+++ packages/cli/generation/ir-migrations/src/migrations/v32-to-v31/migrateFromV32ToV31.ts
@@ -27,9 +27,9 @@ export const V32_TO_V31_MIGRATION: IrMigration<
         [GeneratorName.STOPLIGHT]: GeneratorWasNeverUpdatedToConsumeNewIR,
         [GeneratorName.POSTMAN]: GeneratorWasNeverUpdatedToConsumeNewIR,
         [GeneratorName.PYTHON_SDK]: GeneratorWasNeverUpdatedToConsumeNewIR,
-        [GeneratorName.GO_FIBER]: GeneratorWasNeverUpdatedToConsumeNewIR,
-        [GeneratorName.GO_MODEL]: GeneratorWasNeverUpdatedToConsumeNewIR,
-        [GeneratorName.GO_SDK]: GeneratorWasNeverUpdatedToConsumeNewIR,
+        [GeneratorName.GO_FIBER]: "0.9.4-2-g139cf36",
+        [GeneratorName.GO_MODEL]: "0.9.4-2-g139cf36",
+        [GeneratorName.GO_SDK]: "0.9.4-2-g139cf36",
         [GeneratorName.RUBY_MODEL]: "0.0.0",
         [GeneratorName.RUBY_SDK]: "0.0.0"
     },
