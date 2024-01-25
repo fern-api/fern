cf9351da20c7289ab5bda115fb4b6ddf733d62bc
dsinghvi <dsinghvi@umich.edu>
2024-01-25 09:21:51 -0500

    fix: trace seed test finishes

--- seed/sdk/audiences/src/seed/resources/foo/types/filtered_type.py
+++ seed/sdk/audiences/src/seed/resources/foo/types/filtered_type.py
@@ -3,7 +3,7 @@
 import datetime as dt
 import typing
 
-from ...core.datetime_utils import serialize_datetime
+from ....core.datetime_utils import serialize_datetime
 
 try:
     import pydantic.v1 as pydantic  # type: ignore
@@ -24,5 +24,6 @@ class FilteredType(pydantic.BaseModel):
         return super().dict(**kwargs_with_defaults)
 
     class Config:
-        extra = pydantic.Extra.forbid
+        frozen = True
+        smart_union = True
         json_encoders = {dt.datetime: serialize_datetime}
