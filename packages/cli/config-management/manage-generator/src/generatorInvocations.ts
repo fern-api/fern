79ebe8db1df775530f7a67c1e1fa59fb5c63a647
Danny Sheridan <danny@buildwithfern.com>
2024-01-27 22:18:14 -0600

    Improvement: upgrade generateInvocations

--- packages/cli/config-management/manage-generator/src/generatorInvocations.ts
+++ packages/cli/config-management/manage-generator/src/generatorInvocations.ts
@@ -5,20 +5,20 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
         version: "0.0.122"
     },
     [GeneratorName.TYPESCRIPT_EXPRESS]: {
-        version: "0.7.2",
+        version: "0.9.4",
         output: {
             location: "local-file-system",
             path: "../src/fern"
         }
     },
     [GeneratorName.JAVA_MODEL]: {
-        version: "0.5.7"
+        version: "0.6.1"
     },
     [GeneratorName.JAVA_SDK]: {
-        version: "0.5.7"
+        version: "0.6.1"
     },
     [GeneratorName.JAVA_SPRING]: {
-        version: "0.5.7",
+        version: "0.6.1",
         output: {
             location: "local-file-system",
             path: "../src/main/java/com/fern"
@@ -34,16 +34,16 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
         version: "0.7.2"
     },
     [GeneratorName.TYPESCRIPT_NODE_SDK]: {
-        version: "0.7.2"
+        version: "0.9.4"
     },
     [GeneratorName.TYPESCRIPT_BROWSER_SDK]: {
-        version: "0.7.2"
+        version: "0.9.4"
     },
     [GeneratorName.PYTHON_PYDANTIC]: {
-        version: "0.6.0"
+        version: "0.7.7"
     },
     [GeneratorName.PYTHON_FASTAPI]: {
-        version: "0.6.0",
+        version: "0.7.7",
         output: {
             location: "local-file-system",
             path: "../src/fern"
@@ -57,13 +57,13 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
         }
     },
     [GeneratorName.GO_FIBER]: {
-        version: "0.9.0"
+        version: "0.10.0"
     },
     [GeneratorName.GO_MODEL]: {
-        version: "0.9.0"
+        version: "0.10.0"
     },
     [GeneratorName.GO_SDK]: {
-        version: "0.9.0",
+        version: "0.10.0",
         output: {
             location: "local-file-system",
             path: "../generated/go"
@@ -83,7 +83,7 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
         }
     },
     [GeneratorName.OPENAPI]: {
-        version: "0.0.28",
+        version: "0.0.30",
         config: {
             format: "yaml"
         },
@@ -99,7 +99,7 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
         }
     },
     [GeneratorName.POSTMAN]: {
-        version: "0.0.45",
+        version: "0.0.46",
         output: {
             location: "local-file-system",
             path: "../generated/postman"
