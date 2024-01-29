a4ef3b6e1f957b4b60346c86346dbc1c894e31a5
Danny Sheridan <danny@buildwithfern.com>
2024-01-29 11:37:51 -0600

    upgrade python generators

--- packages/cli/config-management/manage-generator/src/generatorInvocations.ts
+++ packages/cli/config-management/manage-generator/src/generatorInvocations.ts
@@ -1,24 +1,22 @@
 import { GeneratorInvocationSchema, GeneratorName } from "@fern-api/generators-configuration";
 
 export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocationSchema, "name">> = {
-    [GeneratorName.JAVA]: {
-        version: "0.0.122"
-    },
-    [GeneratorName.TYPESCRIPT_EXPRESS]: {
-        version: "0.7.2",
+    [GeneratorName.JAVA_MODEL]: {
+        version: "0.6.1",
         output: {
             location: "local-file-system",
-            path: "../src/fern"
+            path: "../generated/models/java"
         }
     },
-    [GeneratorName.JAVA_MODEL]: {
-        version: "0.5.7"
-    },
     [GeneratorName.JAVA_SDK]: {
-        version: "0.5.7"
+        version: "0.6.1",
+        output: {
+            location: "local-file-system",
+            path: "../generated/sdks/java"
+        }
     },
     [GeneratorName.JAVA_SPRING]: {
-        version: "0.5.7",
+        version: "0.6.1",
         output: {
             location: "local-file-system",
             path: "../src/main/java/com/fern"
@@ -27,23 +25,52 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
             "package-prefix": "com.fern"
         }
     },
-    [GeneratorName.TYPESCRIPT]: {
-        version: "0.7.2"
+    //deprecated
+    [GeneratorName.JAVA]: {
+        version: "0.0.122"
     },
+    [GeneratorName.TYPESCRIPT_EXPRESS]: {
+        version: "0.9.5",
+        output: {
+            location: "local-file-system",
+            path: "../src/fern"
+        }
+    },
+    //deprecated
+    [GeneratorName.TYPESCRIPT]: {
+        version: "0.9.5"
+    },
+    //deprecated
     [GeneratorName.TYPESCRIPT_SDK]: {
-        version: "0.7.2"
+        version: "0.9.5",
+        output: {
+            location: "local-file-system",
+            path: "../generated/sdks/typescript"
+        }
     },
     [GeneratorName.TYPESCRIPT_NODE_SDK]: {
-        version: "0.7.2"
+        version: "0.9.5",
+        output: {
+            location: "local-file-system",
+            path: "../generated/sdks/typescript"
+        }
     },
     [GeneratorName.TYPESCRIPT_BROWSER_SDK]: {
-        version: "0.7.2"
+        version: "0.9.5",
+        output: {
+            location: "local-file-system",
+            path: "../generated/sdks/typescript-browser"
+        }
     },
     [GeneratorName.PYTHON_PYDANTIC]: {
-        version: "0.6.0"
+        version: "0.7.7",
+        output: {
+            location: "local-file-system",
+            path: "../generated/models/pydantic"
+        }
     },
     [GeneratorName.PYTHON_FASTAPI]: {
-        version: "0.6.0",
+        version: "0.7.7",
         output: {
             location: "local-file-system",
             path: "../src/fern"
@@ -53,37 +80,45 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
         version: "0.8.1",
         output: {
             location: "local-file-system",
-            path: "../generated/python"
+            path: "../generated/sdks/python"
         }
     },
     [GeneratorName.GO_FIBER]: {
-        version: "0.9.0"
+        version: "0.10.0"
     },
     [GeneratorName.GO_MODEL]: {
-        version: "0.9.0"
-    },
-    [GeneratorName.GO_SDK]: {
-        version: "0.9.0",
+        version: "0.10.0",
         output: {
             location: "local-file-system",
-            path: "../generated/go"
+            path: "../generated/models/go"
+        }
+    },
+    [GeneratorName.GO_SDK]: {
+        version: "0.10.0",
+        output: {
+            location: "local-file-system",
+            path: "../generated/sdks/go"
         },
         config: {
-            importPath: "go-mod-path/generated/go"
+            importPath: "go-mod-path/generated/sdks/go"
         }
     },
     [GeneratorName.RUBY_MODEL]: {
-        version: "0.0.1"
+        version: "0.0.1",
+        output: {
+            location: "local-file-system",
+            path: "../generated/models/ruby"
+        }
     },
     [GeneratorName.RUBY_SDK]: {
         version: "0.0.1",
         output: {
             location: "local-file-system",
-            path: "../generated/ruby"
+            path: "../generated/sdks/ruby"
         }
     },
     [GeneratorName.OPENAPI]: {
-        version: "0.0.28",
+        version: "0.0.30",
         config: {
             format: "yaml"
         },
@@ -92,25 +127,27 @@ export const GENERATOR_INVOCATIONS: Record<GeneratorName, Omit<GeneratorInvocati
             path: "../generated/openapi"
         }
     },
-    [GeneratorName.STOPLIGHT]: {
-        version: "0.0.24",
-        config: {
-            format: "yaml"
-        }
-    },
     [GeneratorName.POSTMAN]: {
-        version: "0.0.45",
+        version: "0.0.46",
         output: {
             location: "local-file-system",
             path: "../generated/postman"
         }
     },
+    //deprecated
     [GeneratorName.OPENAPI_PYTHON_CLIENT]: {
         version: "0.0.11",
         output: {
             location: "local-file-system",
             path: "../generated/python"
         },
+        config: {
+            format: "yaml"
+        }
+    },
+    //deprecated
+    [GeneratorName.STOPLIGHT]: {
+        version: "0.0.24",
         config: {
             format: "yaml"
         }
