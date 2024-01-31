353dbc34a5b82b4be45ba8a9fc975d13b60209aa
dsinghvi <dsinghvi@umich.edu>
2024-01-30 19:44:58 -0500

    implement v0

--- packages/cli/openapi-parser/src/v3/runResolutions.ts
+++ packages/cli/openapi-parser/src/v3/runResolutions.ts
@@ -1,38 +1,116 @@
-import { TaskContext } from "@fern-api/task-context";
 import { OpenAPIV3 } from "openapi-types";
-import { DummyOpenAPIV3ParserContext } from "./DummyOpenAPIV3ParserContext";
 import { getFernResolutions } from "./extensions/getFernResolutions";
 
-export function runResolutions({
-    openapi,
-    taskContext
-}: {
-    openapi: OpenAPIV3.Document;
-    taskContext: TaskContext;
-}): OpenAPIV3.Document {
-    const dummyContext = new DummyOpenAPIV3ParserContext({ document: openapi, taskContext });
+export function runResolutions({ openapi }: { openapi: OpenAPIV3.Document }): OpenAPIV3.Document {
     const resolutions = getFernResolutions(openapi);
     if (resolutions == null) {
         return openapi;
     }
+
+    /**
+     * A map of references that have been replaced.
+     * This is useful so that we can replace inline
+     * references in schemas that have been replaced.
+     */
+    let replacedReferences: Record<string, string> = {};
     for (const resolution of resolutions) {
         for (const resolutionPath of resolution.resolutions) {
-            const resolvedSchema = dummyContext.resolveSchema(resolutionPath);
-            if (resolvedSchema == null) {
-                throw new Error(`Could not find schema for resolution path ${resolutionPath}`);
-            }
-            openapi.components.schemas[resolution.name] = resolvedSchema;
+            const schemaReference = `#/components/schemas/${resolution.name}`;
+            openapi = addComponentSchema({ openapi, schemaReference, schemaName: resolution.name });
+            openapi = replaceWithSchemaReference({
+                openapi,
+                replaceReference: resolutionPath,
+                schemaReference,
+                replacedReferences
+            });
+            replacedReferences = {
+                ...replacedReferences,
+                ...Object.fromEntries(resolution.resolutions.map((resolution) => [schemaReference, resolution]))
+            };
         }
     }
+    return openapi;
 }
 
-function resolve(ref: string): OpenAPIV3.SchemaObject {
-    const keys = ref.split("/");
-    const schema = this.openapi;
+interface AddComponentSchemaArgs {
+    openapi: OpenAPIV3.Document;
+    /* A reference to the inlined schema which should be made a comonent schema */
+    schemaReference: string;
+    /* The name of the schema to be added */
+    schemaName: string;
+}
+
+function addComponentSchema({ openapi, schemaReference, schemaName }: AddComponentSchemaArgs): OpenAPIV3.Document {
+    const keys = schemaReference.split("/");
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let value = openapi as any;
     for (const key of keys) {
-        if (schema[key] == null) {
-            throw new Error(`Could not resolve ${ref}`);
+        const nextValue = value[key];
+        if (nextValue == null) {
+            return openapi;
         }
-        schema = schema[key];
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        value = nextValue as any;
     }
+
+    if (openapi.components == null) {
+        openapi.components = {};
+    }
+    if (openapi.components.schemas == null) {
+        openapi.components.schemas = {};
+    }
+    openapi.components.schemas[schemaName] = value;
+
+    return openapi;
+}
+
+interface ReplaceWithReferenceArgs {
+    openapi: OpenAPIV3.Document;
+    /* A reference to the inlined schema which should be replaced */
+    replaceReference: string;
+    /* A reference to the schema */
+    schemaReference: string;
+    /* Any references that have been moved so far */
+    replacedReferences: Record<string, string>;
+}
+
+function replaceWithSchemaReference({
+    openapi,
+    replaceReference,
+    schemaReference,
+    replacedReferences
+}: ReplaceWithReferenceArgs): OpenAPIV3.Document {
+    const overlappingReference = Object.keys(replacedReferences)
+        .filter((replacedReference) => schemaReference.includes(replacedReference))
+        .sort((a, b) => b.length - a.length)[0];
+    if (overlappingReference != null) {
+        const resolvedOverlappingReference = replacedReferences[overlappingReference];
+        if (resolvedOverlappingReference != null) {
+            const newSchemaReference = schemaReference.replace(overlappingReference, resolvedOverlappingReference);
+            return replaceWithSchemaReference({
+                openapi,
+                replaceReference,
+                schemaReference: newSchemaReference,
+                replacedReferences: {}
+            });
+        }
+    }
+
+    const keys = replaceReference.split("/");
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let value = openapi as any;
+    for (const key of keys) {
+        const nextValue = value[key];
+        if (nextValue == null) {
+            return openapi;
+        }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        value = nextValue as any;
+    }
+
+    value = {
+        $ref: schemaReference
+    };
+
+    return openapi;
 }
