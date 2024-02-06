3badec098865c2a32434315b0e625e51e3a337d7
Armando Belardo <armandoubelardo@gmail.com>
2024-02-06 13:48:59 -0500

    Fix naming of services within Ruby

--- seed/ruby-sdk/examples/lib/seed_examples_client/file/client.rb
+++ seed/ruby-sdk/examples/lib/seed_examples_client/file/client.rb
@@ -12,7 +12,7 @@ module SeedExamplesClient
       # @param request_client [RequestClient]
       # @return [File::Client]
       def initialize(request_client:)
-        @file = File::Notification::Client.new(service: request_client)
+        @file = File::Notification::Client.new(request_client: request_client)
         @service = File::ServiceClient.new(request_client: request_client)
       end
     end
@@ -23,7 +23,7 @@ module SeedExamplesClient
       # @param request_client [RequestClient]
       # @return [File::AsyncClient]
       def initialize(request_client:)
-        @file = File::Notification::AsyncClient.new(service: request_client)
+        @file = File::Notification::AsyncClient.new(request_client: request_client)
         @service = File::AsyncServiceClient.new(request_client: request_client)
       end
     end
--- seed/ruby-sdk/trace/lib/seed_trace_client.rb
+++ seed/ruby-sdk/trace/lib/seed_trace_client.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require_relative "environment"
 require_relative "types_export"
 require_relative "requests"
@@ -10,20 +11,22 @@ require_relative "seed_trace_client/playlist/client"
 require_relative "seed_trace_client/problem/client"
 require_relative "seed_trace_client/submission/client"
 require_relative "seed_trace_client/sysprop/client"
-require_relative "requests"
 
 module SeedTraceClient
   class Client
-    attr_reader :, :admin, :homepage, :migration, :playlist, :problem, :submission, :sysprop
-    # @param environment [Environment] 
+    attr_reader :v_2, :admin, :homepage, :migration, :playlist, :problem, :submission, :sysprop
+
+    # @param environment [Environment]
     # @param max_retries [Long] The number of times to retry a failed request, defaults to 2.
-    # @param timeout_in_seconds [Long] 
-    # @param token [String] 
-    # @param x_random_header [String] 
+    # @param timeout_in_seconds [Long]
+    # @param token [String]
+    # @param x_random_header [String]
     # @return [Client]
-    def initialize(environment: Environment::PROD, max_retries: nil, timeout_in_seconds: nil, token:, x_random_header: nil)
-      @request_client = RequestClient.new(environment: environment, max_retries: max_retries, timeout_in_seconds: timeout_in_seconds, token: token, x_random_header: x_random_header)
-      @ = V2::Client.new(request_client: @request_client)
+    def initialize(token:, environment: Environment::PROD, max_retries: nil, timeout_in_seconds: nil,
+                   x_random_header: nil)
+      @request_client = RequestClient.new(environment: environment, max_retries: max_retries,
+                                          timeout_in_seconds: timeout_in_seconds, token: token, x_random_header: x_random_header)
+      @v_2 = V2::V2Client.new(request_client: @request_client)
       @admin = AdminClient.new(request_client: @request_client)
       @homepage = HomepageClient.new(request_client: @request_client)
       @migration = MigrationClient.new(request_client: @request_client)
@@ -33,17 +36,21 @@ module SeedTraceClient
       @sysprop = SyspropClient.new(request_client: @request_client)
     end
   end
+
   class AsyncClient
-    attr_reader :, :admin, :homepage, :migration, :playlist, :problem, :submission, :sysprop
-    # @param environment [Environment] 
+    attr_reader :v_2, :admin, :homepage, :migration, :playlist, :problem, :submission, :sysprop
+
+    # @param environment [Environment]
     # @param max_retries [Long] The number of times to retry a failed request, defaults to 2.
-    # @param timeout_in_seconds [Long] 
-    # @param token [String] 
-    # @param x_random_header [String] 
+    # @param timeout_in_seconds [Long]
+    # @param token [String]
+    # @param x_random_header [String]
     # @return [AsyncClient]
-    def initialize(environment: Environment::PROD, max_retries: nil, timeout_in_seconds: nil, token:, x_random_header: nil)
-      @async_request_client = AsyncRequestClient.new(environment: environment, max_retries: max_retries, timeout_in_seconds: timeout_in_seconds, token: token, x_random_header: x_random_header)
-      @ = V2::AsyncClient.new(request_client: @async_request_client)
+    def initialize(token:, environment: Environment::PROD, max_retries: nil, timeout_in_seconds: nil,
+                   x_random_header: nil)
+      @async_request_client = AsyncRequestClient.new(environment: environment, max_retries: max_retries,
+                                                     timeout_in_seconds: timeout_in_seconds, token: token, x_random_header: x_random_header)
+      @v_2 = V2::AsyncV2Client.new(request_client: @async_request_client)
       @admin = AsyncAdminClient.new(request_client: @async_request_client)
       @homepage = AsyncHomepageClient.new(request_client: @async_request_client)
       @migration = AsyncMigrationClient.new(request_client: @async_request_client)
