30706ca49617f8c39ecfb438f19fd5aa84644295
Andrew Jiang <andrew@buildwithfern.com>
2024-02-06 13:46:07 -0500

    gracefully handle light/dark

--- packages/cli/config-management/docs-configuration/src/parseDocsConfiguration.ts
+++ packages/cli/config-management/docs-configuration/src/parseDocsConfiguration.ts
@@ -446,108 +446,142 @@ async function convertImageReference({
     };
 }
 
-function getColorType(colorConfig: RawDocs.ColorConfig | undefined): "dark" | "light" | "darkAndLight" {
-    if (colorConfig == null) {
-        return "dark";
-    }
-
-    if (typeof colorConfig === "string") {
-        if (tinycolor(colorConfig).isValid()) {
-            return tinycolor(colorConfig).isDark() ? "dark" : "light";
-        } else {
-            return "dark";
+function getColorType({ background, accentPrimary }: RawDocs.ColorsConfiguration): "dark" | "light" | "darkAndLight" {
+    if (background != null) {
+        if (typeof background === "string") {
+            return tinycolor(background).isDark() ? "dark" : "light";
+        }
+        if (background.dark != null && background.light != null) {
+            return "darkAndLight";
         }
     }
 
-    if (colorConfig.dark != null && colorConfig.light != null) {
-        return "darkAndLight";
+    if (accentPrimary != null) {
+        if (typeof accentPrimary === "string") {
+            // the luminance of the accent color must always be the opposite of the theme
+            // if the accent color is dark, the theme must be light and vice versa
+            return tinycolor(accentPrimary).isLight() ? "dark" : "light";
+        }
+        if (accentPrimary.dark != null && accentPrimary.light != null) {
+            return "darkAndLight";
+        }
     }
 
-    if (colorConfig.dark != null) {
-        return "dark";
+    if (accentPrimary != null && background != null) {
+        if (background.dark != null && accentPrimary.dark != null) {
+            return "dark";
+        }
+
+        if (background.light != null && accentPrimary.light != null) {
+            return "light";
+        }
     }
 
-    if (colorConfig.light != null) {
-        return "light";
-    }
-
-    return "dark";
+    // prefer "darkAndLight" unless both the accentPrimary and background are pinned to the same theme
+    return "darkAndLight";
 }
 
 function convertColorsConfiguration(
     rawConfig: RawDocs.ColorsConfiguration,
     context: TaskContext
 ): DocsV1Write.ColorsConfigV3 {
-    const colorType = getColorType(rawConfig.background ?? rawConfig.accentPrimary);
+    const colorType = getColorType(rawConfig);
     switch (colorType) {
         case "dark":
             return {
                 type: "dark",
-                accentPrimary:
-                    rawConfig.accentPrimary != null
-                        ? convertColorConfiguration(rawConfig.accentPrimary, context, "accentPrimary", "dark")
-                        : undefined,
-                background:
-                    rawConfig.background != null
-                        ? convertColorConfiguration(rawConfig.background, context, "background", "dark")
-                        : undefined
+                ...convertColorConfig(rawConfig, context, "dark")
             };
         case "light":
             return {
                 type: "light",
-                accentPrimary:
-                    rawConfig.accentPrimary != null
-                        ? convertColorConfiguration(rawConfig.accentPrimary, context, "accentPrimary", "light")
-                        : undefined,
-                background:
-                    rawConfig.background != null
-                        ? convertColorConfiguration(rawConfig.background, context, "background", "light")
-                        : undefined
+                ...convertColorConfig(rawConfig, context, "light")
             };
         case "darkAndLight":
             return {
                 type: "darkAndLight",
-                dark: {
-                    accentPrimary:
-                        rawConfig.accentPrimary != null
-                            ? convertColorConfiguration(rawConfig.accentPrimary, context, "accentPrimary", "dark")
-                            : undefined,
-                    background:
-                        rawConfig.background != null
-                            ? convertColorConfiguration(rawConfig.background, context, "background", "dark")
-                            : undefined
-                },
-                light: {
-                    accentPrimary:
-                        rawConfig.accentPrimary != null
-                            ? convertColorConfiguration(rawConfig.accentPrimary, context, "accentPrimary", "light")
-                            : undefined,
-                    background:
-                        rawConfig.background != null
-                            ? convertColorConfiguration(rawConfig.background, context, "background", "light")
-                            : undefined
-                }
+                dark: convertColorConfig(rawConfig, context, "dark"),
+                light: convertColorConfig(rawConfig, context, "light")
             };
         default:
             assertNever(colorType);
     }
 }
 
-function convertColorConfiguration(
-    raw: RawDocs.ColorConfig,
+function convertColorConfig(
+    rawConfig: RawDocs.ColorsConfiguration,
+    context: TaskContext,
+    theme: "dark" | "light"
+): DocsV1Write.DarkModeConfig | DocsV1Write.LightModeConfig {
+    let accentPrimaryColor = getColorInstanceFromRawConfig(rawConfig.accentPrimary, context, "accentPrimary", theme);
+    let backgroundColor = getColorInstanceFromRawConfig(rawConfig.background, context, "background", theme);
+
+    // the toDark() and toLight() functions are meant to improve color contrast and catch any cases where
+    // only one of the dark or light color is provided for either the accentPrimary or background
+    accentPrimaryColor = theme === "dark" ? toLight(accentPrimaryColor) : toDark(accentPrimaryColor);
+    backgroundColor = theme === "dark" ? toDark(backgroundColor) : toLight(backgroundColor);
+
+    // TODO: move this warning to fern-check
+    if (accentPrimaryColor != null && backgroundColor != null) {
+        if (!tinycolor.isReadable(accentPrimaryColor, backgroundColor)) {
+            const ratio = tinycolor.readability(accentPrimaryColor, backgroundColor);
+            context.logger.warn(
+                `The accent color is not readable on the background color in ${theme} mode. The contrast ratio is ${ratio}.`
+            );
+        }
+    }
+
+    return {
+        accentPrimary: toRgb(accentPrimaryColor),
+        background: toRgb(backgroundColor)
+    };
+}
+
+function toLight(color: tinycolor.Instance | undefined): tinycolor.Instance | undefined {
+    if (color == null) {
+        return undefined;
+    }
+    return color.isLight() ? color : color.lighten(20);
+}
+
+function toDark(color: tinycolor.Instance | undefined): tinycolor.Instance | undefined {
+    if (color == null) {
+        return undefined;
+    }
+    return color.isDark() ? color : color.darken(20);
+}
+
+function toRgb(color: tinycolor.Instance | undefined): DocsV1Write.RgbColor | undefined {
+    if (color == null) {
+        return undefined;
+    }
+    const { r, g, b } = color.toRgb();
+    return { r, g, b };
+}
+
+function getColorInstanceFromRawConfig(
+    raw: RawDocs.ColorConfig | undefined,
     context: TaskContext,
     key: string,
     theme: "dark" | "light"
-): DocsV1Write.RgbColor {
+): tinycolor.Instance | undefined {
+    if (raw == null) {
+        return undefined;
+    }
+
     const rawColor = typeof raw === "string" ? raw : raw[theme] ?? raw.dark ?? raw.light;
+
+    if (rawColor == null) {
+        return undefined;
+    }
+
     const color = tinycolor(rawColor);
     if (!color.isValid()) {
         context.failAndThrow(
             `'${typeof raw === "string" ? key : `${key}.${theme}`}' should be a hex color of the format #FFFFFF`
         );
     }
-    const rgb = color.toRgb();
-    return { r: rgb.r, g: rgb.g, b: rgb.b };
+    return color;
 }
 
 function convertNavbarLinks(rawConfig: RawDocs.NavbarLink[]): DocsV1Write.NavbarLink[] {
