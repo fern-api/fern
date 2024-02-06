91c015174662fbd3e65457570608ed0fbc290288
Andrew Jiang <andrew@buildwithfern.com>
2024-02-05 20:21:52 -0500

    fix dependency

--- .pnp.cjs
+++ .pnp.cjs
@@ -5189,7 +5189,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/core-utils", "workspace:packages/commons/core-utils"],\
             ["@fern-api/fs-utils", "workspace:packages/commons/fs-utils"],\
             ["@fern-api/ir-sdk", "workspace:packages/ir-sdk"],\
-            ["@fern-fern/docs-config", "npm:0.0.53"],\
+            ["@fern-fern/docs-config", "npm:0.0.70"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/js-yaml", "npm:4.0.8"],\
             ["@types/node", "npm:18.7.18"],\
@@ -5206,13 +5206,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["@fern-fern/docs-config", [\
-        ["npm:0.0.53", {\
-          "packageLocation": "./.yarn/cache/@fern-fern-docs-config-npm-0.0.53-c2520b14db-3caa333aef.zip/node_modules/@fern-fern/docs-config/",\
-          "packageDependencies": [\
-            ["@fern-fern/docs-config", "npm:0.0.53"]\
-          ],\
-          "linkType": "HARD"\
-        }],\
         ["npm:0.0.70", {\
           "packageLocation": "./.yarn/cache/@fern-fern-docs-config-npm-0.0.70-89a0ccbe9f-39a0a8e8a5.zip/node_modules/@fern-fern/docs-config/",\
           "packageDependencies": [\
