932df5786c0deb088421d859d226f6e3cd2d0b87
dsinghvi <dsinghvi@umich.edu>
2024-02-17 20:47:27 -0500

    try running with isolated modules

--- shared/jest.config.shared.ts
+++ shared/jest.config.shared.ts
@@ -6,7 +6,8 @@ const esModules = ["lodash-es"].join("|");
 const config: Config = {
     testMatch: ["**/__test__/**/*.test.ts{,x}"],
     transform: {
-        "\\.[jt]sx?$": ["babel-jest", { rootMode: "upward" }]
+        "\\.[jt]sx?$": ["babel-jest", { rootMode: "upward" }],
+        "^.+\\.(t|j)s$": ["ts-jest", { isolatedModules: true }]
     },
     transformIgnorePatterns: [`<rootDir>/node_modules/(?!${esModules})`]
 };
