41c7a7811b190f06c3e7140467728bcf8368d4a8
dsinghvi <dsinghvi@umich.edu>
2024-02-19 21:31:34 -0500

    (ts, release): release browser compatible streaming

--- seed/ts-sdk/streaming/allow-custom-fetcher/src/core/streaming-fetcher/Stream.ts
+++ seed/ts-sdk/streaming/allow-custom-fetcher/src/core/streaming-fetcher/Stream.ts
@@ -21,13 +21,21 @@ export class Stream<T> implements AsyncIterable<T> {
     }
 
     private async *iterMessages(): AsyncGenerator<T, void> {
-        const decoder = new TextDecoder();
+        const decoder = new TextDecoder("utf8");
         const stream = readableStreamAsyncIterable<any>(this.stream);
         let previous = "";
         for await (const chunk of stream) {
-            previous += decoder.decode(chunk.value);
+            let bufferChunk = "";
+            // Buffer is present in Node.js environment
+            if (typeof Buffer !== "undefined") {
+                bufferChunk += Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk);
+            }
+            // TextDecoder is present in Browser environment
+            else if (typeof TextDecoder !== "undefined") {
+                bufferChunk += decoder.decode(chunk);
+            }
+            previous += bufferChunk;
             let terminatorIndex: number;
-
             while ((terminatorIndex = previous.indexOf(this.terminator)) >= 0) {
                 const line = previous.slice(0, terminatorIndex).trimEnd();
                 const message = await this.parse(JSON.parse(line));
@@ -47,15 +55,20 @@ export class Stream<T> implements AsyncIterable<T> {
 /**
  * Browser polyfill for ReadableStream
  */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function readableStreamAsyncIterable<T>(stream: any): AsyncIterableIterator<T> {
-    if (stream[Symbol.asyncIterator]) return stream;
+    if (stream[Symbol.asyncIterator]) {
+        return stream;
+    }
 
     const reader = stream.getReader();
     return {
         async next() {
             try {
                 const result = await reader.read();
-                if (result?.done) reader.releaseLock(); // release lock when stream becomes closed
+                if (result?.done) {
+                    reader.releaseLock();
+                } // release lock when stream becomes closed
                 return result;
             } catch (e) {
                 reader.releaseLock(); // release lock when stream becomes errored
