b8b6c22664b6963dbc497ea9c64515f22ff5c06f
Deep Singhvi <deep@usebirch.com>
2024-02-19 19:38:32 -0500

    (chore, internal): setup browser playground for ts generator (#3019)
    * (chore): setup browser playground for ts generator
    
    * yarn lint:monorepo:fix
    
    * fix lint

---
+++ generators/typescript/playground/browser/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function App() {
+    return (
+        <div>
+            <h1>Hello Vite + React!</h1>
+        </div>
+    );
+}
+
+export default App;
