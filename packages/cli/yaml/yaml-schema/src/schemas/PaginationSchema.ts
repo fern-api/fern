3815b96b2dde1d9b48351c2f4ea3985fc2e9ffa1
Alex McKinney <alexmckinney01@gmail.com>
2024-02-22 10:46:28 -0800

    Add documentation to PaginationSchema

--- packages/cli/yaml/yaml-schema/src/schemas/PaginationSchema.ts
+++ packages/cli/yaml/yaml-schema/src/schemas/PaginationSchema.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const PaginationSchema = z.object({
-    page: z.string(),
-    next: z.string(),
-    results: z.string()
+    page: z.string().describe("The request property name that represents the page cursor or offset."),
+    next: z.string().describe("The response property name that represents the next page cursor or offset to retrieve."),
+    results: z.string().describe("The response property name that represents the page elements.")
 });
 
 export type PaginationSchema = z.infer<typeof PaginationSchema>;
