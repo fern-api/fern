85ef584a0e39ec1f2f0ff7a6e098d0a5c25e6cae
Alex McKinney <alexmckinney01@gmail.com>
2024-02-22 12:47:02 -0800

    Revise getPaginationExtension to support 'boolean' values

--- packages/cli/openapi-parser/src/openapi/v3/extensions/getPaginationExtension.ts
+++ packages/cli/openapi-parser/src/openapi/v3/extensions/getPaginationExtension.ts
@@ -2,26 +2,46 @@ import { OpenAPIV3 } from "openapi-types";
 import { FernOpenAPIExtension } from "./fernExtensions";
 import { getExtension } from "./getExtension";
 
-export interface FernPaginationExtension {
+export type FernPaginationExtension = FernPaginationEnabledExtension | FernPaginationConfigExtension;
+
+export interface FernPaginationEnabledExtension {
+    type: "pagination";
+}
+
+export interface FernPaginationConfigExtension {
+    type: "paginationConfig";
     page: string;
     next: string;
     results: string;
 }
 
 declare namespace Raw {
-    export interface PaginationExtensionSchema {
+    export type PaginationExtensionSchema = boolean | PaginationExtensionObjectSchema;
+
+    export interface PaginationExtensionObjectSchema {
         page: string;
         next: string;
         results: string;
     }
 }
 
-export function getFernPaginationExtension(node: OpenAPIV3.Document | OpenAPIV3.OperationObject): FernPaginationExtension | undefined {
+export function getFernPaginationExtension(
+    node: OpenAPIV3.Document | OpenAPIV3.OperationObject
+): FernPaginationExtension | undefined {
     const pagination = getExtension<Raw.PaginationExtensionSchema>(node, FernOpenAPIExtension.PAGINATION);
     if (pagination == null) {
         return undefined;
     }
+    if (typeof pagination === "boolean") {
+        return pagination
+            ? {
+                  type: "pagination"
+              }
+            : undefined;
+    }
+
     return {
+        type: "paginationConfig",
         page: pagination.page,
         next: pagination.next,
         results: pagination.results
