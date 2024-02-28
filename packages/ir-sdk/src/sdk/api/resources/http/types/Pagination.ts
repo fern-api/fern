c5812ebeec29688e2848a48cb2939f075ef2473c
Alex McKinney <alexmckinney01@gmail.com>
2024-02-27 18:21:12 -0800

    Generate IR types and update tests

--- packages/ir-sdk/src/sdk/api/resources/http/types/Pagination.ts
+++ packages/ir-sdk/src/sdk/api/resources/http/types/Pagination.ts
@@ -6,13 +6,58 @@ import * as FernIr from "../../..";
 
 /**
  * If set, the endpoint will be generated with auto-pagination features.
- *
- * The page must be defined as a query parameter included in the request,
- * whereas the next page and results are resolved from properties defined
- * on the response.
  */
-export interface Pagination {
-    page: FernIr.QueryParameter;
-    next: FernIr.PaginationProperty;
-    results: FernIr.PaginationProperty;
+export type Pagination = FernIr.Pagination.Cursor | FernIr.Pagination.Offset;
+
+export declare namespace Pagination {
+    interface Cursor extends FernIr.CursorPagination, _Utils {
+        type: "cursor";
+    }
+
+    interface Offset extends FernIr.OffsetPagination, _Utils {
+        type: "offset";
+    }
+
+    interface _Utils {
+        _visit: <_Result>(visitor: FernIr.Pagination._Visitor<_Result>) => _Result;
+    }
+
+    interface _Visitor<_Result> {
+        cursor: (value: FernIr.CursorPagination) => _Result;
+        offset: (value: FernIr.OffsetPagination) => _Result;
+        _other: (value: { type: string }) => _Result;
+    }
 }
+
+export const Pagination = {
+    cursor: (value: FernIr.CursorPagination): FernIr.Pagination.Cursor => {
+        return {
+            ...value,
+            type: "cursor",
+            _visit: function <_Result>(this: FernIr.Pagination.Cursor, visitor: FernIr.Pagination._Visitor<_Result>) {
+                return FernIr.Pagination._visit(this, visitor);
+            },
+        };
+    },
+
+    offset: (value: FernIr.OffsetPagination): FernIr.Pagination.Offset => {
+        return {
+            ...value,
+            type: "offset",
+            _visit: function <_Result>(this: FernIr.Pagination.Offset, visitor: FernIr.Pagination._Visitor<_Result>) {
+                return FernIr.Pagination._visit(this, visitor);
+            },
+        };
+    },
+
+    _visit: <_Result>(value: FernIr.Pagination, visitor: FernIr.Pagination._Visitor<_Result>): _Result => {
+        switch (value.type) {
+            case "cursor":
+                return visitor.cursor(value);
+            case "offset":
+                return visitor.offset(value);
+            default:
+                return visitor._other(value as any);
+        }
+    },
+} as const;
