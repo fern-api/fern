818e84da54dd18995cb45b465f7345e76775a906
Deep Singhvi <deep@usebirch.com>
2024-02-29 14:29:02 -0500

    (chore): upgrade mrlint and reenable eslint (#3088)
    * reenable eslint
    
    * fix
    
    * god damn

--- .pnp.cjs
+++ .pnp.cjs
@@ -531,7 +531,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jsonc-parser", "npm:2.2.1"],\
             ["lint-staged", "npm:13.0.3"],\
             ["lodash-es", "npm:4.17.21"],\
-            ["mrlint", "npm:0.0.131"],\
+            ["mrlint", "npm:0.1.0"],\
             ["organize-imports-cli", "npm:0.10.0"],\
             ["prettier", "npm:2.7.1"],\
             ["react", "npm:18.2.0"],\
@@ -15034,7 +15034,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["jsonc-parser", "npm:2.2.1"],\
             ["lint-staged", "npm:13.0.3"],\
             ["lodash-es", "npm:4.17.21"],\
-            ["mrlint", "npm:0.0.131"],\
+            ["mrlint", "npm:0.1.0"],\
             ["organize-imports-cli", "npm:0.10.0"],\
             ["prettier", "npm:2.7.1"],\
             ["react", "npm:18.2.0"],\
@@ -20459,10 +20459,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["mrlint", [\
-        ["npm:0.0.131", {\
-          "packageLocation": "./.yarn/cache/mrlint-npm-0.0.131-a9267ed6b0-664798f33f.zip/node_modules/mrlint/",\
+        ["npm:0.1.0", {\
+          "packageLocation": "./.yarn/cache/mrlint-npm-0.1.0-b4ec7009da-0ae2568c97.zip/node_modules/mrlint/",\
           "packageDependencies": [\
-            ["mrlint", "npm:0.0.131"]\
+            ["mrlint", "npm:0.1.0"]\
           ],\
           "linkType": "HARD"\
         }]\
