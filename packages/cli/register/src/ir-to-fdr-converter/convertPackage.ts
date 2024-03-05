0983e466678fde73ec6d8c3c14698b4e29695a79
armandobelardo <armandoubelardo@gmail.com>
2024-03-05 15:07:26 -0500

    update with provided examples

--- packages/cli/register/src/ir-to-fdr-converter/convertPackage.ts
+++ packages/cli/register/src/ir-to-fdr-converter/convertPackage.ts
@@ -93,7 +93,9 @@ function convertService(
             response: irEndpoint.response != null ? convertResponse(irEndpoint.response) : undefined,
             errors: convertResponseErrors(irEndpoint.errors, ir),
             errorsV2: convertResponseErrorsV2(irEndpoint.errors, ir),
-            examples: irEndpoint.examples.map((example) => convertExampleEndpointCall(example, ir))
+            examples: irEndpoint.examples
+                .filter((example) => example.exampleType === "userProvided")
+                .map((example) => convertHttpEndpointExample(example, ir))
         })
     );
 }
