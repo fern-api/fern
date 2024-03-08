4bb6a9e3d010a3fa486330bdd0bfdc812b231ee7
armandobelardo <armandoubelardo@gmail.com>
2024-03-08 12:10:35 -0500

    default to allow

--- generators/python/src/fern_python/generators/pydantic_model/custom_config.py
+++ generators/python/src/fern_python/generators/pydantic_model/custom_config.py
@@ -17,7 +17,7 @@ class PydanticModelCustomConfig(BasePydanticModelCustomConfig):
     include_validators: bool = False
     # DEPRECATED: use `extra_fields` instead
     forbid_extra_fields: bool = False
-    extra_fields: Optional[Literal["allow", "forbid"]] = None
+    extra_fields: Optional[Literal["allow", "forbid"]] = "allow"
     wrapped_aliases: bool = False
     skip_formatting: bool = False
     include_union_utils: bool = False
