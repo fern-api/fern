9cf48e84ddcd270ea21e2d996eedb61679365650
armandobelardo <armandoubelardo@gmail.com>
2024-03-11 16:18:22 -0400

    fix compile

--- packages/cli/cli/src/commands/generate/generateAPIWorkspace.ts
+++ packages/cli/cli/src/commands/generate/generateAPIWorkspace.ts
@@ -66,7 +66,8 @@ export async function generateWorkspace({
             workspace,
             generatorGroup: group,
             keepDocker,
-            context
+            context,
+            writeUnitTests: false
         });
     } else {
         await runRemoteGenerationForAPIWorkspace({
