8a7389197de84d5d2aeebe9ec8480a2c143746da
dsinghvi <dsinghvi@umich.edu>
2024-03-10 23:00:33 -0400

    fix compile

--- packages/cli/yaml/yaml-migrations/src/migrations/0.15.0-rc0/update-directory-structure/docs-config/index.ts
+++ packages/cli/yaml/yaml-migrations/src/migrations/0.15.0-rc0/update-directory-structure/docs-config/index.ts
@@ -1,4 +1,3 @@
-export { FernDocsConfig as MigratedDocs, type DocsInstances as DocsURL } from "@fern-api/docs-config-sdk";
 export { FernDocsConfig as LegacyDocs } from "@fern-fern/legacy-docs-config";
 export * as LegacyDocsSerializers from "@fern-fern/legacy-docs-config/serialization";
 export {
