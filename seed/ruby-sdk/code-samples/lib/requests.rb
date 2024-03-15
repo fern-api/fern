bd2f8b34e328ab67b839b23ac4d1f00003f1be30
Myles Scolnick <myles@marimo.io>
2024-03-15 11:27:34 -0400

    more generation

---
+++ seed/ruby-sdk/code-samples/lib/requests.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+require "faraday"
+require "faraday/retry"
+require "async/http/faraday"
+
+module SeedCodeSamplesClient
+  class RequestClient
+    attr_reader :headers, :base_url, :conn
+
+    # @param max_retries [Long] The number of times to retry a failed request, defaults to 2.
+    # @param timeout_in_seconds [Long]
+    # @return [RequestClient]
+    def initialize(max_retries: nil, timeout_in_seconds: nil)
+      @headers = {
+        "X-Fern-Language": "Ruby",
+        "X-Fern-SDK-Name": "seed_code_samples_client",
+        "X-Fern-SDK-Version": "0.0.1"
+      }
+      @conn = Faraday.new(headers: @headers) do |faraday|
+        faraday.request :json
+        faraday.response :raise_error, include_request: true
+        faraday.request :retry, { max: max_retries } unless max_retries.nil?
+        faraday.options.timeout = timeout_in_seconds unless timeout_in_seconds.nil?
+      end
+    end
+  end
+
+  class AsyncRequestClient
+    attr_reader :headers, :base_url, :conn
+
+    # @param max_retries [Long] The number of times to retry a failed request, defaults to 2.
+    # @param timeout_in_seconds [Long]
+    # @return [AsyncRequestClient]
+    def initialize(max_retries: nil, timeout_in_seconds: nil)
+      @headers = {
+        "X-Fern-Language": "Ruby",
+        "X-Fern-SDK-Name": "seed_code_samples_client",
+        "X-Fern-SDK-Version": "0.0.1"
+      }
+      @conn = Faraday.new(headers: @headers) do |faraday|
+        faraday.request :json
+        faraday.response :raise_error, include_request: true
+        faraday.adapter :async_http
+        faraday.request :retry, { max: max_retries } unless max_retries.nil?
+        faraday.options.timeout = timeout_in_seconds unless timeout_in_seconds.nil?
+      end
+    end
+  end
+
+  # Additional options for request-specific configuration when calling APIs via the SDK.
+  class RequestOptions
+    attr_reader :additional_headers, :additional_query_parameters, :additional_body_parameters, :timeout_in_seconds
+
+    # @param additional_headers [Hash{String => Object}]
+    # @param additional_query_parameters [Hash{String => Object}]
+    # @param additional_body_parameters [Hash{String => Object}]
+    # @param timeout_in_seconds [Long]
+    # @return [RequestOptions]
+    def initialize(additional_headers: nil, additional_query_parameters: nil, additional_body_parameters: nil,
+                   timeout_in_seconds: nil)
+      # @type [Hash{String => Object}]
+      @additional_headers = additional_headers
+      # @type [Hash{String => Object}]
+      @additional_query_parameters = additional_query_parameters
+      # @type [Hash{String => Object}]
+      @additional_body_parameters = additional_body_parameters
+      # @type [Long]
+      @timeout_in_seconds = timeout_in_seconds
+    end
+  end
+
+  # Additional options for request-specific configuration when calling APIs via the SDK.
+  class IdempotencyRequestOptions
+    attr_reader :additional_headers, :additional_query_parameters, :additional_body_parameters, :timeout_in_seconds
+
+    # @param additional_headers [Hash{String => Object}]
+    # @param additional_query_parameters [Hash{String => Object}]
+    # @param additional_body_parameters [Hash{String => Object}]
+    # @param timeout_in_seconds [Long]
+    # @return [IdempotencyRequestOptions]
+    def initialize(additional_headers: nil, additional_query_parameters: nil, additional_body_parameters: nil,
+                   timeout_in_seconds: nil)
+      # @type [Hash{String => Object}]
+      @additional_headers = additional_headers
+      # @type [Hash{String => Object}]
+      @additional_query_parameters = additional_query_parameters
+      # @type [Hash{String => Object}]
+      @additional_body_parameters = additional_body_parameters
+      # @type [Long]
+      @timeout_in_seconds = timeout_in_seconds
+    end
+  end
+end
