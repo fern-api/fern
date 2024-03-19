25c8f83ced3c6e119e40fc9b29525061f40c3b6f
Omar Rida <omarrida@Omars-MacBook-Pro-2.local>
2024-03-19 23:06:08 +0300

    disable es lint for console logs

--- generators/openapi/src/writeOpenApi.ts
+++ generators/openapi/src/writeOpenApi.ts
@@ -16,15 +16,18 @@ export type Mode = "stoplight" | "openapi";
 
 export async function writeOpenApi(mode: Mode, pathToConfig: string): Promise<void> {
     try {
+        // eslint-disable-next-line no-console
         console.log("beginning writeOpenApi");
         const configStr = await readFile(pathToConfig);
         const config = JSON.parse(configStr.toString()) as GeneratorConfig;
         const customConfig = getCustomConfig(config);
 
+        // eslint-disable-next-line no-console
         console.log("customConfig", customConfig);
         const generatorLoggingClient = new GeneratorLoggingWrapper(config);
 
         try {
+            // eslint-disable-next-line no-console
             console.log("sending update");
             await generatorLoggingClient.sendUpdate(
                 GeneratorUpdate.init({
@@ -32,36 +35,46 @@ export async function writeOpenApi(mode: Mode, pathToConfig: string): Promise<vo
                 })
             );
 
+            // eslint-disable-next-line no-console
             console.log("loading ir");
             const ir = await loadIntermediateRepresentation(config.irFilepath);
+            // eslint-disable-next-line no-console
             console.log("parsed ir", ir);
             const openApiDefinition = convertToOpenApi({
                 apiName: config.workspaceName,
                 ir,
                 mode
             });
+            // eslint-disable-next-line no-console
             console.log("openApiDefinition", openApiDefinition);
 
             const openApiDefinitionWithCustomOverrides = merge(customConfig.customOverrides, openApiDefinition);
+            // eslint-disable-next-line no-console
             console.log("openApiDefinitionWithCustomOverrides", openApiDefinitionWithCustomOverrides);
 
             if (customConfig.format === "json") {
+                // eslint-disable-next-line no-console
                 console.log("writing json");
                 await writeFile(
                     path.join(config.output.path, OPENAPI_JSON_FILENAME),
                     JSON.stringify(openApiDefinitionWithCustomOverrides, undefined, 2)
                 );
+                // eslint-disable-next-line no-console
                 console.log("wrote json");
             } else {
+                // eslint-disable-next-line no-console
                 console.log("writing yml");
                 await writeFile(
                     path.join(config.output.path, OPENAPI_YML_FILENAME),
                     yaml.dump(openApiDefinitionWithCustomOverrides)
                 );
+                // eslint-disable-next-line no-console
                 console.log("wrote yml");
             }
+            // eslint-disable-next-line no-console
             console.log("sending exit status update");
             await generatorLoggingClient.sendUpdate(GeneratorUpdate.exitStatusUpdate(ExitStatusUpdate.successful({})));
+            // eslint-disable-next-line no-console
             console.log("sent exit status update");
         } catch (e) {
             // eslint-disable-next-line no-console
@@ -83,9 +96,12 @@ export async function writeOpenApi(mode: Mode, pathToConfig: string): Promise<vo
 
 async function loadIntermediateRepresentation(pathToFile: string): Promise<IntermediateRepresentation> {
     const irString = (await readFile(pathToFile)).toString();
+    // eslint-disable-next-line no-console
     console.log("irString", irString);
     const irJson = JSON.parse(irString);
+    // eslint-disable-next-line no-console
     console.log("irJson", irJson);
+    // eslint-disable-next-line no-console
     console.log("parsing ir");
     return IrSerialization.IntermediateRepresentation.parseOrThrow(irJson);
 }
