741e0bbde81433cb791eb8226f0da3672c96d4ef
Omar Rida <omarrida@Omars-MacBook-Pro-2.local>
2024-03-19 22:57:12 +0300

    add log statements to debug openapi generation

--- generators/openapi/CHANGELOG.md
+++ generators/openapi/CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## [0.0.30-rc0] - 2024-03-19
+- Fix: Add debug statements to diagnose hanging CLI on openapi generation.
+
 ## [0.0.30] - 2024-01-21
 
 - Chore: Intialize this changelog
--- generators/openapi/src/writeOpenApi.ts
+++ generators/openapi/src/writeOpenApi.ts
@@ -16,40 +16,53 @@ export type Mode = "stoplight" | "openapi";
 
 export async function writeOpenApi(mode: Mode, pathToConfig: string): Promise<void> {
     try {
+        console.log("beginning writeOpenApi");
         const configStr = await readFile(pathToConfig);
         const config = JSON.parse(configStr.toString()) as GeneratorConfig;
         const customConfig = getCustomConfig(config);
 
+        console.log("customConfig", customConfig);
         const generatorLoggingClient = new GeneratorLoggingWrapper(config);
 
         try {
+            console.log("sending update");
             await generatorLoggingClient.sendUpdate(
                 GeneratorUpdate.init({
                     packagesToPublish: []
                 })
             );
 
+            console.log("loading ir");
             const ir = await loadIntermediateRepresentation(config.irFilepath);
+            console.log("parsed ir", ir);
             const openApiDefinition = convertToOpenApi({
                 apiName: config.workspaceName,
                 ir,
                 mode
             });
+            console.log("openApiDefinition", openApiDefinition);
 
             const openApiDefinitionWithCustomOverrides = merge(customConfig.customOverrides, openApiDefinition);
+            console.log("openApiDefinitionWithCustomOverrides", openApiDefinitionWithCustomOverrides);
 
             if (customConfig.format === "json") {
+                console.log("writing json");
                 await writeFile(
                     path.join(config.output.path, OPENAPI_JSON_FILENAME),
                     JSON.stringify(openApiDefinitionWithCustomOverrides, undefined, 2)
                 );
+                console.log("wrote json");
             } else {
+                console.log("writing yml");
                 await writeFile(
                     path.join(config.output.path, OPENAPI_YML_FILENAME),
                     yaml.dump(openApiDefinitionWithCustomOverrides)
                 );
+                console.log("wrote yml");
             }
+            console.log("sending exit status update");
             await generatorLoggingClient.sendUpdate(GeneratorUpdate.exitStatusUpdate(ExitStatusUpdate.successful({})));
+            console.log("sent exit status update");
         } catch (e) {
             // eslint-disable-next-line no-console
             console.log("Encountered error", e);
@@ -70,6 +83,9 @@ export async function writeOpenApi(mode: Mode, pathToConfig: string): Promise<vo
 
 async function loadIntermediateRepresentation(pathToFile: string): Promise<IntermediateRepresentation> {
     const irString = (await readFile(pathToFile)).toString();
+    console.log("irString", irString);
     const irJson = JSON.parse(irString);
+    console.log("irJson", irJson);
+    console.log("parsing ir");
     return IrSerialization.IntermediateRepresentation.parseOrThrow(irJson);
 }
