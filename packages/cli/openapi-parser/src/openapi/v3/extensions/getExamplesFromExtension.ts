f22a5fa513786f6f1ea77dceb6f2368d93a17643
Andrew Jiang <andrew@buildwithfern.com>
2024-03-23 06:05:07 +1100

    feat: merge x-codeSamples with x-fern-examples (#3246)
    * feat: import x-codeSamples into x-fern-examples
    
    * nits
    
    * lint:monorepo:fix
    
    * getExtensionAndValidate
    
    * revert zod version
    
    * rename XCodeSamples to RedoclyCodeSamples
    
    * breadcrumbs
    
    * fix compile break
    
    * update test

---
+++ packages/cli/openapi-parser/src/openapi/v3/extensions/getExamplesFromExtension.ts
@@ -0,0 +1,119 @@
+import { EndpointExample, FernOpenapiIr } from "@fern-api/openapi-ir-sdk";
+import { RawSchemas } from "@fern-api/yaml-schema";
+import { OpenAPIV3 } from "openapi-types";
+import { getExtensionAndValidate } from "../../../getExtension";
+import { AbstractOpenAPIV3ParserContext } from "../AbstractOpenAPIV3ParserContext";
+import { OperationContext } from "../converters/contexts";
+import { RedoclyCodeSampleArraySchema, RedoclyCodeSampleSchema } from "../schemas/RedoclyCodeSampleSchema";
+import { OpenAPIExtension } from "./extensions";
+import { FernOpenAPIExtension } from "./fernExtensions";
+
+export function getExamplesFromExtension(
+    operationContext: OperationContext,
+    operationObject: OpenAPIV3.OperationObject,
+    context: AbstractOpenAPIV3ParserContext
+): EndpointExample[] {
+    const exampleEndpointCalls: RawSchemas.ExampleEndpointCallArraySchema =
+        getExtensionAndValidate(
+            operationObject,
+            FernOpenAPIExtension.EXAMPLES,
+            RawSchemas.ExampleEndpointCallArraySchema,
+            context,
+            [...operationContext.baseBreadcrumbs, `${operationContext.method} ${operationContext.path}`]
+        ) ?? [];
+
+    // TODO: Not validated
+    const fernExamples = exampleEndpointCalls.map(
+        (value): EndpointExample => ({
+            name: value.name,
+            pathParameters: value["path-parameters"]
+                ? Object.entries(value["path-parameters"]).map(([name, value]) => ({
+                      name,
+                      // TODO: Not validated
+                      value: value as FernOpenapiIr.FullExample
+                  }))
+                : undefined,
+            queryParameters: value["query-parameters"]
+                ? Object.entries(value["query-parameters"]).map(([name, value]) => ({
+                      name,
+                      // TODO: Not validated
+                      value: value as FernOpenapiIr.FullExample
+                  }))
+                : undefined,
+            headers: value.headers
+                ? Object.entries(value.headers).map(([name, value]) => ({
+                      name,
+                      // TODO: Not validated
+                      value: value as FernOpenapiIr.FullExample
+                  }))
+                : undefined,
+            request: value.request as FernOpenapiIr.FullExample,
+            response: value.response as FernOpenapiIr.FullExample,
+            codeSamples: (value["code-samples"] ?? []).map(
+                (value): FernOpenapiIr.CustomCodeSample =>
+                    "language" in value
+                        ? FernOpenapiIr.CustomCodeSample.language({
+                              name: value.name,
+                              language: value.language,
+                              code: value.code,
+                              install: value.install,
+                              description: value.docs
+                          })
+                        : FernOpenapiIr.CustomCodeSample.sdk({
+                              name: value.name,
+                              sdk: value.sdk === "c#" ? "csharp" : value.sdk,
+                              code: value.code,
+                              description: value.docs
+                          })
+            ),
+            description: undefined
+        })
+    );
+
+    const redoclyCodeSamplesKebabCase =
+        getExtensionAndValidate<RedoclyCodeSampleArraySchema>(
+            operationObject,
+            OpenAPIExtension.REDOCLY_CODE_SAMPLES_KEBAB,
+            RedoclyCodeSampleArraySchema,
+            context,
+            [...operationContext.baseBreadcrumbs, `${operationContext.method} ${operationContext.path}`]
+        ) ?? [];
+
+    const redoclyCodeSamplesCamelCase =
+        getExtensionAndValidate<RedoclyCodeSampleArraySchema>(
+            operationObject,
+            OpenAPIExtension.REDOCLY_CODE_SAMPLES_CAMEL,
+            RedoclyCodeSampleArraySchema,
+            context,
+            [...operationContext.baseBreadcrumbs, `${operationContext.method} ${operationContext.path}`]
+        ) ?? [];
+
+    const redoclyCodeSamples: RedoclyCodeSampleSchema[] = [
+        ...redoclyCodeSamplesCamelCase,
+        ...redoclyCodeSamplesKebabCase
+    ];
+
+    if (redoclyCodeSamples.length > 0) {
+        fernExamples.push({
+            name: undefined,
+            pathParameters: undefined,
+            queryParameters: undefined,
+            headers: undefined,
+            request: undefined,
+            response: undefined,
+            codeSamples: redoclyCodeSamples.map(
+                (value): FernOpenapiIr.CustomCodeSample =>
+                    FernOpenapiIr.CustomCodeSample.language({
+                        name: value.label ?? value.lang,
+                        language: value.lang,
+                        code: value.source,
+                        install: undefined,
+                        description: undefined
+                    })
+            ),
+            description: undefined
+        });
+    }
+
+    return fernExamples;
+}
