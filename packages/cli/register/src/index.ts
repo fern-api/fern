2e9b6cc7720d4da44aee08de3b0c72597b531062
Andrew Jiang <andrew@buildwithfern.com>
2024-04-09 20:45:50 -0400

    remove unused export

--- packages/cli/register/src/index.ts
+++ packages/cli/register/src/index.ts
@@ -1,3 +1,2 @@
-export { convertIrToDefaultNavigationConfigRoot } from "@fern-api/remote-workspace-runner/src/convertIrToNavigation";
 export { convertIrToFdrApi } from "./ir-to-fdr-converter/convertIrToFdrApi";
 export { registerApi } from "./registerApi";
