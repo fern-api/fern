19460e678d07564bdf237b12621a83f9d2ba39f7
dsinghvi <deep@usebirch.com>
2024-04-21 16:24:33 -0400

    fix inputs

--- packages/seed/src/commands/test/test-runner/DockerTestRunner.ts
+++ packages/seed/src/commands/test/test-runner/DockerTestRunner.ts
@@ -84,7 +84,7 @@ export class DockerTestRunner extends TestRunner {
                 fixtureName: fixture,
                 irVersion,
                 publishMetadata,
-                workspaceName: this.generator.workspaceName,
+                workspaceName: fixture,
                 context: taskContext
             });
         }
