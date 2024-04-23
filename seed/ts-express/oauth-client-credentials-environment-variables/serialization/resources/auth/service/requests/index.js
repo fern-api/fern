ff4e2932b2f40904339d7ca8308767248b211a55
Alex McKinney <alexmckinney01@gmail.com>
2024-04-22 23:53:22 -0700

    Update ts-express snapshots

---
+++ seed/ts-express/oauth-client-credentials-environment-variables/serialization/resources/auth/service/requests/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RefreshTokenRequest = exports.GetTokenRequest = void 0;
+var GetTokenRequest_1 = require("./GetTokenRequest");
+Object.defineProperty(exports, "GetTokenRequest", { enumerable: true, get: function () { return GetTokenRequest_1.GetTokenRequest; } });
+var RefreshTokenRequest_1 = require("./RefreshTokenRequest");
+Object.defineProperty(exports, "RefreshTokenRequest", { enumerable: true, get: function () { return RefreshTokenRequest_1.RefreshTokenRequest; } });
