04bd9cf562863b5168694922487b70b2a61699f9
dsinghvi <deep@usebirch.com>
2024-04-24 06:07:51 -0400

    fix

--- seed/ruby-sdk/code-samples/lib/types_export.rb
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "fern_code_samples/service/types/my_response"
--- seed/ruby-sdk/literal/lib/types_export.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "fern_literal/types/send_response"
-require_relative "fern_literal/reference/types/send_request"
