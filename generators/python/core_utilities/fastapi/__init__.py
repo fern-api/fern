f746c087d8a9c329977292fde84c01982a1fd609
armandobelardo <armandoubelardo@gmail.com>
2024-05-03 21:26:21 -0400

    run

--- generators/python/core_utilities/fastapi/__init__.py
+++ generators/python/core_utilities/fastapi/__init__.py
@@ -1,7 +1,14 @@
 from .datetime_utils import serialize_datetime
 from .exceptions import FernHTTPException, UnauthorizedException
+from .pydantic_utilities import pydantic_v1
 from .route_args import route_args
 from .security import BearerToken
-from .pydantic_utilities import pydantic_v1
 
-__all__ = ["FernHTTPException", "UnauthorizedException", "BearerToken", "route_args", "serialize_datetime"]
+__all__ = [
+    "FernHTTPException",
+    "UnauthorizedException",
+    "BearerToken",
+    "route_args",
+    "serialize_datetime",
+    "pydantic_v1",
+]
