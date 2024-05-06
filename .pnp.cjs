42013b6f002939e9b273a000fff28d84a6f75121
armandobelardo <armandoubelardo@gmail.com>
2024-05-06 15:44:12 -0400

    will fail since we don't have the latest tag

--- .pnp.cjs
+++ .pnp.cjs
@@ -6579,7 +6579,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["@fern-api/docker-utils", "workspace:packages/cli/generation/local-generation/docker-utils"],\
             ["@fern-api/fs-utils", "workspace:packages/commons/fs-utils"],\
-            ["@types/dockerode", "npm:3.3.9"],\
+            ["@types/dockerode", "npm:3.3.29"],\
             ["@types/jest", "npm:29.0.3"],\
             ["@types/node", "npm:18.7.18"],\
             ["depcheck", "npm:1.4.6"],\
@@ -10382,15 +10382,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/docker-modem", "npm:3.0.2"],\
             ["@types/node", "npm:18.7.18"],\
             ["@types/ssh2", "npm:1.11.4"]\
-          ],\
-          "linkType": "HARD"\
-        }],\
-        ["npm:3.3.9", {\
-          "packageLocation": "./.yarn/cache/@types-dockerode-npm-3.3.9-3b2c48453a-3d03c68add.zip/node_modules/@types/dockerode/",\
-          "packageDependencies": [\
-            ["@types/dockerode", "npm:3.3.9"],\
-            ["@types/docker-modem", "npm:3.0.2"],\
-            ["@types/node", "npm:18.7.18"]\
           ],\
           "linkType": "HARD"\
         }]\
---
+++ packages/cli/ete-tests/src/tests/upgrade-generator/upgrade-generator.test.ts
@@ -0,0 +1,25 @@
+import { AbsoluteFilePath, getDirectoryContents } from "@fern-api/fs-utils";
+import { cp } from "fs/promises";
+import path from "path";
+import tmp from "tmp-promise";
+import { runFernCli } from "../../utils/runFernCli";
+
+const FIXTURES_DIR = path.join(__dirname, "fixtures");
+
+describe("fern generator upgrade", () => {
+    it("fern generator upgrade", async () => {
+        // Create tmpdir and copy contents
+        const tmpDir = await tmp.dir();
+        const directory = AbsoluteFilePath.of(tmpDir.path);
+
+        cp(FIXTURES_DIR, directory, { recursive: true });
+
+        const outputPath = AbsoluteFilePath.of(path.join(directory, "fern", "generators.yml"));
+
+        await runFernCli(["generator", "upgrade"], {
+            cwd: directory
+        });
+
+        expect(await getDirectoryContents(outputPath)).toMatchSnapshot();
+    }, 60_000);
+});
