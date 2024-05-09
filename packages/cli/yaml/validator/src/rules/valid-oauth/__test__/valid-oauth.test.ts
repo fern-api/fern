2b538d64626029341e9aed1f9beea671f19faca8
Alex McKinney <alexmckinney01@gmail.com>
2024-05-09 11:48:40 -0400

    Fix test helper function

--- packages/cli/yaml/validator/src/rules/valid-oauth/__test__/valid-oauth.test.ts
+++ packages/cli/yaml/validator/src/rules/valid-oauth/__test__/valid-oauth.test.ts
@@ -69,20 +69,25 @@ describe("valid-oauth", () => {
         const expectedViolations: ValidationViolation[] = [
             {
                 message:
-                    "OAuth configuration for endpoint getToken cannot reference nested $request properties like '$request.credentials.client_id'; expected '$request.client-id' instead. For details, see the docs.",
+                    "OAuth configuration for endpoint getToken cannot reference nested $request properties like '$request.credentials.client_id'; expected '$request.client-id' instead.",
                 nodePath: ["service", "endpoints", "getToken"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             },
             {
                 message:
-                    "OAuth configuration for endpoint getToken cannot reference nested $request properties like '$request.credentials.client_secret'; expected '$request.client-secret' instead. For details, see the docs.",
+                    "OAuth configuration for endpoint getToken cannot reference nested $request properties like '$request.credentials.client_secret'; expected '$request.client-secret' instead.",
                 nodePath: ["service", "endpoints", "getToken"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             }
         ];
-        expect(violations).toEqual(expectedViolations);
+        expect(() =>
+            validateOAuthRuleViolations({
+                expected: expectedViolations,
+                actual: violations
+            })
+        ).not.toThrow();
     });
 
     it("invalid-missing", async () => {
@@ -98,48 +103,53 @@ describe("valid-oauth", () => {
         const expectedViolations: ValidationViolation[] = [
             {
                 message:
-                    "OAuth configuration for endpoint getTokenWithClientCredentials specifies 'access-token' $response.accessToken, which is not a valid 'access-token' type. For details, see the docs.",
+                    "OAuth configuration for endpoint getTokenWithClientCredentials specifies 'access-token' $response.accessToken, which is not a valid 'access-token' type.",
                 nodePath: ["service", "endpoints", "getTokenWithClientCredentials"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             },
             {
                 message:
-                    "OAuth configuration for endpoint getTokenWithClientCredentials specifies 'expires-in' $response.expiresIn, which is not a valid 'expires-in' type. For details, see the docs.",
+                    "OAuth configuration for endpoint getTokenWithClientCredentials specifies 'expires-in' $response.expiresIn, which is not a valid 'expires-in' type.",
                 nodePath: ["service", "endpoints", "getTokenWithClientCredentials"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             },
             {
                 message:
-                    "OAuth configuration for endpoint refreshToken specifies 'refresh-token' $request.refreshTokenDoesNotExist, which is not a valid 'refresh-token' type. For details, see the docs.",
+                    "OAuth configuration for endpoint refreshToken specifies 'refresh-token' $request.refreshTokenDoesNotExist, which is not a valid 'refresh-token' type.",
                 nodePath: ["service", "endpoints", "refreshToken"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             },
             {
                 message:
-                    "OAuth configuration for endpoint refreshToken specifies 'access-token' $response.accessTokenDoesNotExist, which is not a valid 'access-token' type. For details, see the docs.",
+                    "OAuth configuration for endpoint refreshToken specifies 'access-token' $response.accessTokenDoesNotExist, which is not a valid 'access-token' type.",
                 nodePath: ["service", "endpoints", "refreshToken"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             },
             {
                 message:
-                    "OAuth configuration for endpoint refreshToken specifies 'expires-in' $response.expiresInDoesNotExist, which is not a valid 'expires-in' type. For details, see the docs.",
+                    "OAuth configuration for endpoint refreshToken specifies 'expires-in' $response.expiresInDoesNotExist, which is not a valid 'expires-in' type.",
                 nodePath: ["service", "endpoints", "refreshToken"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             },
             {
                 message:
-                    "OAuth configuration for endpoint refreshToken specifies 'refresh-token' $response.refreshTokenDoesNotExist, which is not a valid 'refresh-token' type. For details, see the docs.",
+                    "OAuth configuration for endpoint refreshToken specifies 'refresh-token' $response.refreshTokenDoesNotExist, which is not a valid 'refresh-token' type.",
                 nodePath: ["service", "endpoints", "refreshToken"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             }
         ];
-        expect(violations).toEqual(expectedViolations);
+        expect(() =>
+            validateOAuthRuleViolations({
+                expected: expectedViolations,
+                actual: violations
+            })
+        ).not.toThrow();
     });
 
     it("invalid-types", async () => {
@@ -155,26 +165,49 @@ describe("valid-oauth", () => {
         const expectedViolations: ValidationViolation[] = [
             {
                 message:
-                    "OAuth configuration for endpoint getToken is missing a valid client-id, such as '$request.client_id'. For details, see the docs.",
+                    "OAuth configuration for endpoint getToken is missing a valid client-id, such as '$request.client_id'.",
                 nodePath: ["service", "endpoints", "getToken"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             },
             {
                 message:
-                    "OAuth configuration for endpoint getToken is missing a valid client-secret, such as '$request.client_secret'. For details, see the docs.",
+                    "OAuth configuration for endpoint getToken is missing a valid client-secret, such as '$request.client_secret'.",
                 nodePath: ["service", "endpoints", "getToken"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             },
             {
                 message:
-                    "OAuth configuration for endpoint getToken specifies 'scopes' $request.scopes, which is not a valid 'scopes' type. For details, see the docs.",
+                    "OAuth configuration for endpoint getToken specifies 'scopes' $request.scopes, which is not a valid 'scopes' type.",
                 nodePath: ["service", "endpoints", "getToken"],
                 relativeFilepath: RelativeFilePath.of("auth.yml"),
                 severity: "error"
             }
         ];
-        expect(violations).toEqual(expectedViolations);
+        expect(() =>
+            validateOAuthRuleViolations({
+                expected: expectedViolations,
+                actual: violations
+            })
+        ).not.toThrow();
     });
 });
+
+// validateOAuthRuleViolations ensures all of the expected rule violations match,
+// but only verifies the message prefix because the output differs in certain
+// terminal environments.
+function validateOAuthRuleViolations({
+    expected,
+    actual
+}: {
+    expected: ValidationViolation[];
+    actual: ValidationViolation[];
+}): void {
+    expected.forEach((expected, index) => {
+        const actualMessage = actual?.[index]?.message;
+        expect(actualMessage).toBeDefined();
+        expect(actualMessage?.startsWith(expected.message)).toBe(true);
+    });
+    expect(actual.map(({ message, ...rest }) => rest)).toEqual(actual.map(({ message, ...rest }) => rest));
+}
