2dbe5100f06d04747e7a708fa057a377ac1df1dd
armandobelardo <armandoubelardo@gmail.com>
2024-05-08 20:32:01 -0400

    add some logging

--- packages/cli/configuration/src/generators-yml/addGenerator.ts
+++ packages/cli/configuration/src/generators-yml/addGenerator.ts
@@ -20,6 +20,9 @@ function getGeneratorNameOrThrow(generatorName: string, context: TaskContext): G
 
 async function getLatestGeneratorVersion(generatorName: string): Promise<string> {
     const docker = new Docker();
+    console.log("Testing docker connection...");
+    console.log(docker.checkAuth({}));
+    console.log(docker.ping());
     const image = await docker.getImage(`${generatorName}:latest`).inspect();
 
     // This assumes we have a label of the form version=x.y.z
