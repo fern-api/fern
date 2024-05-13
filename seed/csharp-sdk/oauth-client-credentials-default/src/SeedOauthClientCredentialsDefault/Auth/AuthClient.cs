6299f6f35f307f42628f40f14050441f1c743187
Alex McKinney <alexmckinney01@gmail.com>
2024-05-13 15:42:23 -0400

    Update OAuth snapshots for csharp

--- seed/csharp-sdk/oauth-client-credentials-default/src/SeedOauthClientCredentialsDefault/Auth/AuthClient.cs
+++ seed/csharp-sdk/oauth-client-credentials-default/src/SeedOauthClientCredentialsDefault/Auth/AuthClient.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using SeedOauthClientCredentialsDefault;
 
 namespace SeedOauthClientCredentialsDefault;
@@ -11,5 +12,21 @@ public class AuthClient
         _client = client;
     }
 
-    public async void GetTokenAsync() { }
+    public async Task<TokenResponse> GetTokenAsync(GetTokenRequest request)
+    {
+        var response = await _client.MakeRequestAsync(
+            new RawClient.ApiRequest
+            {
+                Method = HttpMethod.Post,
+                Path = "/token",
+                Body = request
+            }
+        );
+        string responseBody = await response.Raw.Content.ReadAsStringAsync();
+        if (response.StatusCode >= 200 && response.StatusCode < 400)
+        {
+            return JsonSerializer.Deserialize<TokenResponse>(responseBody);
+        }
+        throw new Exception();
+    }
 }
--- seed/csharp-sdk/oauth-client-credentials-default/src/SeedOauthClientCredentialsDefault/SeedOauthClientCredentialsDefaultClient.cs
+++ seed/csharp-sdk/oauth-client-credentials-default/src/SeedOauthClientCredentialsDefault/SeedOauthClientCredentialsDefaultClient.cs
@@ -6,10 +6,10 @@ public partial class SeedOauthClientCredentialsDefaultClient
 {
     private RawClient _client;
 
-    public SeedOauthClientCredentialsDefaultClient(string token, ClientOptions clientOptions)
+    public SeedOauthClientCredentialsDefaultClient(string token, ClientOptions clientOptions = null)
     {
         _client = new RawClient(
-            new Dictionary<string, string> { { "X-Fern-Language", "C#" }, },
+            new Dictionary<string, string>() { { "X-Fern-Language", "C#" }, },
             clientOptions ?? new ClientOptions()
         );
         Auth = new AuthClient(_client);
