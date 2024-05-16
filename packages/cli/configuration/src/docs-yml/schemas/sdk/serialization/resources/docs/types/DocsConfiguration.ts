914f4bed409ecafcc27612c71a59e4aaeb54dd7e
Andrew Jiang <andrew@buildwithfern.com>
2024-05-16 11:19:02 -0400

    feat: add seo and metadata configuration in docs.yml

--- packages/cli/configuration/src/docs-yml/schemas/sdk/serialization/resources/docs/types/DocsConfiguration.ts
+++ packages/cli/configuration/src/docs-yml/schemas/sdk/serialization/resources/docs/types/DocsConfiguration.ts
@@ -13,7 +13,7 @@ export const DocsConfiguration: core.serialization.ObjectSchema<
     instances: core.serialization.list(
         core.serialization.lazyObject(async () => (await import("../../..")).DocsInstances)
     ),
-    navigation: core.serialization.lazy(async () => (await import("../../..")).NavigationConfig).optional(),
+    title: core.serialization.string().optional(),
     tabs: core.serialization
         .record(
             core.serialization.lazy(async () => (await import("../../..")).TabId),
@@ -23,7 +23,19 @@ export const DocsConfiguration: core.serialization.ObjectSchema<
     versions: core.serialization
         .list(core.serialization.lazyObject(async () => (await import("../../..")).VersionConfig))
         .optional(),
-    title: core.serialization.string().optional(),
+    navigation: core.serialization.lazy(async () => (await import("../../..")).NavigationConfig).optional(),
+    navbarLinks: core.serialization.property(
+        "navbar-links",
+        core.serialization.list(core.serialization.lazy(async () => (await import("../../..")).NavbarLink)).optional()
+    ),
+    footerLinks: core.serialization.property(
+        "footer-links",
+        core.serialization.list(core.serialization.lazy(async () => (await import("../../..")).FooterLink)).optional()
+    ),
+    metadata: core.serialization.lazyObject(async () => (await import("../../..")).MetadataConfig).optional(),
+    redirects: core.serialization
+        .list(core.serialization.lazyObject(async () => (await import("../../..")).RedirectConfig))
+        .optional(),
     logo: core.serialization.lazyObject(async () => (await import("../../..")).LogoConfiguration).optional(),
     favicon: core.serialization.string().optional(),
     backgroundImage: core.serialization.property(
@@ -31,12 +43,9 @@ export const DocsConfiguration: core.serialization.ObjectSchema<
         core.serialization.lazy(async () => (await import("../../..")).BackgroundImageConfiguration).optional()
     ),
     colors: core.serialization.lazyObject(async () => (await import("../../..")).ColorsConfiguration).optional(),
-    navbarLinks: core.serialization.property(
-        "navbar-links",
-        core.serialization.list(core.serialization.lazy(async () => (await import("../../..")).NavbarLink)).optional()
-    ),
     typography: core.serialization.lazyObject(async () => (await import("../../..")).DocsTypographyConfig).optional(),
     layout: core.serialization.lazyObject(async () => (await import("../../..")).LayoutConfig).optional(),
+    integrations: core.serialization.lazyObject(async () => (await import("../../..")).IntegrationsConfig).optional(),
     css: core.serialization.lazy(async () => (await import("../../..")).CssConfig).optional(),
     js: core.serialization.lazy(async () => (await import("../../..")).JsConfig).optional(),
 });
@@ -44,17 +53,21 @@ export const DocsConfiguration: core.serialization.ObjectSchema<
 export declare namespace DocsConfiguration {
     interface Raw {
         instances: serializers.DocsInstances.Raw[];
-        navigation?: serializers.NavigationConfig.Raw | null;
+        title?: string | null;
         tabs?: Record<serializers.TabId.Raw, serializers.TabConfig.Raw> | null;
         versions?: serializers.VersionConfig.Raw[] | null;
-        title?: string | null;
+        navigation?: serializers.NavigationConfig.Raw | null;
+        "navbar-links"?: serializers.NavbarLink.Raw[] | null;
+        "footer-links"?: serializers.FooterLink.Raw[] | null;
+        metadata?: serializers.MetadataConfig.Raw | null;
+        redirects?: serializers.RedirectConfig.Raw[] | null;
         logo?: serializers.LogoConfiguration.Raw | null;
         favicon?: string | null;
         "background-image"?: serializers.BackgroundImageConfiguration.Raw | null;
         colors?: serializers.ColorsConfiguration.Raw | null;
-        "navbar-links"?: serializers.NavbarLink.Raw[] | null;
         typography?: serializers.DocsTypographyConfig.Raw | null;
         layout?: serializers.LayoutConfig.Raw | null;
+        integrations?: serializers.IntegrationsConfig.Raw | null;
         css?: serializers.CssConfig.Raw | null;
         js?: serializers.JsConfig.Raw | null;
     }
