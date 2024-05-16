3764b83522805a7f10e1e6dd903570cf9cf59396
dsinghvi <deep@usebirch.com>
2024-05-16 12:48:27 -0400

    fixed

--- packages/cli/docs-preview/src/downloadLocalDocsBundle.ts
+++ packages/cli/docs-preview/src/downloadLocalDocsBundle.ts
@@ -1,6 +1,6 @@
 import { AbsoluteFilePath, doesPathExist, join, RelativeFilePath } from "@fern-api/fs-utils";
 import { createWriteStream } from "fs";
-import { readFile, writeFile } from "fs/promises";
+import { mkdir, readFile, rm, writeFile } from "fs/promises";
 import { homedir } from "os";
 import { pipeline } from "stream/promises";
 import tmp from "tmp-promise";
@@ -8,9 +8,18 @@ import xml2js from "xml2js";
 
 const ETAG_FILENAME = "etag";
 const PREVIEW_FOLDER_NAME = "preview";
+const BUNDLE_FOLDER_NAME = "bundle";
 const LOCAL_STORAGE_FOLDER = process.env.LOCAL_STORAGE_FOLDER ?? ".fern";
 
 function getPathToPreviewFolder(): AbsoluteFilePath {
+    return join(
+        AbsoluteFilePath.of(homedir()),
+        RelativeFilePath.of(LOCAL_STORAGE_FOLDER),
+        RelativeFilePath.of(PREVIEW_FOLDER_NAME)
+    );
+}
+
+function getPathToBundleFolder(): AbsoluteFilePath {
     return join(
         AbsoluteFilePath.of(homedir()),
         RelativeFilePath.of(LOCAL_STORAGE_FOLDER),
@@ -27,7 +36,19 @@ function getPathToEtagFile(): AbsoluteFilePath {
     );
 }
 
-export async function downloadBundle({ bucketUrl }: { bucketUrl: string }) {
+export declare namespace DownloadLocalBundle {
+    type Result = SuccesResult | FailureResult;
+
+    interface SuccesResult {
+        type: "success";
+    }
+
+    interface FailureResult {
+        type: "failure";
+    }
+}
+
+export async function downloadBundle({ bucketUrl }: { bucketUrl: string }): Promise<DownloadLocalBundle.Result> {
     const response = await fetch(bucketUrl);
     const body = await response.text();
     const parser = new xml2js.Parser();
@@ -39,7 +60,9 @@ export async function downloadBundle({ bucketUrl }: { bucketUrl: string }) {
     const currentETag = (await doesPathExist(eTagFilepath)) ? (await readFile(eTagFilepath)).toString() : undefined;
     if (currentETag != null && currentETag === eTag) {
         // The bundle is already downloaded
-        return;
+        return {
+            type: "success"
+        };
     }
 
     // create tmp directory
@@ -49,11 +72,21 @@ export async function downloadBundle({ bucketUrl }: { bucketUrl: string }) {
     // download docs ui
     const request = await fetch(`${bucketUrl}/${key}`);
     const outputZipPath = join(absoluteDirectoryToTmpDir, RelativeFilePath.of("output.zip"));
-    await pipeline(await request.blob(), createWriteStream(outputZipPath));
-    if (await doesPathExist(absolutePathToLocalOutput)) {
-        await rm(absolutePathToLocalOutput, { recursive: true });
+
+    const contents = request.body;
+    if (contents == null) {
+        return {
+            type: "failure"
+        };
     }
-    await mkdir(absolutePathToLocalOutput, { recursive: true });
+    await pipeline(contents, createWriteStream(outputZipPath));
+
+    const absolutePathToPreviewFolder = getPathToPreviewFolder();
+    if (await doesPathExist(absolutePathToPreviewFolder)) {
+        await rm(absolutePathToPreviewFolder, { recursive: true });
+    }
+    const absolutePathToTmpBundleFolder = join(absolutePathToPreviewFolder, RelativeFilePath.of(BUNDLE_FOLDER_NAME));
+    await mkdir(absolutePathToTmpBundleFolder, { recursive: true });
     await decompress(outputZipPath, absolutePathToLocalOutput);
 
     // write etag
