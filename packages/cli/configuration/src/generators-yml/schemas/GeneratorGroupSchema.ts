e17e745a99492b1c26884e47cc04f8529a21abac
Armando Belardo <11140328+armandobelardo@users.noreply.github.com>
2024-05-20 21:41:31 -0400

    improvement: add in config to enrich pypi metadata (#3660)

--- packages/cli/configuration/src/generators-yml/schemas/GeneratorGroupSchema.ts
+++ packages/cli/configuration/src/generators-yml/schemas/GeneratorGroupSchema.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 import { GeneratorInvocationSchema } from "./GeneratorInvocationSchema";
+import { OutputMetadataSchema } from "./OutputMetadataSchema";
 
 export const GeneratorGroupSchema = z.strictObject({
     audiences: z.optional(z.array(z.string())),
-    generators: z.array(GeneratorInvocationSchema)
+    generators: z.array(GeneratorInvocationSchema),
+    metadata: z.optional(OutputMetadataSchema)
 });
 
 export type GeneratorGroupSchema = z.infer<typeof GeneratorGroupSchema>;
