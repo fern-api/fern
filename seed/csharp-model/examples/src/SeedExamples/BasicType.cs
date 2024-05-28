07ef4be43276a6243768e2993361444630cdc421
dsinghvi <deep@usebirch.com>
2024-05-28 08:55:40 +0200

    fix model

--- seed/csharp-model/examples/src/SeedExamples/BasicType.cs
+++ seed/csharp-model/examples/src/SeedExamples/BasicType.cs
@@ -1,5 +1,7 @@
 using System.Runtime.Serialization;
 
+#nullable enable
+
 namespace SeedExamples;
 
 public enum BasicType
--- seed/csharp-model/idempotency-headers/src/SeedIdempotencyHeaders/Payment/Currency.cs
+++ seed/csharp-model/idempotency-headers/src/SeedIdempotencyHeaders/Payment/Currency.cs
@@ -1,5 +1,7 @@
 using System.Runtime.Serialization;
 
+#nullable enable
+
 namespace SeedIdempotencyHeaders;
 
 public enum Currency
