4ce4530a1d82d2b7ba57e1aebff55479355f4f96
dan-thera <danielb14065@gmail.com>
2024-05-29 18:48:21 -0400

    almost there

--- generators/java/generator-utils/src/main/java/com/fern/java/AbstractGeneratorCli.java
+++ generators/java/generator-utils/src/main/java/com/fern/java/AbstractGeneratorCli.java
@@ -202,8 +202,8 @@ public abstract class AbstractGeneratorCli<T extends ICustomConfig, K extends Do
                 mavenGithubPublishInfo.flatMap(MavenGithubPublishInfo::getSignature)));
         // write files to disk
         generatedFiles.forEach(generatedFile -> generatedFile.write(outputDirectory, false, Optional.empty()));
-        runCommandBlocking(new String[] {"gradle", "wrapper"}, outputDirectory, Collections.emptyMap());
-        runCommandBlocking(new String[] {"gradle", "spotlessApply"}, outputDirectory, Collections.emptyMap());
+        runCommandBlocking(new String[] {"./gradlew", "wrapper"}, outputDirectory, Collections.emptyMap());
+        runCommandBlocking(new String[] {"./gradlew", "spotlessApply"}, outputDirectory, Collections.emptyMap());
     }
 
     public abstract void runInGithubModeHook(
