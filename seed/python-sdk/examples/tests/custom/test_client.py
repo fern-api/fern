9e57409c3216f755daa6fc06611f18d13b9b8531
Deep Singhvi <deep@buildwithfern.com>
2024-06-01 06:08:28 -0400

    (feat, python): write out example ids in generated snippets (#3750)

--- seed/python-sdk/examples/tests/custom/test_client.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-# Get started with writing tests with pytest at https://docs.pytest.org
-@pytest.mark.skip(reason="Unimplemented")
-def test_client() -> None:
-    assert True == True
