f3885de535b0f2efddb73bdf820d633d96b77be4
armandobelardo <armandoubelardo@gmail.com>
2024-06-04 06:55:52 -0400

    fix, python: update timeout parameter docs

--- seed/python-sdk/validation/src/seed/client.py
+++ seed/python-sdk/validation/src/seed/client.py
@@ -25,7 +25,7 @@ class SeedValidation:
         The base url to use for requests from the client.
 
     timeout : typing.Optional[float]
-        The timeout to be used, in seconds, for requests by default the timeout is 60 seconds, unless a custom httpx client is used, in which case a default is not set.
+        The timeout to be used, in seconds, for requests. By default the timeout is 60 seconds, unless a custom httpx client is used, in which case this default is not enforced.
 
     follow_redirects : typing.Optional[bool]
         Whether the default httpx client follows redirects or not, this is irrelevant if a custom httpx client is passed in.
@@ -162,7 +162,7 @@ class AsyncSeedValidation:
         The base url to use for requests from the client.
 
     timeout : typing.Optional[float]
-        The timeout to be used, in seconds, for requests by default the timeout is 60 seconds, unless a custom httpx client is used, in which case a default is not set.
+        The timeout to be used, in seconds, for requests. By default the timeout is 60 seconds, unless a custom httpx client is used, in which case this default is not enforced.
 
     follow_redirects : typing.Optional[bool]
         Whether the default httpx client follows redirects or not, this is irrelevant if a custom httpx client is passed in.
