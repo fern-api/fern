fd802a464999787317d9ac2f57a6b9b78c9e25a4
armandobelardo <armandoubelardo@gmail.com>
2024-06-06 18:27:47 -0400

    precommit

--- generators/python/setup-python.sh
+++ generators/python/setup-python.sh
@@ -1,4 +1,4 @@
-# Use 3.9 because some of our internal SDKs are on extremely old generator 
+# Use 3.9 because some of our internal SDKs are on extremely old generator
 # versions that don't support 3.8
 export PYENV_ROOT="$HOME/.pyenv"
 '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"'
