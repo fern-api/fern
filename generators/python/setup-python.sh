4f237812bd8289012411fc4cf7957dd24daefd6c
Dan Burke <danielb14065@gmail.com>
2024-06-07 11:34:52 -0400

    python: improve seed setup script (#3810)

--- generators/python/setup-python.sh
+++ generators/python/setup-python.sh
@@ -1,7 +1,7 @@
 # Use 3.9 because some of our internal SDKs are on extremely old generator
 # versions that don't support 3.8
 export PYENV_ROOT="$HOME/.pyenv"
-'[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"'
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv shell 3.9
 pip install poetry
