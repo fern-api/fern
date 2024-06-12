5dcee245e68707526d8e9ddaaede621c7b21c011
dsinghvi <deep@usebirch.com>
2024-06-12 06:53:24 -0400

    (fix): commit case insensitive file changes with git

---
+++ seed/csharp-sdk/enum/src/SeedEnum/QueryParam/Requests/SendEnumAsQueryParamRequest.cs
@@ -0,0 +1,17 @@
+using OneOf;
+using SeedEnum;
+
+#nullable enable
+
+namespace SeedEnum;
+
+public class SendEnumAsQueryParamRequest
+{
+    public Operand Operand { get; init; }
+
+    public Operand? MaybeOperand { get; init; }
+
+    public OneOf<Color, Operand> OperandOrColor { get; init; }
+
+    public OneOf<Color, Operand>? MaybeOperandOrColor { get; init; }
+}
