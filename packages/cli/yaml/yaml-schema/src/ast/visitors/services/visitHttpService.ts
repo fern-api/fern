5b82af836966dca25f44fdf59f16921426c76b97
dsinghvi <deep@usebirch.com>
2024-06-13 20:34:16 -0400

    (fix): handle code samples without accompanying examples

--- packages/cli/yaml/yaml-schema/src/ast/visitors/services/visitHttpService.ts
+++ packages/cli/yaml/yaml-schema/src/ast/visitors/services/visitHttpService.ts
@@ -299,6 +299,17 @@ async function visitExampleEndpointCall({
     endpoint: RawSchemas.HttpEndpointSchema;
     example: RawSchemas.ExampleEndpointCallSchema;
 }): Promise<void> {
+    // if an example is entirely empty, just skip it
+    if (
+        example.headers == null &&
+        example["path-parameters"] == null &&
+        example["query-parameters"] == null &&
+        example.request == null &&
+        example.response == null
+    ) {
+        return;
+    }
+
     await visitor.exampleHttpEndpointCall?.(
         {
             service,
