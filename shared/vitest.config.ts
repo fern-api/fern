b6769228b2bf42033968d603e81dd7a4d81f4bff
dsinghvi <deep@usebirch.com>
2024-06-16 12:39:53 -0400

    ignore lib files when running vitest

--- shared/vitest.config.ts
+++ shared/vitest.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     test: {
-        globals: true
+        globals: true,
+        exclude: [
+            "**/node_modules/**",
+            "**/dist/**",
+            "**/cypress/**",
+            "**/.{idea,git,cache,output,temp}/**",
+            "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+            "**/lib/**"
+        ]
     }
 });
