554ddcc734f0b5f834599da95942d5ee84ff6bf2
dsinghvi <deep@usebirch.com>
2024-06-21 19:13:47 -0400

    (fix): dont leak json decode errors to user

--- seed/python-sdk/imdb/src/seed/imdb/client.py
+++ seed/python-sdk/imdb/src/seed/imdb/client.py
@@ -95,11 +95,11 @@ class ImdbClient:
         _response = self._client_wrapper.httpx_client.request(
             f"movies/{jsonable_encoder(movie_id)}", method="GET", request_options=request_options
         )
-        if 200 <= _response.status_code < 300:
-            return pydantic_v1.parse_obj_as(Movie, _response.json())  # type: ignore
-        if _response.status_code == 404:
-            raise MovieDoesNotExistError(pydantic_v1.parse_obj_as(MovieId, _response.json()))  # type: ignore
         try:
+            if 200 <= _response.status_code < 300:
+                return pydantic_v1.parse_obj_as(Movie, _response.json())  # type: ignore
+            if _response.status_code == 404:
+                raise MovieDoesNotExistError(pydantic_v1.parse_obj_as(MovieId, _response.json()))  # type: ignore
             _response_json = _response.json()
         except JSONDecodeError:
             raise ApiError(status_code=_response.status_code, body=_response.text)
