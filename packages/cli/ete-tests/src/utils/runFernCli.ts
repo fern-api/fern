a3f5fd0144bdd02652a6492ff6a83f2b9b451caf
Andrew Jiang <andrew@buildwithfern.com>
2024-06-24 13:29:02 -0400

    add check exit code to docsDev.test.ts

--- packages/cli/ete-tests/src/utils/runFernCli.ts
+++ packages/cli/ete-tests/src/utils/runFernCli.ts
@@ -1,5 +1,5 @@
 import { loggingExeca } from "@fern-api/logging-execa";
-import { ExecaChildProcess, Options } from "execa";
+import type { ExecaChildProcess, Options } from "execa";
 import path from "path";
 
 export async function runFernCli(args: string[], options?: Options): Promise<ExecaChildProcess> {
