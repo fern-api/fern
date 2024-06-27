58b6e8634c7af6fced09caa063a9a336607e4362
dsinghvi <deep@usebirch.com>
2024-06-27 07:59:19 -0400

    (fix, typescript): auto pagination handles optional results arrays

--- generators/typescript/utils/contexts/src/model-context/type/TypeContext.ts
+++ generators/typescript/utils/contexts/src/model-context/type/TypeContext.ts
@@ -25,4 +25,5 @@ export interface TypeContext {
     getGeneratedType: (typeName: DeclaredTypeName) => GeneratedType;
     getGeneratedTypeById: (typeId: TypeId) => GeneratedType;
     getGeneratedExample: (example: ExampleTypeReference) => GeneratedTypeReferenceExample;
+    getItemTypeFromListOrOptionalList: (typeReference: TypeReference) => TypeReference | undefined;
 }
