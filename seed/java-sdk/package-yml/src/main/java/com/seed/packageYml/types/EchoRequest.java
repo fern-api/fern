71b48b845a3ce6c385a78b04fe601529e75ff11c
dan-thera <danielb14065@gmail.com>
2024-07-02 21:16:38 -0400

    package-yml seed

--- seed/java-sdk/package-yml/src/main/java/com/seed/packageYml/types/EchoRequest.java
+++ seed/java-sdk/package-yml/src/main/java/com/seed/packageYml/types/EchoRequest.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
 @JsonDeserialize(builder = EchoRequest.Builder.class)
 public final class EchoRequest {
     private final String name;
