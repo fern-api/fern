ac0f015830e70b6331cee4503390a46ee013b892
mikemilla <mike@mikemiller.design>
2024-07-08 19:38:39 -0400

    Template Generator

--- generators/swift/codegen/src/ast/index.ts
+++
@@ -1,9 +0,0 @@
-export { AccessLevel } from "./AccessLevel";
-export { Class } from "./Class";
-export { File } from "./File";
-export { FileHeader } from "./FileHeader";
-export { Func } from "./Func";
-export { FunctionModifier } from "./FunctionModifier";
-export { Import } from "./Import";
-export { Param } from "./Param";
-
