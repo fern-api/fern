57a7411b2330ee1e5fe9fcbc96655811f9c89712
mikemilla <mike@mikemiller.design>
2024-07-09 13:06:04 -0400

    Swift IR dep update

--- .pnp.cjs
+++ .pnp.cjs
@@ -60,7 +60,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         "reference": "workspace:generators/swift/codegen"\
       },\
       {\
-        "name": "@fern-api/SHORT_LANG-codegen",\
+        "name": "@fern-api/template-codegen",\
         "reference": "workspace:generators/template/codegen"\
       },\
       {\
@@ -379,7 +379,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
     "enableTopLevelFallback": true,\
     "ignorePatternData": "(^(?:\\\\.yarn\\\\/sdks(?:\\\\/(?!\\\\.{1,2}(?:\\\\/|$))(?:(?:(?!(?:^|\\\\/)\\\\.{1,2}(?:\\\\/|$)).)*?)|$))$)",\
     "fallbackExclusionList": [\
-      ["@fern-api/SHORT_LANG-codegen", ["workspace:generators/template/codegen"]],\
       ["@fern-api/auth", ["workspace:packages/cli/auth"]],\
       ["@fern-api/browser-playground", ["workspace:generators/typescript/playground/browser"]],\
       ["@fern-api/cli", ["workspace:packages/cli/cli"]],\
@@ -426,6 +425,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
       ["@fern-api/semver-utils", ["workspace:packages/cli/semver-utils"]],\
       ["@fern-api/swift-codegen", ["workspace:generators/swift/codegen"]],\
       ["@fern-api/task-context", ["workspace:packages/cli/task-context"]],\
+      ["@fern-api/template-codegen", ["workspace:generators/template/codegen"]],\
       ["@fern-api/validator", ["workspace:packages/cli/yaml/validator"]],\
       ["@fern-api/workspace-loader", ["workspace:packages/cli/workspace-loader"]],\
       ["@fern-api/yaml-formatter", ["workspace:packages/cli/yaml/formatter"]],\
@@ -5049,30 +5049,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
-      ["@fern-api/SHORT_LANG-codegen", [\
-        ["workspace:generators/template/codegen", {\
-          "packageLocation": "./generators/template/codegen/",\
-          "packageDependencies": [\
-            ["@fern-api/SHORT_LANG-codegen", "workspace:generators/template/codegen"],\
-            ["@fern-api/core-utils", "workspace:packages/commons/core-utils"],\
-            ["@fern-api/fs-utils", "workspace:packages/commons/fs-utils"],\
-            ["@fern-api/generator-commons", "workspace:generators/commons"],\
-            ["@fern-api/logging-execa", "workspace:packages/commons/logging-execa"],\
-            ["@types/jest", "npm:29.5.12"],\
-            ["@types/lodash-es", "npm:4.17.12"],\
-            ["@types/node", "npm:18.19.39"],\
-            ["depcheck", "npm:1.4.7"],\
-            ["eslint", "npm:8.57.0"],\
-            ["jest", "virtual:816fb67d993b0978271f762d4ccbec7209ef2546c234ca6e241662d44336c8e32c1c3c07189cfe14b67974a4840e1ed140408a7403bf9deb68c1953445072efe#npm:29.7.0"],\
-            ["lodash-es", "npm:4.17.21"],\
-            ["organize-imports-cli", "npm:0.10.0"],\
-            ["prettier", "npm:2.8.8"],\
-            ["typescript", "patch:typescript@npm%3A4.6.4#~builtin<compat/typescript>::version=4.6.4&hash=5d3a66"],\
-            ["zod", "npm:3.23.8"]\
-          ],\
-          "linkType": "SOFT"\
-        }]\
-      ]],\
       ["@fern-api/auth", [\
         ["workspace:packages/cli/auth", {\
           "packageLocation": "./packages/cli/auth/",\
@@ -5224,7 +5200,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/dockerode", "npm:3.3.29"],\
             ["@types/jest", "npm:29.5.12"],\
             ["@types/js-yaml", "npm:4.0.9"],\
-            ["@types/lodash-es", "npm:4.17.12"],\
             ["@types/node", "npm:18.19.39"],\
             ["@types/tinycolor2", "npm:1.4.6"],\
             ["depcheck", "npm:1.4.7"],\
@@ -6405,7 +6380,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@fern-api/fs-utils", "workspace:packages/commons/fs-utils"],\
             ["@fern-api/generator-commons", "workspace:generators/commons"],\
             ["@fern-api/logging-execa", "workspace:packages/commons/logging-execa"],\
-            ["@fern-fern/ir-sdk", "npm:32.0.0"],\
+            ["@fern-fern/ir-sdk", "npm:46.2.0"],\
             ["@types/jest", "npm:29.5.12"],\
             ["@types/node", "npm:18.19.39"],\
             ["depcheck", "npm:1.4.7"],\
@@ -6433,6 +6408,28 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["organize-imports-cli", "npm:0.10.0"],\
             ["prettier", "npm:2.8.8"],\
             ["typescript", "patch:typescript@npm%3A4.6.4#~builtin<compat/typescript>::version=4.6.4&hash=5d3a66"]\
+          ],\
+          "linkType": "SOFT"\
+        }]\
+      ]],\
+      ["@fern-api/template-codegen", [\
+        ["workspace:generators/template/codegen", {\
+          "packageLocation": "./generators/template/codegen/",\
+          "packageDependencies": [\
+            ["@fern-api/template-codegen", "workspace:generators/template/codegen"],\
+            ["@fern-api/core-utils", "workspace:packages/commons/core-utils"],\
+            ["@fern-api/fs-utils", "workspace:packages/commons/fs-utils"],\
+            ["@fern-api/generator-commons", "workspace:generators/commons"],\
+            ["@fern-api/logging-execa", "workspace:packages/commons/logging-execa"],\
+            ["@types/jest", "npm:29.5.12"],\
+            ["@types/node", "npm:18.19.39"],\
+            ["depcheck", "npm:1.4.7"],\
+            ["eslint", "npm:8.57.0"],\
+            ["jest", "virtual:816fb67d993b0978271f762d4ccbec7209ef2546c234ca6e241662d44336c8e32c1c3c07189cfe14b67974a4840e1ed140408a7403bf9deb68c1953445072efe#npm:29.7.0"],\
+            ["organize-imports-cli", "npm:0.10.0"],\
+            ["prettier", "npm:2.8.8"],\
+            ["typescript", "patch:typescript@npm%3A4.6.4#~builtin<compat/typescript>::version=4.6.4&hash=5d3a66"],\
+            ["zod", "npm:3.23.8"]\
           ],\
           "linkType": "SOFT"\
         }]\
