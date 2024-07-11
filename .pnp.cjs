6bd4d68aca498b01634db9e79c709cfa0573e5e2
dsinghvi <deep@usebirch.com>
2024-07-11 08:59:31 -0400

    fix yarn install

--- .pnp.cjs
+++ .pnp.cjs
@@ -9379,7 +9379,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./generators/typescript/utils/core-utilities/utils/",\
           "packageDependencies": [\
             ["@fern-typescript/utils", "workspace:generators/typescript/utils/core-utilities/utils"],\
-            ["@types/jest", "npm:29.0.3"],\
+            ["@types/jest", "npm:29.5.12"],\
             ["@types/node", "npm:18.7.18"],\
             ["depcheck", "npm:1.4.6"],\
             ["eslint", "npm:8.56.0"],\
