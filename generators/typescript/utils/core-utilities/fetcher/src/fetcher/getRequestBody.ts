c21717db380a1433030008f9baecddbeb1835059
Rohin Bhargava <rohinbharg@gmail.com>
2024-07-12 16:26:02 -0400

    address bugfix, discussion starting point

--- generators/typescript/utils/core-utilities/fetcher/src/fetcher/getRequestBody.ts
+++ generators/typescript/utils/core-utilities/fetcher/src/fetcher/getRequestBody.ts
@@ -1,25 +1,16 @@
-import { RUNTIME } from "../runtime";
-
 export async function getRequestBody(body: any, contentType: string): Promise<BodyInit | undefined> {
     let requestBody: BodyInit;
-    if (RUNTIME.type === "node") {
-        if (body instanceof (await import("formdata-node")).FormData) {
-            // @ts-expect-error
-            requestBody = body;
-        } else if (body instanceof (await import("stream")).Readable) {
-            // @ts-expect-error
-            requestBody = body;
-        } else {
-            requestBody = maybeStringifyBody(body, contentType ?? "");
-        }
+
+    if (body instanceof (await import("form-data")).default) {
+        // @ts-expect-error
+        requestBody = body;
+    } else if (body instanceof (await import("stream")).Readable) {
+        // @ts-expect-error
+        requestBody = body;
     } else {
-        if (body instanceof (await import("form-data")).default) {
-            // @ts-expect-error
-            requestBody = body;
-        } else {
-            requestBody = maybeStringifyBody(body, contentType ?? "");
-        }
+        requestBody = maybeStringifyBody(body, contentType ?? "");
     }
+
     return requestBody;
 }
 
--- generators/typescript/utils/core-utilities/fetcher/src/form-data-utils/FormDataWrapper.ts
+++ generators/typescript/utils/core-utilities/fetcher/src/form-data-utils/FormDataWrapper.ts
@@ -1,50 +1,26 @@
-import { Readable } from "stream";
-import { RUNTIME } from "../runtime";
-
 interface CrossPlatformFormData {
     append(key: string, value: any): void;
 }
 
 class FormDataRequestBody {
     private fd: any;
-    private encoder: any;
 
     constructor(fd: any) {
         this.fd = fd;
-    }
-
-    async setup(): Promise<void> {
-        if (this.encoder == null && RUNTIME.type === "node") {
-            this.encoder = new (await import("form-data-encoder")).FormDataEncoder(this.fd);
-        }
     }
 
     /**
      * @returns the multipart form data request
      */
     public async getBody(): Promise<any> {
-        if (RUNTIME.type !== "node") {
-            return this.fd;
-        } else {
-            if (this.encoder == null) {
-                await this.setup();
-            }
-            return Readable.from(this.encoder);
-        }
+        this.fd;
     }
 
     /**
      * @returns headers that need to be added to the multipart form data request
      */
     public async getHeaders(): Promise<Record<string, string>> {
-        if (RUNTIME.type !== "node") {
-            return {};
-        } else {
-            if (this.encoder == null) {
-                await this.setup();
-            }
-            return this.encoder.headers;
-        }
+        return this.fd.getHeaders();
     }
 }
 
@@ -56,14 +32,8 @@ export class FormDataWrapper {
     private fd: CrossPlatformFormData | undefined;
 
     public async append(name: string, value: any): Promise<void> {
-        if (this.fd == null) {
-            if (RUNTIME.type === "node") {
-                this.fd = new (await import("formdata-node")).FormData();
-            } else {
-                this.fd = new (await import("form-data")).default();
-            }
-        }
-        this.fd.append(name, value);
+        this.fd = new (await import("form-data")).default();
+        this.fd?.append(name, value);
     }
 
     public getRequest(): FormDataRequestBody {
