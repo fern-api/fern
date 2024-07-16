f253c3f96d28470080a5b74832b3521e519a8dd1
Alex McKinney <alexmckinney01@gmail.com>
2024-07-16 14:18:52 -0400

    Add valid-version fern check rule

---
+++ packages/cli/yaml/validator/src/rules/valid-version/valid-version.ts
@@ -0,0 +1,43 @@
+import { TypeResolver, TypeResolverImpl } from "@fern-api/ir-generator";
+import { RootApiFileSchema } from "@fern-api/yaml-schema";
+import { Rule, RuleViolation } from "../../Rule";
+
+export const ValidVersionRule: Rule = {
+    name: "valid-version",
+    create: ({ workspace }) => {
+        const typeResolver = new TypeResolverImpl(workspace);
+        return {
+            rootApiFile: {
+                file: (root) => {
+                    return validateApiVersionSchema({
+                        root,
+                        typeResolver
+                    });
+                }
+            }
+        };
+    }
+};
+
+function validateApiVersionSchema({
+    root,
+    typeResolver
+}: {
+    root: RootApiFileSchema;
+    typeResolver: TypeResolver;
+}): RuleViolation[] {
+    const violations: RuleViolation[] = [];
+    if (root.version == null || root.version.default == null) {
+        return [];
+    }
+    const enumValues = new Set<string>(
+        root.version.values.map((enumValue) => (typeof enumValue === "string" ? enumValue : enumValue.value))
+    );
+    if (!enumValues.has(root.version.default)) {
+        violations.push({
+            severity: "error",
+            message: `Default version "${root.version.default}" not found in version values`
+        });
+    }
+    return violations;
+}
