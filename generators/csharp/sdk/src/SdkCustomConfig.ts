3d1a351ee4a9fe85f749faad44f729c8779b79f2
dcb6 <danielb14065@gmail.com>
2024-07-29 18:46:46 -0400

    format

--- generators/csharp/sdk/src/SdkCustomConfig.ts
+++ generators/csharp/sdk/src/SdkCustomConfig.ts
@@ -4,7 +4,7 @@ export const SdkCustomConfigSchema = z.strictObject({
     namespace: z.string().optional(),
     "explicit-namespaces": z.boolean().optional(),
     "client-class-name": z.string().optional(),
-    "extra-dependencies": z.record(z.string()).optional(),
+    "extra-dependencies": z.record(z.string()).optional()
 });
 
 export type SdkCustomConfigSchema = z.infer<typeof SdkCustomConfigSchema>;
