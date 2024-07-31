832795df39e18f7841e192c57785c95798318436
Rohin Bhargava <rohin@buildwithfern.com>
2024-07-31 17:00:02 -0400

    maybe better lint?

--- packages/cli/docs-markdown-utils/src/__test__/parseImagePaths.test.ts
+++ packages/cli/docs-markdown-utils/src/__test__/parseImagePaths.test.ts
@@ -384,7 +384,7 @@ describe("parseImagePaths", () => {
         const result = parseImagePaths(page, PATHS);
         expect(result.filepaths).toEqual([]);
         expect(result.markdown.trim()).toMatchInlineSnapshot(
-            "\"This is a test page with an image <img src={path + '/image.png'} /> <img src={'abc' + 'def.png'} />\""
+            `"This is a test page with an image <img src={path + '/image.png'} /> <img src={'abc' + 'def.png'} />"`
         );
     });
 
