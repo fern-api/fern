3cec579bd34b417b56ebf490ff2d4a4d51445ca6
armandobelardo <armandoubelardo@gmail.com>
2024-08-05 11:08:49 -0400

    seed

--- seed/fastapi/examples/resources/types/types/request.py
+++ seed/fastapi/examples/resources/types/types/request.py
@@ -18,7 +18,7 @@ class Request(UniversalBaseModel):
     )
     """
 
-    request: typing.Any
+    request: typing.Optional[typing.Any] = None
 
     if IS_PYDANTIC_V2:
         model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="forbid")  # type: ignore # Pydantic v2
--- seed/fastapi/exhaustive/skip-formatting/resources/no_auth/service/service.py
+++ seed/fastapi/exhaustive/skip-formatting/resources/no_auth/service/service.py
@@ -20,7 +20,7 @@ class AbstractNoAuthService(AbstractFernService):
     """
     
     @abc.abstractmethod
-    def post_with_no_auth(self, *, body: typing.Any) -> bool:
+    def post_with_no_auth(self, *, body: typing.Optional[typing.Any] = None) -> bool:
         """
         POST request with no auth
         """
