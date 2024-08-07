802920876bfac24780eefb265102248a91f9749e
alistair <alistair.t.barrell@gmail.com>
2024-08-06 21:14:14 -0400

    Fix issue where misconfigured directory could cause unhelpful error message (#4206)

--- packages/cli/workspace-loader/src/handleFailedWorkspaceParserResult.ts
+++ packages/cli/workspace-loader/src/handleFailedWorkspaceParserResult.ts
@@ -32,6 +32,11 @@ function handleWorkspaceParserFailureForFile({
     logger: Logger;
 }): void {
     switch (failure.type) {
+        case WorkspaceLoaderFailureType.MISCONFIGURED_DIRECTORY:
+            logger.error(
+                "Misconfigured fern directory: please see the docs at https://buildwithfern.com/learn/api-definition/introduction/what-is-the-fern-folder"
+            );
+            break;
         case WorkspaceLoaderFailureType.FILE_READ:
             logger.error("Failed to open file: " + relativeFilepath);
             break;
