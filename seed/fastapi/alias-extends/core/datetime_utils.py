920db138e0d0cede3f16f39e13efe123987e6cee
dsinghvi <deep@usebirch.com>
2024-08-06 21:46:06 -0400

    fastapi

--- seed/fastapi/alias-extends/core/datetime_utils.py
+++ seed/fastapi/alias-extends/core/datetime_utils.py
@@ -13,7 +13,9 @@ def serialize_datetime(v: dt.datetime) -> str:
     """
 
     def _serialize_zoned_datetime(v: dt.datetime) -> str:
-        if v.tzinfo is not None and v.tzinfo.tzname(None) == dt.timezone.utc.tzname(None):
+        if v.tzinfo is not None and v.tzinfo.tzname(None) == dt.timezone.utc.tzname(
+            None
+        ):
             # UTC is a special case where we use "Z" at the end instead of "+00:00"
             return v.isoformat().replace("+00:00", "Z")
         else:
