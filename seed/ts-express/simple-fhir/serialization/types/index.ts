664e076dcdf0ca0206ab161fa18a5287b34a5fec
Alex McKinney <alexmckinney01@gmail.com>
2024-08-09 11:52:49 -0400

    Add simple-fhir snapshots

---
+++ seed/ts-express/simple-fhir/serialization/types/index.ts
@@ -0,0 +1,7 @@
+export * from "./Memo";
+export * from "./BaseResource";
+export * from "./ResourceList";
+export * from "./Account";
+export * from "./Patient";
+export * from "./Practitioner";
+export * from "./Script";
