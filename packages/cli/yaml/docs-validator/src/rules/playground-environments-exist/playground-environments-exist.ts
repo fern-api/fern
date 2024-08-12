834f08da2a40dddc61192ecd9dad0d01638343bb
Rohin Bhargava <rohin@buildwithfern.com>
2024-08-11 23:32:27 -0400

    simplify conditions

--- packages/cli/yaml/docs-validator/src/rules/playground-environments-exist/playground-environments-exist.ts
+++ packages/cli/yaml/docs-validator/src/rules/playground-environments-exist/playground-environments-exist.ts
@@ -2,38 +2,32 @@ import { Rule, RuleViolation } from "../../Rule";
 
 export const PlaygroundEnvironmentsExistRule: Rule = {
     name: "playground-environments-exist",
-    create: () => {
-        return {
-            apiSection: async ({ workspace, context, config }) => {
-                const maybeApiSpecificationEnvironments = (await workspace.getDefinition({ context })).rootApiFile
-                    .contents.environments;
-                const apiSectionPlaygroundEnvironmentIds = config.playground?.environments;
-                if (!maybeApiSpecificationEnvironments) {
-                    if (apiSectionPlaygroundEnvironmentIds && apiSectionPlaygroundEnvironmentIds.length > 0) {
-                        return [
-                            {
-                                severity: "error",
-                                message:
-                                    "Cannot specify playground environments if there are no environments supplied in the API specification."
-                            }
-                        ];
-                    }
-                } else {
-                    const availableEnvironmentIds = new Set(Object.keys(maybeApiSpecificationEnvironments));
-                    const violations: RuleViolation[] = [];
-                    apiSectionPlaygroundEnvironmentIds?.forEach((environmentId) => {
-                        if (!availableEnvironmentIds.has(environmentId)) {
-                            violations.push({
-                                severity: "error",
-                                message: `Invalid environment id supplied in playground settings: ${environmentId}`
-                            });
-                        }
-                    });
-                    return violations;
-                }
+    create: () => ({
+        apiSection: async ({ workspace, context, config }) => {
+            const apiSpecificationEnvironments = (await workspace.getDefinition({ context })).rootApiFile.contents.environments;
+            const playgroundEnvironmentIds = config.playground?.environments || [];
 
+            if (!apiSpecificationEnvironments) {
+                if (playgroundEnvironmentIds.length > 0) {
+                    return [
+                        {
+                            severity: "error",
+                            message: "Cannot specify playground environments if there are no environments supplied in the API specification."
+                        }
+                    ];
+                }
                 return [];
             }
-        };
-    }
-};
+
+            const availableEnvironmentIds = new Set(Object.keys(apiSpecificationEnvironments));
+            const violations = playgroundEnvironmentIds
+                .filter(id => !availableEnvironmentIds.has(id))
+                .map(id => ({
+                    severity: "error",
+                    message: `Invalid environment id supplied in playground settings: ${id}`
+                }));
+
+            return violations;
+        }
+    })
+}; 
