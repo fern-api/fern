5e65897cd2107f6f12c160ea3d1bb5b8f6b71ce4
dsinghvi <deep@usebirch.com>
2024-08-11 21:48:32 -0400

    make sure node version tests continue to work

--- scripts/node-all-versions-test.sh
+++ scripts/node-all-versions-test.sh
@@ -10,11 +10,11 @@ FIXTURE="Multipart Form Data Tests"
 cd $TEST
 
 # Run the tests with different Node.js versions and update the flag if any command fails
-nvm install 20.4 && nvm use 20.4 && yarn test -t $FIXTURE || EXIT_STATUS=$?
-nvm install 19 && nvm use 19 && yarn test -t $FIXTURE || EXIT_STATUS=$?
-nvm install 18 && nvm use 18 && yarn test -t $FIXTURE || EXIT_STATUS=$?
-nvm install 17 && nvm use 17 && yarn test -t $FIXTURE || EXIT_STATUS=$?
-nvm install 16 && nvm use 16 && yarn test -t $FIXTURE || EXIT_STATUS=$?
+nvm install 20.4 && nvm use 20.4 && pnpm test -t $FIXTURE || EXIT_STATUS=$?
+nvm install 19 && nvm use 19 && pnpm test -t $FIXTURE || EXIT_STATUS=$?
+nvm install 18 && nvm use 18 && pnpm test -t $FIXTURE || EXIT_STATUS=$?
+nvm install 17 && nvm use 17 && pnpm test -t $FIXTURE || EXIT_STATUS=$?
+nvm install 16 && nvm use 16 && pnpm test -t $FIXTURE || EXIT_STATUS=$?
 
 # Exit with the appropriate status
 exit $EXIT_STATUS
