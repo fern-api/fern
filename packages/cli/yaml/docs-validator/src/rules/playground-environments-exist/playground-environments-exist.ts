8b2e4d8748b2da7c19a033f375b2e0562f663d1f
dsinghvi <deep@usebirch.com>
2024-08-12 15:49:35 -0400

    fix ci

--- packages/cli/yaml/docs-validator/src/rules/playground-environments-exist/playground-environments-exist.ts
+++ packages/cli/yaml/docs-validator/src/rules/playground-environments-exist/playground-environments-exist.ts
@@ -6,7 +6,7 @@ export const PlaygroundEnvironmentsExistRule: Rule = {
         apiSection: async ({ workspace, context, config }) => {
             const apiSpecificationEnvironments = (await workspace.getDefinition({ context })).rootApiFile.contents
                 .environments;
-            
+
             const availableEnvironmentIds = new Set(Object.keys(apiSpecificationEnvironments ?? {}));
             const playgroundEnvironmentIds = config.playground?.environments;
 
@@ -20,11 +20,13 @@ export const PlaygroundEnvironmentsExistRule: Rule = {
                 return [];
             }
 
-            if (nonExistentEnviromentIds.length === 1 ) {
+            if (nonExistentEnviromentIds.length === 1) {
                 return [
                     {
                         severity: "error",
-                        message: `The API does not contain the ${nonExistentEnviromentIds[0]} environment. ${getExistingEnviromentIds(Array.from(availableEnvironmentIds)) || ""}`
+                        message: `The API does not contain the ${nonExistentEnviromentIds[0]} environment. ${
+                            getExistingEnviromentIds(Array.from(availableEnvironmentIds)) ?? ""
+                        }`
                     }
                 ];
             }
@@ -32,7 +34,9 @@ export const PlaygroundEnvironmentsExistRule: Rule = {
             return [
                 {
                     severity: "error",
-                    message: `The API does not contain the following enviroments: ${nonExistentEnviromentIds.join(", ")}. ${getExistingEnviromentIds(Array.from(availableEnvironmentIds)) || ""}`
+                    message: `The API does not contain the following enviroments: ${nonExistentEnviromentIds.join(
+                        ", "
+                    )}. ${getExistingEnviromentIds(Array.from(availableEnvironmentIds)) ?? ""}`
                 }
             ];
         }
@@ -46,5 +50,5 @@ function getExistingEnviromentIds(availableEnvironmentIds: string[]): string | u
     if (availableEnvironmentIds.length === 1 && availableEnvironmentIds[0] != null) {
         return `The only configured environment is ${availableEnvironmentIds[0]}`;
     }
-    return `Existing enviroments include ${availableEnvironmentIds.join(", ")}.`
+    return `Existing enviroments include ${availableEnvironmentIds.join(", ")}.`;
 }
