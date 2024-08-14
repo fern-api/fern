89ea0a406ea096c39d8fb84b673937c608679925
Pujit Mehrotra <pujitm2009@gmail.com>
2024-08-14 13:52:44 -0400

    fix: rm overeager jest test matcher

--- packages/commons/core-utils/jest.config.cjs
+++ packages/commons/core-utils/jest.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
-  testMatch: [ "**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)" ],
+  testMatch: [ "**/?(*.)+(spec|test).ts?(x)" ],
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
