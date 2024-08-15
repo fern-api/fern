7313544aaf415392401a7a40bb5a4c3733f204ef
Pujit Mehrotra <pujitm2009@gmail.com>
2024-08-15 11:00:39 -0400

    fix: add validation rule for docs endpoints

--- packages/cli/yaml/docs-validator/src/getAllRules.ts
+++ packages/cli/yaml/docs-validator/src/getAllRules.ts
@@ -3,6 +3,7 @@ import { AccentColorContrastRule } from "./rules/accent-color-contrast";
 import { FilepathsExistRule } from "./rules/filepaths-exist";
 import { OnlyVersionedNavigation } from "./rules/only-versioned-navigation";
 import { PlaygroundEnvironmentsExistRule } from "./rules/playground-environments-exist";
+import { ValidDocsEndpoints } from "./rules/valid-docs-endpoints/valid-docs-endpoints";
 import { ValidFileTypes } from "./rules/valid-file-types";
 import { ValidMarkdownRule } from "./rules/valid-markdown";
 import { ValidMarkdownLinks } from "./rules/valid-markdown-link";
@@ -17,6 +18,7 @@ export function getAllRules(): Rule[] {
         AccentColorContrastRule,
         ValidMarkdownLinks,
         ValidFileTypes,
-        PlaygroundEnvironmentsExistRule
+        PlaygroundEnvironmentsExistRule,
+        ValidDocsEndpoints
     ];
 }
