534c760fc587acfdf32d6a6b13358f649e70c0b0
armandobelardo <armandoubelardo@gmail.com>
2024-08-20 16:24:06 -0400

    cr

--- packages/seed/src/cli.ts
+++ packages/seed/src/cli.ts
@@ -30,7 +30,8 @@ export async function tryRunCli(): Promise<void> {
 
     addTestCommand(cli);
     addRunCommand(cli);
-    addRegisterCommand(cli);
+    addCliRegisterCommand(cli);
+    addGeneratorRegisterCommand(cli);
 
     await cli.parse();
 
@@ -197,22 +198,17 @@ function addRunCommand(cli: Argv) {
     );
 }
 
-function addRegisterCommand(cli: Argv) {
+function addGeneratorRegisterCommand(cli: Argv) {
     cli.command(
-        "register",
-        "Registers all of the generators, as well as the CLI to FDR unless otherwise specified. To filter to certain generators or to not include the CLI, use the --generator, and --skip-cli flags respectively.",
+        "register generator <generators>",
+        "Registers all of the generators to FDR unless otherwise specified. To filter to certain generators pass in the generator IDs as a positional, space delimited list.",
         (yargs) =>
             yargs
-                .option("generator", {
-                    type: "array",
-                    string: true,
+                .positional("generators", {
+                    array: true,
+                    type: "string",
                     demandOption: false,
                     description: "Generator(s) to register"
-                })
-                .option("cli", {
-                    boolean: true,
-                    default: false,
-                    description: "Initialize a docs website."
                 })
                 .option("log-level", {
                     default: LogLevel.Info,
@@ -220,9 +216,41 @@ function addRegisterCommand(cli: Argv) {
                 }),
         async (argv) => {
             const generators = await loadGeneratorWorkspaces();
-            if (argv.generator != null) {
-                throwIfGeneratorDoesNotExist({ seedWorkspaces: generators, generators: argv.generator });
+            if (argv.generators != null) {
+                throwIfGeneratorDoesNotExist({ seedWorkspaces: generators, generators: argv.generators });
             }
+            const taskContextFactory = new TaskContextFactory(argv["log-level"]);
+            const context = taskContextFactory.create("Register");
+            const token = await askToLogin(context);
+
+            const fdrClient = createFdrService({ token: token.value });
+
+            for (const generator of generators) {
+                // If you've specified a list of generators, and the current generator is not in that list, skip it
+                if (argv.generators != null && !argv.generators.includes(generator.workspaceName)) {
+                    continue;
+                }
+                // Register the generator and it's versions
+                await registerGenerator({
+                    generator,
+                    fdrClient,
+                    context
+                });
+            }
+        }
+    );
+}
+
+function addCliRegisterCommand(cli: Argv) {
+    cli.command(
+        "register cli",
+        "Registers the CLI to FDR.",
+        (yargs) =>
+            yargs.option("log-level", {
+                default: LogLevel.Info,
+                choices: LOG_LEVELS
+            }),
+        async (argv) => {
             const taskContextFactory = new TaskContextFactory(argv["log-level"]);
             const context = taskContextFactory.create("Register");
             const token = await askToLogin(context);
@@ -233,20 +261,6 @@ function addRegisterCommand(cli: Argv) {
                 fdrClient,
                 context: taskContextFactory.create("Register")
             });
-            for (const generator of generators) {
-                if (
-                    (argv.cli && argv.generator == null) ||
-                    (argv.generator != null && !argv.generator.includes(generator.workspaceName))
-                ) {
-                    continue;
-                }
-                // Register the generator and it's versions
-                await registerGenerator({
-                    generator,
-                    fdrClient,
-                    context
-                });
-            }
         }
     );
 }
