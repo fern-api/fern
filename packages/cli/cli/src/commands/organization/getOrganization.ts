f220a79d942f1f2baae224bed361508ee271683c
Armando Belardo <11140328+armandobelardo@users.noreply.github.com>
2024-08-21 07:20:25 -0400

    chore(cli): introduce generator list and organization commands (#4356)

---
+++ packages/cli/cli/src/commands/organization/getOrganization.ts
@@ -0,0 +1,25 @@
+import { Project } from "@fern-api/project-loader";
+import { writeFile } from "fs/promises";
+import { CliContext } from "../../cli-context/CliContext";
+
+export async function getOrganziation({
+    project,
+    outputLocation,
+    context
+}: {
+    project: Project;
+    outputLocation: string | undefined;
+    context: CliContext;
+}): Promise<void> {
+    const org = project.config.organization;
+    if (outputLocation == null) {
+        process.stdout.write(org);
+        return;
+    }
+
+    try {
+        await writeFile(outputLocation, org);
+    } catch (error) {
+        context.failAndThrow(`Could not write file to the specified location: ${outputLocation}`, error);
+    }
+}
