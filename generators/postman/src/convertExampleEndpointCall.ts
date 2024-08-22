aa4e0f13c9f295f2dcd17c5cc816897cb72dad3b
Deep Singhvi <deep@buildwithfern.com>
2024-08-21 22:35:34 -0400

    fix(postman): upgrade from ir v23 to ir v53 (#4378)

--- generators/postman/src/convertExampleEndpointCall.ts
+++ generators/postman/src/convertExampleEndpointCall.ts
@@ -6,7 +6,7 @@ import {
     TypeDeclaration
 } from "@fern-fern/ir-sdk/api";
 import { PostmanExampleResponse, PostmanHeader } from "@fern-fern/postman-sdk/api";
-import { isEqual, startCase } from "lodash";
+import { isEqual, startCase, values } from "lodash";
 import { GeneratedExampleRequest } from "./request/GeneratedExampleRequest";
 
 export function convertExampleEndpointCall({
@@ -35,13 +35,38 @@ export function convertExampleEndpointCall({
     return {
         ...getNameAndStatus({ example, allErrors }),
         originalRequest: generatedRequest,
-        description: httpEndpoint.response?.docs ?? undefined,
-        body:
-            example.response.body?.jsonExample != null
-                ? JSON.stringify(example.response.body.jsonExample, undefined, 4)
-                : "",
+        description:
+            httpEndpoint.response?.body?._visit({
+                fileDownload: (value) => value.docs,
+                json: (value) => value.docs,
+                streamParameter: (value) => value.streamResponse.docs,
+                streaming: (value) => value.docs,
+                text: (value) => value.docs,
+                _other: () => undefined
+            }) ?? undefined,
+        body: example.response._visit({
+            ok: (value) => {
+                return value._visit({
+                    body: (value) => maybeStringify({ jsonExample: value?.jsonExample }),
+                    sse: (value) => maybeStringify({ jsonExample: value.map((event) => event?.data.jsonExample) }),
+                    stream: (value) => maybeStringify({ jsonExample: value.map((event) => event.jsonExample) }),
+                    _other: () => ""
+                });
+            },
+            error: (value) => {
+                return maybeStringify({ jsonExample: value?.body?.jsonExample });
+            },
+            _other: () => ""
+        }),
         postmanPreviewlanguage: "json"
     };
+}
+
+function maybeStringify({ jsonExample }: { jsonExample?: unknown }): string {
+    if (jsonExample == null) {
+        return "";
+    }
+    return JSON.stringify(jsonExample, undefined, 4);
 }
 
 function getNameAndStatus({
