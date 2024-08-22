9bab6677fd7ce4997d95ebd988c22acaa93997da
Rohin Bhargava <rohin@buildwithfern.com>
2024-08-22 19:50:07 +0100

    noignore to husky lint?

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "**/*.ts{,x}": "eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js --ignore-path .eslintignore",
+    "**/*.ts{,x}": "eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js --ignore-path .eslintignore --no-ignore",
     "**/*.{ts,js,json,yaml,html,css,less,scss,md}": "pnpm format",
     "**/package.json": () => "pnpm install --frozen-lockfile",
 };
