66f2f95d6e9fb46fa471f719d0bfeb41efbb7766
Alex McKinney <alexmckinney01@gmail.com>
2024-08-27 12:51:09 -0400

    Update snapshots

--- seed/ts-sdk/exhaustive/retain-original-casing/src/api/resources/inlinedRequests/client/Client.ts
+++ seed/ts-sdk/exhaustive/retain-original-casing/src/api/resources/inlinedRequests/client/Client.ts
@@ -45,7 +45,7 @@ export class InlinedRequests {
      *             long: 1000000,
      *             double: 1.1,
      *             bool: true,
-     *             datetime: new Date("2024-01-15T09:30:00.000Z"),
+     *             datetime: "2024-01-15T09:30:00Z",
      *             date: "2023-01-15",
      *             uuid: "d5e9c84f-c2b2-4bf4-b4b0-7ffd7a9ffc32",
      *             base64: "SGVsbG8gd29ybGQh",
