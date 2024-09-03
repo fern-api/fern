2351b81514e9b24709e2c54d5f7e2bd1a630a077
Rohin Bhargava <rohin@buildwithfern.com>
2024-09-03 16:32:07 -0400

    consolidate logic

--- packages/cli/fern-definition/validator/src/utils/getGenericDetails.ts
+++ packages/cli/fern-definition/validator/src/utils/getGenericDetails.ts
@@ -1,14 +1,17 @@
 export function getGenericDetails(name: string): { isGeneric: true; name?: string; arguments?: string[] } | undefined {
     const genericMatch = name.match(/([\w.]+)<([\w,\s]+)>/);
 
-    if (genericMatch?.[0] != null) {
-        if (!genericMatch[1] || !new Set(["optional", "set", "list", "map", "literal"]).has(genericMatch[1]?.trim())) {
-            return {
-                isGeneric: true,
-                name: genericMatch[1]?.trim(),
-                arguments: genericMatch[2]?.split(",").map((arg) => arg.trim())
-            };
-        }
+    if (
+        genericMatch?.[0] != null &&
+        genericMatch[1] != null &&
+        genericMatch[2] != null &&
+        !new Set(["optional", "set", "list", "map", "literal"]).has(genericMatch[1].trim())
+    ) {
+        return {
+            isGeneric: true,
+            name: genericMatch[1]?.trim(),
+            arguments: genericMatch[2]?.split(",").map((arg) => arg.trim())
+        };
     }
 
     return;
--- packages/cli/generation/ir-generator/src/utils/isGeneric.ts
+++ packages/cli/generation/ir-generator/src/utils/isGeneric.ts
@@ -1,9 +1,6 @@
-export function isGeneric(name: string): boolean {
-    const genericMatch = name.match(/([\w.]+)<([\w,\s]+)>/);
+import { getGenericDetails } from "./getGenericDetails";
 
-    return (
-        genericMatch?.[0] != null &&
-        genericMatch[1] != null &&
-        !new Set(["optional", "set", "list", "map", "literal"]).has(genericMatch[1].trim())
-    );
+export function isGeneric(name: string): boolean {
+    const genericDetails = getGenericDetails(name);
+    return genericDetails ? genericDetails.isGeneric : false;
 }
