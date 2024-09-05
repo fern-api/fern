80add7e752304bd6143f5f0cfcbda694ccd0535a
Andrew Jiang <andrew@buildwithfern.com>
2024-09-05 16:37:45 -0400

    fix typescript version

--- packages/vite-project/src/App.tsx
+++ packages/vite-project/src/App.tsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+// import * as Schema from "@fern-api/fern-definition-schema";
 
 function App() {
     const [count, setCount] = useState(0);
+
+    // console.log(Schema.DefinitionFileSchema.safeParse({}));
 
     return (
         <>
