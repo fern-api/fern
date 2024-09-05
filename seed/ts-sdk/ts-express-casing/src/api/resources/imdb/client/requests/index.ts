3a1f76dfe0cca5fca8ed77a47cfa3f3455a08d9c
armandobelardo <armandoubelardo@gmail.com>
2024-09-05 15:51:59 -0400

    update seed

--- seed/ts-sdk/ts-express-casing/src/api/resources/imdb/client/requests/index.ts
+++
@@ -1,1 +0,0 @@
-export { type CreateMovieRequest } from "./CreateMovieRequest";
--- seed/ts-sdk/ts-express-casing/src/errors/index.ts
+++
@@ -1,2 +0,0 @@
-export { SeedApiError } from "./SeedApiError";
-export { SeedApiTimeoutError } from "./SeedApiTimeoutError";
