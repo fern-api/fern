a754e6ba81b30bb1e1cbd2b4979a17a3cb4eb337
Rohin Bhargava <rohin@buildwithfern.com>
2024-09-06 15:24:10 -0400

    default

--- packages/cli/register/src/ir-to-fdr-converter/covertAuth.ts
+++ packages/cli/register/src/ir-to-fdr-converter/covertAuth.ts
@@ -28,13 +28,13 @@ export function convertAuth(auth: Ir.auth.ApiAuth, ir: Ir.ir.IntermediateReprese
                 // TODO: alter if we support more than one scheme
 
                 // We have to do the following, since the id passed on resolved endpoints is originalName
-                let endpointId: string = "";
+                let endpointId = scheme.configuration.tokenEndpoint.endpointReference.endpointId;
                 for (const irService of Object.values(ir.services)) {
                     for (const irEndpoint of irService.endpoints) {
                         if (irEndpoint.id === scheme.configuration.tokenEndpoint.endpointReference.endpointId) {
                             endpointId = irEndpoint.name.originalName;
                     }
-                }
+                } 
                 
                 return {
                     type: "oAuth",
