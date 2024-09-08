4b55d16065ca62a5c18a493fccd07bdbc92593e7
dsinghvi <deep@usebirch.com>
2024-09-08 12:43:55 -0400

    get tests passing

--- generators/swift/codegen/src/ast/core/Writer.ts
+++ generators/swift/codegen/src/ast/core/Writer.ts
@@ -1,3 +1,7 @@
 import { AbstractWriter } from "@fern-api/generator-commons";
 
-export class Writer extends AbstractWriter {}
+export class Writer extends AbstractWriter {
+    public toString(): string {
+        return this.buffer;
+    }
+}
