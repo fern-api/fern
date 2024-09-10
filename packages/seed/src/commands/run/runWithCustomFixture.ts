5fa39bd859fc4e27768e6e9f704fba6959d8818e
dsinghvi <deep@usebirch.com>
2024-09-10 16:14:25 -0400

    fix(seed): seed run command matches with docker config

--- packages/seed/src/commands/run/runWithCustomFixture.ts
+++ packages/seed/src/commands/run/runWithCustomFixture.ts
@@ -1,7 +1,15 @@
 import { DEFINITION_DIRECTORY } from "@fern-api/configuration";
+import {
+    GeneratorGroup,
+    GeneratorInvocation,
+    GeneratorsConfiguration
+} from "@fern-api/configuration/src/generators-yml";
 import { AbsoluteFilePath, join, RelativeFilePath } from "@fern-api/fs-utils";
 import { LogLevel } from "@fern-api/logger";
+import { TaskContext } from "@fern-api/task-context";
+import { FernWorkspace } from "@fern-api/workspace-loader";
 import tmp from "tmp-promise";
+import { group } from "yargs";
 import { GeneratorWorkspace } from "../../loadGeneratorWorkspaces";
 import { Semaphore } from "../../Semaphore";
 import { convertGeneratorWorkspaceToFernWorkspace } from "../../utils/convertSeedWorkspaceToFernWorkspace";
@@ -50,27 +58,24 @@ export async function runWithCustomFixture({
         return;
     }
 
+    const generatorGroup = getGeneratorGroup({
+        fernWorkspace,
+        image: workspace.workspaceConfig.image,
+        absolutePathToOutput
+    });
+    if (generatorGroup == null) {
+        taskContext.logger.error(`Found no generators configuration for the generator ${workspace.workspaceName}`);
+        return;
+    }
+
     try {
         await dockerGeneratorRunner.build();
-        await dockerGeneratorRunner.runGenerator({
+        await dockerGeneratorRunner.runGeneratorFromGroup({
             fernWorkspace,
             absolutePathToFernDefinition: join(pathToFixture, RelativeFilePath.of(DEFINITION_DIRECTORY)),
-            absolutePathToWorkspace: pathToFixture,
-            irVersion: workspace.workspaceConfig.irVersion,
-            outputVersion: customFixtureConfig?.outputVersion,
-            language: workspace.workspaceConfig.language,
-            fixture: "custom",
-            customConfig: customFixtureConfig?.customConfig,
-            publishConfig: customFixtureConfig?.publishConfig,
-            publishMetadata: customFixtureConfig?.publishMetadata,
-            selectAudiences: audience != null ? [audience] : customFixtureConfig?.audiences,
             taskContext,
-            outputDir: absolutePathToOutput,
-            outputMode: customFixtureConfig?.outputMode ?? workspace.workspaceConfig.defaultOutputMode,
-            outputFolder: customFixtureConfig?.outputFolder ?? "custom",
-            id: "custom",
-            keepDocker: true,
-            readme: customFixtureConfig?.readmeConfig
+            irVersion: workspace.workspaceConfig.irVersion,
+            group: generatorGroup
         });
         await writeDotMock({
             absolutePathToDotMockDirectory: absolutePathToOutput,
@@ -80,4 +85,24 @@ export async function runWithCustomFixture({
     } catch (error) {
         taskContext.logger.error(`Encountered error while running generator. ${(error as Error)?.message}`);
     }
+}
+
+function getGeneratorGroup({
+    fernWorkspace,
+    image,
+    absolutePathToOutput
+}: {
+    fernWorkspace: FernWorkspace;
+    image: string;
+    absolutePathToOutput: AbsoluteFilePath;
+}): GeneratorGroup | undefined {
+    const groups = fernWorkspace.generatorsConfiguration?.groups;
+    for (const group of groups ?? []) {
+        for (const generator of group.generators) {
+            if (generator.name === image) {
+                return { ...group, generators: [{ ...generator, absolutePathToLocalOutput: absolutePathToOutput }] };
+            }
+        }
+    }
+    return undefined;
 }
