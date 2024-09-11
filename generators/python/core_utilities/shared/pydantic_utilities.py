9e44dacf50c998b65e347a440a84858ed8563e6c
armandobelardo <armandoubelardo@gmail.com>
2024-09-11 14:53:04 -0400

    precommit

--- generators/python/core_utilities/shared/pydantic_utilities.py
+++ generators/python/core_utilities/shared/pydantic_utilities.py
@@ -93,12 +93,16 @@ class UniversalBaseModel(pydantic.BaseModel):
             smart_union = True
 
     @classmethod
-    def model_construct(cls: type[Model], _fields_set: typing.Optional[typing.Set[str]] = None, **values: typing.Any) -> Model:
+    def model_construct(
+        cls: type[Model], _fields_set: typing.Optional[typing.Set[str]] = None, **values: typing.Any
+    ) -> Model:
         dealiased_object = convert_and_respect_annotation_metadata(object_=values, annotation=cls, direction="read")
         return super().model_construct(_fields_set, **dealiased_object)
-    
+
     @classmethod
-    def construct(cls: type[Model], _fields_set: typing.Optional[typing.Set[str]] = None, **values: typing.Any) -> Model:
+    def construct(
+        cls: type[Model], _fields_set: typing.Optional[typing.Set[str]] = None, **values: typing.Any
+    ) -> Model:
         dealiased_object = convert_and_respect_annotation_metadata(object_=values, annotation=cls, direction="read")
         return super().model_construct(_fields_set, **dealiased_object)
 
--- generators/python/core_utilities/shared/with_pydantic_aliases/pydantic_utilities.py
+++ generators/python/core_utilities/shared/with_pydantic_aliases/pydantic_utilities.py
@@ -93,7 +93,6 @@ class UniversalBaseModel(pydantic.BaseModel):
             allow_population_by_field_name = True
             json_encoders = {dt.datetime: serialize_datetime}
 
-
     def json(self, **kwargs: typing.Any) -> str:
         kwargs_with_defaults: typing.Any = {
             "by_alias": True,
