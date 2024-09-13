cb6eb5c11017960e58a6ceaa784f393b4677c691
Alex McKinney <alexmckinney01@gmail.com>
2024-09-13 17:09:06 -0400

    feat(php): Implement ASTNode types for class (#4634)

---
+++ generators/php/codegen/src/ast/__test__/Class.test.ts
@@ -0,0 +1,63 @@
+import { php } from "../..";
+
+describe("class", () => {
+    it("simple", async () => {
+        const clazz = php.class_({
+            name: "UserClient",
+            namespace: "Acme"
+        });
+        clazz.addConstructor({
+            body: php.codeblock("// TODO: Implement me!"),
+            parameters: [
+                php.parameter({
+                    name: "metadata",
+                    type: php.Type.optional(php.Type.mixed()),
+                    access: "public",
+                    readonly_: true,
+                    docs: "Miscellaneous metadata"
+                }),
+                php.parameter({
+                    name: "dictionary",
+                    type: php.Type.map(php.Type.string(), php.Type.mixed()),
+                    access: "public",
+                    docs: "A collection of arbitrary key-value pairs"
+                })
+            ]
+        });
+        clazz.addField(
+            php.field({
+                name: "name",
+                type: php.Type.string(),
+                access: "private"
+            })
+        );
+        expect(
+            clazz.toString({
+                namespace: "",
+                rootNamespace: "",
+                customConfig: {}
+            })
+        ).toContain(`namespace Acme;
+
+class UserClient
+{
+    /**
+     * @var string name
+     */
+    private string name;
+
+    /**
+     * @param mixed metadata Miscellaneous metadata
+     * @param array<string, mixed> dictionary A collection of arbitrary key-value pairs
+     */
+    function __construct(
+        public readonly mixed metadata,
+        public array dictionary,
+    )
+    {
+        // TODO: Implement me!
+    }
+}
+`);
+    });
+});
