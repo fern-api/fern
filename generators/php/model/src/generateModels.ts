5850927a1ebf5f3cfe18422c90e2db7d29ec2b17
dcb6 <danielb14065@gmail.com>
2024-09-16 18:18:51 -0400

    check in

--- generators/php/model/src/generateModels.ts
+++ generators/php/model/src/generateModels.ts
@@ -1,13 +1,26 @@
 import { PhpFile } from "@fern-api/php-codegen";
+import { EnumTypeDeclaration } from "@fern-fern/ir-sdk/api";
 import { ModelGeneratorContext } from "./ModelGeneratorContext";
 
 export function generateModels({ context }: { context: ModelGeneratorContext }): PhpFile[] {
     const files: PhpFile[] = [];
-    files.push(
-        new PhpFile({
-            namespace: context.getNamespace(),
-            customConfig: context.customConfig
-        })
-    );
+    for (const [typeId, typeDeclaration] of Object.entries(context.ir.types)) {
+        const file = typeDeclaration.shape._visit<PhpFile | undefined>({
+            alias: () => undefined,
+            enum: (etd: EnumTypeDeclaration) => {
+                return undefined;
+                // return new EnumGenerator(context, typeDeclaration, etd).generate();
+            },
+            object: (otd) => {
+                return new ObjectGenerator(context, typeDeclaration, otd).generate();
+            },
+            undiscriminatedUnion: () => undefined,
+            union: () => undefined,
+            _other: () => undefined
+        });
+        if (file != null) {
+            files.push(file);
+        }
+    }
     return files;
 }
