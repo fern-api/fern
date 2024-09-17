8cc46687c83a3dfbe355c367ad52ee8d016fad95
Marcus Stade <mstade@users.noreply.github.com>
2024-09-17 03:14:54 +0200

    fix(cli): improve JSON stream performance
    These changes significantly improve performance of both reading and writing
    JSON files using the streaming methods in `@fern-api/fs-utils`. Total
    runtime, memory and disk utilization are all improved.
    
    This is achieved by:
    
    - Not writing prettified JSON IR files (dramatically reduces on-disk size)
    - Upgrading to a newer and *much* faster version of the
      `json-stream-stringify` package – this upgrade alone improved the
      performance of serializing a ~176 MB ir.json from about 3 minutes down to
      3-4 seconds
    - Reimplementing `streamObjectToFile` using `Assembler` which is lower-level
      and faster than `StreamObject`, and also setting parsing options to avoid
      emitting unnecessary parsing tokens, for a 30-35% improvement in parsing
      performance

--- packages/commons/fs-utils/src/streamObjectFromFile.ts
+++ packages/commons/fs-utils/src/streamObjectFromFile.ts
@@ -1,21 +1,19 @@
 import { createReadStream } from "fs";
 import { AbsoluteFilePath } from "./AbsoluteFilePath";
-import StreamObject from "stream-json/streamers/StreamObject.js";
+import StreamJSON from "stream-json";
+import Assembler from "stream-json/Assembler.js";
 
 export async function streamObjectFromFile(filePath: AbsoluteFilePath): Promise<unknown> {
-    const jsonStream = StreamObject.withParser();
-    const accumulator = {};
-
-    jsonStream.on("data", ({ key, value }: { key: string; value: unknown }) =>
-        Object.assign(accumulator, { [key]: value })
-    );
-
     return new Promise((resolve, reject) => {
+        const jsonStream = StreamJSON.parser({
+            streamValues: false,
+        });
+
+        const asm = Assembler.connectTo(jsonStream);
+        asm.on("done", ({current}) => resolve(current));
+
         createReadStream(filePath)
             .pipe(jsonStream)
-            .on("error", reject)
-            .on("finish", () => {
-                resolve(accumulator);
-            });
+            .on("error", reject);
     });
 }
