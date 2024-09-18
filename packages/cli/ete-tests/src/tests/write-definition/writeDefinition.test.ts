6e53ed426dfbe40abde8bec795e4eacc82ac5ab1
armandobelardo <armandoubelardo@gmail.com>
2024-09-17 20:07:38 -0400

    fix(cli): namespacing configuration now applies to error bodies as well

--- packages/cli/ete-tests/src/tests/write-definition/writeDefinition.test.ts
+++ packages/cli/ete-tests/src/tests/write-definition/writeDefinition.test.ts
@@ -18,6 +18,10 @@ describe("validate namespaced API", () => {
     itFixture("namespaced");
 });
 
+describe("validate namespaced API", () => {
+    itFixture("namespaced-fleshedout");
+});
+
 function itFixture(fixtureName: string) {
     it(
         // eslint-disable-next-line jest/valid-title
@@ -34,6 +38,10 @@ function itFixture(fixtureName: string) {
             });
 
             expect(await getDirectoryContentsForSnapshot(AbsoluteFilePath.of(definitionOutputPath))).toMatchSnapshot();
+
+            await runFernCli(["check", "--log-level", "debug"], {
+                cwd: fixturePath
+            });
         },
         90_000
     );
