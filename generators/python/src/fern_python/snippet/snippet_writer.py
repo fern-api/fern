92a1e8356d2d0e3f0d06df879c3bdfe78979f187
armandobelardo <armandoubelardo@gmail.com>
2024-09-19 07:46:40 -0400

    update tests

--- generators/python/src/fern_python/snippet/snippet_writer.py
+++ generators/python/src/fern_python/snippet/snippet_writer.py
@@ -261,7 +261,13 @@ class SnippetWriter:
         self,
         unknown: Any,
     ) -> AST.Expression:
-        return AST.Expression(str(unknown))
+        if unknown is not None:
+            def write_unknown(writer: AST.NodeWriter) -> None:
+                maybe_stringify_unknown = repr(unknown) if type(unknown) is str else unknown
+                writer.write_line(f"{maybe_stringify_unknown}")
+
+            return AST.Expression(AST.CodeWriter(write_unknown))
+        return AST.Expression("None")
 
     def _get_snippet_for_list_or_set(
         self,
