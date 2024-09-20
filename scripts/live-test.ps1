2b7d03df73d3d10ad8a05167f984e2393b41653d
Rohin Bhargava <rohin@buildwithfern.com>
2024-09-20 14:40:57 -0400

    add silent removal

--- scripts/live-test.ps1
+++ scripts/live-test.ps1
@@ -15,19 +15,24 @@ function New-TemporaryDirectory {
     New-Item -ItemType Directory -Path $path
 }
 
+$original_dir = Get-Location
 $test_dir = New-TemporaryDirectory
 Set-Location $test_dir
 
-$env:FERN_TOKEN = $token
+try {
+    $env:FERN_TOKEN = $token
 
-Write-Host "Running Fern Commands!"
-$DebugPreference = "Continue"
-& node $cli_path init --organization fern
-& node $cli_path add fern-java-sdk
-& node $cli_path add fern-python-sdk
-& node $cli_path add fern-postman
-& node $cli_path generate --log-level debug
-$DebugPreference = "SilentlyContinue"
-& node $cli_path register --log-level debug
-
-Remove-Item -Recurse -Force $test_dir
+    Write-Host "Running Fern Commands!"
+    $DebugPreference = "Continue"
+    & node $cli_path init --organization fern
+    & node $cli_path add fern-java-sdk
+    & node $cli_path add fern-python-sdk
+    & node $cli_path add fern-postman
+    & node $cli_path generate --log-level debug
+    $DebugPreference = "SilentlyContinue"
+    & node $cli_path register --log-level debug
+}
+finally {
+    Set-Location $original_dir
+    Remove-Item -Recurse -Force $test_dir -ErrorAction SilentlyContinue
+}
