f65044496f814377887932234ef2fa2f9d5b27c8
armandobelardo <armandoubelardo@gmail.com>
2024-09-23 22:58:26 -0400

    seed

--- seed/python-sdk/exhaustive/deps_with_min_python_version/src/seed/types/object/types/object_with_optional_field.py
+++ seed/python-sdk/exhaustive/deps_with_min_python_version/src/seed/types/object/types/object_with_optional_field.py
@@ -30,7 +30,7 @@ class ObjectWithOptionalField(UniversalBaseModel):
     bigint: typing.Optional[str] = None
 
     if IS_PYDANTIC_V2:
-        model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(frozen=True)  # type: ignore # Pydantic v2
+        model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="allow", frozen=True)  # type: ignore # Pydantic v2
     else:
 
         class Config:
