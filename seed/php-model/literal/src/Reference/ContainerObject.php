d035b32ab170eb465e793102c3ef6f1a044439be
armandobelardo <armandoubelardo@gmail.com>
2024-09-25 19:23:21 -0400

    missing files

---
+++ seed/php-model/literal/src/Reference/ContainerObject.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Seed\Reference;
+
+use Seed\Core\SerializableType;
+use Seed\Core\JsonProperty;
+use Seed\Core\ArrayType;
+
+class ContainerObject extends SerializableType
+{
+    /**
+     * @var array<NestedObjectWithLiterals> $nestedObjects
+     */
+    #[JsonProperty('nestedObjects'), ArrayType([NestedObjectWithLiterals::class])]
+    public array $nestedObjects;
+
+    /**
+     * @param array{
+     *   nestedObjects: array<NestedObjectWithLiterals>,
+     * } $values
+     */
+    public function __construct(
+        array $values,
+    ) {
+        $this->nestedObjects = $values['nestedObjects'];
+    }
+}
