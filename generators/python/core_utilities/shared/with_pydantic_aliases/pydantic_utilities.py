487db780493237bdfe3a39e0fcb0d9c557d06991
armandobelardo <armandoubelardo@gmail.com>
2024-09-25 20:54:51 -0400

    rerun seed

--- generators/python/core_utilities/shared/with_pydantic_aliases/pydantic_utilities.py
+++ generators/python/core_utilities/shared/with_pydantic_aliases/pydantic_utilities.py
@@ -147,26 +147,31 @@ class UniversalBaseModel(pydantic.BaseModel):
             return super().dict(**kwargs_with_defaults_exclude_unset_include_fields)
 
 
+
+def _union_list_of_pydantic_dicts(source: typing.List[typing.Any], destination: typing.List[typing.Any]) -> typing.List[typing.Any]:
+    converted_list: typing.List[typing.Any] = []
+    for i, item in enumerate(source):
+        destination_value = destination[i]  # type: ignore
+        if isinstance(item, dict):
+            converted_list.append(deep_union_pydantic_dicts(item, destination_value))
+        elif isinstance(item, list):
+            converted_list.append(_union_list_of_pydantic_dicts(item, destination_value))
+        else:
+            converted_list.append(item)
+    return converted_list
+
 def deep_union_pydantic_dicts(
     source: typing.Dict[str, typing.Any], destination: typing.Dict[str, typing.Any]
 ) -> typing.Dict[str, typing.Any]:
     for key, value in source.items():
+        node = destination.setdefault(key, {})
         if isinstance(value, dict):
-            node = destination.setdefault(key, {})
             deep_union_pydantic_dicts(value, node)
         # Note: we do not do this same processing for sets given we do not have sets of models
         # and given the sets are unordered, the processing of the set and matching objects would
         # be non-trivial.
         elif isinstance(value, list):
-            converted_list = []
-            for i, item in enumerate(value):
-                node = destination.setdefault(key, {})
-                destination_value = node[i]  # type: ignore
-                if isinstance(item, dict):
-                    converted_list.append(deep_union_pydantic_dicts(item, destination_value))
-                else:
-                    converted_list.append(item)
-            destination[key] = converted_list
+            destination[key] = _union_list_of_pydantic_dicts(value, node)
         else:
             destination[key] = value
 
