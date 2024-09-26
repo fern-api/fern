145a8790d7e5dcebfb56d08a0bfa714eb6eed4d0
armandobelardo <armandoubelardo@gmail.com>
2024-09-26 08:27:23 -0400

    v

--- packages/cli/generation/remote-generation/remote-workspace-runner/src/runRemoteGenerationForGenerator.ts
+++ packages/cli/generation/remote-generation/remote-workspace-runner/src/runRemoteGenerationForGenerator.ts
@@ -112,6 +112,7 @@ export async function runRemoteGenerationForGenerator({
         version,
         intermediateRepresentation: {
             ...ir,
+            fdrApiDefinitionId,
             publishConfig: getPublishConfig({ generatorInvocation: generatorInvocationWithEnvVarSubstitutions })
         },
         shouldLogS3Url,
