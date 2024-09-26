db65fe37834fd14404003c4011e8e3ad08e34332
Alex McKinney <alexmckinney01@gmail.com>
2024-09-26 12:53:05 -0400

    Update ruby-sdk tests

---
+++ seed/ruby-sdk/literal/lib/fern_literal/query/client.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+require_relative "../../requests"
+require_relative "../types/send_response"
+require "async"
+
+module SeedLiteralClient
+  class QueryClient
+    # @return [SeedLiteralClient::RequestClient]
+    attr_reader :request_client
+
+    # @param request_client [SeedLiteralClient::RequestClient]
+    # @return [SeedLiteralClient::QueryClient]
+    def initialize(request_client:)
+      @request_client = request_client
+    end
+
+    # @param query [String]
+    # @param request_options [SeedLiteralClient::RequestOptions]
+    # @return [SeedLiteralClient::SendResponse]
+    # @example
+    #  literal = SeedLiteralClient::Client.new(
+    #    base_url: "https://api.example.com",
+    #    version: "Version",
+    #    audit_logging: "AuditLogging"
+    #  )
+    #  literal.query.send(query: "What is the weather today")
+    def send(query:, request_options: nil)
+      response = @request_client.conn.post do |req|
+        req.options.timeout = request_options.timeout_in_seconds unless request_options&.timeout_in_seconds.nil?
+        req.headers["X-API-Version"] = request_options.version unless request_options&.version.nil?
+        unless request_options&.audit_logging.nil?
+          req.headers["X-API-Enable-Audit-Logging"] =
+            request_options.audit_logging
+        end
+        req.headers = {
+      **(req.headers || {}),
+      **@request_client.get_headers,
+      **(request_options&.additional_headers || {})
+        }.compact
+        req.params = {
+          **(request_options&.additional_query_parameters || {}),
+          "prompt": "You are a helpful assistant",
+          "stream": false,
+          "query": query
+        }.compact
+        unless request_options.nil? || request_options&.additional_body_parameters.nil?
+          req.body = { **(request_options&.additional_body_parameters || {}) }.compact
+        end
+        req.url "#{@request_client.get_url(request_options: request_options)}/query"
+      end
+      SeedLiteralClient::SendResponse.from_json(json_object: response.body)
+    end
+  end
+
+  class AsyncQueryClient
+    # @return [SeedLiteralClient::AsyncRequestClient]
+    attr_reader :request_client
+
+    # @param request_client [SeedLiteralClient::AsyncRequestClient]
+    # @return [SeedLiteralClient::AsyncQueryClient]
+    def initialize(request_client:)
+      @request_client = request_client
+    end
+
+    # @param query [String]
+    # @param request_options [SeedLiteralClient::RequestOptions]
+    # @return [SeedLiteralClient::SendResponse]
+    # @example
+    #  literal = SeedLiteralClient::Client.new(
+    #    base_url: "https://api.example.com",
+    #    version: "Version",
+    #    audit_logging: "AuditLogging"
+    #  )
+    #  literal.query.send(query: "What is the weather today")
+    def send(query:, request_options: nil)
+      Async do
+        response = @request_client.conn.post do |req|
+          req.options.timeout = request_options.timeout_in_seconds unless request_options&.timeout_in_seconds.nil?
+          req.headers["X-API-Version"] = request_options.version unless request_options&.version.nil?
+          unless request_options&.audit_logging.nil?
+            req.headers["X-API-Enable-Audit-Logging"] =
+              request_options.audit_logging
+          end
+          req.headers = {
+        **(req.headers || {}),
+        **@request_client.get_headers,
+        **(request_options&.additional_headers || {})
+          }.compact
+          req.params = {
+            **(request_options&.additional_query_parameters || {}),
+            "prompt": "You are a helpful assistant",
+            "stream": false,
+            "query": query
+          }.compact
+          unless request_options.nil? || request_options&.additional_body_parameters.nil?
+            req.body = { **(request_options&.additional_body_parameters || {}) }.compact
+          end
+          req.url "#{@request_client.get_url(request_options: request_options)}/query"
+        end
+        SeedLiteralClient::SendResponse.from_json(json_object: response.body)
+      end
+    end
+  end
+end
---
+++ seed/ruby-sdk/literal/lib/types_export.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "fern_literal/types/send_response"
+require_relative "fern_literal/inlined/types/some_aliased_literal"
+require_relative "fern_literal/inlined/types/a_top_level_literal"
+require_relative "fern_literal/inlined/types/a_nested_literal"
+require_relative "fern_literal/reference/types/send_request"
+require_relative "fern_literal/reference/types/container_object"
+require_relative "fern_literal/reference/types/nested_object_with_literals"
+require_relative "fern_literal/reference/types/some_literal"
