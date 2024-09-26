263c7c49ce46f1108703de7c2b8c33428e2e6b25
dsinghvi <deep@usebirch.com>
2024-09-26 08:15:42 -0400

    fix(cli): generate fdr definition id

--- packages/cli/ete-tests/src/tests/generate/generate.test.ts
+++ packages/cli/ete-tests/src/tests/generate/generate.test.ts
@@ -2,6 +2,10 @@ import { AbsoluteFilePath, doesPathExist, join, RelativeFilePath } from "@fern-a
 import stripAnsi from "strip-ansi";
 import { runFernCli } from "../../utils/runFernCli";
 import { init } from "../init/init";
+import { readFile } from "fs/promises";
+import { file } from "tmp-promise";
+import { loggingExeca } from "@fern-api/logging-execa";
+import { CONSOLE_LOGGER } from "../../../../task-context/node_modules/@fern-api/logger/src";
 
 const fixturesDir = join(AbsoluteFilePath.of(__dirname), RelativeFilePath.of("fixtures"));
 // const FIXTURES = ["docs"];
@@ -17,6 +21,25 @@ describe("fern generate", () => {
         expect(await doesPathExist(join(pathOfDirectory, RelativeFilePath.of("sdks/typescript")))).toBe(true);
     }, 180_000);
 
+    it.skip("ir contains fdr definitionid", async () => {
+        const { stdout, stderr } = await runFernCli(["generate", "--log-level", "debug"], {
+            cwd: join(fixturesDir, RelativeFilePath.of("basic")),
+            reject: false
+        });
+
+        console.log("stdout", stdout);
+        console.log("stderr", stderr);
+
+        const filepath = extractFilepath(stdout);
+        if (filepath == null) {
+            throw new Error(`Failed to get path to IR:\n${stdout}`);
+        }
+        
+        // Fails because ts cant issue commands to a temp directory cat ${filepath}
+        const response = await loggingExeca(CONSOLE_LOGGER, `cat ${filepath}`);
+        console.log("stdout", response.stdout);
+    }, 180_000);
+
     it("missing docs page", async () => {
         const { stdout } = await runFernCli(["generate", "--docs"], {
             cwd: join(fixturesDir, RelativeFilePath.of("docs-missing-page")),
@@ -30,3 +53,14 @@ describe("fern generate", () => {
         ).toMatchSnapshot();
     }, 180_000);
 });
+
+function extractFilepath(logLine: string): string | null {
+    const prefix = "Wrote IR to disk:";
+    const index = logLine.indexOf(prefix);
+
+    if (index !== -1) {
+        return logLine.slice(index + prefix.length).trim();
+    }
+
+    return null;
+}
