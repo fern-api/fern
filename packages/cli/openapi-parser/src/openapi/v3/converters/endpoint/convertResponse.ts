462278c485e77f60fef5362d7cb89bb67f094e1b
dsinghvi <deep@usebirch.com>
2024-09-27 16:04:28 -0400

    fix formatting

--- packages/cli/openapi-parser/src/openapi/v3/converters/endpoint/convertResponse.ts
+++ packages/cli/openapi-parser/src/openapi/v3/converters/endpoint/convertResponse.ts
@@ -9,7 +9,11 @@ import { AbstractOpenAPIV3ParserContext } from "../../AbstractOpenAPIV3ParserCon
 import { FernOpenAPIExtension } from "../../extensions/fernExtensions";
 import { OperationContext } from "../contexts";
 import { ERROR_NAMES_BY_STATUS_CODE } from "../convertToHttpError";
-import { getApplicationJsonSchemaMediaObject, getSchemaMediaObject, getTextEventStreamObject } from "./getApplicationJsonSchema";
+import {
+    getApplicationJsonSchemaMediaObject,
+    getSchemaMediaObject,
+    getTextEventStreamObject
+} from "./getApplicationJsonSchema";
 
 // The converter will attempt to get response in priority order
 // (i.e. try for 200, then 201, then 202...)
