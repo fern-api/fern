f3cf58015ac9d517f35c2edde4dd4616f2167af4
Deep Singhvi <deep@buildwithfern.com>
2024-09-27 20:40:49 -0400

    chore(cli): redo docs validation so that we are forced to add fern check rules (#4771)

---
+++ packages/cli/docs-markdown-utils/src/parseMarkdownToTree.ts
@@ -0,0 +1,11 @@
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { Root } from "mdast-util-from-markdown/lib";
+import { mdxFromMarkdown } from "mdast-util-mdx";
+import { mdx } from "micromark-extension-mdx";
+
+export function parseMarkdownToTree(content: string): Root {
+    return fromMarkdown(content, {
+        extensions: [mdx()],
+        mdastExtensions: [mdxFromMarkdown()]
+    });
+}
