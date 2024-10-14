57215d88daba6ff987cc908d4b91574783ae5bdb
dsinghvi <deep@usebirch.com>
2024-10-14 08:34:04 -0400

    restore

---
+++ packages/cli/mock/src/equal/requestEqual.ts
@@ -0,0 +1,50 @@
+import { ExampleEndpointCall } from "@fern-api/ir-sdk";
+import { pathParametersEqual } from "./pathParametersEqual";
+import { queryParametersEqual } from "./queryParametersEqual";
+import { Request } from "express";
+import { headersEqual } from "./headersEqual";
+import { bodyEqual } from "./bodyEqual";
+import { EqualResponse } from "./EqualRequestResponse";
+
+export declare namespace requestEqual {
+    interface Args {
+        request: Request;
+        example: ExampleEndpointCall;
+    }
+}
+
+export function requestEqual({ request, example }: requestEqual.Args): EqualResponse {
+    const pathParameterResponse = pathParametersEqual({
+        request,
+        example
+    });
+    if (pathParameterResponse.type == "notEqual") {
+        return pathParameterResponse;
+    }
+
+    const queryParametersResponse = queryParametersEqual({
+        request,
+        example
+    });
+    if (queryParametersResponse.type == "notEqual") {
+        return queryParametersResponse;
+    }
+
+    const headersResponse = headersEqual({
+        request,
+        example
+    });
+    if (headersResponse.type == "notEqual") {
+        return headersResponse;
+    }
+
+    const bodyResponse = bodyEqual({
+        request,
+        example
+    });
+    if (bodyResponse.type == "notEqual") {
+        return bodyResponse;
+    }
+
+    return { type: "equal" };
+}
