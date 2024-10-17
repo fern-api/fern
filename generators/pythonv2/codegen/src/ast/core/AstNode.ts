bab9e55ee9a446ee207b7d6da6b450b56028c201
Noa Flaherty <noa@vellum.ai>
2024-10-17 19:11:11 -0400

    Add helper for improved devexp

--- generators/pythonv2/codegen/src/ast/core/AstNode.ts
+++ generators/pythonv2/codegen/src/ast/core/AstNode.ts
@@ -1,5 +1,6 @@
 import { AbstractAstNode } from "@fern-api/generator-commons";
 import { Writer } from "./Writer";
+import init, { format } from "@wasm-fmt/ruff_fmt";
 
 export abstract class AstNode extends AbstractAstNode {
     /**
@@ -9,5 +10,13 @@ export abstract class AstNode extends AbstractAstNode {
         const writer = new Writer();
         this.write(writer);
         return writer.toString();
+    }
+
+    /**
+     * Writes the node to a string and formats it.
+     */
+    public async toStringFormatted(): Promise<string> {
+        await init();
+        return format(this.toString());
     }
 }
