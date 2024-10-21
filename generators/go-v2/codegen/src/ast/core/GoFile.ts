23c6b18ec5aa9f2db2c988e79a84fbeb934cd2aa
Alex McKinney <alexmckinney01@gmail.com>
2024-10-21 15:48:47 -0400

    feat(go): Add AST

---
+++ generators/go-v2/codegen/src/ast/core/GoFile.ts
@@ -0,0 +1,35 @@
+import { Writer } from "./Writer";
+import init, { format } from "@wasm-fmt/gofmt";
+
+export declare namespace GoFile {
+    interface Args extends Writer.Args {}
+}
+
+export class GoFile extends Writer {
+    constructor({ packageName, rootImportPath, importPath, customConfig }: GoFile.Args) {
+        super({ packageName, rootImportPath, importPath, customConfig });
+    }
+
+    public async toString(): Promise<string> {
+        const packageStatement = `package ${this.packageName}\n\n`;
+        const imports = this.stringifyImports();
+        const content =
+            imports.length > 0
+                ? `${packageStatement}${imports}
+
+${this.buffer}`
+                : packageStatement + this.buffer;
+
+        await init();
+        return format(content);
+    }
+
+    private stringifyImports(): string {
+        const result = Object.entries(this.imports)
+            .filter(([importPath, _]) => importPath !== this.importPath) // Skip the target import path
+            .map(([importPath, alias]) => `    ${alias} "${importPath}"`)
+            .join("\n");
+
+        return result ? `import (\n${result}\n)` : "";
+    }
+}
---
+++ generators/go-v2/codegen/src/index.ts
@@ -0,0 +1,4 @@
+export { AbstractGoGeneratorContext, type FileLocation } from "./context/AbstractGoGeneratorContext";
+export { BaseGoCustomConfigSchema } from "./custom-config/BaseGoCustomConfigSchema";
+export { FileGenerator } from "./FileGenerator";
+export * as go from "./go";
