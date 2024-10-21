d8a3f800d0a5145474b2ae3c99e81896a132bfa5
Noa Flaherty <noa@vellum.ai>
2024-10-21 08:18:03 -0400

    Initial pass at PR feedback

--- generators/python-v2/codegen/src/ast/PythonFile.ts
+++ generators/python-v2/codegen/src/ast/PythonFile.ts
@@ -57,14 +57,12 @@ export class PythonFile extends AstNode {
     }
 
     private writeImports(writer: Writer): void {
-        const references = this.getReferences();
-
         // Deduplicate references by their fully qualified paths
         const uniqueReferences = new Map<
             string,
             { modulePath: ModulePath; references: Reference[]; referenceNames: Set<string> }
         >();
-        for (const reference of references) {
+        for (const reference of this.references) {
             const fullyQualifiedPath = reference.getFullyQualifiedModulePath();
             const existingRefs = uniqueReferences.get(fullyQualifiedPath);
             const referenceName = reference.getName();
