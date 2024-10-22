31b58497f09a510e76686081b5f2d0f51b28fbd3
dsinghvi <deep@usebirch.com>
2024-10-22 18:54:05 -0400

    fix

--- packages/cli/api-importers/commons/src/index.ts
+++ packages/cli/api-importers/commons/src/index.ts
@@ -1,2 +1,7 @@
 export { APIDefinitionImporter } from "./APIDefinitionImporter";
-export { type FernDefinitionBuilder, FernDefinitionBuilderImpl, type FernDefinition, type HttpServiceInfo } from "./FernDefnitionBuilder";
+export {
+    type FernDefinitionBuilder,
+    FernDefinitionBuilderImpl,
+    type FernDefinition,
+    type HttpServiceInfo
+} from "./FernDefnitionBuilder";
