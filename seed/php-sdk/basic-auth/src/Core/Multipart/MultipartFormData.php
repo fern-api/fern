7917db22dd24f6f078a0c95bfbd42d5be5876fdf
ajgateno <gatenoalberto@gmail.com>
2024-10-30 14:59:59 -0400

    generate classes after renaming template fields

--- seed/php-sdk/basic-auth/src/Core/Multipart/MultipartFormData.php
+++ seed/php-sdk/basic-auth/src/Core/Multipart/MultipartFormData.php
@@ -27,7 +27,7 @@ class MultipartFormData
         self::addPart(
             new MultipartFormDataPart(
                 name: $name,
-                contents: $value,
+                value: $value,
                 headers: $headers,
             )
         );
