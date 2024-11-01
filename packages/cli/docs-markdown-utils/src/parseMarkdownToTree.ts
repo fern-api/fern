9d2d8b07c735ab29b2330356bcf284617cccbf48
dubwub <dingdarwin@gmail.com>
2024-11-01 14:11:00 -0400

    saving changes

--- packages/cli/docs-markdown-utils/src/parseMarkdownToTree.ts
+++ packages/cli/docs-markdown-utils/src/parseMarkdownToTree.ts
@@ -1,5 +1,5 @@
 import { fromMarkdown } from "mdast-util-from-markdown";
-// Hotfix: ../docs-markdown-utils/src/parseMarkdownToTree.ts(2,10): error TS2459: Module '"mdast-util-from-markdown/lib"' declares 'Root' locally, but it is not exported.
+// Hotfix:  ../docs-markdown-utils/src/parseMarkdownToTree.ts(2,10): error TS2459: Module '"mdast-util-from-markdown/lib"' declares 'Root' locally, but it is not exported.
 // import { Root } from "mdast-util-from-markdown/lib";
 import { mdxFromMarkdown } from "mdast-util-mdx";
 import { mdx } from "micromark-extension-mdx";
