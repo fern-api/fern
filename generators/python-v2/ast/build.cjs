a69f744c028ee4e3eb286dbed5e97a9774dff545
David Vargas <dvargas92495@gmail.com>
2024-11-04 20:44:39 -0500

    more build ops

--- generators/python-v2/ast/build.cjs
+++ generators/python-v2/ast/build.cjs
@@ -1,5 +1,5 @@
 const packageJson = require("./package.json");
-const tsup = require('tsup');
+const tsup = require("tsup");
 const { writeFile, rename } = require("fs/promises");
 const path = require("path");
 
@@ -7,12 +7,37 @@ main();
 
 async function main() {
     await tsup.build({
-        entry: ['src/**/*.ts', '!src/__test__'],
-        format: ['cjs'],
+        entry: ["src/**/*.ts", "!src/__test__"],
+        format: ["cjs"],
         clean: true,
-        outDir: 'dist',
-        external: [
-            "@wasm-fmt/ruff_fmt",
-        ],
+        dts: true,
+        outDir: "dist",
+        external: ["@wasm-fmt/ruff_fmt"],
+        tsconfig: "./build.tsconfig.json"
     });
+
+    process.chdir(path.join(__dirname, "dist"));
+
+    // The module expects the imports defined in the index.d.ts file.
+    await rename("index.d.cts", "index.d.ts");
+
+    await writeFile(
+        "package.json",
+        JSON.stringify(
+            {
+                name: packageJson.name,
+                version: process.argv[2] || packageJson.version,
+                repository: packageJson.repository,
+                main: "index.cjs",
+                types: "index.d.ts",
+                type: "module",
+                files: ["*.cjs", "*.d.cts", "core/"],
+                dependencies: {
+                    "@wasm-fmt/ruff_fmt": "^0.6.1"
+                }
+            },
+            undefined,
+            2
+        )
+    );
 }
