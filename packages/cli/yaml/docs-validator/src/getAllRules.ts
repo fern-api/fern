fd0d97ae7e0fd92140b6c2f8c3ffb621686858e5
Rohin Bhargava <rohin@buildwithfern.com>
2024-11-06 09:46:10 -0500

    remove valid markdown parse rule

--- packages/cli/yaml/docs-validator/src/getAllRules.ts
+++ packages/cli/yaml/docs-validator/src/getAllRules.ts
@@ -6,14 +6,12 @@ import { OnlyVersionedNavigation } from "./rules/only-versioned-navigation";
 import { PlaygroundEnvironmentsExistRule } from "./rules/playground-environments-exist";
 import { ValidDocsEndpoints } from "./rules/valid-docs-endpoints";
 import { ValidFileTypes } from "./rules/valid-file-types";
-import { ValidMarkdownRule } from "./rules/valid-markdown";
 import { ValidMarkdownLinks } from "./rules/valid-markdown-link";
 import { ValidateVersionFileRule } from "./rules/validate-version-file";
 
 export function getAllRules(): Rule[] {
     return [
         FilepathsExistRule,
-        ValidMarkdownRule,
         OnlyVersionedNavigation,
         ValidateVersionFileRule,
         AccentColorContrastRule,
