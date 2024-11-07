64445f4753cc4d99a646dd0c597ec58422d1802d
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-11-06 21:07:28 -0500

    WIP: fix license logic for C# SDK generator

---
+++ seed/csharp-sdk/license/custom-license/src/SeedLicense/Core/Public/SeedLicenseException.cs
@@ -0,0 +1,11 @@
+using System;
+
+#nullable enable
+
+namespace SeedLicense;
+
+/// <summary>
+/// Base exception class for all exceptions thrown by the SDK.
+/// </summary>
+public class SeedLicenseException(string message, Exception? innerException = null)
+    : Exception(message, innerException) { }
