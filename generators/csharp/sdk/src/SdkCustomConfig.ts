d6bf7f11f546e61634da3de9a7f7d6e8b0422d8b
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-11-07 19:38:21 -0500

    Update SdkCustomConfig.ts

--- generators/csharp/sdk/src/SdkCustomConfig.ts
+++ generators/csharp/sdk/src/SdkCustomConfig.ts
@@ -14,7 +14,7 @@ export const SdkCustomConfigSchema = z.strictObject({
         .record(z.string())
         .optional()
         .describe(
-            "(Deprecated) The extra dependencies to add into the csproj file. Use the [ProjectName].Custom.Props to configure additional dependencies instead."
+            "(Deprecated) The extra dependencies to add into the csproj file. Use the [ProjectName].Custom.props to configure additional dependencies instead."
         ),
     "read-only-memory-types": z.optional(z.array(z.string())),
     "experimental-enable-forward-compatible-enums": z.boolean().optional(),
