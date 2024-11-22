31b7b468916517ad42baaa797080cecae3d5e23d
Eden <eden@buildwithfern.com>
2024-11-22 17:17:52 -0500

    Add test cases & refine `getSchemaName`.

--- packages/cli/api-importers/openapi/openapi-ir-parser/src/schema/utils/getSchemaName.ts
+++ packages/cli/api-importers/openapi/openapi-ir-parser/src/schema/utils/getSchemaName.ts
@@ -1,21 +1,17 @@
-import { camelCase, upperFirst, lowerFirst } from "lodash-es";
+import { camelCase, upperFirst } from "lodash-es";
 import { replaceStartingNumber } from "./replaceStartingNumber";
 
-function customCamelCase(input: string): string {
-    const tokens = input.split("_");
-    const processedTokens = tokens.map((token, index) => {
-        if (/^[a-zA-Z0-9]+$/.test(token)) {
-            return index === 0 ? lowerFirst(token) : upperFirst(token);
-        }
-        return token;
-    });
-    return processedTokens.join("");
-}
-
 export function getGeneratedTypeName(breadcrumbs: string[], useOriginalSchemaIds: boolean): string {
-    const camelCaseFn = useOriginalSchemaIds ? customCamelCase : camelCase;
-    const underscoreDelimited = breadcrumbs.join("_");
-    const name = upperFirst(camelCaseFn(underscoreDelimited));
+    const processedTokens = breadcrumbs.map((token) => {
+        if (/^[^a-zA-Z0-9]+$/.test(token)) {
+            return token;
+        } else {
+            return upperFirst(camelCase(token));
+        }
+    });
+
+    const name = processedTokens.join("");
+
     if (/^\d/.test(name)) {
         return replaceStartingNumber(name) ?? name;
     }
