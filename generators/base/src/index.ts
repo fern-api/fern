ab88ac7c9b7d0fee95f0f8c2d2acd73dabd22ad3
Alex McKinney <alexmckinney01@gmail.com>
2024-11-26 16:15:12 -0700

    Export browser-compatible-base-generator from base-generator

--- generators/base/src/index.ts
+++ generators/base/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./AbstractGeneratorAgent";
 export * from "./AbstractGeneratorCli";
+export * from "./browserCompatibleExports";
 export * from "./SourceFetcher";
 export * from "./project";
 export * from "./readme";
