baca33e39e78057ab6a70479ddb4547b3b2b6c71
Alex McKinney <alexmckinney01@gmail.com>
2024-11-27 11:46:45 -0700

    Add dependencies-yml package

--- packages/cli/configuration-loader/src/dependencies-yml/index.ts
+++ packages/cli/configuration-loader/src/dependencies-yml/index.ts
@@ -1,6 +1,1 @@
-export {
-    type DependenciesConfiguration,
-    type Dependency,
-    type LocalApiDependency,
-    type VersionedDependency
-} from "./DependenciesConfiguration";
+export { loadDependenciesConfiguration } from "./loadDependenciesConfiguration";
