3664a61855d54dfe62809fb7d3145d88688a95dd
Alex McKinney <alexmckinney01@gmail.com>
2024-11-27 13:11:52 -0700

    Export @fern-api/configuration from @fern-api/configuration-loader

--- packages/cli/configuration-loader/src/index.ts
+++ packages/cli/configuration-loader/src/index.ts
@@ -1,7 +1,5 @@
-export * from "./commons/Audiences";
 export { validateSchema } from "./commons/validateSchema";
 export * from "./commons/WithoutQuestionMarks";
-export * from "./constants";
 export * as dependenciesYml from "./dependencies-yml";
 export * as docsYml from "./docs-yml";
 export { DocsLinks } from "./DocsLinks";
@@ -10,3 +8,7 @@ export * as generatorsYml from "./generators-yml";
 export { GeneratorName } from "./generators-yml/GeneratorName";
 export * from "./getFernDirectory";
 export type { GeneratorGroup, GeneratorInvocation } from "./generators-yml";
+
+// Export everything from @fern-api/configuration so that consumers
+// can simply user @fern-api/configuration-loader on its own.
+export * from "@fern-api/configuration";
