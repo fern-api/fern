dc7c928a562056581ecc3f96dfd63c3b0b367943
Alex McKinney <alexmckinney01@gmail.com>
2024-11-27 12:10:34 -0700

    Start reducing configuration-loader for now

--- packages/cli/configuration-loader/src/docs-yml/index.ts
+++
@@ -1,3 +0,0 @@
-export { getColorFromRawConfig, getColorType } from "./convertColorsConfiguration";
-export { getReferencedApiSections } from "./getReferencedApiSections";
-export { parseDocsConfiguration, resolveFilepath } from "./parseDocsConfiguration";
--- packages/cli/configuration-loader/src/index.ts
+++ packages/cli/configuration-loader/src/index.ts
@@ -1,8 +1,4 @@
 export { validateSchema } from "./commons/validateSchema";
 export * from "./commons/WithoutQuestionMarks";
 export * as dependenciesYml from "./dependencies-yml";
-export * as docsYml from "./docs-yml";
-export * as fernConfigJson from "./fern-config-json";
-export * as generatorsYml from "./generators-yml";
-export { GeneratorName } from "./generators-yml/GeneratorName";
 export * from "./getFernDirectory";
