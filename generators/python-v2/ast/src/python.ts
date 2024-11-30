1b0089e806273c68b15ef01ecc606aa2c625cd56
Noa Flaherty <noa@vellum.ai>
2024-11-30 17:14:32 -0500

    Introduce new comment AstNode

--- generators/python-v2/ast/src/python.ts
+++ generators/python-v2/ast/src/python.ts
@@ -1,29 +1,31 @@
 import { Class } from "./Class";
-import { Field } from "./Field";
-import { Reference } from "./Reference";
-import { CodeBlock } from "./CodeBlock";
-import { Method } from "./Method";
-import { MethodInvocation } from "./MethodInvocation";
 import { ClassInstantiation } from "./ClassInstantiation";
-import { Parameter } from "./Parameter";
-import { MethodArgument } from "./MethodArgument";
-import { PythonFile } from "./PythonFile";
+import { CodeBlock } from "./CodeBlock";
+import { Comment } from "./Comment";
 import { Decorator } from "./Decorator";
+import { Field } from "./Field";
+import { Method } from "./Method";
+import { MethodArgument } from "./MethodArgument";
+import { MethodInvocation } from "./MethodInvocation";
+import { Parameter } from "./Parameter";
+import { PythonFile } from "./PythonFile";
 import { Operator } from "./Operator";
+import { Reference } from "./Reference";
 
 export { AstNode } from "./core/AstNode";
 export { Class } from "./Class";
-export { Field } from "./Field";
-export { Reference } from "./Reference";
-export { CodeBlock } from "./CodeBlock";
-export { Method } from "./Method";
-export { MethodInvocation } from "./MethodInvocation";
 export { ClassInstantiation } from "./ClassInstantiation";
-export { Parameter } from "./Parameter";
-export { MethodArgument } from "./MethodArgument";
-export { PythonFile } from "./PythonFile";
+export { CodeBlock } from "./CodeBlock";
+export { Comment } from "./Comment";
 export { Decorator } from "./Decorator";
+export { Field } from "./Field";
+export { Method } from "./Method";
+export { MethodArgument } from "./MethodArgument";
+export { MethodInvocation } from "./MethodInvocation";
+export { Parameter } from "./Parameter";
+export { PythonFile } from "./PythonFile";
 export { Operator } from "./Operator";
+export { Reference } from "./Reference";
 export { Type } from "./Type";
 export { TypeInstantiation } from "./TypeInstantiation";
 
@@ -53,6 +55,10 @@ export function field(args: Field.Args): Field {
 
 export function codeBlock(args: CodeBlock.Arg): CodeBlock {
     return new CodeBlock(args);
+}
+
+export function comment(args: Comment.Args): Comment {
+    return new Comment(args);
 }
 
 export function method(args: Method.Args): Method {
