79148afa395bd1e9cfc32a08a2bc4476d958b8f7
Alex McKinney <alexmckinney01@gmail.com>
2024-12-03 12:08:51 -0500

    chore(go): Upgrade to latest IRv53

--- generators/go-v2/dynamic-snippets/src/__test__/file-upload.test.ts
+++ generators/go-v2/dynamic-snippets/src/__test__/file-upload.test.ts
@@ -1,81 +1,73 @@
-/* eslint-disable */
-
+import { buildDynamicSnippetsGenerator } from "./utils/buildDynamicSnippetsGenerator";
+import { DYNAMIC_IR_TEST_DEFINITIONS_DIRECTORY } from "./utils/constant";
+import { buildGeneratorConfig } from "./utils/buildGeneratorConfig";
 import { TestCase } from "./utils/TestCase";
+import { AbsoluteFilePath } from "@fern-api/path-utils";
 
-it.skip("TODO: Remove this!", () => {});
+describe("file-upload (success)", () => {
+    const testCases: TestCase[] = [
+        {
+            description: "POST /",
+            giveRequest: {
+                endpoint: {
+                    method: "POST",
+                    path: "/"
+                },
+                auth: undefined,
+                pathParameters: undefined,
+                queryParameters: undefined,
+                headers: undefined,
+                requestBody: {
+                    file: "Hello, world!",
+                    fileList: ["First", "Second"]
+                }
+            }
+        },
+        {
+            description: "POST /just-file (simple)",
+            giveRequest: {
+                endpoint: {
+                    method: "POST",
+                    path: "/just-file"
+                },
+                auth: undefined,
+                pathParameters: undefined,
+                queryParameters: undefined,
+                headers: undefined,
+                requestBody: {
+                    file: "Hello, world!"
+                }
+            }
+        },
+        {
+            description: "POST /just-file-with-query-params (simple)",
+            giveRequest: {
+                endpoint: {
+                    method: "POST",
+                    path: "/just-file-with-query-params"
+                },
+                auth: undefined,
+                pathParameters: undefined,
+                queryParameters: {
+                    integer: 42,
+                    maybeString: "exists"
+                },
+                headers: undefined,
+                requestBody: {
+                    file: "Hello, world!"
+                }
+            }
+        }
+    ];
+    const generator = buildDynamicSnippetsGenerator({
+        irFilepath: AbsoluteFilePath.of(`${DYNAMIC_IR_TEST_DEFINITIONS_DIRECTORY}/file-upload.json`),
+        config: buildGeneratorConfig()
+    });
+    test.each(testCases)("$description", async ({ giveRequest }) => {
+        const response = await generator.generate(giveRequest);
+        expect(response.snippet).toMatchSnapshot();
 
-// TODO: Uncomment this test after the dynamic IR dependency is updated.
-//
-// import { buildDynamicSnippetsGenerator } from "./utils/buildDynamicSnippetsGenerator";
-// import { DYNAMIC_IR_TEST_DEFINITIONS_DIRECTORY } from "./utils/constant";
-// import { buildGeneratorConfig } from "./utils/buildGeneratorConfig";
-// import { TestCase } from "./utils/TestCase";
-// import { AbsoluteFilePath } from "@fern-api/path-utils";
-//
-// describe("file-upload (success)", () => {
-//     const testCases: TestCase[] = [
-//         {
-//             description: "POST /",
-//             giveRequest: {
-//                 endpoint: {
-//                     method: "POST",
-//                     path: "/"
-//                 },
-//                 auth: undefined,
-//                 pathParameters: undefined,
-//                 queryParameters: undefined,
-//                 headers: undefined,
-//                 requestBody: {
-//                     file: "Hello, world!",
-//                     fileList: ["First", "Second"]
-//                 }
-//             }
-//         },
-//         {
-//             description: "POST /just-file (simple)",
-//             giveRequest: {
-//                 endpoint: {
-//                     method: "POST",
-//                     path: "/just-file"
-//                 },
-//                 auth: undefined,
-//                 pathParameters: undefined,
-//                 queryParameters: undefined,
-//                 headers: undefined,
-//                 requestBody: {
-//                     file: "Hello, world!"
-//                 }
-//             }
-//         },
-//         {
-//             description: "POST /just-file-with-query-params (simple)",
-//             giveRequest: {
-//                 endpoint: {
-//                     method: "POST",
-//                     path: "/just-file-with-query-params"
-//                 },
-//                 auth: undefined,
-//                 pathParameters: undefined,
-//                 queryParameters: {
-//                     integer: 42,
-//                     maybeString: "exists"
-//                 },
-//                 headers: undefined,
-//                 requestBody: {
-//                     file: "Hello, world!"
-//                 }
-//             }
-//         }
-//     ];
-//     const generator = buildDynamicSnippetsGenerator({
-//         irFilepath: AbsoluteFilePath.of(`${DYNAMIC_IR_TEST_DEFINITIONS_DIRECTORY}/file-upload.json`),
-//         config: buildGeneratorConfig()
-//     });
-//     test.each(testCases)("$description", async ({ giveRequest }) => {
-//         const response = await generator.generate(giveRequest);
-//         expect(response.snippet).toMatchSnapshot();
-
-//         console.log(response.errors);
-//         expect(response.errors).toBeUndefined();
-//     });
-// });
+        console.log(response.errors);
+        expect(response.errors).toBeUndefined();
+    });
+});
