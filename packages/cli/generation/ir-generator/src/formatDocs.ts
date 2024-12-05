624e9f3763c1e0c0aec4f19151920b3aa9b89ecd
Alex McKinney <alexmckinney01@gmail.com>
2024-12-04 23:11:12 -0500

    fix(cli): @fern-api/ir-generator conditionally imports prettier (#5340)

--- packages/cli/generation/ir-generator/src/formatDocs.ts
+++ packages/cli/generation/ir-generator/src/formatDocs.ts
@@ -1,14 +1,15 @@
-import * as prettier from "prettier";
+const isBrowser = typeof window !== "undefined" && typeof window.document !== "undefined";
 
 export async function formatDocs(docs: string | undefined): Promise<string | undefined> {
-    if (docs != null) {
-        const formattedDocs = prettier.format(docs, {
-            parser: "markdown"
-        });
-        if (formattedDocs.endsWith("\n") || formattedDocs.endsWith("\r")) {
-            return formattedDocs.substring(0, formattedDocs.length - 1);
-        }
-        return formattedDocs;
+    if (docs == null || isBrowser) {
+        return undefined;
     }
-    return docs;
+    const prettier = await import("prettier");
+    const formattedDocs = prettier.format(docs, {
+        parser: "markdown"
+    });
+    if (formattedDocs.endsWith("\n") || formattedDocs.endsWith("\r")) {
+        return formattedDocs.substring(0, formattedDocs.length - 1);
+    }
+    return formattedDocs;
 }
