4927293f39ca5f844577477010f126503e95acfd
Alex McKinney <alexmckinney01@gmail.com>
2024-12-06 17:41:09 -0500

    fix(cli): Remove @redocly/openapi-core from browser workspace (#5359)

--- packages/commons/path-utils/src/join.ts
+++ packages/commons/path-utils/src/join.ts
@@ -4,6 +4,7 @@ import { RelativeFilePath } from "./RelativeFilePath";
 export function join(first: RelativeFilePath, ...parts: RelativeFilePath[]): RelativeFilePath;
 export function join(first: AbsoluteFilePath, ...parts: RelativeFilePath[]): AbsoluteFilePath;
 export function join(...parts: RelativeFilePath[]): RelativeFilePath;
+export function join(first: string, ...parts: string[]): AbsoluteFilePath;
 export function join(...parts: string[]): string {
     const stack: string[] = [];
     for (const part of parts.flatMap((segment) => segment.split("/")).filter(Boolean)) {
