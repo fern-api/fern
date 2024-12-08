9ce516d5a1db463ea382757df423b28fd4cb6de1
Alex McKinney <alexmckinney01@gmail.com>
2024-12-08 13:41:27 -0500

    Add visitObjectAsync for docs-validator

--- packages/commons/core-utils/src/ObjectPropertiesVisitor.ts
+++ packages/commons/core-utils/src/ObjectPropertiesVisitor.ts
@@ -9,3 +9,12 @@ export function visitObject<T extends object>(object: T, visitor: ObjectProperti
         visitor[key](object[key]);
     }
 }
+
+export async function visitObjectAsync<T extends object>(
+    object: T,
+    visitor: ObjectPropertiesVisitor<T, void | Promise<void>>
+): Promise<void> {
+    for (const key of keys(visitor)) {
+        await visitor[key](object[key]);
+    }
+}
