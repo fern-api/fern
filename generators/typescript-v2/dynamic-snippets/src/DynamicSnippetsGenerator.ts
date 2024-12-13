ec4036ffa4e97f46536e7bbca349cacc41586f30
Alex McKinney <alexmckinney01@gmail.com>
2024-12-13 14:47:16 -0500

    chore(typescript): Scaffold dynamic snippet generator

---
+++ generators/typescript-v2/dynamic-snippets/src/DynamicSnippetsGenerator.ts
@@ -0,0 +1,39 @@
+import {
+    AbstractDynamicSnippetsGenerator,
+    FernGeneratorExec
+} from "@fern-api/browser-compatible-base-generator";
+import { DynamicSnippetsGeneratorContext } from "./context/DynamicSnippetsGeneratorContext";
+import { dynamic } from "@fern-fern/ir-sdk/api";
+
+export class DynamicSnippetsGenerator extends AbstractDynamicSnippetsGenerator<
+    dynamic.DynamicIntermediateRepresentation,
+    DynamicSnippetsGeneratorContext,
+    dynamic.EndpointSnippetRequest,
+    dynamic.EndpointSnippetResponse
+> {
+    constructor({
+        ir,
+        config,
+    }: {
+        ir: dynamic.DynamicIntermediateRepresentation;
+        config: FernGeneratorExec.GeneratorConfig;
+    }) {
+        super(new DynamicSnippetsGeneratorContext({ ir, config }));
+    }
+
+    public async generate(
+        request: dynamic.EndpointSnippetRequest
+    ): Promise<dynamic.EndpointSnippetResponse> {
+        return {
+            snippet: "TODO: Implement me!",
+            errors: undefined
+        };
+    }
+
+    public generateSync(request: dynamic.EndpointSnippetRequest): dynamic.EndpointSnippetResponse {
+        return {
+            snippet: "TODO: Implement me!",
+            errors: undefined
+        };
+    }
+}
