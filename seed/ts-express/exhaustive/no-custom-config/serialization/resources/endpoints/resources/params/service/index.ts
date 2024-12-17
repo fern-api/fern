efb53d2804edfaf1a3304e0ac5a44a71c5b20a88
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-12-17 15:59:45 -0500

    Fix exhaustive test definition + rerun seed

--- seed/ts-express/exhaustive/no-custom-config/serialization/resources/endpoints/resources/params/service/index.ts
+++ seed/ts-express/exhaustive/no-custom-config/serialization/resources/endpoints/resources/params/service/index.ts
@@ -1,4 +1,2 @@
 export * as getWithPath from "./getWithPath";
-export * as getWithInlinePath from "./getWithInlinePath";
 export * as modifyWithPath from "./modifyWithPath";
-export * as modifyWithInlinePath from "./modifyWithInlinePath";
