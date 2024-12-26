85b22b41f80099a163e81fa1609a6b0918acc3ab
eden wang <64514273+eyw520@users.noreply.github.com>
2024-12-26 11:15:50 -0500

    fix(cli): Improvements to example parsing error messages. (#5475)
    * Initial improvements to example parsing error messages.
    
    * Concise errors.
    
    * Updates
    
    * US

--- packages/cli/generation/ir-generator/src/converters/type-declarations/convertEnumTypeDeclaration.ts
+++ packages/cli/generation/ir-generator/src/converters/type-declarations/convertEnumTypeDeclaration.ts
@@ -42,12 +42,12 @@ export function getEnumNameFromEnumValue(
     name: string;
     wasExplicitlySet: boolean;
 } {
-    const maybeEnumDefintion = _enum.enum.find((value) =>
+    const maybeEnumDefinition = _enum.enum.find((value) =>
         typeof value === "string" ? false : value.value === enumValue
     );
 
     return {
-        name: typeof maybeEnumDefintion === "string" ? enumValue : maybeEnumDefintion?.name ?? enumValue,
+        name: typeof maybeEnumDefinition === "string" ? enumValue : maybeEnumDefinition?.name ?? enumValue,
         wasExplicitlySet: false
     };
 }
