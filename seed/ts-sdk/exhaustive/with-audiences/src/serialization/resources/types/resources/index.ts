172a455244f72cb159b2970e4c3feba5a38ab4cb
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-01-06 23:59:48 -0500

    re-seed

---
+++ seed/ts-sdk/exhaustive/with-audiences/src/serialization/resources/types/resources/index.ts
@@ -0,0 +1,6 @@
+export * as enum_ from "./enum";
+export * from "./enum/types";
+export * as object from "./object";
+export * from "./object/types";
+export * as union from "./union";
+export * from "./union/types";
