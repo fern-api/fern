aaf326280106a5ea88119413a829287e4ea2d1cb
eden wang <64514273+eyw520@users.noreply.github.com>
2025-01-08 17:05:48 -0500

    feat(cli): `fern check` now runs simple rules on openapi specs & overrides. (#5559)
    * feat(cli): `fern check` now runs simple rules on openapi specs & overrides.
    
    * chore: update changelog
    
    * Add complex test fixture and generate snapshots.
    
    * use `toMatchSnapshot` instead.
    
    ---------
    
    Co-authored-by: eyw520 <eyw520@users.noreply.github.com>

---
+++ packages/cli/workspace/oss-validator/src/testing-utils/getViolationsForRule.ts
@@ -0,0 +1,52 @@
+import stripAnsi from "strip-ansi";
+
+import { AbsoluteFilePath } from "@fern-api/fs-utils";
+import { OSSWorkspace } from "@fern-api/lazy-fern-workspace";
+import { CONSOLE_LOGGER } from "@fern-api/logger";
+import { createMockTaskContext } from "@fern-api/task-context";
+import { loadAPIWorkspace } from "@fern-api/workspace-loader";
+
+import { Rule } from "../Rule";
+import { ValidationViolation } from "../ValidationViolation";
+import { runRulesOnOSSWorkspace } from "../validateOSSWorkspace";
+
+export declare namespace getViolationsForRule {
+    export interface Args {
+        rule: Rule;
+        absolutePathToWorkspace: AbsoluteFilePath;
+        cliVersion?: string;
+    }
+}
+
+export async function getViolationsForRule({
+    rule,
+    absolutePathToWorkspace,
+    cliVersion
+}: getViolationsForRule.Args): Promise<ValidationViolation[]> {
+    const context = createMockTaskContext();
+    const result = await loadAPIWorkspace({
+        absolutePathToWorkspace,
+        context,
+        cliVersion: "0.0.0",
+        workspaceName: undefined
+    });
+
+    if (!result.didSucceed) {
+        throw new Error("API workspace failed to load");
+    }
+
+    if (!(result.workspace instanceof OSSWorkspace)) {
+        throw new Error("Expected an OSS workspace but got a different type");
+    }
+
+    const violations = await runRulesOnOSSWorkspace({
+        workspace: result.workspace as OSSWorkspace,
+        context,
+        rules: [rule]
+    });
+
+    return violations.map((violation) => ({
+        ...violation,
+        message: stripAnsi(violation.message)
+    }));
+}
