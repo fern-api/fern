b79d96310d7a8218d64417f96b509bb16831bb6e
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-01-08 15:22:44 -0500

    Fix Dockerfile/cache script

--- packages/generators/docker/cache_heavy_deps.sh
+++ packages/generators/docker/cache_heavy_deps.sh
@@ -18,12 +18,12 @@ yarn add \
   form-data@4.0.0 \
   jest-environment-jsdom@29.7.0 \
   jest@29.7.0 \
-  js-base64@3.7.2"
+  js-base64@3.7.2 \
   node-fetch@2.7.0 \
   prettier@2.7.1 \
   qs@6.11.2 \
   ts-jest@29.1.1 \
-  typescript@4.6.4" \
+  typescript@4.6.4 \
   url-join@4.0.1
 
 cd ..
