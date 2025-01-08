41882eece63ad34a1679325aab65f57df295f1c5
eden wang <64514273+eyw520@users.noreply.github.com>
2025-01-08 17:35:42 -0500

    fix(cli): Add `in-lined` test case to `no-duplicate-overrides.test.ts` (#5561)
    * fix(cli): Add `in-lined` test case to `no-duplicate-overrides.test.ts`
    
    * us

--- packages/cli/workspace/oss-validator/src/rules/no-duplicate-overrides/__test__/no-duplicate-overrides.test.ts
+++ packages/cli/workspace/oss-validator/src/rules/no-duplicate-overrides/__test__/no-duplicate-overrides.test.ts
@@ -25,7 +25,7 @@ describe("no-duplicate-overrides", () => {
             }
         ];
 
-        expect(violations).toMatchSnapshot("./__snapshots__/openapi/simple.json");
+        expect(violations).toMatchSnapshot();
     }, 10_000);
 
     it("complex failure", async () => {
@@ -48,6 +48,29 @@ describe("no-duplicate-overrides", () => {
             }
         ];
 
-        expect(violations).toMatchSnapshot("./__snapshots__/openapi/complex.json");
+        expect(violations).toMatchSnapshot();
+    }, 10_000);
+
+    it("inlined failure", async () => {
+        const violations = await getViolationsForRule({
+            rule: NoDuplicateOverridesRule,
+            absolutePathToWorkspace: join(
+                AbsoluteFilePath.of(__dirname),
+                RelativeFilePath.of("fixtures"),
+                RelativeFilePath.of("in-lined")
+            ),
+            cliVersion: "0.1.3-rc0"
+        });
+
+        const expectedViolations: ValidationViolation[] = [
+            {
+                severity: "error",
+                relativeFilepath: RelativeFilePath.of("openapi/openapi.yml"),
+                nodePath: ["paths", "/a/b", "get"],
+                message: "SDK method a.b.c.d already exists (x-fern-sdk-group-name: a.b.c, x-fern-sdk-method-name: d)"
+            }
+        ];
+
+        expect(violations).toMatchSnapshot();
     }, 10_000);
 });
