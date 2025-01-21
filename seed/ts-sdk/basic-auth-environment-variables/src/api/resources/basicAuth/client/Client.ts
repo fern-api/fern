7f284a3bb9d4dea034eb910d5e6eb25086e091b1
Alex McKinney <alexmckinney01@gmail.com>
2025-01-21 11:42:37 -0800

    Update ts-sdk snapshots

--- seed/ts-sdk/basic-auth-environment-variables/src/api/resources/basicAuth/client/Client.ts
+++ seed/ts-sdk/basic-auth-environment-variables/src/api/resources/basicAuth/client/Client.ts
@@ -201,19 +201,21 @@ export class BasicAuth {
         const username = (await core.Supplier.get(this._options.username)) ?? process?.env["USERNAME"];
         if (username == null) {
             throw new errors.SeedBasicAuthEnvironmentVariablesError({
-                message: "Please specify USERNAME when instantiating the client.",
+                message:
+                    "Please specify a username by either passing it in to the constructor or initializing a USERNAME environment variable",
             });
         }
 
         const accessToken = (await core.Supplier.get(this._options.accessToken)) ?? process?.env["PASSWORD"];
         if (accessToken == null) {
             throw new errors.SeedBasicAuthEnvironmentVariablesError({
-                message: "Please specify PASSWORD when instantiating the client.",
+                message:
+                    "Please specify a accessToken by either passing it in to the constructor or initializing a PASSWORD environment variable",
             });
         }
 
         return core.BasicAuth.toAuthorizationHeader({
-            username,
+            username: username,
             password: accessToken,
         });
     }
