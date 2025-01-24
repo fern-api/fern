d8a38ad79faba752507da5c459b306f11bfcf357
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-01-24 08:34:56 -0500

    chore(typescript): update typescript-express seed (#5736)
    Co-authored-by: dsinghvi <dsinghvi@users.noreply.github.com>

--- seed/ts-express/pagination/api/resources/users/service/UsersService.ts
+++ seed/ts-express/pagination/api/resources/users/service/UsersService.ts
@@ -60,6 +60,25 @@ export interface UsersServiceMethods {
         },
         next: express.NextFunction,
     ): void | Promise<void>;
+    listWithDoubleOffsetPagination(
+        req: express.Request<
+            never,
+            SeedPagination.ListUsersPaginationResponse,
+            never,
+            {
+                page?: number;
+                per_page?: number;
+                order?: SeedPagination.Order;
+                starting_after?: string;
+            }
+        >,
+        res: {
+            send: (responseBody: SeedPagination.ListUsersPaginationResponse) => Promise<void>;
+            cookie: (cookie: string, value: string, options?: express.CookieOptions) => void;
+            locals: any;
+        },
+        next: express.NextFunction,
+    ): void | Promise<void>;
     listWithBodyOffsetPagination(
         req: express.Request<
             never,
@@ -294,6 +313,38 @@ export class UsersService {
                 if (error instanceof errors.SeedPaginationError) {
                     console.warn(
                         `Endpoint 'listWithOffsetPagination' unexpectedly threw ${error.constructor.name}.` +
+                            ` If this was intentional, please add ${error.constructor.name} to` +
+                            " the endpoint's errors list in your Fern Definition.",
+                    );
+                    await error.send(res);
+                } else {
+                    res.status(500).json("Internal Server Error");
+                }
+                next(error);
+            }
+        });
+        this.router.get("", async (req, res, next) => {
+            try {
+                await this.methods.listWithDoubleOffsetPagination(
+                    req as any,
+                    {
+                        send: async (responseBody) => {
+                            res.json(
+                                serializers.ListUsersPaginationResponse.jsonOrThrow(responseBody, {
+                                    unrecognizedObjectKeys: "strip",
+                                }),
+                            );
+                        },
+                        cookie: res.cookie.bind(res),
+                        locals: res.locals,
+                    },
+                    next,
+                );
+                next();
+            } catch (error) {
+                if (error instanceof errors.SeedPaginationError) {
+                    console.warn(
+                        `Endpoint 'listWithDoubleOffsetPagination' unexpectedly threw ${error.constructor.name}.` +
                             ` If this was intentional, please add ${error.constructor.name} to` +
                             " the endpoint's errors list in your Fern Definition.",
                     );
