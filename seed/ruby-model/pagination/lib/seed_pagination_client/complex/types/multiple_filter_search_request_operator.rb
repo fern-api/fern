6f4aab11ee225e56973e3f674a4516fc0d865191
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-01-30 11:12:53 -0600

    chore(ruby): update ruby-model seed (#5799)

---
+++ seed/ruby-model/pagination/lib/seed_pagination_client/complex/types/multiple_filter_search_request_operator.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module SeedPaginationClient
+  class Complex
+    class MultipleFilterSearchRequestOperator
+      AND = "AND"
+      OR = "OR"
+    end
+  end
+end
