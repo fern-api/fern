fb173f1aff3121adff3854719b558bfe87713c3b
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-01-30 11:12:10 -0600

    chore(php): update php-model seed (#5800)
    Co-authored-by: dsinghvi <dsinghvi@users.noreply.github.com>

---
+++ seed/php-model/pagination/src/Complex/SingleFilterSearchRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Seed\Complex;
+
+use Seed\Core\Json\JsonSerializableType;
+use Seed\Core\Json\JsonProperty;
+
+class SingleFilterSearchRequest extends JsonSerializableType
+{
+    /**
+     * @var ?string $field
+     */
+    #[JsonProperty('field')]
+    public ?string $field;
+
+    /**
+     * @var ?value-of<SingleFilterSearchRequestOperator> $operator
+     */
+    #[JsonProperty('operator')]
+    public ?string $operator;
+
+    /**
+     * @var ?string $value
+     */
+    #[JsonProperty('value')]
+    public ?string $value;
+
+    /**
+     * @param array{
+     *   field?: ?string,
+     *   operator?: ?value-of<SingleFilterSearchRequestOperator>,
+     *   value?: ?string,
+     * } $values
+     */
+    public function __construct(
+        array $values = [],
+    ) {
+        $this->field = $values['field'] ?? null;
+        $this->operator = $values['operator'] ?? null;
+        $this->value = $values['value'] ?? null;
+    }
+}
