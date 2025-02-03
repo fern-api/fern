732b70d4f954b9494465309496f450fc7f56c6a3
Alberto <alberto@buildwithfern.com>
2025-02-03 15:02:28 -0500

    Run seed on exhaustive

--- seed/java-sdk/exhaustive/json-include-non-empty/src/main/java/com/seed/exhaustive/resources/inlinedrequests/InlinedRequestsClient.java
+++ seed/java-sdk/exhaustive/json-include-non-empty/src/main/java/com/seed/exhaustive/resources/inlinedrequests/InlinedRequestsClient.java
@@ -15,6 +15,8 @@ import com.seed.exhaustive.resources.generalerrors.types.BadObjectRequestInfo;
 import com.seed.exhaustive.resources.inlinedrequests.requests.PostWithObjectBody;
 import com.seed.exhaustive.resources.types.object.types.ObjectWithOptionalField;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import okhttp3.Headers;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
@@ -54,6 +56,10 @@ public class InlinedRequestsClient {
         } catch (JsonProcessingException e) {
             throw new SeedExhaustiveException("Failed to serialize request", e);
         }
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("string", request.getString());
+        properties.put("integer", request.getInteger());
+        properties.put("NestedObject", request.getNestedObject());
         Request okhttpRequest = new Request.Builder()
                 .url(httpUrl)
                 .method("POST", body)
