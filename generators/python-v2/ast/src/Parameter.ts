2221e586c4af098c40edbcc9ce9be5485135d67e
Noa Flaherty <noa@vellum.ai>
2025-02-03 20:00:52 -0500

    Update Parameter

--- generators/python-v2/ast/src/Parameter.ts
+++ generators/python-v2/ast/src/Parameter.ts
@@ -8,16 +8,16 @@ export declare namespace Parameter {
         /* The name of the parameter */
         name: string;
         /* The type of the parameter */
-        type: Type;
+        type?: Type;
         /* The initializer for the parameter */
-        initializer?: CodeBlock;
+        initializer?: AstNode;
     }
 }
 
 export class Parameter extends AstNode {
     public readonly name: string;
-    public readonly initializer: CodeBlock | undefined;
-    public readonly type: Type;
+    public readonly initializer: AstNode | undefined;
+    public readonly type: Type | undefined;
 
     constructor({ name, type, initializer }: Parameter.Args) {
         super();
@@ -28,8 +28,11 @@ export class Parameter extends AstNode {
 
     public write(writer: Writer): void {
         writer.write(this.name);
-        writer.write(": ");
-        this.type.write(writer);
+
+        if (this.type) {
+            writer.write(": ");
+            this.type.write(writer);
+        }
 
         if (this.initializer !== undefined) {
             writer.write(" = ");
