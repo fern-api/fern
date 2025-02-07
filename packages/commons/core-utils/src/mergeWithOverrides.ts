145b836c9e9eed299ba0f34d0946d67677cc0c5d
Rohin Bhargava <rohin@buildwithfern.com>
2025-02-07 11:14:43 -0500

    merge conflicts

--- packages/commons/core-utils/src/mergeWithOverrides.ts
+++ packages/commons/core-utils/src/mergeWithOverrides.ts
@@ -1,7 +1,18 @@
 import type { Dictionary, NumericDictionary, PartialObject, PropertyName, ValueKeyIteratee } from "lodash";
 import { isNull, isPlainObject, mergeWith, omitBy } from "lodash-es";
 
-export function mergeWithOverrides<T extends object>({ data, overrides }: { data: T; overrides: object }): T {
+export function mergeWithOverrides<T extends object>({
+    data,
+    overrides,
+    omissionCriteria
+}: {
+    data: T;
+    overrides: object;
+    omissionCriteria?: {
+        ancestorKeys: string[];
+        isDescendant: boolean;
+    };
+}): T {
     const merged = mergeWith(data, mergeWith, overrides, (obj, src) =>
         Array.isArray(obj) && Array.isArray(src)
             ? src.every((element) => typeof element === "object") && obj.every((element) => typeof element === "object")
@@ -12,20 +23,49 @@ export function mergeWithOverrides<T extends object>({ data, overrides }: { data
             : undefined
     ) as T;
     // Remove any nullified values
-    const filtered = omitDeepBy(merged, isNull) as T;
+    const filtered = omitDeepBy<T>(merged, isNull, omissionCriteria);
     return filtered;
 }
 
 // This is essentially lodash's omitBy, but actually running through your object tree.
 // The logic has been adapted from https://github.com/siberiacancode/lodash-omitdeep/tree/main.
 interface OmitDeepBy {
-    <T>(object: Dictionary<T> | null | undefined, predicate?: ValueKeyIteratee<T>): Dictionary<T>;
-    <T>(object: NumericDictionary<T> | null | undefined, predicate?: ValueKeyIteratee<T>): NumericDictionary<T>;
-    <T extends object>(object: T | null | undefined, predicate: ValueKeyIteratee<T[keyof T]>): PartialObject<T>;
+    <T>(
+        object: Dictionary<T> | null | undefined,
+        predicate?: ValueKeyIteratee<T>,
+        omissionCriteria?: {
+            ancestorKeys: string[];
+            isDescendant: boolean;
+        }
+    ): Dictionary<T>;
+    <T>(
+        object: NumericDictionary<T> | null | undefined,
+        predicate?: ValueKeyIteratee<T>,
+        omissionCriteria?: {
+            ancestorKeys: string[];
+            isDescendant: boolean;
+        }
+    ): NumericDictionary<T>;
+    <T extends object>(
+        object: T | null | undefined,
+        predicate: ValueKeyIteratee<T[keyof T]>,
+        omissionCriteria?: {
+            ancestorKeys: string[];
+            isDescendant: boolean;
+        }
+    ): PartialObject<T>;
 }
 
 // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-export const omitDeepBy: OmitDeepBy = (object: unknown, cb: any): any => {
+export const omitDeepBy: OmitDeepBy = (
+    object: unknown,
+    cb: any,
+    omissionCriteria?: {
+        ancestorKeys: string[];
+        isDescendant: boolean;
+        // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+    }
+): any => {
     function omitByDeepByOnOwnProps(object: unknown) {
         if (Array.isArray(object)) {
             return object.map((element) => omitDeepBy(element, cb));
@@ -35,9 +75,23 @@ export const omitDeepBy: OmitDeepBy = (object: unknown, cb: any): any => {
             const temp: Record<string, unknown> = {};
             // eslint-disable-next-line  @typescript-eslint/no-explicit-any
             for (const [key, value] of Object.entries<Record<string, PropertyName | object>>(object as any)) {
-                temp[key] = omitDeepBy(value, cb);
+                temp[key] = omitDeepBy(
+                    value,
+                    cb,
+                    omissionCriteria != null
+                        ? {
+                              ...omissionCriteria,
+                              isDescendant: omissionCriteria.ancestorKeys.includes(key)
+                          }
+                        : undefined
+                );
             }
-            return omitBy(temp, cb);
+
+            if (omissionCriteria == null || omissionCriteria?.isDescendant) {
+                return omitBy(temp, cb);
+            }
+
+            return temp;
         }
 
         return object;
