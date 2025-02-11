368e5af34ba240be0477c1c1ee35b96183949f77
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-02-11 09:34:43 -0500

    feat(php): Add automatic pagination (#5948)

---
+++ seed/php-sdk/pagination/property-accessors/src/Users/Types/WithPage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Seed\Users\Types;
+
+use Seed\Core\Json\JsonSerializableType;
+use Seed\Core\Json\JsonProperty;
+
+class WithPage extends JsonSerializableType
+{
+    /**
+     * @var ?int $page
+     */
+    #[JsonProperty('page')]
+    private ?int $page;
+
+    /**
+     * @param array{
+     *   page?: ?int,
+     * } $values
+     */
+    public function __construct(
+        array $values = [],
+    ) {
+        $this->page = $values['page'] ?? null;
+    }
+
+    /**
+     * @return ?int
+     */
+    public function getPage(): ?int
+    {
+        return $this->page;
+    }
+
+    /**
+     * @param ?int $value
+     */
+    public function setPage(?int $value = null): self
+    {
+        $this->page = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->toJson();
+    }
+}
