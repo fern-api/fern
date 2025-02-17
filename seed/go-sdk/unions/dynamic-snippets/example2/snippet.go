9e644faa9bb40fa65328d0d7b754dff57911784e
Alex McKinney <alexmckinney01@gmail.com>
2025-02-17 18:15:09 -0500

    fix(go): Handle maps in deep object query params (#6030)

---
+++ seed/go-sdk/unions/dynamic-snippets/example2/snippet.go
@@ -0,0 +1,49 @@
+package example
+
+import (
+    client "github.com/fern-api/unions-go/client"
+    option "github.com/fern-api/unions-go/option"
+    context "context"
+    unions "github.com/fern-api/unions-go"
+)
+
+func do() () {
+    client := client.NewClient(
+        option.WithBaseURL(
+            "https://api.fern.com",
+        ),
+    )
+    client.Bigunion.UpdateMany(
+        context.TODO(),
+        []*unions.BigUnion{
+            &unions.BigUnion{
+                NormalSweet: &unions.NormalSweet{
+                    Value: "value",
+                },
+                Id: "id",
+                CreatedAt: unions.MustParseDateTime(
+                    "2024-01-15T09:30:00Z",
+                ),
+                ArchivedAt: unions.Time(
+                    unions.MustParseDateTime(
+                        "2024-01-15T09:30:00Z",
+                    ),
+                ),
+            },
+            &unions.BigUnion{
+                NormalSweet: &unions.NormalSweet{
+                    Value: "value",
+                },
+                Id: "id",
+                CreatedAt: unions.MustParseDateTime(
+                    "2024-01-15T09:30:00Z",
+                ),
+                ArchivedAt: unions.Time(
+                    unions.MustParseDateTime(
+                        "2024-01-15T09:30:00Z",
+                    ),
+                ),
+            },
+        },
+    )
+}
