427dcf12600530f7097f5aa0cb8110b1eda591e7
Deep Singhvi <deep@buildwithfern.com>
2025-02-19 14:40:17 -0500

    fix(typescript): encode each property as a separate key value pair in the form data (#6073)
    * fix(typescript): encode each property as a separate key value pair in the form data
    
    * format

--- generators/typescript/utils/commons/src/core-utilities/form-data-utils/FormDataUtils.ts
+++ generators/typescript/utils/commons/src/core-utilities/form-data-utils/FormDataUtils.ts
@@ -3,7 +3,12 @@ import { ts } from "ts-morph";
 export interface FormDataUtils {
     newFormData: () => ts.AwaitExpression;
 
-    append: (args: { referenceToFormData: ts.Expression; key: string; value: ts.Expression }) => ts.Statement;
+    append: (args: {
+        referenceToFormData: ts.Expression;
+        key: string | ts.Expression;
+        value: ts.Expression;
+    }) => ts.Statement;
+    encodeAsFormParameter: (args: { referenceToArgument: ts.Expression }) => ts.CallExpression;
     appendFile: (args: {
         referenceToFormData: ts.Expression;
         key: string;
