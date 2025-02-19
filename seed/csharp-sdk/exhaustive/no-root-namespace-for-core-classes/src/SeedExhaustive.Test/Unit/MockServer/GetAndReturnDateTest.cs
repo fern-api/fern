fbcad6f40065dcda95bb4b307822e9f852c69ed8
Deep Singhvi <deep@buildwithfern.com>
2025-02-19 15:44:20 -0500

    fix(csharp): prefix Task return types with global to handle conflict (#6076)

--- seed/csharp-sdk/exhaustive/no-root-namespace-for-core-classes/src/SeedExhaustive.Test/Unit/MockServer/GetAndReturnDateTest.cs
+++ seed/csharp-sdk/exhaustive/no-root-namespace-for-core-classes/src/SeedExhaustive.Test/Unit/MockServer/GetAndReturnDateTest.cs
@@ -1,5 +1,5 @@
-using System.Threading.Tasks;
 using FluentAssertions.Json;
+using global::System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using SeedExhaustive.Core;
@@ -10,7 +10,7 @@ namespace SeedExhaustive.Test.Unit.MockServer;
 public class GetAndReturnDateTest : BaseMockServerTest
 {
     [Test]
-    public async Task MockServerTest()
+    public async global::System.Threading.Tasks.Task MockServerTest()
     {
         const string requestJson = """
             "2023-01-15"
