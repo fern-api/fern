8d7db638d2040237d82b87a34c7e120f2dd3d73b
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-02-20 14:35:42 -0500

    delete files to fix renames

--- packages/cli/configuration/src/generators-yml/utils/isAsyncApiSpecSchema.ts
+++
@@ -1,5 +0,0 @@
-import { AsyncApiSpecSchema, SpecSchema } from "../schemas";
-
-export function isAsyncApiSpecSchema(spec: SpecSchema): spec is AsyncApiSpecSchema {
-    return (spec as AsyncApiSpecSchema)?.asyncapi != null;
-}
--- packages/cli/configuration/src/generators-yml/utils/isOpenApiSpecSchema.ts
+++
@@ -1,5 +0,0 @@
-import { OpenApiSpecSchema, SpecSchema } from "../schemas";
-
-export function isOpenApiSpecSchema(spec: SpecSchema): spec is OpenApiSpecSchema {
-    return (spec as OpenApiSpecSchema)?.openapi != null;
-}
