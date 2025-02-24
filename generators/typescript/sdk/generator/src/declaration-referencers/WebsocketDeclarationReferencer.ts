3093383075bb9ebcc906f9181bc1711755da6a04
Raja Shravan <raja@buildwithfern.com>
2025-02-24 11:39:50 -0500

    removes some comments

--- generators/typescript/sdk/generator/src/declaration-referencers/WebsocketDeclarationReferencer.ts
+++ generators/typescript/sdk/generator/src/declaration-referencers/WebsocketDeclarationReferencer.ts
@@ -1,6 +1,9 @@
 import { ExportedFilePath, Reference, getExportedDirectoriesForFernFilepath } from "@fern-typescript/commons";
+
 import { RelativeFilePath } from "@fern-api/fs-utils";
+
 import { Name, WebSocketName } from "@fern-fern/ir-sdk/api";
+
 import { AbstractDeclarationReferencer } from "./AbstractDeclarationReferencer";
 import { DeclarationReferencer } from "./DeclarationReferencer";
 
@@ -10,14 +13,10 @@ export class WebsocketDeclarationReferencer extends AbstractDeclarationReference
     public getExportedFilepath(websocketName: WebSocketName): ExportedFilePath {
         // series of hack to get the filepath to work
 
-        // we need to create an object that implements FernFilepath.
-        // allParts and packagePath are FernIr.Name, and file is a FernIr.Name | undefined
-        // so we need to create a FernFilepath that has allParts and packagePath as FernIr.Name, and file as FernIr.Name | undefined
-        // we can do this by creating a new object that has all the same properties as FernFilepath, but with allParts and packagePath as FernIr.Name, and file as FernIr.Name | undefine
         const hackyFernFilepath = {
             allParts: [websocketName] as Name[],
             packagePath: [websocketName] as Name[],
-            file: websocketName,
+            file: websocketName
         };
 
         return {
@@ -54,4 +53,4 @@ export class WebsocketDeclarationReferencer extends AbstractDeclarationReference
     public getReferenceToWebsocket(args: DeclarationReferencer.getReferenceTo.Options<Name>): Reference {
         return this.getReferenceTo(this.getExportedName(args.name), args);
     }
-} 
+}
