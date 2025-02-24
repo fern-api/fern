d5e68a2917411ec721a37a9889b029036c143697
Eden <eden@buildwithfern.com>
2025-02-24 14:53:15 -0500

    Add baseline tests for v3 parser

--- packages/cli/api-importers/openapi/openapi-ir-to-fern-tests/src/__test__/openapi-v2-baseline.test.ts
+++ packages/cli/api-importers/openapi/openapi-ir-to-fern-tests/src/__test__/openapi-v2-baseline.test.ts
@@ -1,6 +1,8 @@
 import { readdir } from "fs/promises";
 
+import { SourceResolverImpl } from "@fern-api/cli-source-resolver";
 import { AbsoluteFilePath, RelativeFilePath, join } from "@fern-api/fs-utils";
+import { generateIntermediateRepresentation } from "@fern-api/ir-generator";
 import { OSSWorkspace } from "@fern-api/lazy-fern-workspace";
 import { createMockTaskContext } from "@fern-api/task-context";
 import { loadAPIWorkspace } from "@fern-api/workspace-loader";
@@ -8,7 +10,7 @@ import { loadAPIWorkspace } from "@fern-api/workspace-loader";
 const FIXTURES_DIR = join(AbsoluteFilePath.of(__dirname), RelativeFilePath.of("fixtures"));
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-describe("openapi-v2", async () => {
+describe("openapi-v2-baseline", async () => {
     for (const fixture of await readdir(FIXTURES_DIR, { withFileTypes: true })) {
         if (!fixture.isDirectory()) {
             continue;
@@ -32,12 +34,23 @@ describe("openapi-v2", async () => {
                 }
 
                 if (workspace.workspace instanceof OSSWorkspace) {
-                    const intermediateRepresentation = await (
-                        workspace.workspace as OSSWorkspace
-                    ).getIntermediateRepresentation({ context });
+                    const fernWorkspace = await (workspace.workspace as OSSWorkspace).toFernWorkspace({ context });
+                    const intermediateRepresentation = generateIntermediateRepresentation({
+                        workspace: fernWorkspace,
+                        generationLanguage: undefined,
+                        audiences: { type: "all" },
+                        keywords: undefined,
+                        smartCasing: true,
+                        exampleGeneration: { disabled: false },
+                        readme: undefined,
+                        version: undefined,
+                        packageName: undefined,
+                        context,
+                        sourceResolver: new SourceResolverImpl(context, fernWorkspace)
+                    });
                     // eslint-disable-next-line jest/no-standalone-expect
                     await expect(JSON.stringify(intermediateRepresentation, undefined, 2)).toMatchFileSnapshot(
-                        `./__snapshots__/openapi-v2/${fixture.name}.json`
+                        `./__snapshots__/openapi-v2-baseline/${fixture.name}.json`
                     );
                 }
             },
