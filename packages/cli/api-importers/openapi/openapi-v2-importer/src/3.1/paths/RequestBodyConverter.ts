55818ad29d4cfca825ce469fd4e1c7c396c02ae3
dsinghvi <deep@usebirch.com>
2025-02-25 10:54:42 -0500

    return multipart

--- packages/cli/api-importers/openapi/openapi-v2-importer/src/3.1/paths/RequestBodyConverter.ts
+++ packages/cli/api-importers/openapi/openapi-v2-importer/src/3.1/paths/RequestBodyConverter.ts
@@ -1,6 +1,6 @@
 import { OpenAPIV3_1 } from "openapi-types";
 
-import { FileUploadRequestProperty, HttpRequestBody, TypeDeclaration } from "@fern-api/ir-sdk";
+import { FileProperty, FileUploadRequestProperty, HttpRequestBody, TypeDeclaration } from "@fern-api/ir-sdk";
 
 import { AbstractConverter } from "../../AbstractConverter";
 import { ErrorCollector } from "../../ErrorCollector";
@@ -48,10 +48,7 @@ export class RequestBodyConverter extends AbstractConverter<
         }
 
         const jsonContentTypes = Object.keys(this.requestBody.content).filter((type) => type.includes("json"));
-        const multipartContentTypes = Object.keys(this.requestBody.content).filter((type) => type.includes("multipart")); 
-        const urlEncodedContentTypes = Object.keys(this.requestBody.content).filter((type) => type.includes("urlencoded"));
-
-        for (const contentType of [...jsonContentTypes, ...multipartContentTypes, ...urlEncodedContentTypes]) {
+        for (const contentType of [...jsonContentTypes]) {
             const mediaTypeObject = this.requestBody.content[contentType];
             if (mediaTypeObject == null || mediaTypeObject.schema == null) {
                 continue;
@@ -68,50 +65,86 @@ export class RequestBodyConverter extends AbstractConverter<
                 continue;
             }
 
-            console.log("Converted Schema (v3 RequestBodyConverter)", JSON.stringify(convertedSchema, null, 2));
-
             if (convertedSchema.schema?.shape.type === "object") {
-                let requestBody: HttpRequestBody;
-                if (contentType.includes("multipart")) {
-                    // if the schema is an object, return a HttpRequestBody.fileUpload instance
-                    requestBody = HttpRequestBody.fileUpload({
-                        docs: this.requestBody.description,
-                        name: context.casingsGenerator.generateName(schemaId),
-                        properties: convertedSchema.schema?.shape.properties.map((property) => {
-                            return FileUploadRequestProperty.bodyProperty({
-                                ...property,
-                                contentType,
-                                style: "form",
-                                name: property.name
-                            })
-                        })
-                    });
-                } else {
-                    // if the schema is an object, return a HttpRequestBody.inlinedRequestBody instance
-                    requestBody = HttpRequestBody.inlinedRequestBody({
-                        contentType,
-                        docs: this.requestBody.description,
-                        name: context.casingsGenerator.generateName(schemaId),
-                        extendedProperties: convertedSchema.schema?.shape.extendedProperties,
-                        extends: convertedSchema.schema?.shape.extends,
-                        properties: convertedSchema.schema?.shape.properties,
-                        extraProperties: convertedSchema.schema?.shape.extraProperties
-                    });
-                }
-                console.log("Returned (v3 RequestBodyConverter)", JSON.stringify({
-                    requestBody,
-                    inlinedTypes: Object.fromEntries(
-                        Object.entries(convertedSchema.inlinedTypes).filter(([key]) => key !== schemaId)
-                    )
-                }, null, 2));
+                const requestBody = HttpRequestBody.inlinedRequestBody({
+                    contentType,
+                    docs: this.requestBody.description,
+                    name: context.casingsGenerator.generateName(schemaId),
+                    extendedProperties: convertedSchema.schema?.shape.extendedProperties,
+                    extends: convertedSchema.schema?.shape.extends,
+                    properties: convertedSchema.schema?.shape.properties,
+                    extraProperties: convertedSchema.schema?.shape.extraProperties
+                });
+
                 return {
                     requestBody,
                     inlinedTypes: Object.fromEntries(
                         Object.entries(convertedSchema.inlinedTypes).filter(([key]) => key !== schemaId)
                     )
                 };
+            } else {
+                const requestBody = HttpRequestBody.reference({
+                    contentType,
+                    docs: this.requestBody.description,
+                    requestBodyType: convertedSchema.type,
+                });
+
+                return {
+                    requestBody,
+                    inlinedTypes: convertedSchema.inlinedTypes
+                };
             }
         }
+
+        const multipartContentTypes = Object.keys(this.requestBody.content).filter((type) => type.includes("multipart")); 
+        for (const contentType of multipartContentTypes) {
+            const mediaTypeObject = this.requestBody.content[contentType];
+            if (mediaTypeObject == null || mediaTypeObject.schema == null) {
+                continue;
+            }
+
+            const schemaId = [...this.group, this.method, "Request"].join("_");
+            const schemaOrReferenceConverter = new SchemaOrReferenceConverter({
+                breadcrumbs: [...this.breadcrumbs, "schema"],
+                schemaOrReference: mediaTypeObject.schema,
+                schemaIdOverride: schemaId
+            });
+
+            const convertedSchema = schemaOrReferenceConverter.convert({ context, errorCollector });
+            if (convertedSchema == null) {
+                continue;
+            }
+
+            if (convertedSchema.schema?.shape.type === "object") {
+                const requestBody = HttpRequestBody.fileUpload({
+                    docs: this.requestBody.description,
+                    name: context.casingsGenerator.generateName(schemaId),
+                    properties: convertedSchema.schema?.shape.properties.map((property) => {
+                        if (property.valueType.type === "primitive" && 
+                            property.valueType.primitive.v2?.type === "string" &&
+                            property.valueType.primitive.v2.validation?.format === "binary") {
+                            return FileUploadRequestProperty.file(FileProperty.file({
+                                key: property.name,
+                                isOptional: false,
+                                contentType,
+                            }));
+                        }
+                        return FileUploadRequestProperty.bodyProperty({
+                            ...property,
+                            contentType,
+                            style: undefined, 
+                            name: property.name
+                        })
+                    })
+                });
+                return {
+                    requestBody,
+                    inlinedTypes: convertedSchema.inlinedTypes
+                };
+            }
+        }
+
+        const urlEncodedContentTypes = Object.keys(this.requestBody.content).filter((type) => type.includes("urlencoded"));
 
         return undefined;
     }
