cc560938b5511a0608ff92e52a21a8aa5568b06a
ajgateno <ajgateno@users.noreply.github.com>
2025-02-25 18:53:28 +0000

    chore(ruby): update ruby-model seed

--- seed/ruby-model/file-upload/lib/seed_file_upload_client.rb
+++ seed/ruby-model/file-upload/lib/seed_file_upload_client.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
+require_relative "seed_file_upload_client/service/types/my_object_with_optional"
 require_relative "seed_file_upload_client/service/types/my_object"
 require_relative "seed_file_upload_client/service/types/object_type"
