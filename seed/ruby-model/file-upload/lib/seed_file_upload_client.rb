ce9e4e5c436d92604e3951d498f336f369227837
Swimburger <Swimburger@users.noreply.github.com>
2025-02-28 19:59:05 +0000

    chore(ruby): update ruby-model seed

--- seed/ruby-model/file-upload/lib/seed_file_upload_client.rb
+++ seed/ruby-model/file-upload/lib/seed_file_upload_client.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "seed_file_upload_client/service/types/my_object_with_optional"
 require_relative "seed_file_upload_client/service/types/my_object"
+require_relative "seed_file_upload_client/service/types/my_alias_object"
+require_relative "seed_file_upload_client/service/types/my_collection_alias_object"
 require_relative "seed_file_upload_client/service/types/object_type"
