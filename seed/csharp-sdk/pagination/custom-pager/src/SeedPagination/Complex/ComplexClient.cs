b07dfd9620d7763827c6e07c9cc159f623a151d2
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-03-02 19:42:48 -0500

    oops

--- seed/csharp-sdk/pagination/custom-pager/src/SeedPagination/Complex/ComplexClient.cs
+++ seed/csharp-sdk/pagination/custom-pager/src/SeedPagination/Complex/ComplexClient.cs
@@ -53,6 +53,7 @@ public partial class ComplexClient
                 SearchInternalAsync,
                 (request, cursor) =>
                 {
+                    request.Pagination ??= new();
                     request.Pagination.StartingAfter = cursor;
                 },
                 response => response?.Pages?.Next?.StartingAfter,
