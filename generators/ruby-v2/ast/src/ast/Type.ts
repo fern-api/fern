db7b8deb5bde07a326ed02b0db772d412a2db8ea
Ethan Turkeltaub <ethan@buildwithfern.com>
2025-03-10 17:26:00 -0400

    Begin trying to enumerate types in Ruby

--- generators/ruby-v2/ast/src/ast/Type.ts
+++ generators/ruby-v2/ast/src/ast/Type.ts
@@ -3,14 +3,65 @@ import { assertNever } from "@fern-api/core-utils";
 import { AstNode } from "./core/AstNode";
 import { Writer } from "./core/Writer";
 
-type InternalType = Boolean_ | String_;
+type InternalType =
+    | Integer
+    | Float
+    | BigDecimal
+    | Array
+    | Hash
+    | Range
+    | Symbol_
+    | Regexp
+    | Boolean_
+    | String_
+    | Class_;
+
+interface Integer {
+    type: "Integer";
+}
+
+interface Float {
+    type: "Float";
+}
+
+interface BigDecimal {
+    type: "BigDecimal";
+}
+
+interface Array {
+    type: "Array";
+    elem: Type;
+}
+
+interface Hash {
+    type: "Hash";
+    pairs: object;
+}
+
+interface Range {
+    type: "Range";
+    elem: Type;
+}
+
+interface Symbol_ {
+    type: "Symbol";
+}
+
+interface Regexp {
+    type: "Regexp";
+}
 
 interface Boolean_ {
-    type: "boolean";
+    type: "Boolean";
 }
 
 interface String_ {
-    type: "string";
+    type: "String";
+}
+
+interface Class_ {
+    type: "Class";
+    name: string;
 }
 
 export class Type extends AstNode {
@@ -20,11 +71,38 @@ export class Type extends AstNode {
 
     public write(writer: Writer): void {
         switch (this.internalType.type) {
-            case "boolean":
-                writer.write("T::Boolean");
+            case "Boolean":
+                writer.write("bool");
                 break;
-            case "string":
+            case "Integer":
+                writer.write("Integer");
+                break;
+            case "Float":
+                writer.write("Float");
+                break;
+            case "BigDecimal":
+                writer.write("BigDecimal");
+                break;
+            case "String":
                 writer.write("String");
+                break;
+            case "Array":
+                writer.write(`Array[${this.internalType.elem}]`);
+                break;
+            case "Hash":
+                // TODO
+                break;
+            case "Range":
+                writer.write(`Range[${this.internalType.elem}]`);
+                break;
+            case "Symbol":
+                writer.write("Symbol");
+                break;
+            case "Regexp":
+                writer.write("Regexp");
+                break;
+            case "Class":
+                writer.write(this.internalType.name);
                 break;
             default:
                 assertNever(this.internalType);
@@ -34,13 +112,13 @@ export class Type extends AstNode {
     /* Static factory methods for creating a Type */
     public static boolean(): Type {
         return new this({
-            type: "boolean"
+            type: "Boolean"
         });
     }
 
     public static string(): Type {
         return new this({
-            type: "string"
+            type: "String"
         });
     }
 }
