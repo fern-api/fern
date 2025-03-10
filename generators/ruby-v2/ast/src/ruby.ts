5cc1b771186efe19ec9c00a4e5b8cd0f50d94d9d
Ethan Turkeltaub <ethan@buildwithfern.com>
2025-03-10 16:43:26 -0400

    feat(ruby): Add method definition AST (#6359)
    * Add method signatures with parameters for Ruby AST
    
    * Add documentation for args
    
    * Add visibility and kinds of methods
    
    * Add `Comment`, docstrings for methods

--- generators/ruby-v2/ast/src/ruby.ts
+++ generators/ruby-v2/ast/src/ruby.ts
@@ -1,8 +1,53 @@
-import { CodeBlock } from "./ast";
+import {
+    CodeBlock,
+    Comment,
+    KeywordParameter,
+    KeywordSplatParameter,
+    Method,
+    PositionalParameter,
+    PositionalSplatParameter,
+    YieldParameter
+} from "./ast";
+
+export { AstNode } from "./ast/core/AstNode";
+export { CodeBlock, Parameter, Method } from "./ast";
 
 export function codeblock(arg: CodeBlock.Arg): CodeBlock {
     return new CodeBlock(arg);
 }
 
-export { AstNode } from "./ast/core/AstNode";
-export { CodeBlock, Type, TypeLiteral, Writer } from "./ast";
+/*
+Helper for positional parameters -- most of the time when you're talking about a "parameter" in Ruby, you're talking
+about positional parameters
+*/
+export function parameter(args: PositionalParameter.Args): PositionalParameter {
+    return new PositionalParameter(args);
+}
+
+export function positionalParameter(args: PositionalParameter.Args): PositionalParameter {
+    return new PositionalParameter(args);
+}
+
+export function keywordParameter(args: KeywordParameter.Args): KeywordParameter {
+    return new KeywordParameter(args);
+}
+
+export function positionalSplatParameter(args: PositionalSplatParameter.Args): PositionalSplatParameter {
+    return new PositionalSplatParameter(args);
+}
+
+export function keywordSplatParameter(args: KeywordSplatParameter.Args): KeywordSplatParameter {
+    return new KeywordSplatParameter(args);
+}
+
+export function yieldParameter(args: YieldParameter.Args): YieldParameter {
+    return new YieldParameter(args);
+}
+
+export function method(args: Method.Args): Method {
+    return new Method(args);
+}
+
+export function comment(args: Comment.Args): Comment {
+    return new Comment(args);
+}
