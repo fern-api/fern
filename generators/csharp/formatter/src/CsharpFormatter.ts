3f9bae235f9e6fc70dffaa0396a7d97402cd7145
Alex McKinney <alexmckinney01@gmail.com>
2025-03-12 14:36:59 -0400

    Add formatter

--- generators/csharp/formatter/src/CsharpFormatter.ts
+++ generators/csharp/formatter/src/CsharpFormatter.ts
@@ -3,10 +3,21 @@ import { execSync } from "child_process";
 
 export class CsharpFormatter extends AbstractFormatter {
     public async format(content: string): Promise<string> {
-        return execSync("dotnet csharpier", { input: content, encoding: "utf-8" });
+        return this.formatCode(content);
     }
 
     public formatSync(content: string): string {
-        return execSync("dotnet csharpier", { input: content, encoding: "utf-8" });
+        return this.formatCode(content);
+    }
+
+    private formatCode(content: string): string {
+        if (!content.endsWith(";")) {
+            content += ";";
+        }
+        try {
+            return execSync("dotnet csharpier", { input: content, encoding: "utf-8" });
+        } catch (e: unknown) {
+            return content;
+        }
     }
 }
