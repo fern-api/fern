44a542a5a01de912b828ae4ec7687d9d57c337da
amckinney <amckinney@users.noreply.github.com>
2025-03-12 23:56:11 +0000

    chore(ruby): update ruby-model seed

--- seed/ruby-model/enum/lib/seed_enum_client.rb
+++ seed/ruby-model/enum/lib/seed_enum_client.rb
@@ -3,3 +3,4 @@
 require_relative "seed_enum_client/types/operand"
 require_relative "seed_enum_client/types/color"
 require_relative "seed_enum_client/types/color_or_operand"
+require_relative "seed_enum_client/unknown/types/status"
