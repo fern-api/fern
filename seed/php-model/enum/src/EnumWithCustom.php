8b9f85c3f1156e61b3518b5fbf9755cae6cec265
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-03-13 15:05:52 -0400

    fix(csharp): Rename Custom method to FromCustom in String Enums (#6438)

---
+++ seed/php-model/enum/src/EnumWithCustom.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Seed;
+
+enum EnumWithCustom: string
+{
+    case Safe = "safe";
+    case Custom = "Custom";
+}
--- seed/python-sdk/enum/real-enum-forward-compat/src/seed/types/__init__.py
+++ seed/python-sdk/enum/real-enum-forward-compat/src/seed/types/__init__.py
@@ -2,6 +2,7 @@
 
 from .color import Color
 from .color_or_operand import ColorOrOperand
+from .enum_with_custom import EnumWithCustom
 from .operand import Operand
 
-__all__ = ["Color", "ColorOrOperand", "Operand"]
+__all__ = ["Color", "ColorOrOperand", "EnumWithCustom", "Operand"]
