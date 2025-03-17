fb39efdb09d097efa895c76de148bbdd90d6c4a1
Alberto <alberto@buildwithfern.com>
2025-03-17 17:02:51 -0400

    Add generator agent to sdk generator context

--- generators/java-v2/sdk/src/SdkGeneratorContext.ts
+++ generators/java-v2/sdk/src/SdkGeneratorContext.ts
@@ -4,9 +4,13 @@ import { AbstractJavaGeneratorContext } from "@fern-api/java-base";
 import { FernGeneratorExec } from "@fern-fern/generator-exec-sdk";
 import { IntermediateRepresentation } from "@fern-fern/ir-sdk/api";
 
+import { JavaGeneratorAgent } from "./JavaGeneratorAgent";
 import { SdkCustomConfigSchema } from "./SdkCustomConfig";
+import { ReadmeConfigBuilder } from "./readme/ReadmeConfigBuilder";
 
 export class SdkGeneratorContext extends AbstractJavaGeneratorContext<SdkCustomConfigSchema> {
+    public readonly generatorAgent: JavaGeneratorAgent;
+
     public constructor(
         public readonly ir: IntermediateRepresentation,
         public readonly config: FernGeneratorExec.config.GeneratorConfig,
@@ -14,5 +18,10 @@ export class SdkGeneratorContext extends AbstractJavaGeneratorContext<SdkCustomC
         public readonly generatorNotificationService: GeneratorNotificationService
     ) {
         super(ir, config, customConfig, generatorNotificationService);
+        this.generatorAgent = new JavaGeneratorAgent({
+            logger: this.logger,
+            config: this.config,
+            readmeConfigBuilder: new ReadmeConfigBuilder()
+        });
     }
 }
