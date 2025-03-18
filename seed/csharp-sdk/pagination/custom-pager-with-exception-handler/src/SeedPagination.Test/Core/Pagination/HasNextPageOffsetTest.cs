8d4ee24cf168f2ae8736b22acf70da2ac99c5b66
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-03-18 14:14:45 -0400

    simplify pagination C# tests

--- seed/csharp-sdk/pagination/custom-pager-with-exception-handler/src/SeedPagination.Test/Core/Pagination/HasNextPageOffsetTest.cs
+++ seed/csharp-sdk/pagination/custom-pager-with-exception-handler/src/SeedPagination.Test/Core/Pagination/HasNextPageOffsetTest.cs
@@ -10,12 +10,6 @@ public class HasNextPageOffsetTest
     [Test]
     public async SystemTask OffsetPagerShouldWorkWithHasNextPage()
     {
-        var pager = await CreatePagerAsync();
-        await AssertPagerAsync(pager);
-    }
-
-    private static async Task<Pager<object>> CreatePagerAsync()
-    {
         var responses = new List<Response>
         {
             new()
@@ -42,7 +36,7 @@ public class HasNextPageOffsetTest
             object?,
             object
         >.CreateInstanceAsync(
-            new() { Pagination = new() { Page = 1 } },
+            new Request { Pagination = new Pagination { Page = 1 } },
             null,
             (_, _, _) =>
             {
@@ -59,11 +53,7 @@ public class HasNextPageOffsetTest
             response => response?.Data?.Items?.ToList(),
             response => response.HasNext
         );
-        return pager;
-    }
 
-    private static async SystemTask AssertPagerAsync(Pager<object> pager)
-    {
         var pageCounter = 0;
         var itemCounter = 0;
         await foreach (var page in pager.AsPagesAsync())
@@ -81,7 +71,7 @@ public class HasNextPageOffsetTest
 
     private class Request
     {
-        public Pagination Pagination { get; set; }
+        public Pagination? Pagination { get; set; }
     }
 
     private class Pagination
@@ -91,12 +81,12 @@ public class HasNextPageOffsetTest
 
     private class Response
     {
-        public Data Data { get; set; }
+        public Data? Data { get; set; }
         public bool HasNext { get; set; }
     }
 
     private class Data
     {
-        public IEnumerable<string> Items { get; set; }
+        public IEnumerable<string>? Items { get; set; }
     }
 }
