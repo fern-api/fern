eee7517701dc1f25c31b0095094813ae6c09114e
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-03-20 19:38:03 -0400

    simplify C# formatter

--- generators/csharp/formatter/src/CsharpFormatter.ts
+++ generators/csharp/formatter/src/CsharpFormatter.ts
@@ -1,4 +1,4 @@
-import { ChildProcessWithoutNullStreams, execSync, spawn } from "child_process";
+import execa from "execa";
 
 import { AbstractFormatter } from "@fern-api/base-generator";
 
@@ -17,58 +17,23 @@ export class CsharpFormatter extends AbstractFormatter {
             content += ";";
         }
 
-        const childProcess = spawn(this.csharpier, ["--fast", "--no-msbuild-check", "--pipe-multiple-files"], {
-            stdio: "pipe"
+        const { stdout } = await execa(this.csharpier, ["--fast", "--no-msbuild-check"], {
+            input: content,
+            encoding: "utf-8",
+            stripFinalNewline: false
         });
-        let stdout = "";
-        let stderr = "";
-
-        return new Promise<string>((resolve, reject) => {
-            // csharpier allows multiple files, so it expects you to send the filename first, then the file contents
-            childProcess.stdin.write("dummy.cs");
-            childProcess.stdin.write("\u0003");
-            childProcess.stdin.write(content);
-            if (!content.endsWith("\u0003")) {
-                childProcess.stdin.write("\u0003");
-            }
-            childProcess.stdin.end();
-
-            childProcess.stdout.on("data", (data) => {
-                stdout += data.toString();
-            });
-
-            childProcess.stderr.on("data", (data) => {
-                stderr += data.toString();
-            });
-
-            childProcess.on("close", (code) => {
-                if (code === 0) {
-                    if (stdout.endsWith("\u0003")) {
-                        stdout = stdout.slice(0, -1);
-                    }
-                    resolve(stdout);
-                } else {
-                    reject(new Error(`Formatting failed with code ${code}: ${stderr}`));
-                }
-            });
-
-            childProcess.on("error", (error) => {
-                reject(error);
-            });
-        });
+        return stdout;
     }
 
     public formatSync(content: string): string {
         if (!content.endsWith(";")) {
             content += ";";
         }
-        try {
-            return execSync([this.csharpier, "--fast", "--no-msbuild-check"].join(" "), {
-                input: content,
-                encoding: "utf-8"
-            });
-        } catch (e: unknown) {
-            return content;
-        }
+        const { stdout } = execa.sync(this.csharpier, ["--fast", "--no-msbuild-check"], {
+            input: content,
+            encoding: "utf-8",
+            stripFinalNewline: false
+        });
+        return stdout;
     }
 }
