ad9a3aa71c56913181f6f75302e05c59ec88bd1d
Alex McKinney <alexmckinney01@gmail.com>
2025-03-25 17:42:07 -0400

    Filter OpenAPI specs

--- packages/cli/workspace/lazy-fern-workspace/src/OSSWorkspace.ts
+++ packages/cli/workspace/lazy-fern-workspace/src/OSSWorkspace.ts
@@ -45,7 +45,11 @@ export class OSSWorkspace extends BaseOpenAPIWorkspace {
             onlyIncludeReferencedSchemas: specs.every((spec) => spec.settings?.onlyIncludeReferencedSchemas),
             inlinePathParameters: specs.every((spec) => spec.settings?.inlinePathParameters),
             objectQueryParameters: specs.every((spec) => spec.settings?.objectQueryParameters),
-            useBytesForBinaryResponse: specs.every((spec) => spec.settings?.useBytesForBinaryResponse),
+            useBytesForBinaryResponse: specs
+                .filter((spec) => spec.type === "openapi" && spec.source.type === "openapi")
+
+                // TODO: Update this to '.every' once AsyncAPI sources are correctly recognized.
+                .some((spec) => spec.settings?.useBytesForBinaryResponse),
             exampleGeneration: specs[0]?.settings?.exampleGeneration
         });
         this.specs = specs;
