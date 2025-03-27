9fd0bb01277fd8c4a1d7591fdb7dbebadc9882e9
Alberto <alberto@buildwithfern.com>
2025-03-27 17:05:37 -0400

    fix(java): Apply content type to request body creation

---
+++ seed/java-sdk/content-type/src/main/java/com/snippets/Example0.java
@@ -0,0 +1,22 @@
+package com.snippets;
+
+import com.seed.contentTypes.SeedContentTypesClient;
+import com.seed.contentTypes.resources.service.requests.PatchProxyRequest;
+
+public class Example0 {
+    public static void main(String[] args) {
+        SeedContentTypesClient client = SeedContentTypesClient
+            .builder()
+            .url("https://api.fern.com")
+            .build();
+
+        client.service().patch(
+            "id",
+            PatchProxyRequest
+                .builder()
+                .application("application")
+                .requireAuth(true)
+                .build()
+        );
+    }
+}
