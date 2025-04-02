c4189f933ff11f8f96d2c6a581cda8f354f430d0
Eden <eden@buildwithfern.com>
2025-04-02 09:56:52 -0400

    Introduce IR changes for v2 examples

--- packages/cli/api-importers/openapi-to-ir/src/3.1/paths/operations/OperationConverter.ts
+++ packages/cli/api-importers/openapi-to-ir/src/3.1/paths/operations/OperationConverter.ts
@@ -118,7 +118,8 @@ export class OperationConverter extends AbstractOperationConverter {
                 fullPath: constructHttpPath(this.path),
                 allPathParameters: pathParameters,
                 pagination: undefined,
-                transport: undefined
+                transport: undefined,
+                v2Examples: undefined
             },
             inlinedTypes: this.inlinedTypes,
             servers: this.operation.servers
