885848d56421e5df38e3e2cd235e57fa3a2c192c
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-04-03 17:35:19 -0400

    clean up more dangling seed definitions

---
+++ seed/ts-express/content-type/errors/index.ts
@@ -0,0 +1,1 @@
+export { SeedContentTypesError } from "./SeedContentTypesError";
--- seed/ts-express/grpc-proto-exhaustive/api/resources/dataservice/service/requests/index.ts
+++
@@ -1,5 +0,0 @@
-export { UploadRequest } from "./UploadRequest";
-export { DeleteRequest } from "./DeleteRequest";
-export { DescribeRequest } from "./DescribeRequest";
-export { QueryRequest } from "./QueryRequest";
-export { UpdateRequest } from "./UpdateRequest";
