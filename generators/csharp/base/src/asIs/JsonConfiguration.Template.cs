13d24b2c2637e46f9ea93419992559f0ad35f3d3
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-04-07 11:21:41 -0400

    Fix issue where read-only list properties wouldn't deserialize correctly.

--- generators/csharp/base/src/asIs/JsonConfiguration.Template.cs
+++ generators/csharp/base/src/asIs/JsonConfiguration.Template.cs
@@ -50,7 +50,6 @@ internal static partial class JsonOptions
                                 switch (jsonAccessAttribute.AccessType)
                                 {
                                     case JsonAccessType.ReadOnly:
-                                        propertyInfo.Get = null;
                                         propertyInfo.ShouldSerialize = (_, _) => false;
                                         break;
                                     case JsonAccessType.WriteOnly:
