cbb2b961decd189dd7fe8e63f2349fca9bb5ba9b
Alex McKinney <alexmckinney01@gmail.com>
2025-04-10 16:32:19 -0400

    feat(python): Add exclude_types_from_init_exports configuration (#6719)

--- generators/python/src/fern_python/generators/sdk/client_generator/base_wrapped_client_generator.py
+++ generators/python/src/fern_python/generators/sdk/client_generator/base_wrapped_client_generator.py
@@ -44,7 +44,7 @@ class BaseWrappedClientGenerator(BaseClientGenerator):
     ) -> None:
         for endpoint in service.endpoints:
             # TODO: Roll out Stream Parameter endpoints.
-            if self._is_stream_parameter_endpoint(endpoint):
+            if self._is_stream_parameter_endpoint(endpoint) or self._treat_as_paginated(endpoint):
                 endpoint_generator = EndpointFunctionGenerator(
                     context=self._context,
                     package=self._package,
@@ -63,7 +63,18 @@ class BaseWrappedClientGenerator(BaseClientGenerator):
                 for generated_function in generated_endpoint_functions:
                     if generated_function.is_default_body_parameter_used:
                         self._is_default_body_parameter_used = True
-                        break
+
+                    # TODO: Remove this. Currently, we need to preserve snippets for paginated endpoints (since we're not doing this in the raw client generator)
+                    if self._treat_as_paginated(endpoint):
+                        for snippet in generated_function.snippets or []:
+                            if is_async:
+                                self._snippet_registry.register_async_client_endpoint_snippet(
+                                    endpoint=endpoint, expr=snippet.snippet, example_id=snippet.example_id
+                                )
+                            else:
+                                self._snippet_registry.register_sync_client_endpoint_snippet(
+                                    endpoint=endpoint, expr=snippet.snippet, example_id=snippet.example_id
+                                )
 
                 for overload_function in generated_endpoint_functions[:-1]:
                     class_declaration.add_method(overload_function.function)
