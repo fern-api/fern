f8bd5a301d681f220e2ca5c9dbd7519e93e69ce3
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-04-11 11:19:10 -0400

    chore(java): update java-spring seed (#6703)
    Co-authored-by: amckinney <amckinney@users.noreply.github.com>
    Co-authored-by: David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>

--- seed/java-spring/nullable/resources/nullable/types/Metadata.java
+++ seed/java-spring/nullable/resources/nullable/types/Metadata.java
@@ -15,6 +15,7 @@ import java.lang.Boolean;
 import java.lang.Object;
 import java.lang.String;
 import java.time.OffsetDateTime;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
@@ -34,13 +35,16 @@ public final class Metadata {
 
   private final Status status;
 
+  private final Optional<Map<String, Optional<String>>> values;
+
   private Metadata(OffsetDateTime createdAt, OffsetDateTime updatedAt, Optional<String> avatar,
-      Optional<Boolean> activated, Status status) {
+      Optional<Boolean> activated, Status status, Optional<Map<String, Optional<String>>> values) {
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
     this.avatar = avatar;
     this.activated = activated;
     this.status = status;
+    this.values = values;
   }
 
   @JsonProperty("createdAt")
@@ -68,6 +72,11 @@ public final class Metadata {
     return status;
   }
 
+  @JsonProperty("values")
+  public Optional<Map<String, Optional<String>>> getValues() {
+    return values;
+  }
+
   @java.lang.Override
   public boolean equals(Object other) {
     if (this == other) return true;
@@ -75,12 +84,12 @@ public final class Metadata {
   }
 
   private boolean equalTo(Metadata other) {
-    return createdAt.equals(other.createdAt) && updatedAt.equals(other.updatedAt) && avatar.equals(other.avatar) && activated.equals(other.activated) && status.equals(other.status);
+    return createdAt.equals(other.createdAt) && updatedAt.equals(other.updatedAt) && avatar.equals(other.avatar) && activated.equals(other.activated) && status.equals(other.status) && values.equals(other.values);
   }
 
   @java.lang.Override
   public int hashCode() {
-    return Objects.hash(this.createdAt, this.updatedAt, this.avatar, this.activated, this.status);
+    return Objects.hash(this.createdAt, this.updatedAt, this.avatar, this.activated, this.status, this.values);
   }
 
   @java.lang.Override
@@ -116,6 +125,10 @@ public final class Metadata {
     _FinalStage activated(Optional<Boolean> activated);
 
     _FinalStage activated(Boolean activated);
+
+    _FinalStage values(Optional<Map<String, Optional<String>>> values);
+
+    _FinalStage values(Map<String, Optional<String>> values);
   }
 
   @JsonIgnoreProperties(
@@ -127,6 +140,8 @@ public final class Metadata {
     private OffsetDateTime updatedAt;
 
     private Status status;
+
+    private Optional<Map<String, Optional<String>>> values = Optional.empty();
 
     private Optional<Boolean> activated = Optional.empty();
 
@@ -142,6 +157,7 @@ public final class Metadata {
       avatar(other.getAvatar());
       activated(other.getActivated());
       status(other.getStatus());
+      values(other.getValues());
       return this;
     }
 
@@ -163,6 +179,22 @@ public final class Metadata {
     @JsonSetter("status")
     public _FinalStage status(@NotNull Status status) {
       this.status = Objects.requireNonNull(status, "status must not be null");
+      return this;
+    }
+
+    @java.lang.Override
+    public _FinalStage values(Map<String, Optional<String>> values) {
+      this.values = Optional.ofNullable(values);
+      return this;
+    }
+
+    @java.lang.Override
+    @JsonSetter(
+        value = "values",
+        nulls = Nulls.SKIP
+    )
+    public _FinalStage values(Optional<Map<String, Optional<String>>> values) {
+      this.values = values;
       return this;
     }
 
@@ -200,7 +232,7 @@ public final class Metadata {
 
     @java.lang.Override
     public Metadata build() {
-      return new Metadata(createdAt, updatedAt, avatar, activated, status);
+      return new Metadata(createdAt, updatedAt, avatar, activated, status, values);
     }
   }
 }
--- seed/java-spring/undiscriminated-unions/resources/union/UnionService.java
+++ seed/java-spring/undiscriminated-unions/resources/union/UnionService.java
@@ -6,10 +6,13 @@ package resources.union;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import resources.union.types.Metadata;
+import resources.union.types.MetadataUnion;
 import resources.union.types.MyUnion;
+import resources.union.types.Request;
 
 @RequestMapping(
     path = "/"
@@ -27,4 +30,18 @@ public interface UnionService {
       produces = "application/json"
   )
   Metadata getMetadata();
+
+  @PutMapping(
+      value = "/metadata",
+      produces = "application/json",
+      consumes = "application/json"
+  )
+  boolean updateMetadata(@RequestBody MetadataUnion body);
+
+  @PostMapping(
+      value = "/call",
+      produces = "application/json",
+      consumes = "application/json"
+  )
+  boolean call(@RequestBody Request body);
 }
