875d29994f77966dc367517be6aac7bd3ec99c0f
Eden <eden@buildwithfern.com>
2025-04-13 15:09:47 -0400

    address nit

--- packages/cli/api-importers/v2-importer-commons/src/converters/abstract/AbstractParameterConverter.ts
+++ packages/cli/api-importers/v2-importer-commons/src/converters/abstract/AbstractParameterConverter.ts
@@ -159,7 +159,7 @@ export abstract class AbstractParameterConverter<
             if (context.isReferenceObject(example)) {
                 const resolved = await context.resolveReference(example);
                 if (resolved.resolved) {
-                    if (typeof resolved.value === "object" && resolved.value != null && "value" in resolved.value) {
+                    if (this.isExampleWithValue(resolved.value)) {
                         userSpecifiedExamples[key] = resolved.value.value;
                     }
                 }
@@ -179,5 +179,9 @@ export abstract class AbstractParameterConverter<
                   }
                 : userSpecifiedExamples;
         return userSpecifiedExamples;
+    }
+
+    private isExampleWithValue(example: unknown): example is { value: unknown } {
+        return typeof example === "object" && example != null && "value" in example;
     }
 }
