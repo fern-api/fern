3e86d0159716704bef109709b3e58f748a9135f9
Deep Singhvi <deep@buildwithfern.com>
2025-05-02 10:53:50 -0400

    fix(cli): openprc supports a top level x-fern-parameters

---
+++ packages/cli/api-importers/openrpc-to-ir/src/1.x/ParameterConverter.ts
@@ -0,0 +1,40 @@
+import { OpenAPIV3_1 } from "openapi-types";
+
+import { TypeDeclaration, TypeId, TypeReference } from "@fern-api/ir-sdk";
+import { Converters } from "@fern-api/v2-importer-commons";
+
+export class ParameterConverter extends Converters.AbstractConverters
+    .AbstractParameterConverter<OpenAPIV3_1.ParameterObject> {
+    constructor({
+        context,
+        breadcrumbs,
+        parameter
+    }: Converters.AbstractConverters.AbstractParameterConverter.Args<OpenAPIV3_1.ParameterObject>) {
+        super({ context, breadcrumbs, parameter });
+    }
+
+    public async convert(): Promise<Converters.AbstractConverters.AbstractParameterConverter.Output | undefined> {
+        let typeReference: TypeReference | undefined;
+        let inlinedTypes: Record<TypeId, TypeDeclaration> = {};
+
+        if (this.parameter.schema != null) {
+            const schemaOrReferenceConverter = new Converters.SchemaConverters.SchemaOrReferenceConverter({
+                context: this.context,
+                breadcrumbs: [...this.breadcrumbs, "schema"],
+                schemaOrReference: this.parameter.schema,
+                wrapAsOptional: this.parameter.required == null || !this.parameter.required
+            });
+            const converted = await schemaOrReferenceConverter.convert();
+            if (converted != null) {
+                typeReference = converted.type;
+                inlinedTypes = converted.inlinedTypes ?? {};
+            }
+        }
+
+        return this.convertToOutput({
+            schema: this.parameter.schema ?? { type: "string" },
+            typeReference,
+            inlinedTypes
+        });
+    }
+}
