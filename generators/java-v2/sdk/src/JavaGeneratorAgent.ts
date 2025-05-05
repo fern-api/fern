71b0d1d3f10cfca38e022b862ef09146d83db75a
Deep Singhvi <deep@buildwithfern.com>
2025-05-05 18:42:00 -0400

    feat(java): support pushing to github enterprise (#6987)

--- generators/java-v2/sdk/src/JavaGeneratorAgent.ts
+++ generators/java-v2/sdk/src/JavaGeneratorAgent.ts
@@ -6,23 +6,27 @@ import { FernGeneratorExec } from "@fern-fern/generator-exec-sdk";
 
 import { SdkGeneratorContext } from "./SdkGeneratorContext";
 import { ReadmeConfigBuilder } from "./readme/ReadmeConfigBuilder";
+import { PublishingConfig } from "@fern-fern/ir-sdk/api";
 
 export class JavaGeneratorAgent extends AbstractGeneratorAgent<SdkGeneratorContext> {
     private readmeConfigBuilder: ReadmeConfigBuilder;
+    private publishConfig: PublishingConfig | undefined;
 
     public constructor({
         logger,
         config,
-        readmeConfigBuilder
+        readmeConfigBuilder,
+        publishConfig
     }: {
         logger: Logger;
         config: FernGeneratorExec.GeneratorConfig;
         readmeConfigBuilder: ReadmeConfigBuilder;
+        publishConfig: PublishingConfig | undefined;
     }) {
         super({ logger, config });
         this.readmeConfigBuilder = readmeConfigBuilder;
+        this.publishConfig = publishConfig;
     }
-
     public getReadmeConfig(
         args: AbstractGeneratorAgent.ReadmeConfigArgs<SdkGeneratorContext>
     ): FernGeneratorCli.ReadmeConfig {
@@ -41,12 +45,25 @@ export class JavaGeneratorAgent extends AbstractGeneratorAgent<SdkGeneratorConte
     public getGitHubConfig(
         args: AbstractGeneratorAgent.GitHubConfigArgs<SdkGeneratorContext>
     ): FernGeneratorCli.GitHubConfig {
-        // TODO: get from env
+        if (this.publishConfig == null) {
+            args.context.logger.error("Publishing config is missing");
+            throw new Error("Publishing config is required for GitHub actions");
+        }
+
+        if (this.publishConfig.type !== "github") {
+            args.context.logger.error(`Publishing type ${this.publishConfig.type} is not supported`);
+            throw new Error("Only GitHub publishing is supported");
+        }
+
+        if (this.publishConfig.uri == null || this.publishConfig.token == null) {
+            args.context.logger.error("GitHub URI or token is missing in publishing config");
+            throw new Error("GitHub URI and token are required in publishing config");
+        }
+
         return {
-            sourceDirectory: "NONE",
-            uri: "NONE",
-            token: "token",
-            branch: "NONE"
+            sourceDirectory: "fern/output",
+            uri: this.publishConfig.uri,
+            token: this.publishConfig.token
         };
     }
 }
