89d16141188906f66e33c20ac90b7f60dada4d9b
hughhan1 <hughhan1@users.noreply.github.com>
2025-05-08 14:36:23 +0000

    chore(typescript): update typescript-express seed

--- seed/ts-express/server-sent-events/errors/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SeedServerSentEventsError = void 0;
-var SeedServerSentEventsError_1 = require("./SeedServerSentEventsError");
-Object.defineProperty(exports, "SeedServerSentEventsError", { enumerable: true, get: function () { return SeedServerSentEventsError_1.SeedServerSentEventsError; } });
