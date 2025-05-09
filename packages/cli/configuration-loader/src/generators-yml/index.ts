dd3086a5d74063db3d161ef1fc79b614bc5d0f57
fern-support <fern-support@users.noreply.github.com>
2025-05-09 11:58:36 +0000

    fix(lint): auto-fix lint issues

--- packages/cli/configuration-loader/src/generators-yml/index.ts
+++ packages/cli/configuration-loader/src/generators-yml/index.ts
@@ -10,4 +10,3 @@ export {
 } from "./loadGeneratorsConfiguration";
 export { updateGeneratorGroup } from "./updateGeneratorGroup";
 export { isGithubSelfhosted } from "./convertGeneratorsConfiguration";
-
