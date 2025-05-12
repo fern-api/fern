65dd442a504e7d84dee62138b44f0115c6835af0
Deep Singhvi <deep@buildwithfern.com>
2025-05-12 17:20:00 -0400

    do not merge: how to test docs definition resolver

--- packages/cli/ete-tests/src/tests/write-docs-definition/writeDocsDefinition.test.ts
+++ packages/cli/ete-tests/src/tests/write-docs-definition/writeDocsDefinition.test.ts
@@ -9,6 +9,7 @@ const FIXTURES_DIR = path.join(__dirname, "fixtures");
 
 describe("write-docs-definition", () => {
     itFixture("petstore");
+    itFixture("products");
 });
 
 function itFixture(fixtureName: string) {
