c165e785195aece648d4576417db689f9e40a58a
Eden <eden@buildwithfern.com>
2025-05-22 17:32:07 -0400

    correct assign isValid when defaulting to resolvedSchema example.

--- packages/cli/api-importers/v2-importer-commons/src/converters/ExampleConverter.ts
+++ packages/cli/api-importers/v2-importer-commons/src/converters/ExampleConverter.ts
@@ -469,6 +469,7 @@ export class ExampleConverter extends AbstractConverter<AbstractConverterContext
         if (resolvedSchema.items == null) {
             resolvedSchema.items = { type: "string" };
         }
+        const usedFallbackExample = this.example == null;
         const maybeExampleArray = this.example ?? resolvedSchema.example;
         const exampleArray = Array.isArray(maybeExampleArray) ? maybeExampleArray : [maybeExampleArray];
         const results = exampleArray.map((item) => {
@@ -484,7 +485,7 @@ export class ExampleConverter extends AbstractConverter<AbstractConverterContext
             return exampleConverter.convert();
         });
 
-        const isValid = results.every((result) => result?.isValid ?? false);
+        const isValid = results.every((result) => result?.isValid ?? false) && !usedFallbackExample;
 
         return {
             isValid,
