fad9709297abe873f6eb05bfea5742f5844cb35c
amckinney <amckinney@users.noreply.github.com>
2025-05-22 16:36:05 +0000

    chore(csharp): update csharp-sdk seed

---
+++ seed/csharp-sdk/http-head/src/SeedHttpHead/SeedHttpHeadClient.cs
@@ -0,0 +1,33 @@
+using SeedHttpHead.Core;
+
+namespace SeedHttpHead;
+
+public partial class SeedHttpHeadClient
+{
+    private readonly RawClient _client;
+
+    public SeedHttpHeadClient(ClientOptions? clientOptions = null)
+    {
+        var defaultHeaders = new Headers(
+            new Dictionary<string, string>()
+            {
+                { "X-Fern-Language", "C#" },
+                { "X-Fern-SDK-Name", "SeedHttpHead" },
+                { "X-Fern-SDK-Version", Version.Current },
+                { "User-Agent", "Fernhttp-head/0.0.1" },
+            }
+        );
+        clientOptions ??= new ClientOptions();
+        foreach (var header in defaultHeaders)
+        {
+            if (!clientOptions.Headers.ContainsKey(header.Key))
+            {
+                clientOptions.Headers[header.Key] = header.Value;
+            }
+        }
+        _client = new RawClient(clientOptions);
+        User = new UserClient(_client);
+    }
+
+    public UserClient User { get; }
+}
