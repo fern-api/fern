818ab7c3910147a1a725c79c5cddeea001d72eb5
dsinghvi <dsinghvi@users.noreply.github.com>
2025-05-27 15:46:00 +0000

    chore(csharp): update csharp-model seed

--- seed/csharp-model/http-head/src/SeedHttpHead/Core/Public/Version.cs
+++
@@ -1,6 +0,0 @@
-namespace SeedHttpHead;
-
-internal class Version
-{
-    public const string Current = "0.0.1";
-}
--- seed/csharp-model/idempotency-headers/src/SeedIdempotencyHeaders/Core/Public/Version.cs
+++
@@ -1,6 +0,0 @@
-namespace SeedIdempotencyHeaders;
-
-internal class Version
-{
-    public const string Current = "0.0.1";
-}
