5fb70088b84165d8a5de6be8f1cd20059d1aaf6a
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-06-05 18:46:17 -0400

    chore(python): update python-sdk seed (#7450)
    Co-authored-by: Swimburger <Swimburger@users.noreply.github.com>

--- seed/python-sdk/unions/no-custom-config/src/seed/union/__init__.py
+++ seed/python-sdk/unions/no-custom-config/src/seed/union/__init__.py
@@ -3,4 +3,5 @@
 # isort: skip_file
 
 from .types import Circle, GetShapeRequest, Shape, Shape_Circle, Shape_Square, Square
+
 __all__ = ["Circle", "GetShapeRequest", "Shape", "Shape_Circle", "Shape_Square", "Square"]
