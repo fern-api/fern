212f4f02c9feff4d5d2faa41f593ac96fba192d0
jsklan <jsklan.development@gmail.com>
2025-06-13 20:41:50 -0400

    nit

--- generators/java/sdk/src/main/java/com/fern/java/client/generators/endpoint/AbstractEndpointWriter.java
+++ generators/java/sdk/src/main/java/com/fern/java/client/generators/endpoint/AbstractEndpointWriter.java
@@ -46,11 +46,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.lang.model.element.Modifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public abstract class AbstractEndpointWriter {
-    private static final Logger log = LoggerFactory.getLogger(AbstractEndpointWriter.class);
 
     public static final String CONTENT_TYPE_HEADER = "Content-Type";
     public static final String ACCEPT_HEADER = "Accept";
