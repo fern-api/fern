d63da6b51c563452aa24a7c1203d8c9f9e1671dc
Deep Singhvi <deep@buildwithfern.com>
2025-06-14 11:29:01 -0400

    chore(php): setup readme generation (#7514)

---
+++ generators/php/sdk/src/readme/ReadmeSnippetBuilder.ts
@@ -0,0 +1,155 @@
+import { AbstractReadmeSnippetBuilder } from "@fern-api/base-generator";
+
+import { FernGeneratorCli } from "@fern-fern/generator-cli-sdk";
+import { FernGeneratorExec } from "@fern-fern/generator-exec-sdk";
+import { EndpointId, FeatureId, FernFilepath, HttpEndpoint } from "@fern-fern/ir-sdk/api";
+
+import { SdkGeneratorContext } from "../SdkGeneratorContext";
+
+interface EndpointWithFilepath {
+    endpoint: HttpEndpoint;
+    fernFilepath: FernFilepath;
+}
+
+export class ReadmeSnippetBuilder extends AbstractReadmeSnippetBuilder {
+    private static EXCEPTION_HANDLING_FEATURE_ID: FernGeneratorCli.FeatureId = "EXCEPTION_HANDLING";
+
+    private readonly context: SdkGeneratorContext;
+    private readonly endpoints: Record<EndpointId, EndpointWithFilepath> = {};
+    private readonly snippets: Record<EndpointId, string> = {};
+    private readonly defaultEndpointId: EndpointId;
+    private readonly isPaginationEnabled: boolean;
+
+    constructor({
+        context,
+        endpointSnippets
+    }: {
+        context: SdkGeneratorContext;
+        endpointSnippets: FernGeneratorExec.Endpoint[];
+    }) {
+        super({ endpointSnippets });
+        this.context = context;
+        this.isPaginationEnabled = context.config.generatePaginatedClients ?? false;
+        this.endpoints = this.buildEndpoints();
+        this.snippets = this.buildSnippets(endpointSnippets);
+        this.defaultEndpointId =
+            this.context.ir.readmeConfig?.defaultEndpoint != null
+                ? this.context.ir.readmeConfig.defaultEndpoint
+                : this.getDefaultEndpointId();
+    }
+
+    public buildReadmeSnippets(): Record<FernGeneratorCli.FeatureId, string[]> {
+        const snippets: Record<FernGeneratorCli.FeatureId, string[]> = {};
+        snippets[FernGeneratorCli.StructuredFeatureId.Usage] = this.buildUsageSnippets();
+        return snippets;
+    }
+
+    private buildUsageSnippets(): string[] {
+        const usageEndpointIds = this.getEndpointIdsForFeature(FernGeneratorCli.StructuredFeatureId.Usage);
+        if (usageEndpointIds != null) {
+            return usageEndpointIds.map((endpointId) => this.getSnippetForEndpointId(endpointId));
+        }
+        return [this.getSnippetForEndpointId(this.defaultEndpointId)];
+    }
+
+    private buildPaginationSnippets(): string[] {
+        const explicitlyConfigured = this.getExplicitlyConfiguredSnippets(
+            FernGeneratorCli.StructuredFeatureId.Pagination
+        );
+        if (explicitlyConfigured != null) {
+            return explicitlyConfigured;
+        }
+        const paginationEndpoint = this.getEndpointWithPagination();
+        if (paginationEndpoint != null) {
+            const snippet = this.getSnippetForEndpointId(paginationEndpoint.endpoint.id);
+            return snippet != null ? [snippet] : [];
+        }
+        return [];
+    }
+
+    private getEndpointWithPagination(): EndpointWithFilepath | undefined {
+        return this.filterEndpoint((endpointWithFilepath) => {
+            if (endpointWithFilepath.endpoint.pagination != null) {
+                return endpointWithFilepath;
+            }
+            return undefined;
+        });
+    }
+
+    private filterEndpoint<T>(transform: (endpoint: EndpointWithFilepath) => T | undefined): T | undefined {
+        for (const endpointWithFilepath of Object.values(this.endpoints)) {
+            const result = transform(endpointWithFilepath);
+            if (result !== undefined) {
+                return result;
+            }
+        }
+        return undefined;
+    }
+
+    private getExplicitlyConfiguredSnippets(featureId: FeatureId): string[] | undefined {
+        const endpointIds = this.getEndpointIdsForFeature(featureId);
+        if (endpointIds != null) {
+            return endpointIds.map((endpointId) => this.getSnippetForEndpointId(endpointId)).filter((e) => e != null);
+        }
+        return undefined;
+    }
+
+    private buildEndpoints(): Record<EndpointId, EndpointWithFilepath> {
+        const endpoints: Record<EndpointId, EndpointWithFilepath> = {};
+        for (const service of Object.values(this.context.ir.services)) {
+            for (const endpoint of service.endpoints) {
+                endpoints[endpoint.id] = {
+                    endpoint,
+                    fernFilepath: service.name.fernFilepath
+                };
+            }
+        }
+        return endpoints;
+    }
+
+    private buildSnippets(endpointSnippets: FernGeneratorExec.Endpoint[]): Record<EndpointId, string> {
+        const snippets: Record<EndpointId, string> = {};
+        for (const endpointSnippet of Object.values(endpointSnippets)) {
+            if (endpointSnippet.id.identifierOverride == null) {
+                throw new Error("Internal error; snippets must define the endpoint id to generate README.md");
+            }
+            snippets[endpointSnippet.id.identifierOverride] = this.getEndpointSnippetString(endpointSnippet);
+        }
+        return snippets;
+    }
+
+    private getSnippetForEndpointId(endpointId: EndpointId): string {
+        const snippet = this.snippets[endpointId];
+        if (snippet == null) {
+            throw new Error(`Internal error; missing snippet for endpoint ${endpointId}`);
+        }
+        return snippet;
+    }
+
+    private getEndpointsForFeature(featureId: FeatureId): EndpointWithFilepath[] {
+        const endpointIds = this.getEndpointIdsForFeature(featureId);
+        return endpointIds != null ? this.getEndpoints(endpointIds) : this.getEndpoints([this.defaultEndpointId]);
+    }
+
+    private getEndpointIdsForFeature(featureId: FeatureId): EndpointId[] | undefined {
+        return this.context.ir.readmeConfig?.features?.[this.getFeatureKey(featureId)];
+    }
+
+    private getEndpoints(endpointIds: EndpointId[]): EndpointWithFilepath[] {
+        return endpointIds.map((endpointId) => {
+            const endpoint = this.endpoints[endpointId];
+            if (endpoint == null) {
+                throw new Error(`Internal error; missing endpoint ${endpointId}`);
+            }
+            return endpoint;
+        });
+    }
+
+    private getEndpointSnippetString(endpoint: FernGeneratorExec.Endpoint): string {
+        // Note: this is a shim since php snippets are not supported yet in FernGeneratorExec
+        if (endpoint.snippet.type !== "java") {
+            throw new Error(`Internal error; expected csharp snippet but got: ${endpoint.snippet.type}`);
+        }
+        return endpoint.snippet.syncClient;
+    }
+}
---
+++ seed/php-sdk/imdb/clientName/README.md
@@ -0,0 +1,46 @@
+# Seed PHP Library
+
+[![fern shield](https://img.shields.io/badge/%F0%9F%8C%BF-Built%20with%20Fern-brightgreen)](https://buildwithfern.com?utm_source=github&utm_medium=github&utm_campaign=readme&utm_source=Seed%2FPHP)
+[![php shield](https://img.shields.io/badge/php-packagist-pink)](https://packagist.org/packages/seed/seed)
+
+The Seed PHP library provides convenient access to the Seed API from PHP.
+
+## Installation
+
+```sh
+composer require seed/seed
+```
+
+## Usage
+
+Instantiate and use the client with the following:
+
+```php
+<?php
+
+namespace Example;
+
+use Seed\FernClient;
+use Seed\Imdb\Types\CreateMovieRequest;
+
+$client = new FernClient(
+    token: '<token>',
+);
+$client->imdb->createMovie(
+    new CreateMovieRequest([
+        'title' => 'title',
+        'rating' => 1.1,
+    ]),
+);
+
+```
+
+## Contributing
+
+While we value open-source contributions to this SDK, this library is generated programmatically.
+Additions made directly to this library would have to be moved over to our generation code,
+otherwise they would be overwritten upon the next generated release. Feel free to open a PR as
+a proof of concept, but know that we will not be able to merge it as-is. We suggest opening
+an issue first to discuss with us!
+
+On the other hand, contributions to the README are always very welcome!
