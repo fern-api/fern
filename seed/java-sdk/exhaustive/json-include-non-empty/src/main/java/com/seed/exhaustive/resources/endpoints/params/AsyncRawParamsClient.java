78255056e3cc50f88ce57d920a768c56a161ad5a
jsklan <100491078+jsklan@users.noreply.github.com>
2025-06-16 17:23:40 -0400

    fix(java): Remove Content-Type from headers when request has no body (#7510)
    * add putWithNoRequestBody case to no-req-body.yml test fixture
    
    * rerun seed test with java-sdk on exhaustive test fixture
    
    * update java endpoint generators to not include content type when body is empty
    
    * remove put request from no-req-body.yml because the error was not caused by the request type, so this test doesn't add any information
    
    * run seed test on fixture exhaustive to demonstrate change
    
    * rerun seed build to revert addition of putWithNoBody endpoint
    
    * rerun seed after removal of putWithNoBody
    
    * nit
    
    * manually revert changes to trace.json
    
    * release new rc version for java sdk
    
    * chore: update changelog
    
    * nit formatting

--- seed/java-sdk/exhaustive/json-include-non-empty/src/main/java/com/seed/exhaustive/resources/endpoints/params/AsyncRawParamsClient.java
+++ seed/java-sdk/exhaustive/json-include-non-empty/src/main/java/com/seed/exhaustive/resources/endpoints/params/AsyncRawParamsClient.java
@@ -60,7 +60,6 @@ public class AsyncRawParamsClient {
                 .url(httpUrl)
                 .method("GET", null)
                 .headers(Headers.of(clientOptions.headers(requestOptions)))
-                .addHeader("Content-Type", "application/json")
                 .addHeader("Accept", "application/json")
                 .build();
         OkHttpClient client = clientOptions.httpClient();
@@ -120,7 +119,6 @@ public class AsyncRawParamsClient {
                 .url(httpUrl)
                 .method("GET", null)
                 .headers(Headers.of(clientOptions.headers(requestOptions)))
-                .addHeader("Content-Type", "application/json")
                 .addHeader("Accept", "application/json");
         Request okhttpRequest = _requestBuilder.build();
         OkHttpClient client = clientOptions.httpClient();
