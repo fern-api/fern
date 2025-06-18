d3921ba0e21281a5bb4d352cbd7a31c8c8690438
Deep Singhvi <deep@buildwithfern.com>
2025-06-18 09:53:44 -0400

    chore(python): remove ruff format (#7547)

--- seed/pydantic-v2/exhaustive/resources/endpoints/resources/put/types/put_response.py
+++ seed/pydantic-v2/exhaustive/resources/endpoints/resources/put/types/put_response.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
+from typing import Optional, List
+from .error import Error
 from dt import datetime
 from core.datetime_utils import serialize_datetime
 
-class ObjectWithRequiredField(BaseModel):
-    string: str
+class PutResponse(BaseModel):
+    errors: Optional[List[Error]] = None
     class Config:
         frozen = True
         smart_union = True
