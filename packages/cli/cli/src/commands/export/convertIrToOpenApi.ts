d3a6f80e0ffdc1830f75ba4ff7d974e2b9242eaa
Zach Kirsch <zachkirsch@users.noreply.github.com>
2025-06-23 12:27:30 -0700

    feat(cli): add 'fern export' command (#7586)
    * feat(cli): add 'fern export' command
    
    * Add E2E test
    
    * Update packages/cli/cli/src/commands/export/generateOpenAPIForWorkspaces.ts
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * Simplify
    
    * Update packages/cli/cli/src/commands/export/converters/typeConverter.ts
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * Update packages/cli/cli/src/commands/export/security.ts
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * Update packages/cli/cli/src/commands/export/converters/typeConverter.ts
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * Simplify
    
    * Handle case where dir doesn't exist
    
    * fix eslint
    
    * fix format
    
    * Add changelog
    
    * chore: update changelog
    
    * Add docs
    
    * Remove hideOnThisPage
    
    ---------
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    Co-authored-by: Deep Singhvi <deep@buildwithfern.com>
    Co-authored-by: zachkirsch <8902272+zachkirsch@users.noreply.github.com>

---
+++ packages/cli/cli/src/commands/export/convertIrToOpenApi.ts
@@ -0,0 +1,103 @@
+import { OpenAPIV3 } from "openapi-types";
+
+import {
+    DeclaredErrorName,
+    DeclaredTypeName,
+    ErrorDeclaration,
+    IntermediateRepresentation,
+    TypeDeclaration
+} from "@fern-api/ir-sdk";
+
+import { convertServices } from "./converters/servicesConverter";
+import { convertType } from "./converters/typeConverter";
+import { constructEndpointSecurity, constructSecuritySchemes } from "./security";
+
+export type Mode = "stoplight" | "openapi";
+
+export function convertIrToOpenApi({
+    apiName,
+    ir,
+    mode
+}: {
+    apiName: string;
+    ir: IntermediateRepresentation;
+    mode: Mode;
+}): OpenAPIV3.Document | undefined {
+    const schemas: Record<string, OpenAPIV3.ReferenceObject | OpenAPIV3.SchemaObject> = {};
+
+    const typesByName: Record<string, TypeDeclaration> = {};
+    Object.values(ir.types).forEach((typeDeclaration) => {
+        // convert type to open api schema
+        const convertedType = convertType(typeDeclaration, ir);
+        schemas[convertedType.schemaName] = {
+            title: convertedType.schemaName,
+            ...convertedType.openApiSchema
+        };
+        // populates typesByName map
+        typesByName[getDeclaredTypeNameKey(typeDeclaration.name)] = typeDeclaration;
+    });
+
+    const errorsByName: Record<string, ErrorDeclaration> = {};
+    Object.values(ir.errors).forEach((errorDeclaration) => {
+        errorsByName[getErrorTypeNameKey(errorDeclaration.name)] = errorDeclaration;
+    });
+
+    const security = constructEndpointSecurity(ir.auth);
+
+    const paths = convertServices({
+        ir,
+        httpServices: Object.values(ir.services),
+        typesByName,
+        errorsByName,
+        errorDiscriminationStrategy: ir.errorDiscriminationStrategy,
+        security,
+        environments: ir.environments ?? undefined,
+        mode
+    });
+
+    const info: OpenAPIV3.InfoObject = {
+        title: ir.apiDisplayName ?? apiName,
+        version: ""
+    };
+    if (ir.apiDocs != null) {
+        info.description = ir.apiDocs;
+    }
+
+    const openAPISpec: OpenAPIV3.Document = {
+        openapi: "3.0.1",
+        info,
+        paths,
+        components: {
+            schemas,
+            securitySchemes: constructSecuritySchemes(ir.auth)
+        }
+    };
+
+    if (ir.environments != null && ir.environments.environments.type === "singleBaseUrl") {
+        openAPISpec.servers = ir.environments.environments.environments.map((environment) => {
+            return {
+                url: environment.url,
+                description:
+                    environment.docs != null
+                        ? `${environment.name.originalName} (${environment.docs})`
+                        : environment.name.originalName
+            };
+        });
+    }
+
+    return openAPISpec;
+}
+
+export function getDeclaredTypeNameKey(declaredTypeName: DeclaredTypeName): string {
+    return [
+        ...declaredTypeName.fernFilepath.allParts.map((part) => part.originalName),
+        declaredTypeName.name.originalName
+    ].join("-");
+}
+
+export function getErrorTypeNameKey(declaredErrorName: DeclaredErrorName): string {
+    return [
+        ...declaredErrorName.fernFilepath.allParts.map((part) => part.originalName),
+        declaredErrorName.name.originalName
+    ].join("-");
+}
