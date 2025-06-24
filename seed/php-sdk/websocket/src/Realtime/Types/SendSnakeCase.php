54e0ab895a8cde880697af6d29c5c5a95638918b
Swimburger <Swimburger@users.noreply.github.com>
2025-06-24 19:12:52 +0000

    chore(php): update php-sdk seed

---
+++ seed/php-sdk/websocket/src/Realtime/Types/SendSnakeCase.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Seed\Realtime\Types;
+
+use Seed\Core\Json\JsonSerializableType;
+use Seed\Core\Json\JsonProperty;
+
+class SendSnakeCase extends JsonSerializableType
+{
+    /**
+     * @var string $sendText
+     */
+    #[JsonProperty('send_text')]
+    public string $sendText;
+
+    /**
+     * @var int $sendParam
+     */
+    #[JsonProperty('send_param')]
+    public int $sendParam;
+
+    /**
+     * @param array{
+     *   sendText: string,
+     *   sendParam: int,
+     * } $values
+     */
+    public function __construct(
+        array $values,
+    ) {
+        $this->sendText = $values['sendText'];
+        $this->sendParam = $values['sendParam'];
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->toJson();
+    }
+}
