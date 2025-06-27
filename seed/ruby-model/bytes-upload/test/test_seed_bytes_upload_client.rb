5144cecf32382b09a00f0e625946761ca1d0a4b7
Swimburger <Swimburger@users.noreply.github.com>
2025-06-27 15:57:56 +0000

    chore(ruby): update ruby-model seed

---
+++ seed/ruby-model/bytes-upload/test/test_seed_bytes_upload_client.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+require "seed_bytes_upload_client"
+
+# Basic SeedBytesUploadClient tests
+class TestSeedBytesUploadClient < Minitest::Test
+  def test_function
+    # SeedBytesUploadClient::Client.new
+  end
+end
