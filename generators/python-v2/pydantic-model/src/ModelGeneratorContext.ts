3011b9d3f4c381d9d7171cd1b70d09bcf2addde7
nevilgeorge <nevil@buildwithfern.com>
2025-06-30 14:58:02 -0400

    chore(python-v2): generate gitignore file for pydantic-v2

--- generators/python-v2/pydantic-model/src/ModelGeneratorContext.ts
+++ generators/python-v2/pydantic-model/src/ModelGeneratorContext.ts
@@ -1,5 +1,10 @@
 import { AbstractPythonGeneratorContext } from "@fern-api/python-base";
+import { core } from "@fern-api/python-base";
 
 import { PydanticModelCustomConfigSchema } from "./ModelCustomConfig";
 
-export class PydanticModelGeneratorContext extends AbstractPythonGeneratorContext<PydanticModelCustomConfigSchema> {}
+export class PydanticModelGeneratorContext extends AbstractPythonGeneratorContext<PydanticModelCustomConfigSchema> {
+    public getRawAsIsFiles(): string[] {
+        return [core.AsIsFiles.GitIgnore];
+    }
+}
