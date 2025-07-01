8f689246a1d2c2bea7ed3b461b87c639bd9ca81e
Sahil <sahil@buildwithfern.com>
2025-07-01 15:59:32 -0400

    feat(cli): use fern protoc-gen-fern in shell proxy for buf generate

--- packages/cli/workspace/lazy-fern-workspace/src/protobuf/utils.ts
+++ packages/cli/workspace/lazy-fern-workspace/src/protobuf/utils.ts
@@ -24,8 +24,7 @@ export const PROTOBUF_MODULE_PACKAGE_JSON = `{
 `;
 
 export const PROTOBUF_SHELL_PROXY = `#!/usr/bin/env bash
-export FERN_NO_VERSION_REDIRECTION=true
-exec node ~/Fern/fern/packages/cli/cli/dist/dev/cli.cjs protoc-gen-fern "$@"
+fern protoc-gen-fern "$@"
 `;
 
 export const createEmptyProtobufLogger = (): Logger => {
