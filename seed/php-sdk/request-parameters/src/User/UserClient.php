f23d36b1e0cb36fcae409081fe75b90c757aeece
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-07-01 18:12:19 -0400

    chore(php): update php-sdk seed (#7781)
    Co-authored-by: mattblank11 <mattblank11@users.noreply.github.com>

---
+++ seed/php-sdk/request-parameters/src/User/UserClient.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Seed\User;
+
+use GuzzleHttp\ClientInterface;
+use Seed\Core\Client\RawClient;
+use Seed\User\Requests\CreateUsernameRequest;
+use Seed\Exceptions\SeedException;
+use Seed\Exceptions\SeedApiException;
+use Seed\Core\Json\JsonApiRequest;
+use Seed\Core\Client\HttpMethod;
+use GuzzleHttp\Exception\RequestException;
+use Psr\Http\Client\ClientExceptionInterface;
+use Seed\User\Requests\GetUsersRequest;
+use Seed\User\Types\User;
+use Seed\Core\Types\Constant;
+use JsonException;
+
+class UserClient
+{
+    /**
+     * @var array{
+     *   baseUrl?: string,
+     *   client?: ClientInterface,
+     *   maxRetries?: int,
+     *   timeout?: float,
+     *   headers?: array<string, string>,
+     * } $options
+     */
+    private array $options;
+
+    /**
+     * @var RawClient $client
+     */
+    private RawClient $client;
+
+    /**
+     * @param RawClient $client
+     * @param ?array{
+     *   baseUrl?: string,
+     *   client?: ClientInterface,
+     *   maxRetries?: int,
+     *   timeout?: float,
+     *   headers?: array<string, string>,
+     * } $options
+     */
+    public function __construct(
+        RawClient $client,
+        ?array $options = null,
+    ) {
+        $this->client = $client;
+        $this->options = $options ?? [];
+    }
+
+    /**
+     * @param CreateUsernameRequest $request
+     * @param ?array{
+     *   baseUrl?: string,
+     *   maxRetries?: int,
+     *   timeout?: float,
+     *   headers?: array<string, string>,
+     *   queryParameters?: array<string, mixed>,
+     *   bodyProperties?: array<string, mixed>,
+     * } $options
+     * @throws SeedException
+     * @throws SeedApiException
+     */
+    public function createUsername(CreateUsernameRequest $request, ?array $options = null): void
+    {
+        $options = array_merge($this->options, $options ?? []);
+        try {
+            $response = $this->client->sendRequest(
+                new JsonApiRequest(
+                    baseUrl: $options['baseUrl'] ?? $this->client->options['baseUrl'] ?? '',
+                    path: "/user/username",
+                    method: HttpMethod::POST,
+                    body: $request,
+                ),
+                $options,
+            );
+            $statusCode = $response->getStatusCode();
+            if ($statusCode >= 200 && $statusCode < 400) {
+                return;
+            }
+        } catch (RequestException $e) {
+            $response = $e->getResponse();
+            if ($response === null) {
+                throw new SeedException(message: $e->getMessage(), previous: $e);
+            }
+            throw new SeedApiException(
+                message: "API request failed",
+                statusCode: $response->getStatusCode(),
+                body: $response->getBody()->getContents(),
+            );
+        } catch (ClientExceptionInterface $e) {
+            throw new SeedException(message: $e->getMessage(), previous: $e);
+        }
+        throw new SeedApiException(
+            message: 'API request failed',
+            statusCode: $statusCode,
+            body: $response->getBody()->getContents(),
+        );
+    }
+
+    /**
+     * @param GetUsersRequest $request
+     * @param ?array{
+     *   baseUrl?: string,
+     *   maxRetries?: int,
+     *   timeout?: float,
+     *   headers?: array<string, string>,
+     *   queryParameters?: array<string, mixed>,
+     *   bodyProperties?: array<string, mixed>,
+     * } $options
+     * @return User
+     * @throws SeedException
+     * @throws SeedApiException
+     */
+    public function getUsername(GetUsersRequest $request, ?array $options = null): User
+    {
+        $options = array_merge($this->options, $options ?? []);
+        $query = [];
+        $query['limit'] = $request->limit;
+        $query['id'] = $request->id;
+        $query['date'] = $request->date->format(Constant::DateFormat);
+        $query['deadline'] = $request->deadline->format(Constant::DateTimeFormat);
+        $query['bytes'] = $request->bytes;
+        $query['user'] = $request->user;
+        $query['userList'] = $request->userList;
+        $query['keyValue'] = $request->keyValue;
+        $query['nestedUser'] = $request->nestedUser;
+        $query['excludeUser'] = $request->excludeUser;
+        $query['filter'] = $request->filter;
+        $query['longParam'] = $request->longParam;
+        $query['bigIntParam'] = $request->bigIntParam;
+        if ($request->optionalDeadline != null) {
+            $query['optionalDeadline'] = $request->optionalDeadline;
+        }
+        if ($request->optionalString != null) {
+            $query['optionalString'] = $request->optionalString;
+        }
+        if ($request->optionalUser != null) {
+            $query['optionalUser'] = $request->optionalUser;
+        }
+        try {
+            $response = $this->client->sendRequest(
+                new JsonApiRequest(
+                    baseUrl: $options['baseUrl'] ?? $this->client->options['baseUrl'] ?? '',
+                    path: "/user",
+                    method: HttpMethod::GET,
+                    query: $query,
+                ),
+                $options,
+            );
+            $statusCode = $response->getStatusCode();
+            if ($statusCode >= 200 && $statusCode < 400) {
+                $json = $response->getBody()->getContents();
+                return User::fromJson($json);
+            }
+        } catch (JsonException $e) {
+            throw new SeedException(message: "Failed to deserialize response: {$e->getMessage()}", previous: $e);
+        } catch (RequestException $e) {
+            $response = $e->getResponse();
+            if ($response === null) {
+                throw new SeedException(message: $e->getMessage(), previous: $e);
+            }
+            throw new SeedApiException(
+                message: "API request failed",
+                statusCode: $response->getStatusCode(),
+                body: $response->getBody()->getContents(),
+            );
+        } catch (ClientExceptionInterface $e) {
+            throw new SeedException(message: $e->getMessage(), previous: $e);
+        }
+        throw new SeedApiException(
+            message: 'API request failed',
+            statusCode: $statusCode,
+            body: $response->getBody()->getContents(),
+        );
+    }
+}
