d97e36c3df53e98c15455fa74a876a6349193844
Matt Blank <mattblank11@gmail.com>
2025-07-01 13:58:20 -0400

    Use types instead of any

--- generators/typescript/utils/contexts/src/sdk-context/request-wrapper/types.ts
+++ generators/typescript/utils/contexts/src/sdk-context/request-wrapper/types.ts
@@ -2,10 +2,16 @@ export interface RequestWrapperNonBodyProperty {
     propertyName: string;
     safeName: string;
 }
+import {
+    FileProperty,
+    HttpHeader,
+    PathParameter,
+    QueryParameter
+} from "@fern-fern/ir-sdk/api";
 
 export interface RequestWrapperNonBodyPropertyWithData extends RequestWrapperNonBodyProperty {
     originalParameter?: {
         type: "query" | "path" | "header" | "file";
-        parameter: any; // QueryParameter | PathParameter | HttpHeader | FileProperty
+        parameter: QueryParameter | PathParameter | HttpHeader | FileProperty
     };
 }
