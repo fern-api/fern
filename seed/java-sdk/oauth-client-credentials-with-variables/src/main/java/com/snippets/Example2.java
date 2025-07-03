85f300e5c8183ffe67c7984aa099c6680461e966
dsinghvi <dsinghvi@users.noreply.github.com>
2025-07-03 18:00:31 +0000

    chore(java): update java-sdk seed

---
+++ seed/java-sdk/oauth-client-credentials-with-variables/src/main/java/com/snippets/Example2.java
@@ -0,0 +1,16 @@
+package com.snippets;
+
+import com.seed.oauthClientCredentialsWithVariables.SeedOauthClientCredentialsWithVariablesClient;
+
+public class Example2 {
+    public static void main(String[] args) {
+        SeedOauthClientCredentialsWithVariablesClient client = SeedOauthClientCredentialsWithVariablesClient
+            .builder()
+            .clientId("<clientId>")
+            .clientSecret("<clientSecret>")
+            .url("https://api.fern.com")
+            .build();
+
+        client.service().post("endpointParam");
+    }
+}
