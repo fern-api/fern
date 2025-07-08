dba95bc8f2795b17bf32b0c4de451f6093a9133b
Anar Kafkas <anarkafkas@gmail.com>
2025-07-08 14:09:12 +0300

    Load sample ir from definition

--- generators/swift/model/src/__test__/createSampleGeneratorContext.ts
+++ generators/swift/model/src/__test__/createSampleGeneratorContext.ts
@@ -1,13 +1,19 @@
+import { resolve } from "node:path";
+
 import { FernGeneratorExec, GeneratorNotificationService } from "@fern-api/base-generator";
+import { SourceResolverImpl } from "@fern-api/cli-source-resolver";
 import { AbsoluteFilePath } from "@fern-api/fs-utils";
+import { generateIntermediateRepresentation } from "@fern-api/ir-generator";
+import { createMockTaskContext } from "@fern-api/task-context";
+import { loadAPIWorkspace } from "@fern-api/workspace-loader";
 
 import { IntermediateRepresentation } from "@fern-fern/ir-sdk/api";
 
 import { ModelCustomConfigSchema } from "../_ModelCustomConfig";
 import { ModelGeneratorContext } from "../_ModelGeneratorContext";
 
-export function createSampleGeneratorContext() {
-    const ir = createSampleIr();
+export async function createSampleGeneratorContext(fixtureName: string) {
+    const ir = await createSampleIr(fixtureName);
     const generatorConfig = createSampleGeneratorConfig();
     const customConfig: ModelCustomConfigSchema = {};
     const notificationService = new GeneratorNotificationService({
@@ -17,114 +23,32 @@ export function createSampleGeneratorContext() {
     return new ModelGeneratorContext(ir, generatorConfig, customConfig, notificationService);
 }
 
-function createSampleIr(): IntermediateRepresentation {
-    return {
-        fdrApiDefinitionId: undefined,
-        apiVersion: undefined,
-        apiName: {
-            originalName: "My API",
-            camelCase: {
-                safeName: "myApi",
-                unsafeName: "MyApi"
-            },
-            pascalCase: {
-                safeName: "MyApi",
-                unsafeName: "MyApi"
-            },
-            snakeCase: {
-                safeName: "my_api",
-                unsafeName: "My_API"
-            },
-            screamingSnakeCase: {
-                safeName: "MY_API",
-                unsafeName: "MY_API"
-            }
-        },
-        apiDisplayName: undefined,
-        apiDocs: undefined,
-        auth: {
-            docs: undefined,
-            requirement: "ANY",
-            schemes: []
-        },
-        headers: [],
-        idempotencyHeaders: [],
-        types: {},
-        services: {},
-        webhookGroups: {},
-        websocketChannels: {},
-        errors: {},
-        subpackages: {},
-        rootPackage: {
-            docs: undefined,
-            fernFilepath: {
-                allParts: [],
-                file: undefined,
-                packagePath: []
-            },
-            service: undefined,
-            types: [],
-            errors: [],
-            webhooks: undefined,
-            websocket: undefined,
-            subpackages: [],
-            hasEndpointsInTree: false,
-            navigationConfig: undefined
-        },
-        constants: {
-            errorInstanceIdKey: {
-                name: {
-                    originalName: "errorInstanceId",
-                    camelCase: {
-                        safeName: "errorInstanceId",
-                        unsafeName: "errorInstanceId"
-                    },
-                    pascalCase: {
-                        safeName: "ErrorInstanceId",
-                        unsafeName: "ErrorInstanceId"
-                    },
-                    snakeCase: {
-                        safeName: "error_instance_id",
-                        unsafeName: "ERROR_INSTANCE_ID"
-                    },
-                    screamingSnakeCase: {
-                        safeName: "ERROR_INSTANCE_ID",
-                        unsafeName: "ERROR_INSTANCE_ID"
-                    }
-                },
-                wireValue: "error_instance_id"
-            }
-        },
-        environments: undefined,
-        basePath: undefined,
-        pathParameters: [],
-        errorDiscriminationStrategy: {
-            type: "statusCode",
-            _visit: (visitor) => visitor.statusCode()
-        },
-        sdkConfig: {
-            isAuthMandatory: false,
-            hasStreamingEndpoints: false,
-            hasPaginatedEndpoints: false,
-            hasFileDownloadEndpoints: false,
-            platformHeaders: {
-                language: "swift",
-                sdkName: "My SDK",
-                sdkVersion: "1.0.0",
-                userAgent: undefined
-            }
-        },
-        variables: [],
-        serviceTypeReferenceInfo: {
-            typesReferencedOnlyByService: {},
-            sharedTypes: []
-        },
-        readmeConfig: undefined,
-        sourceConfig: undefined,
-        publishConfig: undefined,
-        dynamic: undefined,
-        selfHosted: false
-    };
+async function createSampleIr(fixtureName: string): Promise<IntermediateRepresentation> {
+    const context = createMockTaskContext();
+    const absolutePathToWorkspace = AbsoluteFilePath.of(resolve(__dirname, "test-definitions", fixtureName));
+    const workspace = await loadAPIWorkspace({
+        absolutePathToWorkspace,
+        context,
+        cliVersion: "0.0.0",
+        workspaceName: fixtureName
+    });
+    if (!workspace.didSucceed) {
+        throw new Error(`Failed to load workspace '${absolutePathToWorkspace}'`);
+    }
+    const fernWorkspace = await workspace.workspace.toFernWorkspace({ context });
+    return generateIntermediateRepresentation({
+        workspace: fernWorkspace,
+        generationLanguage: undefined,
+        audiences: { type: "all" },
+        keywords: undefined,
+        smartCasing: true,
+        exampleGeneration: { disabled: true },
+        readme: undefined,
+        version: undefined,
+        packageName: undefined,
+        context,
+        sourceResolver: new SourceResolverImpl(context, fernWorkspace)
+    });
 }
 
 function createSampleGeneratorConfig(): FernGeneratorExec.config.GeneratorConfig {
--- generators/swift/model/src/__test__/generateModels.test.ts
+++ generators/swift/model/src/__test__/generateModels.test.ts
@@ -2,8 +2,8 @@ import { generateModels } from "../generateModels";
 import { createSampleGeneratorContext } from "./createSampleGeneratorContext";
 
 describe("generateModels", () => {
-    it("should generate models", () => {
-        const context = createSampleGeneratorContext();
+    it("should generate models", async () => {
+        const context = await createSampleGeneratorContext("basic-object");
         const files = generateModels({ context });
         expect(files).toMatchSnapshot();
     });
