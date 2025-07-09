2956e7a848f8cb6249158e426b6a7419f00ff82c
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2025-07-10 01:05:41 +0300

    feat(swift): connect model generator to cli (#7924)
    * Start adding cli code
    
    * Fix compiler errors
    
    * Remove unused
    
    * Fix directories in other package.jsons
    
    * Fix package.json fields

--- generators/swift/base/src/project/SwiftProject.ts
+++ generators/swift/base/src/project/SwiftProject.ts
@@ -2,13 +2,29 @@ import { AbstractProject } from "@fern-api/base-generator";
 import { BaseSwiftCustomConfigSchema } from "@fern-api/swift-codegen";
 
 import { AbstractSwiftGeneratorContext } from "../context/AbstractSwiftGeneratorContext";
+import { SwiftFile } from "./SwiftFile";
 
 export class SwiftProject extends AbstractProject<AbstractSwiftGeneratorContext<BaseSwiftCustomConfigSchema>> {
-    public constructor({ context }: { context: AbstractSwiftGeneratorContext<BaseSwiftCustomConfigSchema> }) {
+    private name: string;
+    private sourceFiles: SwiftFile[] = [];
+
+    public constructor({
+        context,
+        name
+    }: {
+        context: AbstractSwiftGeneratorContext<BaseSwiftCustomConfigSchema>;
+        name: string;
+    }) {
         super(context);
+        this.name = name;
+    }
+
+    public addSourceFiles(...files: SwiftFile[]): void {
+        this.sourceFiles.push(...files);
     }
 
     public async persist(): Promise<void> {
         // TODO: Implement
+        await Promise.all(this.sourceFiles.map((file) => file.write(this.absolutePathToOutputDirectory)));
     }
 }
