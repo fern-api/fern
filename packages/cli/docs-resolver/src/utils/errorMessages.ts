45e3312ea602bfac0b344ab4aa398b219d12e392
jsklan <jsklan.development@gmail.com>
2025-07-11 16:28:08 -0400

    fixed eslint issue

--- packages/cli/docs-resolver/src/utils/errorMessages.ts
+++ packages/cli/docs-resolver/src/utils/errorMessages.ts
@@ -11,11 +11,11 @@ export function cannotFindSubpackageByLocatorError(locator: string, existingLoca
         normalizeLocatorString,
         LEVENSHTEIN_THRESHOLD
     );
+    const availableSections = Array.from(existingLocators);
     let msg = `Failed to locate API section ${locator}.`;
     switch (nearestThreeMatches.length) {
         case 0:
             msg = `${msg} No similar API sections found. Available API sections:`;
-            const availableSections = Array.from(existingLocators);
             return availableSections.length === 0
                 ? `${msg} []`
                 : `${msg} [\n${availableSections.map((s) => `\t${s}`).join(",\n")}\n]`;
