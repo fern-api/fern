f547fc377f0ddf98d08607dcf5f3e418d4b1abc0
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2025-07-15 15:41:49 +0100

    made all circles/squares hashable

--- generators/python/tests/utils/typeddict_models/types/resources/types/circle.py
+++ generators/python/tests/utils/typeddict_models/types/resources/types/circle.py
@@ -11,9 +11,10 @@ class Circle(UncheckedBaseModel):
 
     if IS_PYDANTIC_V2:
         model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(
-            extra="allow"
+            extra="allow", frozen=True
         )  # type: ignore # Pydantic v2
     else:
 
         class Config:
+            frozen = True
             extra = pydantic.Extra.allow
