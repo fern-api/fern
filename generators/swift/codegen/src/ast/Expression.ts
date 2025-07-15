afa86ff594eb77a816415d37fe4e1e5fd480160c
Anar Kafkas <anarkafkas@gmail.com>
2025-07-15 20:42:16 +0300

    New expression types

--- generators/swift/codegen/src/ast/Expression.ts
+++ generators/swift/codegen/src/ast/Expression.ts
@@ -12,10 +12,28 @@ type Reference = {
     unsafeName: string;
 };
 
+type MemberAccess = {
+    type: "member-access";
+    target: Expression;
+    memberName: string;
+};
+
 type FunctionCall = {
     type: "function-call";
     unsafeName: string;
     arguments_?: FunctionArgument[];
+};
+
+type MethodCall = {
+    type: "method-call";
+    target: Expression;
+    methodName: string;
+    arguments_?: FunctionArgument[];
+};
+
+type Try = {
+    type: "try";
+    expression: Expression;
 };
 
 type RawValue = {
@@ -23,7 +41,7 @@ type RawValue = {
     value: string;
 };
 
-type InternalExpression = Reference | FunctionCall | RawValue;
+type InternalExpression = Reference | MemberAccess | FunctionCall | MethodCall | Try | RawValue;
 
 export class Expression extends AstNode {
     private internalExpression: InternalExpression;
@@ -38,16 +56,38 @@ export class Expression extends AstNode {
             case "reference":
                 writer.write(escapeReservedKeyword(this.internalExpression.unsafeName));
                 break;
+            case "member-access":
+                this.internalExpression.target.write(writer);
+                writer.write(".");
+                writer.write(this.internalExpression.memberName);
+                break;
             case "function-call":
                 writer.write(escapeReservedKeyword(this.internalExpression.unsafeName));
                 writer.write("(");
-                this.internalExpression.arguments_?.forEach((argument, argumentIdx) => {
+                this.internalExpression.arguments_?.forEach((argument: FunctionArgument, argumentIdx: number) => {
                     if (argumentIdx > 0) {
                         writer.write(", ");
                     }
                     argument.write(writer);
                 });
                 writer.write(")");
+                break;
+            case "method-call":
+                this.internalExpression.target.write(writer);
+                writer.write(".");
+                writer.write(this.internalExpression.methodName);
+                writer.write("(");
+                this.internalExpression.arguments_?.forEach((argument: FunctionArgument, argumentIdx: number) => {
+                    if (argumentIdx > 0) {
+                        writer.write(", ");
+                    }
+                    argument.write(writer);
+                });
+                writer.write(")");
+                break;
+            case "try":
+                writer.write("try ");
+                this.internalExpression.expression.write(writer);
                 break;
             case "raw-value":
                 writer.write(this.internalExpression.value);
@@ -61,8 +101,20 @@ export class Expression extends AstNode {
         return new this({ type: "reference", unsafeName });
     }
 
+    public static memberAccess(target: Expression, memberName: string): Expression {
+        return new this({ type: "member-access", target, memberName });
+    }
+
     public static functionCall(unsafeName: string, arguments_?: FunctionArgument[]): Expression {
         return new this({ type: "function-call", unsafeName, arguments_ });
+    }
+
+    public static methodCall(target: Expression, methodName: string, arguments_?: FunctionArgument[]): Expression {
+        return new this({ type: "method-call", target, methodName, arguments_ });
+    }
+
+    public static try(expression: Expression): Expression {
+        return new this({ type: "try", expression });
     }
 
     public static rawStringValue(value: string): Expression {
