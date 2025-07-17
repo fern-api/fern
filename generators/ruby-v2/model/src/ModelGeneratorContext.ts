06b9702e5d14d2cf1339f39ee085b9a87e0cf275
paarth <paarth@Roberts-MacBook-Pro.local>
2025-07-17 17:44:19 -0400

    files getting produced

---
+++ generators/ruby-v2/model/src/ModelGeneratorContext.ts
@@ -0,0 +1,18 @@
+import { AbstractFormatter, FernGeneratorExec, GeneratorNotificationService } from "@fern-api/base-generator";
+import { AbstractRubyGeneratorContext } from "@fern-api/ruby-ast";
+import { RelativeFilePath } from "@fern-api/fs-utils";
+
+import { FernFilepath, IntermediateRepresentation, TypeId } from "@fern-fern/ir-sdk/api";
+
+import { ModelCustomConfigSchema } from "./ModelCustomConfig";
+
+export class ModelGeneratorContext extends AbstractRubyGeneratorContext<ModelCustomConfigSchema> {
+    public constructor(
+        ir: IntermediateRepresentation,
+        config: FernGeneratorExec.config.GeneratorConfig,
+        customConfig: ModelCustomConfigSchema,
+        generatorNotificationService: GeneratorNotificationService
+    ) {
+        super(ir, config, customConfig, generatorNotificationService);
+    }
+}
---
+++ seed/ruby-sdk-v2/imdb/lib/CreateMovieRequest.rb
@@ -0,0 +1,1 @@
+class CreateMovieRequest; end
