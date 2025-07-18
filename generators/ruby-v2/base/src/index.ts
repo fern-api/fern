5449a81e01cc00a7433f03df9cf891c3543d55ec
paarth <paarth@Roberts-MacBook-Pro.local>
2025-07-18 10:02:31 -0400

    basic field gen working for umdn

--- generators/ruby-v2/base/src/index.ts
+++ generators/ruby-v2/base/src/index.ts
@@ -2,3 +2,4 @@ export { AbstractRubyGeneratorCli } from "./cli/AbstractRubyGeneratorCli";
 export { RubyFile } from "./project/RubyFile";
 export { RubyProject } from "./project/RubyProject";
 export { FileGenerator } from "./FileGenerator";
+
