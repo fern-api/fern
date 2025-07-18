fe794d9f49e581ddab6990dc531fd70d47d9fb5f
paarth <paarth@Roberts-MacBook-Pro.local>
2025-07-18 12:49:41 -0400

    add model.rb

--- generators/ruby-v2/base/src/asIs/model.Template.rb
+++ generators/ruby-v2/base/src/asIs/model.Template.rb
@@ -3,175 +3,188 @@
 module <%= gem_namespace %>
   module Internal
     module Types
+      # @abstract
+      #
+      # An abstract model that all data objects will inherit from
       class Model
+        extend Type
+
         class << self
-          # Define a field on this model
-          def field(name, type, **options)
-            field_definition = Field.new(
-              name: name,
-              type: type,
-              optional: options[:optional] || false,
-              nullable: options[:nullable] || false,
-              api_name: options[:api_name],
-              value: options[:value],
-              default: options[:default]
-            )
-            
-            fields[name.to_sym] = field_definition
-            
-            # Define getter method
-            define_method(name) do
-              instance_variable_get("@#{name}")
-            end
-            
-            # Define setter method
-            define_method("#{name}=") do |value|
-              instance_variable_set("@#{name}", value)
-            end
-          end
-          
-          # Get all defined fields for this model
+          # The defined fields for this model
+          #
+          # @api private
+          #
+          # @return [Hash<Symbol, Field>]
           def fields
-            @fields ||= {}
+            @fields ||= if self < <%= gem_namespace %>::Internal::Types::Model
+                          superclass.fields.dup
+                        else
+                          {}
+                        end
           end
-          
-          # Create instance from hash
-          def from_hash(hash)
-            instance = new
-            hash.each do |key, value|
-              field_name = key.to_sym
-              if fields.key?(field_name)
-                field_def = fields[field_name]
-                converted_value = convert_value(value, field_def.type)
-                instance.send("#{field_name}=", converted_value)
-              end
-            end
-            instance
+
+          # Any extra fields that have been created from instantiation
+          #
+          # @api private
+          #
+          # @return [Hash<Symbol, Field>]
+          def extra_fields
+            @extra_fields ||= {}
           end
-          
-          # Convert a value to the appropriate type
-          def convert_value(value, type)
-            return nil if value.nil?
-            
-            case type.to_s
-            when 'String'
-              value.to_s
-            when 'Integer'
-              value.to_i
-            when 'Float'
-              value.to_f
-            when 'Boolean'
-              !!value
-            when 'Array'
-              Array(value)
-            when 'Hash'
-              value.is_a?(Hash) ? value : {}
-            else
-              value
+
+          # Define a new field on this model
+          #
+          # @param name [Symbol] The name of the field
+          # @param type [Class] Type of the field
+          # @option optional [Boolean] If it is an optional field
+          # @option nullable [Boolean] If it is a nullable field
+          # @option api_name [Symbol, String] Name in the API of this field. When serializing/deserializing, will use
+          #   this field name
+          # @return [void]
+          def field(name, type, optional: false, nullable: false, api_name: nil, default: nil)
+            add_field_definition(name: name, type: type, optional: optional, nullable: nullable, api_name: api_name,
+                                 default: default)
+
+            define_accessor(name)
+            define_setter(name)
+          end
+
+          # Define a new literal for this model
+          #
+          # @param name [Symbol]
+          # @param value [Object]
+          # @option api_name [Symbol, String]
+          # @return [void]
+          def literal(name, value, api_name: nil)
+            add_field_definition(name: name, type: value.class, optional: false, nullable: false, api_name: api_name,
+                                 value: value)
+
+            define_accessor(name)
+          end
+
+          # Adds a new field definition into the class's fields registry
+          #
+          # @api private
+          #
+          # @param name [Symbol]
+          # @param type [Class]
+          # @option optional [Boolean]
+          # @return [void]
+          private def add_field_definition(name:, type:, optional:, nullable:, api_name:, default: nil, value: nil)
+            fields[name.to_sym] =
+              Field.new(name: name, type: type, optional: optional, nullable: nullable, api_name: api_name,
+                        value: value, default: default)
+          end
+
+          # Adds a new field definition into the class's extra fields registry
+          #
+          # @api private
+          #
+          # @param name [Symbol]
+          # @param type [Class]
+          # @option required [Boolean]
+          # @option optional [Boolean]
+          # @return [void]
+          def add_extra_field_definition(name:, type:)
+            return if extra_fields.key?(name.to_sym)
+
+            extra_fields[name.to_sym] = Field.new(name: name, type: type, optional: true, nullable: false)
+
+            define_accessor(name)
+            define_setter(name)
+          end
+
+          # @api private
+          private def define_accessor(name)
+            method_name = name.to_sym
+
+            define_method(method_name) do
+              @data[name]
             end
+          end
+
+          # @api private
+          private def define_setter(name)
+            method_name = :"#{name}="
+
+            define_method(method_name) do |val|
+              @data[name] = val
+            end
+          end
+
+          def coerce(value, strict: strict?)
+            return value if value.is_a?(self)
+
+            return value unless value.is_a?(::Hash)
+
+            new(value)
+          end
+
+          def load(str)
+            coerce(::JSON.parse(str, symbolize_names: true))
+          end
+
+          def ===(instance)
+            instance.class.ancestors.include?(self)
           end
         end
-        
-        def initialize(attributes = {})
-          attributes.each do |key, value|
-            field_name = key.to_sym
-            if self.class.fields.key?(field_name)
-              field_def = self.class.fields[field_name]
-              converted_value = self.class.convert_value(value, field_def.type)
-              send("#{field_name}=", converted_value)
-            end
+
+        # Creates a new instance of this model
+        # TODO: Should all this logic be in `#coerce` instead?
+        #
+        # @param values [Hash]
+        # @option strict [Boolean]
+        # @return [self]
+        def initialize(values = {})
+          @data = {}
+
+          values = Utils.symbolize_keys(values.dup)
+
+          self.class.fields.each do |field_name, field|
+            value = values.delete(field.api_name)
+
+            field_value = value || (if field.literal?
+                                      field.value
+                                    elsif field.default
+                                      field.default
+                                    end)
+
+            @data[field_name] = Utils.coerce(field.type, field_value)
+          end
+
+          # Any remaining values in the input become extra fields
+          values.each do |name, value|
+            self.class.add_extra_field_definition(name: name, type: value.class)
+
+            @data[name.to_sym] = value
           end
         end
-        
-        # Convert model to hash representation
-        def to_hash
-          hash = {}
-          self.class.fields.each do |field_name, field_def|
-            value = send(field_name)
-            next if value.nil? && field_def.optional
-            
-            api_key = field_def.api_name.to_s
-            hash[api_key] = serialize_value(value)
-          end
-          hash
-        end
-        
-        # Convert model to JSON
-        def to_json(*args)
-          to_hash.to_json(*args)
-        end
-        
-        # Check if model is valid
-        def valid?
-          self.class.fields.all? do |field_name, field_def|
-            value = send(field_name)
-            
-            # Check required fields
-            if !field_def.optional && (value.nil? || (value.respond_to?(:empty?) && value.empty?))
-              return false
-            end
-            
-            # Check nullable fields
-            if !field_def.nullable && value.nil?
-              return false
-            end
-            
-            true
+
+        def to_h
+          self.class.fields.merge(self.class.extra_fields).each_with_object({}) do |(name, field), acc|
+            # If there is a value present in the data, use that value
+            # If there is a `nil` value present in the data, and it is optional but NOT nullable, exclude key altogether
+            # If there is a `nil` value present in the data, and it is optional and nullable, use the nil value
+
+            value = @data[name]
+
+            next if value.nil? && field.optional && !field.nullable
+
+            acc[field.api_name] = value
           end
         end
-        
-        # Get validation errors
-        def errors
-          errors = []
-          self.class.fields.each do |field_name, field_def|
-            value = send(field_name)
-            
-            if !field_def.optional && (value.nil? || (value.respond_to?(:empty?) && value.empty?))
-              errors << "#{field_name} is required"
-            end
-            
-            if !field_def.nullable && value.nil?
-              errors << "#{field_name} cannot be null"
-            end
-          end
-          errors
-        end
-        
-        # Compare models for equality
+
         def ==(other)
-          return false unless other.is_a?(self.class)
-          
-          self.class.fields.all? do |field_name, _|
-            send(field_name) == other.send(field_name)
-          end
+          self.class == other.class && to_h == other.to_h
         end
-        
-        # String representation
+
+        # @return [String]
         def inspect
-          field_values = self.class.fields.map do |field_name, _|
-            value = send(field_name)
-            "#{field_name}=#{value.inspect}"
-          end.join(', ')
-          
-          "#<#{self.class.name}:#{object_id} #{field_values}>"
-        end
-        
-        alias_method :to_s, :inspect
-        
-        private
-        
-        # Serialize a value for hash/JSON conversion
-        def serialize_value(value)
-          case value
-          when Model
-            value.to_hash
-          when Array
-            value.map { |v| serialize_value(v) }
-          when Hash
-            value.transform_values { |v| serialize_value(v) }
-          else
-            value
+          attrs = @data.map do |name, value|
+            "#{name}=#{value.inspect}"
           end
+
+          "#<#{self.class.name}:0x#{object_id&.to_s(16)} #{attrs.join(" ")}>"
         end
       end
     end
