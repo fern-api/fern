32f398da19e1783138eaa9fb3fd42b3b8a3f8a54
Anar Kafkas <anarkafkas@gmail.com>
2025-07-21 14:54:08 +0300

    Implement `parseEndpointPath`

---
+++ generators/swift/sdk/src/generators/util/parse-endpoint-path.ts
@@ -0,0 +1,44 @@
+import { HttpEndpoint } from "@fern-fern/ir-sdk/api";
+
+type PathPart =
+    | {
+          type: "literal";
+          value: string;
+      }
+    | {
+          type: "path-parameter";
+          unsafeNameCamelCase: string;
+      };
+
+export type ParsedEndpointResult = {
+    pathParts: PathPart[];
+};
+
+export function parseEndpointPath(endpoint: HttpEndpoint): ParsedEndpointResult {
+    const pathParts: PathPart[] = [];
+
+    const pathParameterNamesByOriginalName = Object.fromEntries(
+        endpoint.allPathParameters.map((pathParam) => [
+            pathParam.name.originalName,
+            pathParam.name.camelCase.unsafeName
+        ])
+    );
+
+    if (endpoint.fullPath.head != null) {
+        pathParts.push({ type: "literal", value: endpoint.fullPath.head });
+    }
+
+    for (const pathPart of endpoint.fullPath.parts) {
+        const pathParameterName = pathParameterNamesByOriginalName[pathPart.pathParameter];
+        if (pathParameterName != null) {
+            pathParts.push({
+                type: "path-parameter",
+                unsafeNameCamelCase: pathParameterName
+            });
+        }
+        if (pathPart.tail != null) {
+            pathParts.push({ type: "literal", value: pathPart.tail });
+        }
+    }
+    return { pathParts };
+}
