ed9a8acf5d96232865a319f988057b1713d6259b
Chris Mallinson <chris.mallinson@buildwithfern.com>
2025-07-22 15:25:36 -0400

    Updating output file format

--- generators/ruby-v2/model/src/ModelCustomConfig.ts
+++ generators/ruby-v2/model/src/ModelCustomConfig.ts
@@ -1,4 +1,8 @@
 import { z } from "zod";
 
-export const ModelCustomConfigSchema = z.object({});
+export const ModelCustomConfigSchema = z.object({
+    clientModuleName: z.optional(z.string()),
+    typesModuleName: z.optional(z.string()).default("Types")
+});
+
 export type ModelCustomConfigSchema = z.infer<typeof ModelCustomConfigSchema>;
