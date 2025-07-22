e7d5a83f7519b2f3d0070f7a486dbb186af24f9c
dsinghvi <dsinghvi@users.noreply.github.com>
2025-07-22 21:58:27 +0000

    chore(typescript): update typescript-express seed

--- seed/ts-express/enum/api/resources/index.ts
+++ seed/ts-express/enum/api/resources/index.ts
@@ -2,5 +2,6 @@ export * as unknown from "./unknown";
 export * from "./unknown/types";
 export * as inlinedRequest from "./inlinedRequest";
 export * from "./inlinedRequest/service/requests";
+export * as headers from "./headers";
 export * as pathParam from "./pathParam";
 export * as queryParam from "./queryParam";
