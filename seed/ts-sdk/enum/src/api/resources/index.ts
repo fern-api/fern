782602f3e235177fec93af4e2731d7cc9363e56d
dsinghvi <dsinghvi@users.noreply.github.com>
2025-07-22 22:27:05 +0000

    chore(typescript): update typescript-sdk seed

--- seed/ts-sdk/enum/src/api/resources/index.ts
+++ seed/ts-sdk/enum/src/api/resources/index.ts
@@ -1,7 +1,9 @@
 export * as unknown from "./unknown/index.js";
 export * from "./unknown/types/index.js";
+export * as headers from "./headers/index.js";
 export * as inlinedRequest from "./inlinedRequest/index.js";
 export * as pathParam from "./pathParam/index.js";
 export * as queryParam from "./queryParam/index.js";
+export * from "./headers/client/requests/index.js";
 export * from "./inlinedRequest/client/requests/index.js";
 export * from "./queryParam/client/requests/index.js";
