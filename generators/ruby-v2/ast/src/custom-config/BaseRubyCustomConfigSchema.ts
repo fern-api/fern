d9c53ec03659082a6268f121b16edcc6d3907ecb
Chris Mallinson <chris.mallinson@buildwithfern.com>
2025-07-22 17:31:49 -0400

    Updating flexibility of superclass

--- generators/ruby-v2/ast/src/custom-config/BaseRubyCustomConfigSchema.ts
+++ generators/ruby-v2/ast/src/custom-config/BaseRubyCustomConfigSchema.ts
@@ -1,7 +1,14 @@
 import { z } from "zod";
 
 export const BaseRubyCustomConfigSchema = z.object({
-    clientModuleName: z.optional(z.string())
+    clientModuleName: z.optional(z.string()),
+    superclass: z.optional(z.object({
+        name: z.string(),
+        modules: z.array(z.string()).optional()
+    })).default({
+        name: "Model",
+        modules: ["Internal", "Types"]
+    })
 });
 
 export type BaseRubyCustomConfigSchema = z.infer<typeof BaseRubyCustomConfigSchema>;
