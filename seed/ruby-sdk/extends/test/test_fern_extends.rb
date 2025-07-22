1a74e0b8eb81273a151cd53c9f5223e902c6d26b
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-07-22 18:22:07 -0400

    chore(ruby): update ruby-sdk seed (#8264)
    Co-authored-by: dsinghvi <dsinghvi@users.noreply.github.com>

---
+++ seed/ruby-sdk/extends/test/test_fern_extends.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+require "fern_extends"
+
+# Basic SeedExtendsClient tests
+class TestSeedExtendsClient < Minitest::Test
+  def test_function
+    # SeedExtendsClient::Client.new
+  end
+end
