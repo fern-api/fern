de2483c46f712591b8e19d988ee4f26676e750cd
tstanmay13 <tstanmay13@users.noreply.github.com>
2025-07-23 20:49:20 +0000

    chore(java): update java-sdk seed

--- seed/java-sdk/bytes-upload/src/main/java/com/snippets/Example0.java
+++ seed/java-sdk/bytes-upload/src/main/java/com/snippets/Example0.java
@@ -9,6 +9,6 @@ public class Example0 {
             .url("https://api.fern.com")
             .build();
 
-        client.service().upload("Hello World!".getBytes());
+        client.service().upload();
     }
 }
