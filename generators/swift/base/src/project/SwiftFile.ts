af91e728b01396e630e884b485300be490ddd6ab
Anar Kafkas <anarkafkas@gmail.com>
2025-07-25 00:21:02 +0300

    Implement `FileComponent`

--- generators/swift/base/src/project/SwiftFile.ts
+++ generators/swift/base/src/project/SwiftFile.ts
@@ -1,16 +1,24 @@
 import { File } from "@fern-api/base-generator";
 import { RelativeFilePath } from "@fern-api/fs-utils";
+import { swift } from "@fern-api/swift-codegen";
 
 export declare namespace SwiftFile {
     interface Args {
         filename: string;
         directory: RelativeFilePath;
-        fileContents: string;
+        fileContents: string | swift.FileComponent[];
     }
 }
 
 export class SwiftFile extends File {
+    private static getFileContents(fileContents: string | swift.FileComponent[]) {
+        if (typeof fileContents === "string") {
+            return fileContents;
+        }
+        return fileContents.map((component) => component.toString()).join("");
+    }
+
     constructor(args: SwiftFile.Args) {
-        super(args.filename, args.directory, args.fileContents);
+        super(args.filename, args.directory, SwiftFile.getFileContents(args.fileContents));
     }
 }
--- generators/swift/codegen/src/ast/index.ts
+++ generators/swift/codegen/src/ast/index.ts
@@ -1,10 +1,12 @@
 export { AccessLevel } from "./AccessLevel";
 export { Class } from "./Class";
 export { CodeBlock } from "./CodeBlock";
+export { Comment } from "./Comment";
 export { DeclarationType } from "./DeclarationType";
 export { EnumWithAssociatedValues } from "./EnumWithAssociatedValues";
 export { EnumWithRawValues } from "./EnumWithRawValues";
 export { Expression } from "./Expression";
+export type { FileComponent } from "./FileComponent";
 export { FunctionArgument } from "./FunctionArgument";
 export { FunctionParameter } from "./FunctionParameter";
 export { Initializer } from "./Initializer";
