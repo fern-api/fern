2fc8df3d2cc06df3db3b40143c3db586fef736e6
jsklan <jsklan@users.noreply.github.com>
2025-07-25 18:46:35 +0000

    chore(ruby): update ruby-sdk seed

---
+++ seed/ruby-sdk/query-parameters-openapi-as-objects/lib/gemconfig.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module SeedApiClient
+  module Gemconfig
+    VERSION = ""
+    AUTHORS = [""].freeze
+    EMAIL = ""
+    SUMMARY = ""
+    DESCRIPTION = ""
+    HOMEPAGE = "https://github.com/query-parameters-openapi-as-objects/fern"
+    SOURCE_CODE_URI = "https://github.com/query-parameters-openapi-as-objects/fern"
+    CHANGELOG_URI = "https://github.com/query-parameters-openapi-as-objects/fern/blob/master/CHANGELOG.md"
+  end
+end
