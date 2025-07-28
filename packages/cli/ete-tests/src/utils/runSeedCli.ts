a8b47a4cc3f82e648c5ea5ae17e291b67dd99c7e
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-07-28 15:19:02 -0400

    format

--- packages/cli/ete-tests/src/utils/runSeedCli.ts
+++ packages/cli/ete-tests/src/utils/runSeedCli.ts
@@ -4,11 +4,16 @@ import path from "path";
 import { loggingExeca } from "@fern-api/logging-execa";
 
 export async function runSeedCli(args: string[], options?: Options): Promise<ExecaChildProcess> {
-    return loggingExeca(undefined, "node", ["--enable-source-maps", path.join(__dirname, "../../../../seed/dist/cli.cjs"), ...args], {
-        ...options,
-        env: {
-            ...options?.env
-        },
-        doNotPipeOutput: options?.reject === false
-    });
+    return loggingExeca(
+        undefined,
+        "node",
+        ["--enable-source-maps", path.join(__dirname, "../../../../seed/dist/cli.cjs"), ...args],
+        {
+            ...options,
+            env: {
+                ...options?.env
+            },
+            doNotPipeOutput: options?.reject === false
+        }
+    );
 }
