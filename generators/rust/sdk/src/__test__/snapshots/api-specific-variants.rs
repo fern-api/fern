30851b6684ea40d08988a7bc1041a6e557edf5a4
Naman Anand <iamnamananand996@gmail.com>
2025-07-29 19:20:31 +0530

    refactor(rust): error file not needed

--- generators/rust/sdk/src/__test__/snapshots/api-specific-variants.rs
+++ generators/rust/sdk/src/__test__/snapshots/api-specific-variants.rs
@@ -15,6 +15,7 @@ pub enum ApiError {
     #[error("Serialization error: {0}")]
     Serialization(serde_json::Error),
 }
+
 impl ApiError {
     pub fn from_response(status_code: u16, body: Option<&str>) -> Self {
     match status_code {
@@ -74,3 +75,4 @@ impl ApiError {
     }
 }
 }
+
