5516148ee1ba2a2056ac296f5dbe7ae15cf4f9c7
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-07-29 18:07:05 -0400

    chore(internal): Fix some tsup related things (#8422)

--- packages/cli/cli/build.prod.cjs
+++ packages/cli/cli/build.prod.cjs
@@ -1,6 +1,6 @@
 const packageJson = require("./package.json");
 const tsup = require('tsup');
-const { writeFile, truncate } = require("fs/promises");
+const { writeFile } = require("fs/promises");
 const path = require("path");
 
 main();
@@ -19,7 +19,7 @@ async function main() {
             DEFAULT_FDR_ORIGIN: "https://registry.buildwithfern.com",
             VENUS_AUDIENCE: "venus-prod",
             LOCAL_STORAGE_FOLDER: ".fern",
-            POSTHOG_API_KEY: process.env.POSTHOG_API_KEY,
+            POSTHOG_API_KEY: process.env.POSTHOG_API_KEY ?? "",
             DOCS_DOMAIN_SUFFIX: "docs.buildwithfern.com",
             DOCS_PREVIEW_BUCKET: 'https://prod-local-preview-bundle2.s3.amazonaws.com/',
             APP_DOCS_TAR_PREVIEW_BUCKET: 'https://prod-local-preview-bundle4.s3.amazonaws.com/',
