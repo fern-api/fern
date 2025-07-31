06f814bb0e12a8212ab5fe651fb3074e8d85e1d5
Swimburger <Swimburger@users.noreply.github.com>
2025-07-31 00:01:30 +0000

    chore(php): update php-sdk seed

--- seed/php-sdk/file-upload/src/Service/ServiceClient.php
+++ seed/php-sdk/file-upload/src/Service/ServiceClient.php
@@ -21,6 +21,7 @@ use Seed\Service\Requests\MyOtherRequest;
 use Seed\Service\Requests\OptionalArgsRequest;
 use Seed\Core\Json\JsonDecoder;
 use JsonException;
+use Seed\Service\Requests\InlineTypeRequest;
 use Seed\Core\Json\JsonApiRequest;
 
 class ServiceClient
@@ -527,6 +528,62 @@ class ServiceClient
                 new MultipartApiRequest(
                     baseUrl: $options['baseUrl'] ?? $this->client->options['baseUrl'] ?? '',
                     path: "/optional-args",
+                    method: HttpMethod::POST,
+                    body: $body,
+                ),
+                $options,
+            );
+            $statusCode = $response->getStatusCode();
+            if ($statusCode >= 200 && $statusCode < 400) {
+                $json = $response->getBody()->getContents();
+                return JsonDecoder::decodeString($json);
+            }
+        } catch (JsonException $e) {
+            throw new SeedException(message: "Failed to deserialize response: {$e->getMessage()}", previous: $e);
+        } catch (RequestException $e) {
+            $response = $e->getResponse();
+            if ($response === null) {
+                throw new SeedException(message: $e->getMessage(), previous: $e);
+            }
+            throw new SeedApiException(
+                message: "API request failed",
+                statusCode: $response->getStatusCode(),
+                body: $response->getBody()->getContents(),
+            );
+        } catch (ClientExceptionInterface $e) {
+            throw new SeedException(message: $e->getMessage(), previous: $e);
+        }
+        throw new SeedApiException(
+            message: 'API request failed',
+            statusCode: $statusCode,
+            body: $response->getBody()->getContents(),
+        );
+    }
+
+    /**
+     * @param InlineTypeRequest $request
+     * @param ?array{
+     *   baseUrl?: string,
+     *   maxRetries?: int,
+     *   timeout?: float,
+     *   headers?: array<string, string>,
+     *   queryParameters?: array<string, mixed>,
+     * } $options
+     * @return string
+     * @throws SeedException
+     * @throws SeedApiException
+     */
+    public function withInlineType(InlineTypeRequest $request, ?array $options = null): string
+    {
+        $options = array_merge($this->options, $options ?? []);
+        $body = new MultipartFormData();
+        $body->addPart($request->file->toMultipartFormDataPart('file'));
+        $body->add(name: 'request', value: $request->request->toJson());
+        try {
+            $response = $this->client->sendRequest(
+                new MultipartApiRequest(
+                    baseUrl: $options['baseUrl'] ?? $this->client->options['baseUrl'] ?? '',
+                    path: "/inline-type",
                     method: HttpMethod::POST,
                     body: $body,
                 ),
