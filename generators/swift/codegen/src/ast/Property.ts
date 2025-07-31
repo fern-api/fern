20e4e90b54c32106bf4f1bbf32c2c154fba7fdac
Anar Kafkas <anarkafkas@gmail.com>
2025-07-31 23:10:20 +0300

    Add `defaultValue` to `Property`

--- generators/swift/codegen/src/ast/Property.ts
+++ generators/swift/codegen/src/ast/Property.ts
@@ -1,8 +1,9 @@
 import { AccessLevel } from "./AccessLevel";
-import { DeclarationType } from "./DeclarationType";
-import { Type } from "./Type";
 import { AstNode, Writer } from "./core";
+import { DeclarationType } from "./DeclarationType";
+import { Expression } from "./Expression";
 import { isReservedKeyword } from "./syntax";
+import { Type } from "./Type";
 
 export declare namespace Property {
     interface Args {
@@ -11,6 +12,7 @@ export declare namespace Property {
         static_?: boolean;
         declarationType: DeclarationType;
         type: Type;
+        defaultValue?: Expression;
     }
 }
 
@@ -20,14 +22,16 @@ export class Property extends AstNode {
     public readonly static_?: boolean;
     public readonly declarationType: DeclarationType;
     public readonly type: Type;
+    public readonly defaultValue?: Expression;
 
-    constructor({ unsafeName, accessLevel, static_, declarationType, type }: Property.Args) {
+    constructor({ unsafeName, accessLevel, static_, declarationType, type, defaultValue }: Property.Args) {
         super();
         this.unsafeName = unsafeName;
         this.accessLevel = accessLevel;
         this.static_ = static_;
         this.declarationType = declarationType;
         this.type = type;
+        this.defaultValue = defaultValue;
     }
 
     public write(writer: Writer): void {
@@ -47,5 +51,9 @@ export class Property extends AstNode {
         }
         writer.write(": ");
         this.type.write(writer);
+        if (this.defaultValue != null) {
+            writer.write(" = ");
+            this.defaultValue.write(writer);
+        }
     }
 }
