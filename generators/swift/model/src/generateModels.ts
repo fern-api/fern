cc8db7f449e79fc51d48a0fb69a616c44ba1428b
Anar Kafkas <anarkafkas@gmail.com>
2025-08-03 13:40:08 +0300

    Implement alias generator

--- generators/swift/model/src/generateModels.ts
+++ generators/swift/model/src/generateModels.ts
@@ -1,16 +1,21 @@
 import { SwiftFile } from "@fern-api/swift-base";
 
+import { AliasGenerator } from "./alias";
 import { StringEnumGenerator } from "./enum";
 import { ModelGeneratorContext } from "./ModelGeneratorContext";
 import { ObjectGenerator } from "./object";
-import { DiscriminatedUnionGenerator } from "./union";
-import { UndiscriminatedUnionGenerator } from "./union/UndiscriminatedUnionGenerator";
+import { DiscriminatedUnionGenerator, UndiscriminatedUnionGenerator } from "./union";
 
 export function generateModels({ context }: { context: ModelGeneratorContext }): SwiftFile[] {
     const files: SwiftFile[] = [];
     for (const [_typeId, typeDeclaration] of Object.entries(context.ir.types)) {
         const file = typeDeclaration.shape._visit<SwiftFile | undefined>({
-            alias: () => undefined,
+            alias: (atd) =>
+                new AliasGenerator(
+                    typeDeclaration.name.name.pascalCase.unsafeName,
+                    context.schemasDirectory,
+                    atd
+                ).generate(),
             enum: (etd) =>
                 new StringEnumGenerator(
                     typeDeclaration.name.name.pascalCase.unsafeName,
