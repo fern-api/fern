681b7987c704ce05e2bc8f6467948641cd40ea85
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-08-04 16:25:41 -0400

    chore(typescript): update typescript-sdk seed (#8577)
    Co-authored-by: fern-support <fern-support@users.noreply.github.com>

---
+++ seed/ts-sdk/simple-api/src/index.ts
@@ -0,0 +1,4 @@
+export * as SeedSimpleApi from "./api/index.js";
+export { SeedSimpleApiError, SeedSimpleApiTimeoutError } from "./errors/index.js";
+export { SeedSimpleApiClient } from "./Client.js";
+export { SeedSimpleApiEnvironment } from "./environments.js";
