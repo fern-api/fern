56306cba4be04da163b2bfb722bc9dac67b0436e
Swimburger <Swimburger@users.noreply.github.com>
2025-08-04 17:49:01 +0000

    chore(ruby): update ruby-sdk seed

--- seed/ruby-sdk/examples/lib/fern_examples/service/client.rb
+++ seed/ruby-sdk/examples/lib/fern_examples/service/client.rb
@@ -6,6 +6,7 @@ require "json"
 require_relative "../types/types/metadata"
 require_relative "../types/types/big_entity"
 require_relative "../types/types/response"
+require_relative "../types/types/refresh_token_request"
 require "async"
 
 module SeedExamplesClient
@@ -174,6 +175,30 @@ module SeedExamplesClient
         req.url "#{@request_client.get_url(request_options: request_options)}/big-entity"
       end
       SeedExamplesClient::Types::Response.from_json(json_object: response.body)
+    end
+
+    # @param request [Hash] Request of type SeedExamplesClient::Types::RefreshTokenRequest, as a Hash
+    #   * :ttl (Integer)
+    # @param request_options [SeedExamplesClient::RequestOptions]
+    # @return [Void]
+    # @example
+    #  examples = SeedExamplesClient::Client.new(base_url: "https://api.example.com", token: "YOUR_AUTH_TOKEN")
+    #  examples.service.refresh_token
+    def refresh_token(request: nil, request_options: nil)
+      @request_client.conn.post do |req|
+        req.options.timeout = request_options.timeout_in_seconds unless request_options&.timeout_in_seconds.nil?
+        req.headers["Authorization"] = request_options.token unless request_options&.token.nil?
+        req.headers = {
+      **(req.headers || {}),
+      **@request_client.get_headers,
+      **(request_options&.additional_headers || {})
+        }.compact
+        unless request_options.nil? || request_options&.additional_query_parameters.nil?
+          req.params = { **(request_options&.additional_query_parameters || {}) }.compact
+        end
+        req.body = { **(request || {}), **(request_options&.additional_body_parameters || {}) }.compact
+        req.url "#{@request_client.get_url(request_options: request_options)}/refresh-token"
+      end
     end
   end
 
@@ -354,6 +379,32 @@ module SeedExamplesClient
           req.url "#{@request_client.get_url(request_options: request_options)}/big-entity"
         end
         SeedExamplesClient::Types::Response.from_json(json_object: response.body)
+      end
+    end
+
+    # @param request [Hash] Request of type SeedExamplesClient::Types::RefreshTokenRequest, as a Hash
+    #   * :ttl (Integer)
+    # @param request_options [SeedExamplesClient::RequestOptions]
+    # @return [Void]
+    # @example
+    #  examples = SeedExamplesClient::Client.new(base_url: "https://api.example.com", token: "YOUR_AUTH_TOKEN")
+    #  examples.service.refresh_token
+    def refresh_token(request: nil, request_options: nil)
+      Async do
+        @request_client.conn.post do |req|
+          req.options.timeout = request_options.timeout_in_seconds unless request_options&.timeout_in_seconds.nil?
+          req.headers["Authorization"] = request_options.token unless request_options&.token.nil?
+          req.headers = {
+        **(req.headers || {}),
+        **@request_client.get_headers,
+        **(request_options&.additional_headers || {})
+          }.compact
+          unless request_options.nil? || request_options&.additional_query_parameters.nil?
+            req.params = { **(request_options&.additional_query_parameters || {}) }.compact
+          end
+          req.body = { **(request || {}), **(request_options&.additional_body_parameters || {}) }.compact
+          req.url "#{@request_client.get_url(request_options: request_options)}/refresh-token"
+        end
       end
     end
   end
