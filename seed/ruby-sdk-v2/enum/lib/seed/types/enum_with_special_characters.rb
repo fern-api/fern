7309d2ccb5575e12fcea41403233e2b61a16e668
Deep Singhvi <deep@buildwithfern.com>
2025-08-10 14:49:29 -0600

    seed

---
+++ seed/ruby-sdk-v2/enum/lib/seed/types/enum_with_special_characters.rb
@@ -0,0 +1,9 @@
+
+module Seed
+    module Types
+        module EnumWithSpecialCharacters
+            extends Seed::Internal::Types::Enum
+            BLA = "\$bla"
+            YO = "\$yo"end
+    end
+end
