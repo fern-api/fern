5193e4443c165f13968447303faec0174a053a38
jsklan <jsklan.development@gmail.com>
2025-08-12 18:12:42 -0400

    run seed test on exhaustive

---
+++ seed/ruby-sdk-v2/exhaustive/SeedExhaustiveClient.rb
@@ -0,0 +1,26 @@
+
+
+module Seed
+    class SeedExhaustiveClient
+        # @return [String]
+        def endpoints
+            @endpoints ||= Seed::Endpoints::Client.new(client: @raw_client)
+        end
+        # @return [String]
+        def inlinedRequests
+            @inlinedRequests ||= Seed::InlinedRequests::Client.new(client: @raw_client)
+        end
+        # @return [String]
+        def noAuth
+            @noAuth ||= Seed::NoAuth::Client.new(client: @raw_client)
+        end
+        # @return [String]
+        def noReqBody
+            @noReqBody ||= Seed::NoReqBody::Client.new(client: @raw_client)
+        end
+        # @return [String]
+        def reqWithHeaders
+            @reqWithHeaders ||= Seed::ReqWithHeaders::Client.new(client: @raw_client)
+        end
+
+end
