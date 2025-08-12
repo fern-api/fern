0e3b199b79003e27357fabbe70fcdc9b362a429e
Deep Singhvi <deep@buildwithfern.com>
2025-08-12 17:28:27 -0400

    fix(ruby): fields correctly set optional and nullable

---
+++ seed/ruby-sdk-v2/optional/lib/seed/optional/client.rb
@@ -0,0 +1,18 @@
+
+module Seed
+    module Optional
+        class Client
+            # @option client [Seed::Internal::Http::RawClient]
+            #
+            # @return [Seed::Optional::Client]
+            def initialize(client)
+                @client = client
+            end
+
+            # @return [String]
+            def send_optional_body
+                raise NotImplementedError, 'This method is not yet implemented.'
+            end
+        end
+    end
+end
