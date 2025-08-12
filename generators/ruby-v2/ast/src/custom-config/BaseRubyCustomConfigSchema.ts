0d3ff8e8d0376830fc20897d9a670bf2e9620af9
jsklan <jsklan.development@gmail.com>
2025-08-12 13:58:07 -0400

    remove unused ruby custom config

--- generators/ruby-v2/ast/src/custom-config/BaseRubyCustomConfigSchema.ts
+++ generators/ruby-v2/ast/src/custom-config/BaseRubyCustomConfigSchema.ts
@@ -2,8 +2,7 @@ import { z } from "zod";
 
 export const BaseRubyCustomConfigSchema = z.object({
     module: z.optional(z.string()),
-    clientModuleName: z.optional(z.string()),
-    environmentClassName: z.optional(z.string())
+    clientModuleName: z.optional(z.string())
 });
 
 export type BaseRubyCustomConfigSchema = z.infer<typeof BaseRubyCustomConfigSchema>;
