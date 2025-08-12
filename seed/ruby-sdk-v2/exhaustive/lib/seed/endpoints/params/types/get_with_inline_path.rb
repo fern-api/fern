56a8463c411ad9fb12ff6af666fd8574f755c480
Deep Singhvi <deep@buildwithfern.com>
2025-08-12 10:13:49 -0400

    feat(ruby): generate wrapped requests (#8676)

---
+++ seed/ruby-sdk-v2/exhaustive/lib/seed/endpoints/params/types/get_with_inline_path.rb
@@ -0,0 +1,10 @@
+
+module Seed
+    module Endpoints
+        module Params
+            class GetWithInlinePath
+                field :param, String, optional: true, nullable: true
+            end
+        end
+    end
+end
