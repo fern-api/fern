fde7653021bd070aed2d637b554cb3ac89399ea3
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-08-13 14:34:23 -0400

    chore(ruby): update ruby-model seed (#8720)
    Co-authored-by: Swimburger <Swimburger@users.noreply.github.com>

---
+++ seed/ruby-model/client-side-params/lib/seed_client_side_params_client.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require_relative "seed_client_side_params_client/types/types/resource"
+require_relative "seed_client_side_params_client/types/types/search_response"
