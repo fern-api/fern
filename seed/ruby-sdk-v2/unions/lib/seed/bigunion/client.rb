a1e20819948eb06cce8313192dec1bcbb39dcc28
Deep Singhvi <deep@buildwithfern.com>
2025-08-13 17:22:59 -0400

    fix

--- seed/ruby-sdk-v2/unions/lib/seed/bigunion/client.rb
+++ seed/ruby-sdk-v2/unions/lib/seed/bigunion/client.rb
@@ -9,41 +9,9 @@ module Seed
                 @client = client
             end
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            # @return [Seed::Bigunion::BigUnion]
-            def get
-                raise NotImplementedError, 'This method is not yet implemented.'
-=======
-            # @return [Seed::bigunion::BigUnion]
-            def get(request_options: {}, **params)
-                _request = Seed::Internal::Http::JSONRequest.new(
-                    method: GET,
-                    path: "/#{params[:id]}"
-                )
->>>>>>> b7b4afb47e (fix(ruby): add basic endpoint implementation)
-=======
             # @return [Seed::Bigunion::BigUnion]
             def get(request_options: {}, **params)
                 raise NotImplementedError, 'This method is not yet implemented.'
->>>>>>> 591a1624b0 (fix)
-=======
-=======
->>>>>>> 2f201d7634 (fix)
-            # @return [Seed::Bigunion::BigUnion]
-            def get(request_options: {}, **params)
-<<<<<<< HEAD
-                _request = Seed::Internal::Http::JSONRequest.new(
-                    method: GET,
-                    path: "/#{params[:id]}"
-                )
->>>>>>> b7b4afb47e (fix(ruby): add basic endpoint implementation)
->>>>>>> 84556af643 (fix)
-=======
-                raise NotImplementedError, 'This method is not yet implemented.'
->>>>>>> 2f201d7634 (fix)
             end
 
             # @return [bool]
