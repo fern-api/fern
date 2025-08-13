494c4ac8a110cff2adae584842dc637283cf6f3f
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-08-13 14:30:47 -0400

    chore(go): update go-sdk seed (#8717)
    Co-authored-by: tstanmay13 <tstanmay13@users.noreply.github.com>

---
+++ seed/go-sdk/client-side-params/dynamic-snippets/example0/snippet.go
@@ -0,0 +1,32 @@
+package example
+
+import (
+    client "github.com/client-side-params/fern/client"
+    option "github.com/client-side-params/fern/option"
+    context "context"
+    fern "github.com/client-side-params/fern"
+)
+
+func do() {
+    client := client.NewClient(
+        option.WithBaseURL(
+            "https://api.fern.com",
+        ),
+    )
+    client.Service.ListResources(
+        context.TODO(),
+        &fern.ListResourcesRequest{
+            Page: 1,
+            PerPage: 1,
+            Sort: "created_at",
+            Order: "desc",
+            IncludeTotals: true,
+            Fields: fern.String(
+                "fields",
+            ),
+            Search: fern.String(
+                "search",
+            ),
+        },
+    )
+}
