2a6d7fc7400a1b15206dcbaabb8008f30b0f074a
Anar Kafkas <anarkafkas@gmail.com>
2025-08-13 17:44:37 +0300

    Remove unused

--- generators/swift/model/src/helpers/ir-types.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectProperty, TypeReference } from "@fern-fern/ir-sdk/api";
-
-export function isOptionalProperty(p: ObjectProperty): boolean {
-    return isOptionalType(p.valueType);
-}
-
-export function isOptionalType(typeReference: TypeReference): boolean {
-    return typeReference.type === "container" && typeReference.container.type === "optional";
-}
