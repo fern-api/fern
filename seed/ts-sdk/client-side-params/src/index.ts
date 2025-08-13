25d202424f9848109b3a5033185c1fb9410266c6
Swimburger <Swimburger@users.noreply.github.com>
2025-08-13 18:54:09 +0000

    chore(typescript): update typescript-sdk seed

---
+++ seed/ts-sdk/client-side-params/src/index.ts
@@ -0,0 +1,3 @@
+export * as SeedClientSideParams from "./api/index.js";
+export { SeedClientSideParamsError, SeedClientSideParamsTimeoutError } from "./errors/index.js";
+export { SeedClientSideParamsClient } from "./Client.js";
