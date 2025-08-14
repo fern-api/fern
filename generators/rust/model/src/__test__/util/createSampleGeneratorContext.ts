5c14cec0043a69b32dd25af81e62c6bc353636bf
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2025-08-14 07:26:22 -0400

    resolve lint

--- generators/rust/model/src/__test__/util/createSampleGeneratorContext.ts
+++ generators/rust/model/src/__test__/util/createSampleGeneratorContext.ts
@@ -20,7 +20,7 @@ export async function createSampleGeneratorContext(testDefinitionName: string): 
         type: "local",
         _visit: (visitor) => visitor.local()
     });
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    // biome-ignore lint/suspicious/noExplicitAny: allow
     return new ModelGeneratorContext(ir as any, generatorConfig, customConfig, notificationService);
 }
 
