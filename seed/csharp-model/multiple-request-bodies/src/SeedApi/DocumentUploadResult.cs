08a5e40d74ac27465fa401cf7cd7c02ed6702f97
Swimburger <Swimburger@users.noreply.github.com>
2025-08-15 02:29:37 +0000

    chore(csharp): update csharp-model seed

---
+++ seed/csharp-model/multiple-request-bodies/src/SeedApi/DocumentUploadResult.cs
@@ -0,0 +1,31 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SeedApi.Core;
+
+namespace SeedApi;
+
+[Serializable]
+public record DocumentUploadResult : IJsonOnDeserialized
+{
+    [JsonExtensionData]
+    private readonly IDictionary<string, JsonElement> _extensionData =
+        new Dictionary<string, JsonElement>();
+
+    [JsonPropertyName("fileId")]
+    public string? FileId { get; set; }
+
+    [JsonPropertyName("status")]
+    public string? Status { get; set; }
+
+    [JsonIgnore]
+    public ReadOnlyAdditionalProperties AdditionalProperties { get; private set; } = new();
+
+    void IJsonOnDeserialized.OnDeserialized() =>
+        AdditionalProperties.CopyFromExtensionData(_extensionData);
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return JsonUtils.Serialize(this);
+    }
+}
