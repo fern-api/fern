4907e49d20d8fa8426ca860a0eedec8f25f24fe7
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-08-15 15:14:22 -0700

    chore(java): update java-sdk seed (#8805)
    Co-authored-by: tstanmay13 <tstanmay13@users.noreply.github.com>

---
+++ seed/java-sdk/content-type/src/main/java/com/snippets/Example3.java
@@ -0,0 +1,24 @@
+package com.snippets;
+
+import com.seed.contentTypes.SeedContentTypesClient;
+import com.seed.contentTypes.resources.service.requests.OptionalMergePatchRequest;
+
+public class Example3 {
+    public static void main(String[] args) {
+        SeedContentTypesClient client = SeedContentTypesClient
+            .builder()
+            .url("https://api.fern.com")
+            .build();
+
+        client.service().optionalMergePatchTest(
+            OptionalMergePatchRequest
+                .builder()
+                .requiredField("requiredField")
+                .optionalString("optionalString")
+                .optionalInteger(1)
+                .optionalBoolean(true)
+                .nullableString("nullableString")
+                .build()
+        );
+    }
+}
