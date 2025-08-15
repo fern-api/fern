b94dcb835942c2b76917699766cc81531ae24d65
Naman Anand <iamnamananand996@gmail.com>
2025-08-16 00:56:25 +0530

    chore(rust): update test

--- generators/rust/model/src/__test__/snapshots/union-types/credit_card.rs
+++ generators/rust/model/src/__test__/snapshots/union-types/credit_card.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub struct CreditCard {
     pub card_number: String,
     pub expiry_date: String,
--- generators/rust/model/src/__test__/snapshots/union-types/rectangle.rs
+++ generators/rust/model/src/__test__/snapshots/union-types/rectangle.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub struct Rectangle {
     pub width: f64,
     pub height: f64,
