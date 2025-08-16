706f5c6439e96bf81b344f9292a0ee8cc327f192
Anar Kafkas <anarkafkas@gmail.com>
2025-08-16 17:13:06 +0300

    Init LiteralEnumGenerator

---
+++ generators/swift/model/src/literal/LiteralEnumGenerator.ts
@@ -0,0 +1,69 @@
+import { swift } from "@fern-api/swift-codegen";
+import { camelCase } from "lodash-es";
+
+import { StringEnumGenerator } from "../enum";
+import { pascalCase } from "./pascal-case";
+
+export declare namespace LiteralEnumGenerator {
+    interface Args {
+        name: string;
+        literalValue: string;
+        docsContent?: string;
+    }
+}
+
+export class LiteralEnumGenerator {
+    /**
+     * Generates a safe name for a string literal enum from the literal value.
+     */
+    public static generateName(literalValue: string): string {
+        const sanitizedLiteralValue = this.sanitizeLiteralValue(literalValue);
+        return pascalCase(sanitizedLiteralValue);
+    }
+
+    /**
+     * Generates a safe enum case label for a string literal enum from the literal value.
+     */
+    public static generateEnumCaseLabel(literalValue: string): string {
+        const sanitizedLiteralValue = this.sanitizeLiteralValue(literalValue);
+        return camelCase(sanitizedLiteralValue);
+    }
+
+    /**
+     * Sanitizes a literal value to ensure it is a valid Swift identifier.
+     */
+    public static sanitizeLiteralValue(literalValue: string): string {
+        if (literalValue === "") {
+            return "empty";
+        }
+        // TODO: Handle other bad values like those starting with numbers, etc.
+        return literalValue;
+    }
+
+    private readonly name: string;
+    private readonly literalValue: string;
+    private readonly docsContent?: string;
+
+    public constructor({ name, literalValue, docsContent }: LiteralEnumGenerator.Args) {
+        this.name = name;
+        this.literalValue = literalValue;
+        this.docsContent = docsContent;
+    }
+
+    public generate(): swift.EnumWithRawValues {
+        const stringEnumGenerator = new StringEnumGenerator({
+            name: this.name,
+            source: {
+                type: "custom",
+                values: [
+                    {
+                        unsafeName: LiteralEnumGenerator.generateEnumCaseLabel(this.literalValue),
+                        rawValue: this.literalValue
+                    }
+                ]
+            },
+            docsContent: this.docsContent
+        });
+        return stringEnumGenerator.generate();
+    }
+}
