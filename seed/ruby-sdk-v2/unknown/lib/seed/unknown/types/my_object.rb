3b98e58b14548e2e8c31abf8f021c9fdf9f207e3
Deep Singhvi <deep@buildwithfern.com>
2025-08-18 13:23:07 -0400

    feat(ruby): unknown type signatures (#8827)

---
+++ seed/ruby-sdk-v2/unknown/lib/seed/unknown/types/my_object.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Seed
+  module Unknown
+    module Types
+      class MyObject < Internal::Types::Model
+        field :unknown, Internal::Types::Hash[String, Object], optional: false, nullable: false
+
+      end
+    end
+  end
+end
