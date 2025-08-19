4284d7a1e66ef7fac8ab5663749db86f4128f09d
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-08-19 10:48:38 -0400

    chore(java): update java-spring seed (#8861)
    Co-authored-by: tstanmay13 <tstanmay13@users.noreply.github.com>

--- seed/java-spring/exhaustive/resources/endpoints/object/ObjectService.java
+++ seed/java-spring/exhaustive/resources/endpoints/object/ObjectService.java
@@ -75,4 +75,13 @@ public interface ObjectService {
   NestedObjectWithRequiredField getAndReturnNestedWithRequiredFieldAsList(
       @RequestHeader("Authorization") BearerAuth auth, Principal principal,
       @RequestBody List<NestedObjectWithRequiredField> body);
+
+  @PostMapping(
+      value = "/test-integer-overflow-edge-cases",
+      produces = "application/json",
+      consumes = "application/json"
+  )
+  ObjectWithOptionalField testIntegerOverflowEdgeCases(
+      @RequestHeader("Authorization") BearerAuth auth, Principal principal,
+      @RequestBody ObjectWithOptionalField body);
 }
