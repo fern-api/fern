60eaca578c39bbc5d27bba73614672bb2f88eca9
Naman Anand <iamnamananand996@gmail.com>
2025-08-20 17:14:55 +0530

    chore(rust): tests

--- generators/rust/model/src/__test__/snapshots/undiscriminated-union-types/product.rs
+++ generators/rust/model/src/__test__/snapshots/undiscriminated-union-types/product.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Product {
     pub id: String,
     pub title: String,
--- generators/rust/model/src/__test__/snapshots/union-types/motorcycle.rs
+++ generators/rust/model/src/__test__/snapshots/union-types/motorcycle.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Motorcycle {
     pub engine_size: f64,
     pub has_sidecar: bool,
