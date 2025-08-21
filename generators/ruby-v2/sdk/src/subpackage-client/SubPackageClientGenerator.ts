7c441aa21d198d725ce4aeda4a37cb626ca232da
Deep Singhvi <deep@buildwithfern.com>
2025-08-20 20:29:48 -0400

    fix(ruby): nested subpackage clients (#8919)

--- generators/ruby-v2/sdk/src/subpackage-client/SubPackageClientGenerator.ts
+++ generators/ruby-v2/sdk/src/subpackage-client/SubPackageClientGenerator.ts
@@ -1,6 +1,7 @@
 import { RelativeFilePath } from "@fern-api/path-utils";
 import { ruby } from "@fern-api/ruby-ast";
 import { FileGenerator, RubyFile } from "@fern-api/ruby-base";
+import { FernIr } from "@fern-fern/ir-sdk";
 import { HttpService, Subpackage, SubpackageId } from "@fern-fern/ir-sdk/api";
 import { RawClient } from "../endpoint/http/RawClient";
 import { SdkCustomConfigSchema } from "../SdkCustomConfig";
@@ -30,6 +31,7 @@ export class SubPackageClientGenerator extends FileGenerator<RubyFile, SdkCustom
     }
 
     public doGenerate(): RubyFile {
+        const rootModule = this.context.getRootModule();
         const clientClass = ruby.class_({
             name: CLIENT_CLASS_NAME
         });
@@ -58,6 +60,14 @@ export class SubPackageClientGenerator extends FileGenerator<RubyFile, SdkCustom
                 statements: [ruby.codeblock("@client = client")]
             })
         );
+
+        for (const subpackage of this.getSubpackages()) {
+            // skip subpackages that have no endpoints (recursively)
+            if (!this.context.subPackageHasEndpoints(subpackage)) {
+                continue;
+            }
+            clientClass.addMethod(this.getSubpackageClientGetter(subpackage, rootModule));
+        }
 
         if (this.subpackage.service != null) {
             const service = this.context.getHttpServiceOrThrow(this.subpackage.service);
@@ -106,5 +116,35 @@ export class SubPackageClientGenerator extends FileGenerator<RubyFile, SdkCustom
             }
         }
         return methods;
+    }
+
+    private getSubpackageClientGetter(subpackage: FernIr.Subpackage, rootModule: ruby.Module_): ruby.Method {
+        return new ruby.Method({
+            name: subpackage.name.camelCase.safeName,
+            kind: ruby.MethodKind.Instance,
+            returnType: ruby.Type.class_(
+                ruby.classReference({
+                    name: "Client",
+                    modules: [rootModule.name, subpackage.name.pascalCase.safeName],
+                    fullyQualified: true
+                })
+            ),
+            statements: [
+                ruby.codeblock((writer) => {
+                    writer.writeLine(
+                        `@${subpackage.name.camelCase.safeName} ||= ` +
+                            `${rootModule.name}::` +
+                            `${subpackage.name.pascalCase.safeName}::` +
+                            `Client.new(client: @raw_client)`
+                    );
+                })
+            ]
+        });
+    }
+
+    private getSubpackages(): Subpackage[] {
+        return this.context.ir.rootPackage.subpackages.map((subpackageId) => {
+            return this.context.getSubpackageOrThrow(subpackageId);
+        });
     }
 }
--- seed/ruby-sdk-v2/exhaustive/lib/seed/endpoints/params/client.rb
+++ seed/ruby-sdk-v2/exhaustive/lib/seed/endpoints/params/client.rb
@@ -148,6 +148,31 @@ module Seed
 
           raise _response.body
         end
+
+        # @return [Seed::Endpoints::Client]
+        def endpoints
+          @endpoints ||= Seed::Endpoints::Client.new(client: @raw_client)
+        end
+
+        # @return [Seed::InlinedRequests::Client]
+        def inlinedRequests
+          @inlinedRequests ||= Seed::InlinedRequests::Client.new(client: @raw_client)
+        end
+
+        # @return [Seed::NoAuth::Client]
+        def noAuth
+          @noAuth ||= Seed::NoAuth::Client.new(client: @raw_client)
+        end
+
+        # @return [Seed::NoReqBody::Client]
+        def noReqBody
+          @noReqBody ||= Seed::NoReqBody::Client.new(client: @raw_client)
+        end
+
+        # @return [Seed::ReqWithHeaders::Client]
+        def reqWithHeaders
+          @reqWithHeaders ||= Seed::ReqWithHeaders::Client.new(client: @raw_client)
+        end
       end
     end
   end
