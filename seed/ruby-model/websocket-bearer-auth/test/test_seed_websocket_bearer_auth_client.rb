719bc5e378be50facaaf2f8e77d95172b551835c
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-08-21 17:55:43 -0400

    chore(ruby): update ruby-model seed (#8937)
    Co-authored-by: Swimburger <Swimburger@users.noreply.github.com>

---
+++ seed/ruby-model/websocket-bearer-auth/test/test_seed_websocket_bearer_auth_client.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+require "seed_websocket_bearer_auth_client"
+
+# Basic SeedWebsocketBearerAuthClient tests
+class TestSeedWebsocketBearerAuthClient < Minitest::Test
+  def test_function
+    # SeedWebsocketBearerAuthClient::Client.new
+  end
+end
