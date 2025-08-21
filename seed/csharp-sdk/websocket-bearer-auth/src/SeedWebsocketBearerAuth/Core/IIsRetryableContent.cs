c801da4db2bf3634a46686f7521c3aea8720d7da
Swimburger <Swimburger@users.noreply.github.com>
2025-08-21 22:06:16 +0000

    chore(csharp): update csharp-sdk seed

---
+++ seed/csharp-sdk/websocket-bearer-auth/src/SeedWebsocketBearerAuth/Core/IIsRetryableContent.cs
@@ -0,0 +1,6 @@
+namespace SeedWebsocketBearerAuth.Core;
+
+public interface IIsRetryableContent
+{
+    public bool IsRetryable { get; }
+}
---
+++ seed/csharp-sdk/websocket-inferred-auth/src/SeedWebsocketAuth/Realtime/Types/ReceiveSnakeCase.cs
@@ -0,0 +1,31 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SeedWebsocketAuth.Core;
+
+namespace SeedWebsocketAuth;
+
+[Serializable]
+public record ReceiveSnakeCase : IJsonOnDeserialized
+{
+    [JsonExtensionData]
+    private readonly IDictionary<string, JsonElement> _extensionData =
+        new Dictionary<string, JsonElement>();
+
+    [JsonPropertyName("receive_text")]
+    public required string ReceiveText { get; set; }
+
+    [JsonPropertyName("receive_int")]
+    public required int ReceiveInt { get; set; }
+
+    [JsonIgnore]
+    public ReadOnlyAdditionalProperties AdditionalProperties { get; private set; } = new();
+
+    void IJsonOnDeserialized.OnDeserialized() =>
+        AdditionalProperties.CopyFromExtensionData(_extensionData);
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return JsonUtils.Serialize(this);
+    }
+}
