40fea2ab228901e2d8c3235657eac0cf42b3f6e5
Anar Kafkas <anarkafkas@gmail.com>
2025-08-24 20:14:11 +0300

    Render empty enums in one line

--- seed/swift-sdk/simple-fhir/Sources/Requests/Requests.swift
+++ seed/swift-sdk/simple-fhir/Sources/Requests/Requests.swift
@@ -3,5 +3,4 @@ import Foundation
 /// Container for all inline request types used throughout the SDK.
 ///
 /// This enum serves as a namespace to organize request types that are defined inline within endpoint specifications.
-public enum Requests {
-}
+public enum Requests {}
