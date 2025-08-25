3a4456f2a231eb0c8e67781114eb12dfc54ba21b
tstanmay13 <tstanmay13@users.noreply.github.com>
2025-08-25 20:32:15 +0000

    chore(csharp): update csharp-sdk seed

---
+++ seed/csharp-sdk/nullable-optional/src/SeedNullableOptional/Core/IIsRetryableContent.cs
@@ -0,0 +1,6 @@
+namespace SeedNullableOptional.Core;
+
+public interface IIsRetryableContent
+{
+    public bool IsRetryable { get; }
+}
---
+++ seed/csharp-sdk/nullable-optional/src/SeedNullableOptional/Core/Public/SeedNullableOptionalApiException.cs
@@ -0,0 +1,18 @@
+namespace SeedNullableOptional;
+
+/// <summary>
+/// This exception type will be thrown for any non-2XX API responses.
+/// </summary>
+public class SeedNullableOptionalApiException(string message, int statusCode, object body)
+    : SeedNullableOptionalException(message)
+{
+    /// <summary>
+    /// The error code of the response that triggered the exception.
+    /// </summary>
+    public int StatusCode => statusCode;
+
+    /// <summary>
+    /// The body of the response that triggered the exception.
+    /// </summary>
+    public object Body => body;
+}
