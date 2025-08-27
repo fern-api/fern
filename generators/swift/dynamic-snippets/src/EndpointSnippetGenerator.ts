54e6b8ca2021a750b3ab7f2847f300cb496e8b68
Anar Kafkas <anarkafkas@gmail.com>
2025-08-27 18:17:52 +0300

    Start adding logic

--- generators/swift/dynamic-snippets/src/EndpointSnippetGenerator.ts
+++ generators/swift/dynamic-snippets/src/EndpointSnippetGenerator.ts
@@ -1,8 +1,11 @@
-import { Options } from "@fern-api/browser-compatible-base-generator";
+import { Options, Style } from "@fern-api/browser-compatible-base-generator";
 import { FernIr } from "@fern-api/dynamic-ir-sdk";
+import { swift } from "@fern-api/swift-codegen";
 
 import { Config } from "./Config";
-import { DynamicSnippetsGeneratorContext } from "./context/DynamicSnippetsGeneratorContext";
+import { DynamicSnippetsGeneratorContext } from "./context";
+
+const CLIENT_CONST_NAME = "client";
 
 export class EndpointSnippetGenerator {
     private context: DynamicSnippetsGeneratorContext;
@@ -20,9 +23,7 @@ export class EndpointSnippetGenerator {
         request: FernIr.dynamic.EndpointSnippetRequest;
         options: Options;
     }): Promise<string> {
-        const _config = this.getConfig(options);
-        // TODO(kafkas): Implement this
-        return 'print("Hello, world!")';
+        return this.buildCodeBlock({ endpoint, snippetRequest: request, options });
     }
 
     public generateSnippetSync({
@@ -34,9 +35,111 @@ export class EndpointSnippetGenerator {
         request: FernIr.dynamic.EndpointSnippetRequest;
         options: Options;
     }): string {
-        const _config = this.getConfig(options);
-        // TODO(kafkas): Implement this
-        return 'print("Hello, world!")';
+        return this.buildCodeBlock({ endpoint, snippetRequest: request, options });
+    }
+
+    private buildCodeBlock({
+        endpoint,
+        snippetRequest,
+        options
+    }: {
+        endpoint: FernIr.dynamic.Endpoint;
+        snippetRequest: FernIr.dynamic.EndpointSnippetRequest;
+        options: Options;
+    }) {
+        const methodName = "getUsers"; // TODO(kafkas): Implement
+        const fileComponents: swift.FileComponent[] = [
+            swift.Statement.import("Acme"), // TODO(kafkas): Implement
+            swift.LineBreak.single(),
+            this.generateRootClientInitializationSnippet(),
+            swift.LineBreak.single(),
+            swift.Statement.expressionStatement(
+                swift.Expression.try(
+                    swift.Expression.await(
+                        swift.Expression.methodCall({
+                            target: swift.Expression.rawValue(
+                                [
+                                    CLIENT_CONST_NAME
+                                    // TODO(kafkas): Add subpackages
+                                ].join(".")
+                            ),
+                            methodName,
+                            arguments_: [], // TODO(kafkas): Implement
+                            multiline: true
+                        })
+                    )
+                )
+            )
+        ];
+        return fileComponents.map((c) => c.toString()).join("");
+    }
+
+    private generateRootClientInitializationSnippet() {
+        type ParamsByScheme = {
+            header?: {
+                param: swift.FunctionParameter;
+                wireValue: string;
+            };
+            bearer?: {
+                stringParam: swift.FunctionParameter;
+                asyncProviderParam: swift.FunctionParameter;
+            };
+            basic?: {
+                usernameParam: swift.FunctionParameter;
+                passwordParam: swift.FunctionParameter;
+            };
+        };
+
+        const authSchemes: ParamsByScheme = {}; // TODO(kafkas): Implement
+        const clientClassName = "AcmeClient"; // TODO(kafkas): Implement
+
+        const rootClientArgs: swift.FunctionArgument[] = [];
+
+        if (authSchemes.header) {
+            rootClientArgs.push(
+                swift.functionArgument({
+                    label: authSchemes.header.param.argumentLabel,
+                    value: swift.Expression.rawStringValue("YOUR_TOKEN")
+                })
+            );
+        }
+
+        if (authSchemes.bearer) {
+            rootClientArgs.push(
+                swift.functionArgument({
+                    label: authSchemes.bearer.stringParam.argumentLabel,
+                    value: swift.Expression.rawStringValue("YOUR_TOKEN")
+                })
+            );
+        }
+
+        if (authSchemes.basic) {
+            rootClientArgs.push(
+                swift.functionArgument({
+                    label: authSchemes.basic.usernameParam.argumentLabel,
+                    value: swift.Expression.rawStringValue("YOUR_USERNAME")
+                })
+            );
+            rootClientArgs.push(
+                swift.functionArgument({
+                    label: authSchemes.basic.passwordParam.argumentLabel,
+                    value: swift.Expression.rawStringValue("YOUR_PASSWORD")
+                })
+            );
+        }
+
+        return swift.Statement.constantDeclaration({
+            unsafeName: CLIENT_CONST_NAME,
+            value: swift.Expression.classInitialization({
+                unsafeName: clientClassName,
+                arguments_: rootClientArgs,
+                multiline: rootClientArgs.length > 1 ? true : undefined
+            })
+        });
+    }
+
+    private getStyle(options: Options): Style {
+        return options.style ?? this.context.options.style ?? Style.Full;
     }
 
     private getConfig(options: Options): Config {
