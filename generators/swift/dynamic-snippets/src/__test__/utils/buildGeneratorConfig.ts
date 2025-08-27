7829c8ac410540e05059d0eb209786cfbf44e77c
Anar Kafkas <anarkafkas@gmail.com>
2025-08-27 17:18:13 +0300

    Init tests

---
+++ generators/swift/dynamic-snippets/src/__test__/utils/buildGeneratorConfig.ts
@@ -0,0 +1,38 @@
+import { FernGeneratorExec } from "@fern-api/browser-compatible-base-generator";
+import { BaseSwiftCustomConfigSchema } from "@fern-api/swift-codegen";
+
+const DEFAULT_CONFIG: FernGeneratorExec.GeneratorConfig = {
+    dryRun: false,
+    irFilepath: "<placeholder>",
+    output: {
+        path: "<placeholder>",
+        mode: FernGeneratorExec.OutputMode.github({
+            version: "v1.0.0",
+            repoUrl: "https://github.com/acme/acme-swift"
+        })
+    },
+    organization: "acme",
+    workspaceName: "acme",
+    environment: FernGeneratorExec.GeneratorEnvironment.local(),
+    whitelabel: false,
+    writeUnitTests: false,
+    generateOauthClients: false,
+    customConfig: {
+        "inline-file-properties": true,
+        "package-layout": "flat"
+    } as BaseSwiftCustomConfigSchema
+};
+
+export function buildGeneratorConfig({
+    customConfig
+}: {
+    customConfig?: Partial<BaseSwiftCustomConfigSchema>;
+} = {}): FernGeneratorExec.GeneratorConfig {
+    return {
+        ...DEFAULT_CONFIG,
+        customConfig: {
+            ...(DEFAULT_CONFIG.customConfig as BaseSwiftCustomConfigSchema),
+            ...customConfig
+        }
+    };
+}
