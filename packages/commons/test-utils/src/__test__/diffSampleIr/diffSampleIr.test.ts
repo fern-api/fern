1dd5502c31beaa15e0e6d85790f678fa0ee904c1
jsklan <jsklan.development@gmail.com>
2025-08-31 13:54:09 -0400

    new samples

--- packages/commons/test-utils/src/__test__/diffSampleIr/diffSampleIr.test.ts
+++ packages/commons/test-utils/src/__test__/diffSampleIr/diffSampleIr.test.ts
@@ -4,18 +4,21 @@ import { AbsoluteFilePath } from "@fern-api/fs-utils";
 import { IntermediateRepresentationChangeDetector } from "@fern-api/ir-utils";
 import { createSampleIr } from "../../createSampleIr";
 
-describe("diff sample ir - stable versions", () => {
-    const pathToBaseWorkspace = AbsoluteFilePath.of(resolve(__dirname, "stable/base"));
-    const pathToAddedEndpointWorkspace = AbsoluteFilePath.of(resolve(__dirname, "stable/addedEndpoint"));
-    const pathToAddedTypeWorkspace = AbsoluteFilePath.of(resolve(__dirname, "stable/addedType"));
-    const pathToAddedRequiredTypePropertyWorkspace = AbsoluteFilePath.of(
-        resolve(__dirname, "stable/addedRequiredTypeProperty")
-    );
-    const pathToAddedOptionalTypePropertyWorkspace = AbsoluteFilePath.of(
-        resolve(__dirname, "stable/addedOptionalTypeProperty")
-    );
-    const pathToAddedHeaderWorkspace = AbsoluteFilePath.of(resolve(__dirname, "stable/addedHeader"));
+const pathToBaseWorkspace = AbsoluteFilePath.of(resolve(__dirname, "stable/base"));
+const pathToAddedEndpointWorkspace = AbsoluteFilePath.of(resolve(__dirname, "stable/addedEndpoint"));
+const pathToAddedTypeWorkspace = AbsoluteFilePath.of(resolve(__dirname, "stable/addedType"));
+const pathToAddedRequiredTypePropertyWorkspace = AbsoluteFilePath.of(
+    resolve(__dirname, "stable/addedRequiredTypeProperty")
+);
+const pathToAddedOptionalTypePropertyWorkspace = AbsoluteFilePath.of(
+    resolve(__dirname, "stable/addedOptionalTypeProperty")
+);
+const pathToAddedHeaderWorkspace = AbsoluteFilePath.of(resolve(__dirname, "stable/addedHeader"));
+const pathToAddedUnstableEndpointWorkspace = AbsoluteFilePath.of(resolve(__dirname, "unstable/addedUnstableEndpoint"));
+const pathToAddedUnstableTypeWorkspace = AbsoluteFilePath.of(resolve(__dirname, "unstable/addedUnstableType"));
+const pathToServiceUnstableWorkspace = AbsoluteFilePath.of(resolve(__dirname, "unstable/serviceUnstable"));
 
+describe("diff sample ir", () => {
     it("comparing ", async () => {
         const ir = await createSampleIr(pathToBaseWorkspace);
         const ir2 = await createSampleIr(pathToBaseWorkspace);
@@ -119,5 +122,46 @@ describe("diff sample ir - stable versions", () => {
         const changes = await changeDetector.check({ from, to });
         expect(changes.isBreaking).toBe(true);
         expect(changes.bump).toBe("major");
+    });
+
+
+    it("comparing removing endpoint from unstable service", async () => {
+        const from = await createSampleIr(pathToAddedEndpointWorkspace);
+        const to = await createSampleIr(pathToBaseWorkspace);
+        const changeDetector = new IntermediateRepresentationChangeDetector();
+        const changes = await changeDetector.check({ from, to });
+        expect(changes.isBreaking).toBe(false);
+        expect(changes.bump).toBe("minor");
+        expect(changes.errors).toHaveLength(0);
+    });
+
+    it("comparing removed unstable endpoint", async () => {
+        const from = await createSampleIr(pathToAddedUnstableEndpointWorkspace);
+        const to = await createSampleIr(pathToBaseWorkspace);
+        const changeDetector = new IntermediateRepresentationChangeDetector();
+        const changes = await changeDetector.check({ from, to });
+        expect(changes.isBreaking).toBe(false);
+        expect(changes.bump).toBe("minor");
+        expect(changes.errors).toHaveLength(0);
+    });
+
+    it("comparing removed unstable type", async () => {
+        const from = await createSampleIr(pathToAddedUnstableTypeWorkspace);
+        const to = await createSampleIr(pathToBaseWorkspace);
+        const changeDetector = new IntermediateRepresentationChangeDetector();
+        const changes = await changeDetector.check({ from, to });
+        expect(changes.isBreaking).toBe(false);
+        expect(changes.bump).toBe("minor");
+        expect(changes.errors).toHaveLength(0);
+    });
+
+    it("comparing adding endpoint to unstable service", async () => {
+        const from = await createSampleIr(pathToServiceUnstableWorkspace);
+        const to = await createSampleIr(pathToAddedEndpointWorkspace);
+        const changeDetector = new IntermediateRepresentationChangeDetector();
+        const changes = await changeDetector.check({ from, to });
+        expect(changes.isBreaking).toBe(false);
+        expect(changes.bump).toBe("minor");
+        expect(changes.errors).toHaveLength(0);
     });
 });
