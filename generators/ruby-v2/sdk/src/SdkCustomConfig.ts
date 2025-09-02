7f65604a01fd6767a9f1d4877bdfcef0daa0c76b
Chris Mallinson <chris.mallinson@buildwithfern.com>
2025-09-02 17:53:47 -0400

    Example change to test CI

--- generators/ruby-v2/sdk/src/SdkCustomConfig.ts
+++ generators/ruby-v2/sdk/src/SdkCustomConfig.ts
@@ -3,3 +3,4 @@ import { BaseRubyCustomConfigSchema } from "@fern-api/ruby-ast";
 export const SdkCustomConfigSchema: typeof BaseRubyCustomConfigSchema = BaseRubyCustomConfigSchema;
 
 export type SdkCustomConfigSchema = BaseRubyCustomConfigSchema;
+// example change to force a change for CI
