21556498ad000143a647390c416743040ab156a1
Chris Mallinson <chris.mallinson@buildwithfern.com>
2025-09-03 14:34:01 -0400

    Example for CI to run

--- generators/ruby-v2/sdk/src/SdkCustomConfig.ts
+++ generators/ruby-v2/sdk/src/SdkCustomConfig.ts
@@ -3,3 +3,4 @@ import { BaseRubyCustomConfigSchema } from "@fern-api/ruby-ast";
 export const SdkCustomConfigSchema: typeof BaseRubyCustomConfigSchema = BaseRubyCustomConfigSchema;
 
 export type SdkCustomConfigSchema = BaseRubyCustomConfigSchema;
+// example to kick off CI
