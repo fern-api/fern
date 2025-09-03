a7e8f04bb61a0d9cb74ab2fe930a2d94daa7381d
jsklan <100491078+jsklan@users.noreply.github.com>
2025-09-03 10:21:07 -0400

    feat(cli): Fern diff command can consider from and to generator versions (#9151)
    * initial changes
    
    * fix tests
    
    * updated logic to throw on inverted generator version params
    
    * nits
    
    * named type at source instead of in tests
    
    * fix type references
    
    * update syntax with new nullish helpers
    
    * cli version
    
    * update error messages when generator versions aren't valid
    
    * check
    
    * update per pr feedback

--- packages/cli/cli/src/commands/diff/diff.ts
+++ packages/cli/cli/src/commands/diff/diff.ts
@@ -1,3 +1,4 @@
+import { diffSemverOrThrow } from "@fern-api/core-utils";
 import { AbsoluteFilePath, cwd, doesPathExist, resolve } from "@fern-api/fs-utils";
 import { IntermediateRepresentation, serialization } from "@fern-api/ir-sdk";
 import { IntermediateRepresentationChangeDetector } from "@fern-api/ir-utils";
@@ -7,9 +8,15 @@ import semver from "semver";
 
 import { CliContext } from "../../cli-context/CliContext";
 
+export type Bump = "major" | "minor" | "patch";
 export interface Result {
-    bump: "major" | "minor" | "patch";
+    bump: Bump;
     nextVersion?: string;
+    errors: string[];
+}
+
+interface InternalResult {
+    bump?: Bump;
     errors: string[];
 }
 
@@ -17,35 +24,34 @@ export async function diff({
     context,
     from,
     to,
-    fromVersion
+    fromVersion,
+    generatorVersions
 }: {
     context: CliContext;
     from: string;
     to: string;
     fromVersion: string | undefined;
+    generatorVersions: { from: string; to: string } | undefined;
 }): Promise<Result> {
     const detector = new IntermediateRepresentationChangeDetector();
-    const change = await detector.check({
-        from: await readIr({ context, filepath: from, flagName: "from" }),
-        to: await readIr({ context, filepath: to, flagName: "to" })
-    });
-    const errors = change.errors.map((error) => error.message);
+    const irChange = resultFromIRChangeResults(
+        await detector.check({
+            from: await readIr({ context, filepath: from, flagName: "from" }),
+            to: await readIr({ context, filepath: to, flagName: "to" })
+        })
+    );
+    const generatorChange = diffGeneratorVersions(context, generatorVersions);
+    const { bump: bumpOrUndefined, errors } = mergeDiffResults(irChange, generatorChange);
+    const bump = bumpOrUndefined || "patch";
     if (fromVersion == null) {
-        return {
-            bump: change.bump,
-            errors
-        };
+        return { bump, errors };
     }
-    const nextVersion = semver.inc(fromVersion, change.bump);
+    const nextVersion = semver.inc(fromVersion, bump);
     if (!nextVersion) {
         context.failWithoutThrowing(`Invalid current version: ${fromVersion}`);
         throw new FernCliError();
     }
-    return {
-        bump: change.bump,
-        nextVersion,
-        errors
-    };
+    return { bump, nextVersion, errors };
 }
 
 async function readIr({
@@ -69,4 +75,83 @@ async function readIr({
         throw new FernCliError();
     }
     return parsed.value;
+}
+
+function resultFromIRChangeResults(results: IntermediateRepresentationChangeDetector.Result): Result {
+    return {
+        bump: results.bump,
+        errors: results.errors.map((error) => error.message)
+    };
+}
+
+// export for testing
+export function mergeDiffResults(diffA: InternalResult, diffB: InternalResult): InternalResult {
+    return {
+        bump: maxBump(diffA.bump, diffB.bump),
+        errors: [...diffA.errors, ...diffB.errors]
+    };
+}
+
+function maxBump(bumpA: Bump | undefined, bumpB: Bump | undefined): Bump | undefined {
+    if (bumpA === undefined && bumpB === undefined) {
+        return undefined;
+    }
+    if (bumpA === undefined) {
+        return bumpB;
+    }
+    if (bumpB === undefined) {
+        return bumpA;
+    }
+    if (bumpA === "major" || bumpB === "major") {
+        return "major";
+    }
+    if (bumpA === "minor" || bumpB === "minor") {
+        return "minor";
+    }
+    return "patch";
+}
+
+// export for testing
+export function diffGeneratorVersions(
+    context: CliContext,
+    generatorVersions: { from: string; to: string } | undefined
+): InternalResult {
+    if (generatorVersions === undefined) {
+        return {
+            bump: undefined,
+            errors: []
+        };
+    }
+    const { from, to } = generatorVersions;
+    try {
+        const bump = bumpFromDiff(diffSemverOrThrow(from, to));
+
+        let errors: string[] = [];
+        if (bump === "major") {
+            errors.push("Generator version changed by major version.");
+        }
+
+        return {
+            bump,
+            errors
+        };
+    } catch (error) {
+        context.failWithoutThrowing(`Error diffing generator versions ${from} and ${to}: ${error}`);
+        throw new FernCliError();
+    }
+}
+
+function bumpFromDiff(diff: semver.ReleaseType | null): Bump | undefined {
+    if (diff === null) {
+        return undefined;
+    }
+
+    switch (diff) {
+        case "major":
+        case "minor":
+        case "patch":
+            return diff;
+        default:
+            return undefined;
+    }
 }
