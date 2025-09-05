73b9ab92bff3a527053c62eb75a8229f7919ca0a
Chris Mallinson <chris.mallinson@buildwithfern.com>
2025-09-04 20:55:49 -0400

    Fail ruby-v2 for check

--- generators/ruby-v2/sdk/src/SdkCustomConfig.ts
+++ generators/ruby-v2/sdk/src/SdkCustomConfig.ts
@@ -1,5 +1,5 @@
 import { BaseRubyCustomConfigSchema } from "@fern-api/ruby-ast";
 
 export const SdkCustomConfigSchema: typeof BaseRubyCustomConfigSchema = BaseRubyCustomConfigSchema;
-
+// force a failure for CI test
 export type SdkCustomConfigSchema = BaseRubyCustomConfigSchema;
