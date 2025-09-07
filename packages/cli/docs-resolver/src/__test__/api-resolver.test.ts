0fdf4fd1e4b8c88468d4814d4cd4f803a657dd2b
Catherine Deskur <46695336+chdeskur@users.noreply.github.com>
2025-09-07 09:46:49 -0400

    feat(cli): parse changelog tags (#8980)
    * feat(cli): parse changelog tags
    
    * update
    
    * use map
    
    * add version
    
    * format
    
    * use undefined
    
    * Update packages/cli/docs-resolver/src/DocsDefinitionResolver.ts
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * feature tag
    
    * Update packages/cli/docs-resolver/src/DocsDefinitionResolver.ts
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * format
    
    * force re-run
    
    ---------
    
    Co-authored-by: fern-bot <info@buildwithfern.com>
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- packages/cli/docs-resolver/src/__test__/api-resolver.test.ts
+++ packages/cli/docs-resolver/src/__test__/api-resolver.test.ts
@@ -82,6 +82,7 @@ it.skip("converts to api reference node", async () => {
         context,
         new Map(),
         new Map(),
+        new Map(),
         NodeIdGenerator.init(),
         apiWorkspace
     ).get();
