4af9d4fdfc3f1064a3bd9146c348ff942f810f92
Chris Mallinson <chris.mallinson@buildwithfern.com>
2025-09-11 19:25:51 -0400

    force another change

--- generators/swift/sdk/src/cli.ts
+++ generators/swift/sdk/src/cli.ts
@@ -1,7 +1,7 @@
 import { SdkGeneratorCLI } from "./SdkGeneratorCli";
 
 void runCli();
-// Force update to swift and see if PR triggers.
+// Force update to swift and see if PR triggers. moar
 export async function runCli(): Promise<void> {
     const cli = new SdkGeneratorCLI();
     await cli.run();
