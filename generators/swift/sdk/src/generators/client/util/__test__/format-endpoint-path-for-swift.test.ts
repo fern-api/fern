dbaf6eece7f6c2851ff95501347784196204dc79
jsklan <jsklan.development@gmail.com>
2025-09-12 16:37:39 -0400

    Revert "feat(internal): add new snippet language (#9367)"
    This reverts commit 071ea70620142dc6e54a6f3a614a5cceb42bfd00.

--- generators/swift/sdk/src/generators/client/util/__test__/format-endpoint-path-for-swift.test.ts
+++ generators/swift/sdk/src/generators/client/util/__test__/format-endpoint-path-for-swift.test.ts
@@ -3,7 +3,7 @@ import { resolve } from "node:path";
 
 import { AbsoluteFilePath } from "@fern-api/fs-utils";
 import { createSampleIr } from "@fern-api/test-utils";
-import { IntermediateRepresentation } from "@fern-fern/ir-sdk/api";
+
 import { formatEndpointPathForSwift } from "../format-endpoint-path-for-swift";
 
 const pathToTestDefinitions = resolve(__dirname, "../../../../../../../../test-definitions/fern/apis");
@@ -11,11 +11,9 @@ const testDefinitionNames = readdirSync(pathToTestDefinitions, { withFileTypes: 
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name);
 
-async function getIRForTestDefinition(testDefinitionName: string): Promise<IntermediateRepresentation> {
+async function getIRForTestDefinition(testDefinitionName: string) {
     const absolutePathToWorkspace = AbsoluteFilePath.of(resolve(pathToTestDefinitions, testDefinitionName));
-    return await createSampleIr(absolutePathToWorkspace, {
-        version: "v59" // make sure to upgrade this when the IR version is upgraded
-    });
+    return await createSampleIr(absolutePathToWorkspace);
 }
 
 describe.each(testDefinitionNames)("formatEndpointPathForSwift - %s", (testDefinitionName) => {
