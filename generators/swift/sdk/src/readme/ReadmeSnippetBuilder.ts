d40e2838763a971c3b0e377808d069a336c73009
Anar Kafkas <anarkafkas@gmail.com>
2025-09-14 16:31:23 +0300

    Implement additional headers section

--- generators/swift/sdk/src/readme/ReadmeSnippetBuilder.ts
+++ generators/swift/sdk/src/readme/ReadmeSnippetBuilder.ts
@@ -3,13 +3,15 @@ import { SwiftFile } from "@fern-api/swift-base";
 import { swift } from "@fern-api/swift-codegen";
 import { FernGeneratorCli } from "@fern-fern/generator-cli-sdk";
 import { FernGeneratorExec } from "@fern-fern/generator-exec-sdk";
-import { EndpointId, FeatureId } from "@fern-fern/ir-sdk/api";
+import { EndpointId, FeatureId, HttpEndpoint } from "@fern-fern/ir-sdk/api";
 import { SdkGeneratorContext } from "../SdkGeneratorContext";
 
 export class ReadmeSnippetBuilder extends AbstractReadmeSnippetBuilder {
     private static readonly REQUEST_TYPES_FEATURE_ID: FernGeneratorCli.FeatureId = "REQUEST_TYPES";
+    private static readonly ADDITIONAL_HEADERS_FEATURE_ID: FernGeneratorCli.FeatureId = "ADDITIONAL_HEADERS";
 
     private readonly context: SdkGeneratorContext;
+    private readonly endpointsById: Record<string, HttpEndpoint>;
     private readonly endpointSnippetsById: Record<string, FernGeneratorExec.Endpoint>;
 
     public constructor({
@@ -21,9 +23,20 @@ export class ReadmeSnippetBuilder extends AbstractReadmeSnippetBuilder {
     }) {
         super({ endpointSnippets });
         this.context = context;
+        this.endpointsById = this.buildEndpoints();
         this.endpointSnippetsById = Object.fromEntries(
             endpointSnippets.map((snippet) => [snippet.id.identifierOverride, snippet])
         );
+    }
+
+    private buildEndpoints(): Record<EndpointId, HttpEndpoint> {
+        const endpoints: Record<EndpointId, HttpEndpoint> = {};
+        for (const service of Object.values(this.context.ir.services)) {
+            for (const endpoint of service.endpoints) {
+                endpoints[endpoint.id] = endpoint;
+            }
+        }
+        return endpoints;
     }
 
     public override getDefaultEndpointId(): EndpointId {
@@ -34,25 +47,22 @@ export class ReadmeSnippetBuilder extends AbstractReadmeSnippetBuilder {
         const snippets: Record<FernGeneratorCli.FeatureId, string[]> = {};
         snippets[FernGeneratorCli.StructuredFeatureId.Usage] = this.buildUsageSnippets();
         snippets[ReadmeSnippetBuilder.REQUEST_TYPES_FEATURE_ID] = this.buildRequestTypesSnippets();
+        snippets[ReadmeSnippetBuilder.ADDITIONAL_HEADERS_FEATURE_ID] = this.buildAdditionalHeadersSnippets();
         return snippets;
     }
 
     private buildUsageSnippets(): string[] {
         const snippets: string[] = [];
         const usageEndpointIds = this.getEndpointIdsForFeature(FernGeneratorCli.StructuredFeatureId.Usage);
-        if (usageEndpointIds != null) {
-            usageEndpointIds.forEach((endpointId) => {
-                const snippet = this.getUsageSnippetForEndpoint(endpointId);
-                if (snippet?.type === "typescript") {
-                    snippets.push(snippet.client);
-                }
-            });
-        } else {
-            const snippet = this.getUsageSnippetForEndpoint(this.getDefaultEndpointId());
+
+        usageEndpointIds.forEach((endpointId) => {
+            const snippet = this.getUsageSnippetForEndpoint(endpointId);
+            // Snippets are marked as 'typescript' for compatibility with FernGeneratorExec, which will be deprecated.
             if (snippet?.type === "typescript") {
                 snippets.push(snippet.client);
             }
-        }
+        });
+
         return snippets;
     }
 
@@ -82,11 +92,90 @@ export class ReadmeSnippetBuilder extends AbstractReadmeSnippetBuilder {
         return [content];
     }
 
+    private buildAdditionalHeadersSnippets(): string[] {
+        return this.getEndpointIdsForFeature(ReadmeSnippetBuilder.ADDITIONAL_HEADERS_FEATURE_ID).map((endpointId) => {
+            const endpoint = this.endpointsById[endpointId];
+            if (endpoint == null) {
+                throw new Error(`Internal error; missing endpoint ${endpointId}`);
+            }
+            return SwiftFile.getRawContents([
+                swift.Statement.expressionStatement(
+                    swift.Expression.try(
+                        swift.Expression.await(
+                            swift.Expression.methodCall({
+                                target: swift.Expression.reference("client"),
+                                methodName: this.getMethodName(endpoint),
+                                arguments_: [
+                                    swift.functionArgument({ value: swift.Expression.rawValue("...") }),
+                                    swift.functionArgument({
+                                        label: "requestOptions",
+                                        value: swift.Expression.contextualMethodCall({
+                                            methodName: "init",
+                                            arguments_: [
+                                                swift.functionArgument({
+                                                    label: "additionalHeaders",
+                                                    value: swift.Expression.dictionaryLiteral({
+                                                        entries: [
+                                                            [
+                                                                swift.Expression.stringLiteral("X-Custom-Header"),
+                                                                swift.Expression.stringLiteral("custom value")
+                                                            ]
+                                                        ],
+                                                        multiline: true
+                                                    })
+                                                })
+                                            ],
+                                            multiline: true
+                                        })
+                                    })
+                                ]
+                            })
+                        )
+                    )
+                )
+            ]);
+        });
+    }
+
+    private getMethodName(endpoint: HttpEndpoint): string {
+        const packageOrSubpackage = this.getPackageOrSubpackageForEndpoint(endpoint);
+        if (packageOrSubpackage == null) {
+            throw new Error(`Internal error; missing package or subpackage for endpoint ${endpoint.id}`);
+        }
+        return [
+            ...packageOrSubpackage.fernFilepath.allParts.map((p) => p.camelCase.unsafeName),
+            endpoint.name.camelCase.unsafeName
+        ].join(".");
+    }
+
+    private getPackageOrSubpackageForEndpoint(endpoint: HttpEndpoint) {
+        const rootPackageServiceId = this.context.ir.rootPackage.service;
+        if (rootPackageServiceId) {
+            const rootPackageService = this.context.getHttpServiceOrThrow(rootPackageServiceId);
+            if (rootPackageService.endpoints.some((e) => e.id === endpoint.id)) {
+                return this.context.ir.rootPackage;
+            }
+        }
+        for (const subpackage of Object.values(this.context.ir.subpackages)) {
+            if (typeof subpackage.service === "string") {
+                const service = this.context.getHttpServiceOrThrow(subpackage.service);
+                if (service.endpoints.some((e) => e.id === endpoint.id)) {
+                    return subpackage;
+                }
+            }
+        }
+        return null;
+    }
+
     private getUsageSnippetForEndpoint(endpointId: string) {
         return this.endpointSnippetsById[endpointId]?.snippet;
     }
 
-    private getEndpointIdsForFeature(featureId: FeatureId): EndpointId[] | undefined {
-        return this.context.ir.readmeConfig?.features?.[this.getFeatureKey(featureId)];
+    private getEndpointIdsForFeature(featureId: FeatureId): EndpointId[] {
+        const endpointIds = this.context.ir.readmeConfig?.features?.[this.getFeatureKey(featureId)];
+        if (endpointIds == null) {
+            return [this.getDefaultEndpointId()];
+        }
+        return endpointIds;
     }
 }
