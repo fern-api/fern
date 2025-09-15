65e8cfc0b5d0753c8e49fb52810735ce26da5eeb
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-09-15 14:05:06 -0700

    chore(java): update java-sdk seed (#9431)
    Co-authored-by: tstanmay13 <tstanmay13@users.noreply.github.com>

---
+++ seed/java-sdk/property-access/src/main/java/com/snippets/Example0.java
@@ -0,0 +1,37 @@
+package com.snippets;
+
+import com.seed.propertyAccess.SeedPropertyAccessClient;
+import com.seed.propertyAccess.types.User;
+import com.seed.propertyAccess.types.UserProfile;
+import com.seed.propertyAccess.types.UserProfileVerification;
+
+public class Example0 {
+    public static void main(String[] args) {
+        SeedPropertyAccessClient client = SeedPropertyAccessClient
+            .builder()
+            .url("https://api.fern.com")
+            .build();
+
+        client.createUser(
+            User
+                .builder()
+                .id("id")
+                .email("email")
+                .password("password")
+                .profile(
+                    UserProfile
+                        .builder()
+                        .name("name")
+                        .verification(
+                            UserProfileVerification
+                                .builder()
+                                .verified("verified")
+                                .build()
+                        )
+                        .ssn("ssn")
+                        .build()
+                )
+                .build()
+        );
+    }
+}
