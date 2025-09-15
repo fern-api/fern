66df0b72c4e3be448d6d4711da7ea52d14299f8e
musicpulpite <musicpulpite@gmail.com>
2025-09-15 12:49:01 -0400

    fixes to include error decder and error decoder test

--- generators/go-v2/base/src/AsIs.ts
+++ generators/go-v2/base/src/AsIs.ts
@@ -6,5 +6,6 @@ export enum AsIsFiles {
     Time = "internal/time.go_",
     MainTest = "test/main_test.go_",
     ErrorCodes = "internal/error_codes.go_",
-    ErrorDecoder = "internal/error_decoder.go_"
+    ErrorDecoder = "internal/error_decoder.go_",
+    ErrorDecoderTest = "internal/error_decoder_test.go_"
 }
