0af2db8d900d9bdc0d7308bfb6a7329c9dbfbb03
tstanmay13 <tstanmay13@users.noreply.github.com>
2025-09-16 18:51:17 +0000

    chore(php): update php-sdk seed

--- seed/php-sdk/basic-auth/src/dynamic-snippets/example6/snippet.php
+++ seed/php-sdk/basic-auth/src/dynamic-snippets/example6/snippet.php
@@ -5,8 +5,14 @@ namespace Example;
 use Seed\SeedClient;
 
 $client = new SeedClient(
+    username: '<username>',
+    password: '<password>',
     options: [
         'baseUrl' => 'https://api.fern.com',
     ],
 );
-$client->nestedNoAuth->api->getSomething();
+$client->basicAuth->postWithBasicAuth(
+    [
+        'key' => "value",
+    ],
+);
--- seed/php-sdk/oauth-client-credentials/src/dynamic-snippets/example3/snippet.php
+++ seed/php-sdk/oauth-client-credentials/src/dynamic-snippets/example3/snippet.php
@@ -3,10 +3,20 @@
 namespace Example;
 
 use Seed\SeedClient;
+use Seed\Auth\Requests\RefreshTokenRequest;
 
 $client = new SeedClient(
     options: [
         'baseUrl' => 'https://api.fern.com',
     ],
 );
-$client->nested->api->getSomething();
+$client->auth->refreshToken(
+    new RefreshTokenRequest([
+        'clientId' => 'client_id',
+        'clientSecret' => 'client_secret',
+        'refreshToken' => 'refresh_token',
+        'audience' => 'https://api.example.com',
+        'grantType' => 'refresh_token',
+        'scope' => 'scope',
+    ]),
+);
