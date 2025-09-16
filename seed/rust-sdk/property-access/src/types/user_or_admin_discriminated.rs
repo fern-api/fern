fa7270b419e944c04e308a95fc80fe7d0c7644dd
musicpulpite <musicpulpite@users.noreply.github.com>
2025-09-16 18:37:45 +0000

    chore(rust): update rust-sdk seed

---
+++ seed/rust-sdk/property-access/src/types/user_or_admin_discriminated.rs
@@ -0,0 +1,40 @@
+use crate::admin::Admin;
+use crate::user::User;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(tag = "type")]
+pub enum UserOrAdminDiscriminated {
+    User {
+        #[serde(flatten)]
+        data: User,
+        normal: String,
+        foo: Foo,
+    },
+
+    Admin {
+        admin: Admin,
+        normal: String,
+        foo: Foo,
+    },
+
+    Empty,
+}
+
+impl UserOrAdminDiscriminated {
+    pub fn get_normal(&self) -> &String {
+        match self {
+            Self::User { normal, .. } => normal,
+            Self::Admin { normal, .. } => normal,
+            Self::Empty { normal, .. } => normal,
+        }
+    }
+
+    pub fn get_foo(&self) -> &Foo {
+        match self {
+            Self::User { foo, .. } => foo,
+            Self::Admin { foo, .. } => foo,
+            Self::Empty { foo, .. } => foo,
+        }
+    }
+}
