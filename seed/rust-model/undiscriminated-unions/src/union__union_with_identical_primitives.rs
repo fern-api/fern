b2b600671e083640d0e6b9c7e19e46ef7690163d
fern-support <fern-support@users.noreply.github.com>
2025-09-16 18:55:47 +0000

    chore(rust): update rust-model seed

---
+++ seed/rust-model/undiscriminated-unions/src/union__union_with_identical_primitives.rs
@@ -0,0 +1,69 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(untagged)]
+pub enum UnionWithIdenticalPrimitives {
+        Integer(i32),
+
+        Double(f64),
+
+        String(String),
+}
+
+impl UnionWithIdenticalPrimitives {
+    pub fn is_integer(&self) -> bool {
+        matches!(self, Self::Integer(_))
+    }
+
+    pub fn is_double(&self) -> bool {
+        matches!(self, Self::Double(_))
+    }
+
+    pub fn is_string(&self) -> bool {
+        matches!(self, Self::String(_))
+    }
+
+
+    pub fn as_integer(&self) -> Option<&i32> {
+        match self {
+                    Self::Integer(value) => Some(value),
+                    _ => None,
+                }
+    }
+
+    pub fn into_integer(self) -> Option<i32> {
+        match self {
+                    Self::Integer(value) => Some(value),
+                    _ => None,
+                }
+    }
+
+    pub fn as_double(&self) -> Option<&f64> {
+        match self {
+                    Self::Double(value) => Some(value),
+                    _ => None,
+                }
+    }
+
+    pub fn into_double(self) -> Option<f64> {
+        match self {
+                    Self::Double(value) => Some(value),
+                    _ => None,
+                }
+    }
+
+    pub fn as_string(&self) -> Option<&String> {
+        match self {
+                    Self::String(value) => Some(value),
+                    _ => None,
+                }
+    }
+
+    pub fn into_string(self) -> Option<String> {
+        match self {
+                    Self::String(value) => Some(value),
+                    _ => None,
+                }
+    }
+
+}
