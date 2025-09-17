4a0cbb1c0471fe8907b09a42144a9d534c5fa50a
Tanmay Singh <tstanmay13@gmail.com>
2025-09-17 14:55:06 -0400

    fix import order

--- generators/java-v2/sdk/src/SdkGeneratorCli.ts
+++ generators/java-v2/sdk/src/SdkGeneratorCli.ts
@@ -8,9 +8,9 @@ import * as FernGeneratorExecSerializers from "@fern-fern/generator-exec-sdk/ser
 import { IntermediateRepresentation } from "@fern-fern/ir-sdk/api";
 import { writeFile } from "fs/promises";
 import { buildReference } from "./reference/buildReference";
-import { SdkContractTestGenerator } from "./sdk-contract-tests/SdkContractTestGenerator";
 import { SdkCustomConfigSchema } from "./SdkCustomConfig";
 import { SdkGeneratorContext } from "./SdkGeneratorContext";
+import { SdkContractTestGenerator } from "./sdk-contract-tests/SdkContractTestGenerator";
 import { convertDynamicEndpointSnippetRequest } from "./utils/convertEndpointSnippetRequest";
 import { convertIr } from "./utils/convertIr";
 
