a65f9abac0df29649613d1e8758b07ba95d5e281
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2025-09-17 18:11:38 -0400

    removed temporary debugging logs

--- packages/cli/api-importers/openapi/openapi-ir-parser/src/openapi/v3/extensions/getFernRetriesExtension.ts
+++ packages/cli/api-importers/openapi/openapi-ir-parser/src/openapi/v3/extensions/getFernRetriesExtension.ts
@@ -12,28 +12,15 @@ interface RetriesFernExtensionSchema {
 }
 
 export function getFernRetriesExtension(operation: OpenAPIV3.OperationObject): finalIr.RetriesConfiguration | undefined {
-    // Add logging for when we check for the extension
-    // eslint-disable-next-line no-console
-    console.debug("[getFernRetriesExtension] Checking for Fern retries extension on operation:", operation.operationId);
-
     const retriesExtension = getExtension<RetriesFernExtensionSchema>(operation, FernOpenAPIExtension.RETRIES);
 
-    // eslint-disable-next-line no-console
-    console.debug("[getFernRetriesExtension] Retrieved retries extension:", retriesExtension);
-
     if (retriesExtension == null) {
-        // eslint-disable-next-line no-console
-        console.debug("[getFernRetriesExtension] No retries extension found.");
         return undefined;
     }
     if (retriesExtension.disabled === true) {
-        // eslint-disable-next-line no-console
-        console.debug("[getFernRetriesExtension] Retries are disabled for this operation.");
         return finalIr.RetriesConfiguration.disabled();
     }
     // If you want to support more fields, map them here.
     // For now, only handle 'disabled'
-    // eslint-disable-next-line no-console
-    console.debug("[getFernRetriesExtension] Retries extension present but not handled (fields other than 'disabled').");
     return undefined;
 }
