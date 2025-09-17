92d17a5d371f6849ab4077a0f989adec37b2d16d
jsklan <jsklan.development@gmail.com>
2025-09-17 17:54:04 -0400

    nit

--- generators/java-v2/ast/src/custom-config/CustomReadmeSectionSchema.ts
+++ generators/java-v2/ast/src/custom-config/CustomReadmeSectionSchema.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 
-// The full set of configuration options supported by the TypeScript SDK generator.
 export const CustomReadmeSectionSchema = z.strictObject({
     title: z.string(),
     content: z.string()
