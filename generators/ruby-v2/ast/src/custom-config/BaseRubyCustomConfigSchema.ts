8ac7bf022004a975b356713500e819684b7ca471
jsklan <jsklan.development@gmail.com>
2025-09-18 15:11:28 -0400

    format

--- generators/ruby-v2/ast/src/custom-config/BaseRubyCustomConfigSchema.ts
+++ generators/ruby-v2/ast/src/custom-config/BaseRubyCustomConfigSchema.ts
@@ -1,10 +1,10 @@
-import { CustomReadmeSectionSchema } from "./CustomReadmeSectionSchema";
 import { z } from "zod";
+import { CustomReadmeSectionSchema } from "./CustomReadmeSectionSchema";
 
 export const BaseRubyCustomConfigSchema = z.object({
     module: z.optional(z.string()),
     clientModuleName: z.optional(z.string()),
-    customReadmeSections: z.optional(z.array(CustomReadmeSectionSchema)),
+    customReadmeSections: z.optional(z.array(CustomReadmeSectionSchema))
 });
 
 export type BaseRubyCustomConfigSchema = z.infer<typeof BaseRubyCustomConfigSchema>;
