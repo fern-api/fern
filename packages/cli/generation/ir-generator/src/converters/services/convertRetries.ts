955a39ecf7a1fb4402ec62e83cbf9a826be48396
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2025-09-18 11:29:52 -0400

    IR generation: separated out retry conversion to a convertRetries() function. pt 2

---
+++ packages/cli/generation/ir-generator/src/converters/services/convertRetries.ts
@@ -0,0 +1,18 @@
+import { RawSchemas } from "@fern-api/fern-definition-schema";
+import { RetriesConfiguration } from "@fern-api/ir-sdk";
+
+export function convertRetries({
+    endpointSchema
+}: {
+    endpointSchema: RawSchemas.HttpEndpointSchema;
+}): RetriesConfiguration | undefined {
+    if (!endpointSchema.retries) {
+        return undefined;
+    }
+
+    if (endpointSchema.retries.disabled === true) {
+        return RetriesConfiguration.disabled();
+    }
+
+    return undefined;
+}
