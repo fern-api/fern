c15c139d159a1598192f2be4689bf5ee184d1755
Garrett Serack <garretts@microsoft.com>
2025-09-26 13:11:33 -0700

    websocket fixture regenerated

---
+++ seed/csharp-sdk/websocket/with-websockets/src/SeedWebsocket/SeedWebsocketClient.cs
@@ -0,0 +1,40 @@
+using SeedWebsocket.Core;
+
+namespace SeedWebsocket;
+
+public partial class SeedWebsocketClient
+{
+    private readonly RawClient _client;
+
+    public SeedWebsocketClient(ClientOptions? clientOptions = null)
+    {
+        var defaultHeaders = new Headers(
+            new Dictionary<string, string>()
+            {
+                { "X-Fern-Language", "C#" },
+                { "X-Fern-SDK-Name", "SeedWebsocket" },
+                { "X-Fern-SDK-Version", Version.Current },
+                { "User-Agent", "Fernwebsocket/0.0.1" },
+            }
+        );
+        clientOptions ??= new ClientOptions();
+        foreach (var header in defaultHeaders)
+        {
+            if (!clientOptions.Headers.ContainsKey(header.Key))
+            {
+                clientOptions.Headers[header.Key] = header.Value;
+            }
+        }
+        _client = new RawClient(clientOptions);
+    }
+
+    public RealtimeApi CreateRealtimeApi()
+    {
+        return new RealtimeApi(new RealtimeApi.Options());
+    }
+
+    public RealtimeApi CreateRealtimeApi(RealtimeApi.Options options)
+    {
+        return new RealtimeApi(options);
+    }
+}
