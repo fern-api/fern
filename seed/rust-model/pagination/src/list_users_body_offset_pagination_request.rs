0a1bec4d5f40fc656fa303403b7cd79739b19986
iamnamananand996 <iamnamananand996@users.noreply.github.com>
2025-10-02 19:37:21 +0000

    Automated update of seed files

--- seed/rust-model/pagination/src/list_users_body_offset_pagination_request.rs
+++ seed/rust-model/pagination/src/list_users_body_offset_pagination_request.rs
@@ -3,5 +3,6 @@ pub use crate::prelude::*;
 #[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq, Eq, Hash)]
 pub struct ListUsersBodyOffsetPaginationRequest {
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub pagination: Option<InlineUsersInlineUsersWithPage>,
+    pub pagination: Option<UsersWithPage>,
+}UsersWithPage>,
 }
