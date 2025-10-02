f73e76e58c1bd1b1961fba0f9bfc38b2fff95876
fern-support <fern-support@users.noreply.github.com>
2025-10-02 19:45:46 +0000

    Automated update of seed files

--- seed/rust-model/pagination/src/list_users_body_cursor_pagination_request.rs
+++ seed/rust-model/pagination/src/list_users_body_cursor_pagination_request.rs
@@ -3,5 +3,6 @@ pub use crate::prelude::*;
 #[derive(Debug, Clone, Serialize, Deserialize, Default, PartialEq, Eq, Hash)]
 pub struct ListUsersBodyCursorPaginationRequest {
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub pagination: Option<InlineUsersInlineUsersWithCursor>,
+    pub pagination: Option<UsersWithCursor>,
+}ersWithCursor>,
 }
