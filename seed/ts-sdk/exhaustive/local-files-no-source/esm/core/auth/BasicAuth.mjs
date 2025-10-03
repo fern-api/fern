b4950655956a3b19a8639b6a0419de8ce446ad26
tjb9dc <tjb9dc@users.noreply.github.com>
2025-10-03 15:04:21 +0000

    Automated update of seed files

--- seed/ts-sdk/exhaustive/local-files-no-source/esm/core/auth/BasicAuth.mjs
+++ seed/ts-sdk/exhaustive/local-files-no-source/esm/core/auth/BasicAuth.mjs
@@ -11,7 +11,8 @@ export const BasicAuth = {
     fromAuthorizationHeader: (header) => {
         const credentials = header.replace(BASIC_AUTH_HEADER_PREFIX, "");
         const decoded = base64Decode(credentials);
-        const [username, password] = decoded.split(":", 2);
+        const [username, ...passwordParts] = decoded.split(":");
+        const password = passwordParts.length > 0 ? passwordParts.join(":") : undefined;
         if (username == null || password == null) {
             throw new Error("Invalid basic auth");
         }
