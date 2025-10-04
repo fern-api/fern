f093ef667e0d2f232dd8541828913feab4d94763
kafkas <kafkas@users.noreply.github.com>
2025-10-04 13:49:06 +0000

    Automated update of seed files

--- seed/swift-sdk/oauth-client-credentials/Sources/Resources/NestedNoAuth/NestedNoAuthClient.swift
+++ seed/swift-sdk/oauth-client-credentials/Sources/Resources/NestedNoAuth/NestedNoAuthClient.swift
@@ -4,7 +4,7 @@ public final class NestedNoAuthClient: Sendable {
     public let api: ApiClient
     private let httpClient: HTTPClient
 
-    public init(config: ClientConfig) {
+    init(config: ClientConfig) {
         self.api = ApiClient(config: config)
         self.httpClient = HTTPClient(config: config)
     }
--- seed/swift-sdk/pagination/no-custom-config/Sources/Resources/InlineUsers/InlineUsersClient.swift
+++ seed/swift-sdk/pagination/no-custom-config/Sources/Resources/InlineUsers/InlineUsersClient.swift
@@ -4,7 +4,7 @@ public final class InlineUsersClient: Sendable {
     public let inlineUsers: InlineUsersInlineUsersClient
     private let httpClient: HTTPClient
 
-    public init(config: ClientConfig) {
+    init(config: ClientConfig) {
         self.inlineUsers = InlineUsersInlineUsersClient(config: config)
         self.httpClient = HTTPClient(config: config)
     }
