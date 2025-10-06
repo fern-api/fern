385a08078d806121031e6ff38e165e2535b6901e
patrick thornton <patrickthornton@g.harvard.edu>
2025-10-06 10:16:55 -0400

    update github workflows

--- generators/python/setup-python.sh
+++ generators/python/setup-python.sh
@@ -4,5 +4,4 @@ export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv shell 3.9
-pip install poetry
-poetry env use 3.9
+pip install uv
