0450e2e350d79d3027aca6d80b46eb0fa27de5b1
Anar Kafkas <anarkafkas@gmail.com>
2025-10-13 22:39:09 -0400

    Update snapshots

--- seed/swift-sdk/audiences/Sources/Resources/Foo/FooClient.swift
+++ seed/swift-sdk/audiences/Sources/Resources/Foo/FooClient.swift
@@ -12,7 +12,7 @@ public final class FooClient: Sendable {
             method: .post,
             path: "/",
             queryParams: [
-                "optionalString": .unknown(optionalString)
+                "optionalString": optionalString.map { .string($0) }
             ],
             body: request,
             requestOptions: requestOptions,
