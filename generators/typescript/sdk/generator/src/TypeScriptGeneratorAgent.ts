7fc36bec9cd7619cd4c3ae6728ff0aa063968a44
jsklan <jsklan.development@gmail.com>
2025-10-15 15:53:00 -0400

    update ts sdk generator to pass through local config

--- generators/typescript/sdk/generator/src/TypeScriptGeneratorAgent.ts
+++ generators/typescript/sdk/generator/src/TypeScriptGeneratorAgent.ts
@@ -1,4 +1,4 @@
-import { AbstractGeneratorAgent } from "@fern-api/base-generator";
+import { AbstractGeneratorAgent, RawGithubConfig } from "@fern-api/base-generator";
 import { Logger } from "@fern-api/logger";
 import { FernGeneratorCli } from "@fern-fern/generator-cli-sdk";
 import { FernGeneratorExec } from "@fern-fern/generator-exec-sdk";
@@ -60,13 +60,13 @@ export class TypeScriptGeneratorAgent extends AbstractGeneratorAgent<SdkContext>
         };
     }
 
-    public getGitHubConfig(args: AbstractGeneratorAgent.GitHubConfigArgs<SdkContext>): FernGeneratorCli.GitHubConfig {
-        // TODO: get from env
+    public getGitHubConfig(args: AbstractGeneratorAgent.GitHubConfigArgs<SdkContext>): RawGithubConfig {
         return {
-            sourceDirectory: "NONE",
-            uri: "NONE",
-            token: "token",
-            branch: "NONE"
+            sourceDirectory: "fern/output",
+            type: this.publishConfig?.type,
+            uri: this.publishConfig?.type === "github" ? this.publishConfig.uri : undefined,
+            token: this.publishConfig?.type === "github" ? this.publishConfig.token : undefined,
+            mode: this.publishConfig?.type === "github" ? this.publishConfig.mode : undefined
         };
     }
 }
