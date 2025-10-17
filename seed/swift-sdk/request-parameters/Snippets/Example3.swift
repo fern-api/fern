7b3ed155ec8b10a33b4f79d979a8f1e97e39a7a5
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-10-17 16:08:45 -0400

    chore(swift): update swift-sdk seed (#9918)
    Co-authored-by: chrismallinson <chrismallinson@users.noreply.github.com>

--- seed/swift-sdk/request-parameters/Snippets/Example3.swift
+++ seed/swift-sdk/request-parameters/Snippets/Example3.swift
@@ -5,7 +5,9 @@ private func main() async throws {
     let client = RequestParametersClient(baseURL: "https://api.fern.com")
 
     _ = try await client.user.createUsernameOptional(request: .value(CreateUsernameBodyOptionalProperties(
-
+        username: "username",
+        password: "password",
+        name: "test"
     )))
 }
 
