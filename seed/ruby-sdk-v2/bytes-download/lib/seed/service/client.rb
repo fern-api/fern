01e6c2224791c506012542faa086a7efe9e93b29
chrismallinson <chrismallinson@users.noreply.github.com>
2025-10-17 20:00:36 +0000

    chore(ruby): update ruby-sdk seed

--- seed/ruby-sdk-v2/bytes-download/lib/seed/service/client.rb
+++ seed/ruby-sdk-v2/bytes-download/lib/seed/service/client.rb
@@ -9,6 +9,25 @@ module Seed
       end
 
       # @return [untyped]
+      def simple(request_options: {}, **_params)
+        _request = Seed::Internal::JSON::Request.new(
+          base_url: request_options[:base_url],
+          method: "POST",
+          path: "snippet"
+        )
+        begin
+          _response = @client.send(_request)
+        rescue Net::HTTPRequestTimeout
+          raise Seed::Errors::TimeoutError
+        end
+        code = _response.code.to_i
+        return if code.between?(200, 299)
+
+        error_class = Seed::Errors::ResponseError.subclass_for_code(code)
+        raise error_class.new(_response.body, code: code)
+      end
+
+      # @return [untyped]
       def download(request_options: {}, **params)
         _request = Seed::Internal::JSON::Request.new(
           base_url: request_options[:base_url],
