6d02a565b0b9f0477c8b13c8ae0fec351592ab9b
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-10-23 15:40:02 -0400

    chore(rust): update rust-sdk seed (#10039)
    Co-authored-by: iamnamananand996 <iamnamananand996@users.noreply.github.com>

--- seed/rust-sdk/file-upload/dynamic-snippets/example2.rs
+++ seed/rust-sdk/file-upload/dynamic-snippets/example2.rs
@@ -7,13 +7,5 @@ async fn main() {
         ..Default::default()
     };
     let client = FileUploadClient::new(config).expect("Failed to build client");
-    client
-        .service
-        .just_file(
-            &JustFileRequest {
-                file: todo!("Missing file value"),
-            },
-            None,
-        )
-        .await;
+    client.service.simple(None).await;
 }
