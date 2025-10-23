89d2545e9f93f81dfe89fac0cc7d5a30ed40be6e
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-10-23 13:20:17 -0400

    chore(typescript): Simplify seed configurations (#10031)
    * Simplify seed.yml configurations
    
    * update TS seed snapshots

--- seed/ts-sdk/exhaustive/custom-package-json/src/api/resources/endpoints/resources/object/client/index.ts
+++
@@ -1,6 +0,0 @@
-export * as getAndReturnNestedWithOptionalField from "./getAndReturnNestedWithOptionalField.js";
-export * as getAndReturnNestedWithRequiredField from "./getAndReturnNestedWithRequiredField.js";
-export * as getAndReturnNestedWithRequiredFieldAsList from "./getAndReturnNestedWithRequiredFieldAsList.js";
-export * as getAndReturnWithMapOfMap from "./getAndReturnWithMapOfMap.js";
-export * as getAndReturnWithOptionalField from "./getAndReturnWithOptionalField.js";
-export * as getAndReturnWithRequiredField from "./getAndReturnWithRequiredField.js";
--- seed/ts-sdk/imdb/noScripts/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * as SeedApi from "./api/index.js";
-export { SeedApiError, SeedApiTimeoutError } from "./errors/index.js";
-export { type BaseClientOptions, type BaseRequestOptions } from "./BaseClient.js";
-export { SeedApiClient } from "./Client.js";
