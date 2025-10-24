9dc342f16d0f526a901024ffaebdb906b47e3ff2
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2025-10-24 23:44:10 +0300

    fix(swift): remove `base-generator` dependency from `codegen` (#10077)
    * Import AbstractAstNode and AbstractWriter from @fern-api/browser-compatible-base-generator
    
    * Remove @fern-api/base-generator from deps
    
    * Remove external config

--- generators/swift/codegen/src/ast/core/Writer.ts
+++ generators/swift/codegen/src/ast/core/Writer.ts
@@ -1,4 +1,4 @@
-import { AbstractWriter } from "@fern-api/base-generator";
+import { AbstractWriter } from "@fern-api/browser-compatible-base-generator";
 
 export class Writer extends AbstractWriter {
     public toString(): string {
