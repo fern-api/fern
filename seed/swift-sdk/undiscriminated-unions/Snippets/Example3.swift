f5e3f211601dce4c045b0e4715e89ff546867fb7
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-10-29 16:19:22 -0400

    chore(swift): update swift-sdk seed (#10179)
    Co-authored-by: pgragg <pgragg@users.noreply.github.com>

--- seed/swift-sdk/undiscriminated-unions/Snippets/Example3.swift
+++ seed/swift-sdk/undiscriminated-unions/Snippets/Example3.swift
@@ -4,7 +4,7 @@ import UndiscriminatedUnions
 private func main() async throws {
     let client = UndiscriminatedUnionsClient(baseURL: "https://api.fern.com")
 
-    _ = try await client.union.updateMetadata(request: MetadataUnion.optionalStringToJsonDictionary(
+    _ = try await client.union.updateMetadata(request: MetadataUnion.optionalMetadata(
         [
             "string": .object([
                 "key": .string("value")
