c2c34fbdf13afda1c88b2ed7a541a10062cf6d10
Tanmay Singh <tstanmay13@gmail.com>
2025-10-29 19:35:55 -0400

    fix(java): add boolean and long to abstract writer primtive types (#10156)

--- generators/java/sdk/src/main/java/com/fern/java/client/generators/endpoint/AbstractEndpointWriter.java
+++ generators/java/sdk/src/main/java/com/fern/java/client/generators/endpoint/AbstractEndpointWriter.java
@@ -128,6 +128,10 @@ public abstract class AbstractEndpointWriter {
             return CodeBlock.of("$T.toString($L)", Double.class, reference);
         } else if (typeName.equals(TypeName.INT)) {
             return CodeBlock.of("$T.toString($L)", Integer.class, reference);
+        } else if (typeName.equals(TypeName.LONG)) {
+            return CodeBlock.of("$T.toString($L)", Long.class, reference);
+        } else if (typeName.equals(TypeName.BOOLEAN)) {
+            return CodeBlock.of("$T.toString($L)", Boolean.class, reference);
         } else {
             return CodeBlock.of("$L.toString()", reference);
         }
