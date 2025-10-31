024d688a73bd069aba626fc088042381a0349358
Garrett Serack <garrett@serack.org>
2025-10-31 14:23:18 -0700

    csharp-model changes

--- generators/csharp/model/src/enum/EnumGenerator.ts
+++ generators/csharp/model/src/enum/EnumGenerator.ts
@@ -16,26 +16,26 @@ export class EnumGenerator extends FileGenerator<CSharpFile, ModelCustomConfigSc
         private readonly enumDeclaration: EnumTypeDeclaration
     ) {
         super(context);
-        this.classReference = this.context.csharpTypeMapper.convertToClassReference(this.typeDeclaration.name);
+        this.classReference = this.context.csharpTypeMapper.convertToClassReference(this.typeDeclaration);
     }
 
     protected doGenerate(): CSharpFile {
-        const serializerAnnotation = this.csharp.annotation({
-            reference: this.csharp.System.Text.Json.Serialization.JsonConverter(),
-            argument: this.csharp.codeblock((writer) => {
-                writer.write("typeof(");
-                writer.writeNode(this.csharp.classReference(this.context.getEnumSerializerClassReference()));
-                writer.write("<");
-                writer.writeNode(this.classReference);
-                writer.write(">");
-                writer.write(")");
-            })
-        });
-
         const enum_ = this.csharp.enum_({
             ...this.classReference,
             access: ast.Access.Public,
-            annotations: [serializerAnnotation]
+            annotations: [
+                this.csharp.annotation({
+                    reference: this.extern.System.Text.Json.Serialization.JsonConverter(),
+                    argument: this.csharp.codeblock((writer) => {
+                        writer.write("typeof(");
+                        writer.writeNode(this.csharp.classReferenceInternal(this.types.EnumSerializer));
+                        writer.write("<");
+                        writer.writeNode(this.classReference);
+                        writer.write(">");
+                        writer.write(")");
+                    })
+                })
+            ]
         });
 
         this.enumDeclaration.values.forEach((member) =>
@@ -47,15 +47,12 @@ export class EnumGenerator extends FileGenerator<CSharpFile, ModelCustomConfigSc
             directory: this.context.getDirectoryForTypeId(this.typeDeclaration.name.typeId),
             allNamespaceSegments: this.context.getAllNamespaceSegments(),
             allTypeClassReferences: this.context.getAllTypeClassReferences(),
-            namespace: this.context.getNamespace(),
-            customConfig: this.context.customConfig
+            namespace: this.namespaces.root,
+            generation: this.generation
         });
     }
 
     protected getFilepath(): RelativeFilePath {
-        return join(
-            this.context.project.filepaths.getSourceFileDirectory(),
-            RelativeFilePath.of(this.classReference.name + ".cs")
-        );
+        return join(this.constants.folders.sourceFiles, RelativeFilePath.of(`${this.classReference.name}.cs`));
     }
 }
