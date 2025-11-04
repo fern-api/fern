c2b5aeb4452ec9497a62f24125a6a76e2d3a89bc
jsklan <jsklan.development@gmail.com>
2025-11-04 15:54:46 -0500

    interim

---
+++ packages/commons/github/src/constants.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_REMOTE = "github.com";
+export const README_FILEPATH = "README.md";
+export const DEFAULT_REMOTE_NAME = "origin";
+export const FERNIGNORE: string = ".fernignore";
+export const GITIGNORE: string = ".gitignore";
+export const GIT_DIR: string = ".git";
