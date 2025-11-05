41ea0f5bd09d0d0e00d390e13effa6ec918062e6
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-11-05 11:09:33 -0500

    chore(php): update php-sdk seed (#10339)
    Co-authored-by: dsinghvi <dsinghvi@users.noreply.github.com>

---
+++ seed/php-sdk/path-parameters/no-custom-config/src/dynamic-snippets/example7/snippet.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Example;
+
+use Seed\SeedClient;
+
+$client = new SeedClient(
+    options: [
+        'baseUrl' => 'https://api.fern.com',
+    ],
+);
+$client->user->getUserMetadata(
+    'tenant_id',
+    'user_id',
+    1,
+);
