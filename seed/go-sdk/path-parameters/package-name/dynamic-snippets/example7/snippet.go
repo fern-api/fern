1a769558d23bc726b8096925e2868dd7cc8b2fae
Fern Support <126544928+fern-support@users.noreply.github.com>
2025-11-05 11:10:54 -0500

    chore(go): update go-sdk seed (#10337)
    Co-authored-by: dsinghvi <dsinghvi@users.noreply.github.com>

---
+++ seed/go-sdk/path-parameters/package-name/dynamic-snippets/example7/snippet.go
@@ -0,0 +1,21 @@
+package example
+
+import (
+    client "github.com/fern-api/path-parameters-go/client"
+    option "github.com/fern-api/path-parameters-go/option"
+    context "context"
+)
+
+func do() {
+    client := client.NewClient(
+        option.WithBaseURL(
+            "https://api.fern.com",
+        ),
+    )
+    client.User.GetUserMetadata(
+        context.TODO(),
+        "tenant_id",
+        "user_id",
+        1,
+    )
+}
