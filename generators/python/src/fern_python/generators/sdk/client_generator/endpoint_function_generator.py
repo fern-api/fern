1eacae88170b219a1a1340e61c066b6a5704a99d
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2025-11-06 13:21:27 -0500

    updated type narrowing in codegen and type hinting in outputted code

--- generators/python/src/fern_python/generators/sdk/client_generator/endpoint_function_generator.py
+++ generators/python/src/fern_python/generators/sdk/client_generator/endpoint_function_generator.py
@@ -613,23 +613,22 @@ class EndpointFunctionGenerator:
             if named_parameters and len(named_parameters) > 0:
                 last_param = named_parameters[-1]
                 request_options_variable_name = last_param.name
+            
+            if endpoint.retries is not None:
+                if isinstance(endpoint.retries, ir_types.RetriesDisabledSchema) and endpoint.retries.disabled:
+                    overridden_request_options_var = "_request_options_with_retries_disabled"
 
-            # If retries are disabled, enforce max_retries=0 by overriding request_options
-            if endpoint.retries is not None and hasattr(endpoint.retries, "disabled") and endpoint.retries.disabled:
-                overridden_request_options_var = "_request_options_with_retries_disabled"
+                    def write_override_logic(writer: AST.NodeWriter) -> None:
+                        writer.write(f"{{**{request_options_variable_name}, \"max_retries\": 0}} if {request_options_variable_name} is not None else {{\"max_retries\": 0}}")
 
-                def write_override_logic(writer: AST.NodeWriter) -> None:
-                    writer.write(f'({{**{request_options_variable_name}, "max_retries": 0}} ')
-                    writer.write(f"if {request_options_variable_name} is not None ")
-                    writer.write('else {"max_retries": 0})')
-
-                writer.write_node(
-                    AST.VariableDeclaration(
-                        name=overridden_request_options_var,
-                        initializer=AST.Expression(AST.CodeWriter(write_override_logic)),
+                    writer.write_node(
+                        AST.VariableDeclaration(
+                            name=overridden_request_options_var,
+                            type_hint=AST.TypeHint.optional(AST.TypeHint(self._context.core_utilities.get_reference_to_request_options())),
+                            initializer=AST.Expression(AST.CodeWriter(write_override_logic)),
+                        )
                     )
-                )
-                request_options_variable_name = overridden_request_options_var
+                    request_options_variable_name = overridden_request_options_var
 
             def get_httpx_request(
                 is_streaming: bool, response_code_writer: EndpointResponseCodeWriter
