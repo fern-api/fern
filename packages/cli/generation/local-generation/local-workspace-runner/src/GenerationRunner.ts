4bcdbede4a2269711b55f02a31860b5b37da1b37
jsklan <jsklan.development@gmail.com>
2025-11-13 15:48:38 -0500

    fix compile

--- packages/cli/generation/local-generation/local-workspace-runner/src/GenerationRunner.ts
+++ packages/cli/generation/local-generation/local-workspace-runner/src/GenerationRunner.ts
@@ -162,6 +162,7 @@ export class GenerationRunner {
             workspace,
             generatorInvocation,
             absolutePathToLocalOutput: generatorInvocation.absolutePathToLocalOutput,
+            absolutePathToLocalPreview: undefined,
             absolutePathToLocalSnippetJSON: generatorInvocation.absolutePathToLocalOutput
                 ? AbsoluteFilePath.of(
                       join(generatorInvocation.absolutePathToLocalOutput, RelativeFilePath.of(SNIPPET_JSON_FILENAME))
