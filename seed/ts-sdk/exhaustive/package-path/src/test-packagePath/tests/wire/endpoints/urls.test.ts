cea0a944d88bf8e7499f3fc937a9c0250f17e8c6
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-11-18 22:24:13 -0500

    seed

--- seed/ts-sdk/exhaustive/package-path/src/test-packagePath/tests/wire/endpoints/urls.test.ts
+++ seed/ts-sdk/exhaustive/package-path/src/test-packagePath/tests/wire/endpoints/urls.test.ts
@@ -6,7 +6,7 @@ import { mockServerPool } from "../../mock-server/MockServerPool";
 describe("UrlsClient", () => {
     test("withMixedCase", async () => {
         const server = mockServerPool.createServer();
-        const client = new SeedExhaustiveClient({ token: "test", environment: server.baseUrl });
+        const client = new SeedExhaustiveClient({ maxRetries: 0, token: "test", environment: server.baseUrl });
 
         const rawResponseBody = "string";
         server.mockEndpoint().get("/urls/MixedCase").respondWith().statusCode(200).jsonBody(rawResponseBody).build();
@@ -17,7 +17,7 @@ describe("UrlsClient", () => {
 
     test("noEndingSlash", async () => {
         const server = mockServerPool.createServer();
-        const client = new SeedExhaustiveClient({ token: "test", environment: server.baseUrl });
+        const client = new SeedExhaustiveClient({ maxRetries: 0, token: "test", environment: server.baseUrl });
 
         const rawResponseBody = "string";
         server
@@ -34,7 +34,7 @@ describe("UrlsClient", () => {
 
     test("withEndingSlash", async () => {
         const server = mockServerPool.createServer();
-        const client = new SeedExhaustiveClient({ token: "test", environment: server.baseUrl });
+        const client = new SeedExhaustiveClient({ maxRetries: 0, token: "test", environment: server.baseUrl });
 
         const rawResponseBody = "string";
         server
@@ -51,7 +51,7 @@ describe("UrlsClient", () => {
 
     test("withUnderscores", async () => {
         const server = mockServerPool.createServer();
-        const client = new SeedExhaustiveClient({ token: "test", environment: server.baseUrl });
+        const client = new SeedExhaustiveClient({ maxRetries: 0, token: "test", environment: server.baseUrl });
 
         const rawResponseBody = "string";
         server
