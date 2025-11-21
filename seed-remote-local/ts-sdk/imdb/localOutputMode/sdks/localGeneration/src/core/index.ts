d76c6fba50125143318c5a657a8dc83e1df2bb58
jsklan <100491078+jsklan@users.noreply.github.com>
2025-11-21 13:45:18 -0500

    chore(internal): Add php sdk to remote vs local testing (#10775)
    * Update test-remote-local to include php
    
    * remove local output mode snapshots
    
    * remove remote vs local snapshots
    
    * update gitignore to ignore everything except config files in seed-remote-local
    
    * update configuration to handle php case

--- seed-remote-local/ts-sdk/imdb/localOutputMode/sdks/localGeneration/src/core/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./auth/index.js";
-export * from "./base64.js";
-export * from "./fetcher/index.js";
-export * as logging from "./logging/index.js";
-export * from "./runtime/index.js";
-export * as url from "./url/index.js";
