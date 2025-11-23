4f1f3467df1c9467b7a3301d7d3845113513449d
Anar Kafkas <anarkafkas@gmail.com>
2025-11-23 22:02:53 +0300

    Fixes

--- generators/swift/sdk-runner/src/initProject.ts
+++ generators/swift/sdk-runner/src/initProject.ts
@@ -4,13 +4,13 @@ import { createMockTaskContext } from "@fern-api/task-context";
 export async function initProject(
     args: Omit<loadProject.Args, "context" | "cliName" | "cliVersion">
 ): Promise<Project> {
-    const cliName = "swift-sdk-runner"; // TODO: Update
-    const cliVersion = "0.0.1"; // TODO: Update
+    const cliName = "generate-swift-sdk";
+    const cliVersion = "0.0.1"; // TODO: Make dynamic
     const project = await loadProject({
         ...args,
         cliName,
         cliVersion,
-        context: createMockTaskContext() // TODO: Update
+        context: createMockTaskContext()
     });
 
     return project;
