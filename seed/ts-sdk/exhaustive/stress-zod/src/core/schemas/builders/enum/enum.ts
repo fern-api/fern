b7cbe9f662e057aba71607318e816149983f89d5
Brendan Conron <conronb@gmail.com>
2025-12-04 17:59:29 -0500

    removed tests

--- seed/ts-sdk/exhaustive/stress-zod/src/core/schemas/builders/enum/enum.ts
+++
@@ -1,3 +0,0 @@
-import { z } from "zod";
-import { getSchemaUtils } from "../schema-utils/getSchemaUtils.js";
-export const enum_ = <T extends string>(values: T[]) => getSchemaUtils(z.enum(values as [T, ...T[]]));
