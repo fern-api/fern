20f322d0a6849a6b1c643fbeda96080f65e1ee6c
jsklan <jsklan.development@gmail.com>
2025-12-05 12:20:23 -0500

    Update relevant fixtures with no code changes to demonstrate current state

--- seed/ruby-sdk-v2/multi-url-environment/lib/seed/s_3/client.rb
+++ seed/ruby-sdk-v2/multi-url-environment/lib/seed/s_3/client.rb
@@ -27,7 +27,8 @@ module Seed
           base_url: request_options[:base_url] || Seed::Environment::PRODUCTION,
           method: "POST",
           path: "/s3/presigned-url",
-          body: Seed::S3::Types::GetPresignedUrlRequest.new(body_bag).to_h
+          body: Seed::S3::Types::GetPresignedUrlRequest.new(body_bag).to_h,
+          request_options: request_options
         )
         begin
           response = @client.send(request)
