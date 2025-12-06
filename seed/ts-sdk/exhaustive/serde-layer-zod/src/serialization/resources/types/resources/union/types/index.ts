653ae9232b8b764dbc49f255fc3529be0ad56a72
Brendan Conron <conronb@gmail.com>
2025-12-06 13:25:04 -0500

    removed generated seed tests for pr review

--- seed/ts-sdk/exhaustive/serde-layer-zod/src/serialization/resources/types/resources/union/types/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./Animal.js";
-export * from "./Cat.js";
-export * from "./Dog.js";
--- seed/ts-sdk/exhaustive/serde-layer-zurg/tests/mock-server/randomBaseUrl.ts
+++
@@ -1,4 +0,0 @@
-export function randomBaseUrl(): string {
-    const randomString = Math.random().toString(36).substring(2, 15);
-    return `http://${randomString}.localhost`;
-}
