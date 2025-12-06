21150fb0a63180c4e7edeceb5c790c1b1ebf45ef
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-12-05 22:40:29 -0500

    seed

--- seed/ts-sdk/bearer-token-environment-variable/src/auth/BearerAuthProvider.ts
+++ seed/ts-sdk/bearer-token-environment-variable/src/auth/BearerAuthProvider.ts
@@ -4,9 +4,11 @@ import * as core from "../core/index.js";
 import * as errors from "../errors/index.js";
 
 export namespace BearerAuthProvider {
-    export interface Options {
+    export interface AuthOptions {
         apiKey?: core.Supplier<core.BearerToken | undefined>;
     }
+
+    export interface Options extends AuthOptions {}
 }
 
 export class BearerAuthProvider implements core.AuthProvider {
