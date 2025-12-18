64f910258a499de3f86bd39199c87faac45b9864
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2025-12-18 17:53:55 -0500

    seed

--- seed/ts-sdk/endpoint-security-auth/src/auth/index.ts
+++ seed/ts-sdk/endpoint-security-auth/src/auth/index.ts
@@ -1,3 +1,4 @@
 export { BearerAuthProvider } from "./BearerAuthProvider.js";
 export { HeaderAuthProvider } from "./HeaderAuthProvider.js";
 export { OAuthAuthProvider } from "./OAuthAuthProvider.js";
+export { RoutingAuthProvider } from "./RoutingAuthProvider.js";
