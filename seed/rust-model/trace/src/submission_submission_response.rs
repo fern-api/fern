7a90e6dd31a42afb8890410d59d18fe7f3b1ad00
Fern Support <126544928+fern-support@users.noreply.github.com>
2026-01-26 17:25:25 -0500

    chore(rust): update rust-model seed (#11807)
    Co-authored-by: fern-support <fern-support@users.noreply.github.com>

--- seed/rust-model/trace/src/submission_submission_response.rs
+++ seed/rust-model/trace/src/submission_submission_response.rs
@@ -3,23 +3,29 @@ pub use crate::prelude::*;
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "type")]
 pub enum SubmissionResponse {
+        #[serde(rename = "serverInitialized")]
         ServerInitialized,
 
+        #[serde(rename = "problemInitialized")]
         ProblemInitialized {
             value: ProblemId,
         },
 
+        #[serde(rename = "workspaceInitialized")]
         WorkspaceInitialized,
 
+        #[serde(rename = "serverErrored")]
         ServerErrored {
             #[serde(flatten)]
             data: ExceptionInfo,
         },
 
+        #[serde(rename = "codeExecutionUpdate")]
         CodeExecutionUpdate {
             value: CodeExecutionUpdate,
         },
 
+        #[serde(rename = "terminated")]
         Terminated {
             #[serde(flatten)]
             data: TerminatedResponse,
--- seed/rust-model/trace/src/v_2_problem_function_signature.rs
+++ seed/rust-model/trace/src/v_2_problem_function_signature.rs
@@ -3,16 +3,19 @@ pub use crate::prelude::*;
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "type")]
 pub enum FunctionSignature {
+        #[serde(rename = "void")]
         Void {
             #[serde(flatten)]
             data: VoidFunctionSignature,
         },
 
+        #[serde(rename = "nonVoid")]
         NonVoid {
             #[serde(flatten)]
             data: NonVoidFunctionSignature,
         },
 
+        #[serde(rename = "voidThatTakesActualResult")]
         VoidThatTakesActualResult {
             #[serde(flatten)]
             data: VoidFunctionSignatureThatTakesActualResult,
