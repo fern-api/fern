4de1f9413d80ddc0500979f123716e398ebc8bc0
Fern Support <126544928+fern-support@users.noreply.github.com>
2026-01-28 18:15:47 -0500

    chore(ruby): update ruby-model seed (#11881)
    Co-authored-by: seymurkafkas <seymurkafkas@users.noreply.github.com>

---
+++ seed/ruby-model/file-upload/lib/seed_file_upload_client/service/types/open_enum_type.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module SeedFileUploadClient
+  class Service
+    class OpenEnumType
+      OPTION_A = "OPTION_A"
+      OPTION_B = "OPTION_B"
+      OPTION_C = "OPTION_C"
+    end
+  end
+end
