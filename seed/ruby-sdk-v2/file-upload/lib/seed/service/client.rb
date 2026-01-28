838019d61a322e85709b68f1e5280dda7db11247
seymurkafkas <seymurkafkas@users.noreply.github.com>
2026-01-28 23:10:38 +0000

    chore(ruby): update ruby-sdk-v2 seed

--- seed/ruby-sdk-v2/file-upload/lib/seed/service/client.rb
+++ seed/ruby-sdk-v2/file-upload/lib/seed/service/client.rb
@@ -540,6 +540,58 @@ module Seed
         error_class = Seed::Errors::ResponseError.subclass_for_code(code)
         raise error_class.new(response.body, code: code)
       end
+
+      # @param request_options [Hash]
+      # @param params [void]
+      # @option request_options [String] :base_url
+      # @option request_options [Hash{String => Object}] :additional_headers
+      # @option request_options [Hash{String => Object}] :additional_query_parameters
+      # @option request_options [Hash{String => Object}] :additional_body_parameters
+      # @option request_options [Integer] :timeout_in_seconds
+      #
+      # @return [String]
+      def with_literal_and_enum_types(request_options: {}, **params)
+        params = Seed::Internal::Types::Utils.normalize_keys(params)
+        body = Internal::Multipart::FormData.new
+
+        body.add_part(params[:file].to_form_data_part(name: "file")) if params[:file]
+        if params[:model_type]
+          body.add(
+            name: "model_type",
+            value: params[:model_type]
+          )
+        end
+        if params[:open_enum]
+          body.add(
+            name: "open_enum",
+            value: params[:open_enum]
+          )
+        end
+        if params[:maybe_name]
+          body.add(
+            name: "maybe_name",
+            value: params[:maybe_name]
+          )
+        end
+
+        request = Seed::Internal::Multipart::Request.new(
+          base_url: request_options[:base_url],
+          method: "POST",
+          path: "/with-literal-enum",
+          body: body,
+          request_options: request_options
+        )
+        begin
+          response = @client.send(request)
+        rescue Net::HTTPRequestTimeout
+          raise Seed::Errors::TimeoutError
+        end
+        code = response.code.to_i
+        return if code.between?(200, 299)
+
+        error_class = Seed::Errors::ResponseError.subclass_for_code(code)
+        raise error_class.new(response.body, code: code)
+      end
     end
   end
 end
