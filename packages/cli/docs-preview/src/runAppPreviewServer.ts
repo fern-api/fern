d00d46c46fa5771c515323c30b2174ef761cc46f
Sarah Bawabe <sarah@buildwithfern.com>
2026-01-29 20:51:40 -0500

    fix ts errors from new docs def

--- packages/cli/docs-preview/src/runAppPreviewServer.ts
+++ packages/cli/docs-preview/src/runAppPreviewServer.ts
@@ -55,7 +55,8 @@ const EMPTY_DOCS_DEFINITION: DocsV1Read.DocsDefinition = {
         footer: undefined
     },
     jsFiles: undefined,
-    id: undefined
+    id: undefined,
+    apiNameToId: {}
 };
 
 /**
