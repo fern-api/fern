73bb153564b07162077c3123edd02f86c0df5531
fern-support <fern-support@users.noreply.github.com>
2026-02-12 21:44:23 +0000

    chore(ruby): update ruby-model seed

---
+++ seed/ruby-model/pagination-uri-path/test/test_seed_pagination_uri_path_client.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+require "seed_pagination_uri_path_client"
+
+# Basic SeedPaginationUriPathClient tests
+class TestSeedPaginationUriPathClient < Minitest::Test
+  def test_function
+    # SeedPaginationUriPathClient::Client.new
+  end
+end
