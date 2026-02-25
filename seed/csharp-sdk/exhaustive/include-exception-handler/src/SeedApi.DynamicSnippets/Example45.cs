b31552df6b03995e870346f95afed8eaaf32537d
fern-support <fern-support@users.noreply.github.com>
2026-02-25 04:06:33 +0000

    chore(csharp): update csharp-sdk seed

--- seed/csharp-sdk/exhaustive/include-exception-handler/src/SeedApi.DynamicSnippets/Example45.cs
+++ seed/csharp-sdk/exhaustive/include-exception-handler/src/SeedApi.DynamicSnippets/Example45.cs
@@ -12,7 +12,7 @@ public class Example45
             }
         );
 
-        await client.Endpoints.Urls.NoEndingSlashAsync();
+        await client.Endpoints.Urls.WithMixedCaseAsync();
     }
 
 }
