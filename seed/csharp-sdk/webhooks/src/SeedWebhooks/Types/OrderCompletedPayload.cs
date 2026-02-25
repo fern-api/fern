5a8da6fd4167c0284fd2b02977b25298a8a4f442
aditya-arolkar-swe <aditya-arolkar-swe@users.noreply.github.com>
2026-02-25 16:44:11 +0000

    chore(csharp): update csharp-sdk seed

---
+++ seed/csharp-sdk/webhooks/src/SeedWebhooks/Types/OrderCompletedPayload.cs
@@ -0,0 +1,34 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SeedWebhooks.Core;
+
+namespace SeedWebhooks;
+
+[Serializable]
+public record OrderCompletedPayload : IJsonOnDeserialized
+{
+    [JsonExtensionData]
+    private readonly IDictionary<string, JsonElement> _extensionData =
+        new Dictionary<string, JsonElement>();
+
+    [JsonPropertyName("orderId")]
+    public required string OrderId { get; set; }
+
+    [JsonPropertyName("total")]
+    public required double Total { get; set; }
+
+    [JsonPropertyName("currency")]
+    public required string Currency { get; set; }
+
+    [JsonIgnore]
+    public ReadOnlyAdditionalProperties AdditionalProperties { get; private set; } = new();
+
+    void IJsonOnDeserialized.OnDeserialized() =>
+        AdditionalProperties.CopyFromExtensionData(_extensionData);
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        return JsonUtils.Serialize(this);
+    }
+}
