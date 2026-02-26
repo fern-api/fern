75b25a733c359dd46a0dd57ab9d39bc5d50910e2
jsklan <jsklan.development@gmail.com>
2026-02-26 20:51:23 +0000

    Fix schemas initialization reading from wrong source in generateOverridesContent

--- packages/cli/cli/src/commands/generate-overrides/writeOverridesForWorkspaces.ts
+++ packages/cli/cli/src/commands/generate-overrides/writeOverridesForWorkspaces.ts
@@ -91,8 +91,8 @@ export function generateOverridesContent({
         }
     }
 
-    const schemas: Record<string, Record<string, unknown>> = hasExisting && "path" in existingOverrides
-        ? (existingOverrides.path as Record<string, Record<string, unknown>>)
+    const schemas: Record<string, Record<string, unknown>> = hasExisting && "components" in existingOverrides && existingOverrides.components?.schemas != null
+        ? (existingOverrides.components.schemas as Record<string, Record<string, unknown>>)
         : {};
     if (includeModels) {
         writeModels(schemas, ir.groupedSchemas.rootSchemas);
