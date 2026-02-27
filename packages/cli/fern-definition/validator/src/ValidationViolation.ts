aeee2a584e43d059d1676707834cd85255d22c89
Aditya Arolkar <aditya.arolkar@berkeley.edu>
2026-02-27 15:11:52 -0500

    oversight from original --json feature PR - all validations should have names

--- packages/cli/fern-definition/validator/src/ValidationViolation.ts
+++ packages/cli/fern-definition/validator/src/ValidationViolation.ts
@@ -2,7 +2,7 @@ import { NodePath } from "@fern-api/fern-definition-schema";
 import { RelativeFilePath } from "@fern-api/fs-utils";
 
 export interface ValidationViolation {
-    name?: string;
+    name: string;
     severity: "fatal" | "error" | "warning";
     relativeFilepath: RelativeFilePath;
     nodePath: NodePath;
