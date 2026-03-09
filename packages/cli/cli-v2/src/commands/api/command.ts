20628bcbdd61d416f42bd9269d5c4a782e70643e
Alex McKinney <alexmckinney01@gmail.com>
2026-03-09 10:10:15 -0400

    feat(cli): Add api check command

--- packages/cli/cli-v2/src/commands/api/command.ts
+++ packages/cli/cli-v2/src/commands/api/command.ts
@@ -2,8 +2,12 @@ import type { Argv } from "yargs";
 
 import type { GlobalArgs } from "../../context/GlobalArgs.js";
 import { commandGroup } from "../_internal/commandGroup.js";
+import { addCheckCommand } from "./check/index.js";
 import { addCompileCommand } from "./compile/index.js";
 
 export function addApiCommand(cli: Argv<GlobalArgs>): void {
-    commandGroup(cli, "api", "Configure, compile, edit, and inspect your API specs", [addCompileCommand]);
+    commandGroup(cli, "api", "Configure, compile, edit, and inspect your API specs", [
+        addCheckCommand,
+        addCompileCommand
+    ]);
 }
