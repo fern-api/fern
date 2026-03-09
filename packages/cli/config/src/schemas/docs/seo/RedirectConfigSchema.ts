47bef038334f4df70a8d5e653116b063be834796
Alex McKinney <alexmckinney01@gmail.com>
2026-03-09 13:47:09 -0400

    chore(cli): Use original docs schema

--- packages/cli/config/src/schemas/docs/seo/RedirectConfigSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const RedirectConfigSchema = z.object({
-    source: z.string(),
-    destination: z.string(),
-    permanent: z.boolean().optional()
-});
-
-export type RedirectConfigSchema = z.infer<typeof RedirectConfigSchema>;
