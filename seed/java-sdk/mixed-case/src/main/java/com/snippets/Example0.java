ca4333bb3d5ba6d12a4fc1059b851cdc72463e3a
fern-support <fern-support@users.noreply.github.com>
2026-03-09 17:57:57 +0000

    chore(java): update java-sdk seed

--- seed/java-sdk/mixed-case/src/main/java/com/snippets/Example0.java
+++ seed/java-sdk/mixed-case/src/main/java/com/snippets/Example0.java
@@ -4,10 +4,8 @@ import com.seed.mixedCase.SeedMixedCaseClient;
 
 public class Example0 {
     public static void main(String[] args) {
-        SeedMixedCaseClient client = SeedMixedCaseClient
-            .builder()
-            .url("https://api.fern.com")
-            .build();
+        SeedMixedCaseClient client =
+                SeedMixedCaseClient.builder().url("https://api.fern.com").build();
 
         client.service().getResource("rsc-xyz");
     }
