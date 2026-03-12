c7c327e81a99dbffacb0ecf17f8a311f79b6b831
fern-support <fern-support@users.noreply.github.com>
2026-03-12 03:53:09 +0000

    chore(java): update java-sdk seed

--- seed/java-sdk/property-access/src/main/java/com/seed/propertyAccess/types/UserOrAdminDiscriminated.java
+++ seed/java-sdk/property-access/src/main/java/com/seed/propertyAccess/types/UserOrAdminDiscriminated.java
@@ -75,6 +75,22 @@ public final class UserOrAdminDiscriminated {
         return Optional.empty();
     }
 
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) return true;
+        return other instanceof UserOrAdminDiscriminated && value.equals(((UserOrAdminDiscriminated) other).value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
     @JsonValue
     private Value getValue() {
         return this.value;
