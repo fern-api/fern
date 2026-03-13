ce81f96c794ede39e5f64432683d7543f2c44726
patrick thornton <patrickthornton@g.harvard.edu>
2026-03-13 13:40:35 -0400

    format agnostic

--- seed/csharp-sdk/unions/no-custom-config/src/SeedUnions.Test/Core/RawClientTests/RetriesTests.cs
+++ seed/csharp-sdk/unions/no-custom-config/src/SeedUnions.Test/Core/RawClientTests/RetriesTests.cs
@@ -1,4 +1,5 @@
 using global::System.Net.Http;
+using global::System.Text.Json;
 using NUnit.Framework;
 using SeedUnions.Core;
 using WireMock.Server;
@@ -321,8 +322,6 @@ public class RetriesTests
     [Test]
     public async SystemTask SendRequestAsync_ShouldPreserveJsonBody_OnRetry()
     {
-        const string expectedBody = """{"key":"value"}""";
-
         _server
             .Given(WireMockRequest.Create().WithPath("/test").UsingPost())
             .InScenario("RetryWithBody")
@@ -352,9 +351,10 @@ public class RetriesTests
             Assert.That(content, Is.EqualTo("Success"));
             Assert.That(_server.LogEntries, Has.Count.EqualTo(2));
 
-            // Verify the retried request preserved the JSON body
+            // Verify the retried request preserved the JSON body (compare parsed to ignore formatting differences)
             var retriedEntry = _server.LogEntries.ElementAt(1);
-            Assert.That(retriedEntry.RequestMessage.Body, Is.EqualTo(expectedBody));
+            var actualJson = JsonDocument.Parse(retriedEntry.RequestMessage.Body!);
+            Assert.That(actualJson.RootElement.GetProperty("key").GetString(), Is.EqualTo("value"));
         }
     }
 
@@ -390,9 +390,10 @@ public class RetriesTests
             Assert.That(content, Is.EqualTo("Success"));
             Assert.That(_server.LogEntries, Has.Count.EqualTo(2));
 
-            // Verify the retried request preserved the multipart body
+            // Verify the retried request preserved the multipart body (check key/value presence to ignore formatting differences)
             var retriedEntry = _server.LogEntries.ElementAt(1);
-            Assert.That(retriedEntry.RequestMessage.Body, Does.Contain("""{"key":"value"}"""));
+            Assert.That(retriedEntry.RequestMessage.Body, Does.Contain("\"key\""));
+            Assert.That(retriedEntry.RequestMessage.Body, Does.Contain("\"value\""));
         }
     }
 
