32db798eb081565d6bb748fe223cebf4eab29ac3
fern-support <fern-support@users.noreply.github.com>
2026-03-13 20:14:35 +0000

    chore(go): update go-sdk seed

--- seed/go-sdk/go-deterministic-ordering/dynamic-snippets/example56/snippet.go
+++ seed/go-sdk/go-deterministic-ordering/dynamic-snippets/example56/snippet.go
@@ -1,9 +1,8 @@
 package example
 
 import (
-    client "github.com/exhaustive/fern/client"
-    option "github.com/exhaustive/fern/option"
-    fern "github.com/exhaustive/fern"
+    client "github.com/go-deterministic-ordering/fern/client"
+    option "github.com/go-deterministic-ordering/fern/option"
     context "context"
 )
 
@@ -16,13 +15,7 @@ func do() {
             "<token>",
         ),
     )
-    request := &fern.ReqWithHeaders{
-        XTestServiceHeader: "X-TEST-SERVICE-HEADER",
-        XTestEndpointHeader: "X-TEST-ENDPOINT-HEADER",
-        Body: "string",
-    }
-    client.ReqWithHeaders.GetWithCustomHeader(
+    client.Endpoints.Urls.WithMixedCase(
         context.TODO(),
-        request,
     )
 }
