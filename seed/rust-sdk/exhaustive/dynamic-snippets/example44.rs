3f46f68670e7b71e36d3b833ad89dc73d294a3d7
jsklan <jsklan@users.noreply.github.com>
2026-03-17 01:59:27 +0000

    chore(rust): update rust-sdk seed

--- seed/rust-sdk/exhaustive/dynamic-snippets/example44.rs
+++ seed/rust-sdk/exhaustive/dynamic-snippets/example44.rs
@@ -11,8 +11,8 @@ async fn main() {
     client
         .endpoints
         .primitive
-        .get_and_return_uuid(
-            &Uuid::parse_str("d5e9c84f-c2b2-4bf4-b4b0-7ffd7a9ffc32").unwrap(),
+        .get_and_return_date(
+            &NaiveDate::parse_from_str("2023-01-15", "%Y-%m-%d").unwrap(),
             None,
         )
         .await;
