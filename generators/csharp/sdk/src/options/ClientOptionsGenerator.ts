6ae08d505851cc54df8d9bd82aa19b9ca2881450
patrick thornton <patrickthornton@g.harvard.edu>
2026-03-18 16:32:08 -0400

    unified client options

--- generators/csharp/sdk/src/options/ClientOptionsGenerator.ts
+++ generators/csharp/sdk/src/options/ClientOptionsGenerator.ts
@@ -1,3 +1,4 @@
+import { assertNever } from "@fern-api/core-utils";
 import { CSharpFile, FileGenerator } from "@fern-api/csharp-base";
 import { ast } from "@fern-api/csharp-codegen";
 import { join, RelativeFilePath } from "@fern-api/fs-utils";
@@ -5,9 +6,16 @@ import { join, RelativeFilePath } from "@fern-api/fs-utils";
 import { FernIr } from "@fern-fern/ir-sdk";
 
 import { SdkGeneratorContext } from "../SdkGeneratorContext.js";
+import { collectInferredAuthCredentials } from "../utils/inferredAuthUtils.js";
 import { BaseOptionsGenerator, OptionArgs } from "./BaseOptionsGenerator.js";
 
-export class ClientOptionsGenerator extends FileGenerator<CSharpFile> {
+interface UnifiedField {
+    name: string;
+    type: ast.Type;
+    docs?: string;
+}
+
+export class ClientOptionsGenerator extends FileGenerator<CSharpFile, SdkGeneratorContext> {
     private baseOptionsGenerator: BaseOptionsGenerator;
 
     constructor(context: SdkGeneratorContext, baseOptionsGenerator: BaseOptionsGenerator) {
@@ -17,6 +25,7 @@ export class ClientOptionsGenerator extends FileGenerator<CSharpFile> {
     }
     private baseUrlField: ast.Field | undefined;
     private environmentField: ast.Field | undefined;
+    private unifiedFields: UnifiedField[] = [];
 
     public doGenerate(): CSharpFile {
         const class_ = this.csharp.class_({
@@ -48,6 +57,11 @@ export class ClientOptionsGenerator extends FileGenerator<CSharpFile> {
         this.baseOptionsGenerator.getMaxRetriesField(class_, optionArgs);
         this.baseOptionsGenerator.getTimeoutField(class_, optionArgs);
         this.baseOptionsGenerator.getLiteralHeaderOptions(class_, optionArgs);
+
+        if (this.settings.unifiedClientOptions) {
+            this.addUnifiedAuthAndHeaderFields(class_);
+        }
+
         if (this.context.hasGrpcEndpoints()) {
             this.getGrpcOptionsField(class_);
         }
@@ -181,6 +195,145 @@ export class ClientOptionsGenerator extends FileGenerator<CSharpFile> {
         });
     }
 
+    private addUnifiedAuthAndHeaderFields(class_: ast.Class): void {
+        const seenNames = new Set<string>();
+
+        for (const scheme of this.context.ir.auth.schemes) {
+            for (const field of this.getFieldsFromAuthScheme(scheme)) {
+                if (!seenNames.has(field.name)) {
+                    seenNames.add(field.name);
+                    class_.addField({
+                        origin: class_.explicit(field.name),
+                        access: ast.Access.Public,
+                        get: true,
+                        init: true,
+                        type: field.type.asOptional(),
+                        summary: field.docs
+                    });
+                    this.unifiedFields.push(field);
+                }
+            }
+        }
+
+        for (const header of this.context.ir.headers) {
+            // Skip literal headers (already handled by getLiteralHeaderOptions)
+            if (header.valueType.type === "container" && header.valueType.container.type === "literal") {
+                continue;
+            }
+            const name = header.name.name.pascalCase.safeName;
+            if (!seenNames.has(name)) {
+                seenNames.add(name);
+                const type = this.context.csharpTypeMapper.convert({ reference: header.valueType });
+                const field: UnifiedField = {
+                    name,
+                    type,
+                    docs: header.docs
+                };
+                class_.addField({
+                    origin: class_.explicit(name),
+                    access: ast.Access.Public,
+                    get: true,
+                    init: true,
+                    type: type.asOptional(),
+                    summary: header.docs
+                });
+                this.unifiedFields.push(field);
+            }
+        }
+    }
+
+    private getFieldsFromAuthScheme(scheme: FernIr.AuthScheme): UnifiedField[] {
+        if (scheme.type === "bearer") {
+            return [
+                {
+                    name: scheme.token.pascalCase.safeName,
+                    type: this.Primitive.string,
+                    docs: scheme.docs ?? `The ${scheme.token.camelCase.safeName} to use for authentication.`
+                }
+            ];
+        } else if (scheme.type === "basic") {
+            return [
+                {
+                    name: scheme.username.pascalCase.safeName,
+                    type: this.Primitive.string,
+                    docs: scheme.docs ?? `The ${scheme.username.camelCase.safeName} to use for authentication.`
+                },
+                {
+                    name: scheme.password.pascalCase.safeName,
+                    type: this.Primitive.string,
+                    docs: scheme.docs ?? `The ${scheme.password.camelCase.safeName} to use for authentication.`
+                }
+            ];
+        } else if (scheme.type === "header") {
+            return [
+                {
+                    name: scheme.name.name.pascalCase.safeName,
+                    type: this.context.csharpTypeMapper.convert({ reference: scheme.valueType }),
+                    docs: scheme.docs ?? `The ${scheme.name.name.camelCase.safeName} to use for authentication.`
+                }
+            ];
+        } else if (scheme.type === "oauth") {
+            const fields: UnifiedField[] = [
+                {
+                    name: "ClientId",
+                    type: this.Primitive.string,
+                    docs: "The clientId to use for authentication."
+                },
+                {
+                    name: "ClientSecret",
+                    type: this.Primitive.string,
+                    docs: "The clientSecret to use for authentication."
+                }
+            ];
+            for (const customProperty of scheme.configuration.tokenEndpoint.requestProperties.customProperties ?? []) {
+                if (
+                    customProperty.property.valueType.type === "container" &&
+                    customProperty.property.valueType.container.type === "literal"
+                ) {
+                    continue;
+                }
+                const typeRef = this.context.csharpTypeMapper.convert({
+                    reference: customProperty.property.valueType
+                });
+                if (typeRef.isOptional) {
+                    continue;
+                }
+                fields.push({
+                    name: customProperty.property.name.name.pascalCase.safeName,
+                    type: typeRef,
+                    docs: `The ${customProperty.property.name.name.camelCase.safeName} for OAuth authentication.`
+                });
+            }
+            return fields;
+        } else if (scheme.type === "inferred") {
+            // Inferred auth credentials become fields on ClientOptions
+            const fields: UnifiedField[] = [];
+            const tokenEndpointReference = scheme.tokenEndpoint.endpoint;
+            const tokenEndpointHttpService = this.context.getHttpService(tokenEndpointReference.serviceId);
+            if (tokenEndpointHttpService == null) {
+                return [];
+            }
+            const tokenEndpoint = this.context.resolveEndpoint(
+                tokenEndpointHttpService,
+                tokenEndpointReference.endpointId
+            );
+            const credentials = collectInferredAuthCredentials(this.context, tokenEndpoint);
+            for (const credential of credentials) {
+                const typeRef = this.context.csharpTypeMapper.convert({
+                    reference: credential.typeReference
+                });
+                fields.push({
+                    name: credential.pascalName,
+                    type: typeRef,
+                    docs: credential.docs ?? `The ${credential.camelName} for authentication.`
+                });
+            }
+            return fields;
+        } else {
+            assertNever(scheme);
+        }
+    }
+
     private getCloneMethod(cls: ast.Class): void {
         // TODO: add the GRPC options here eventually
         // TODO: iterate over all public fields and generate the clone logic
@@ -191,6 +344,9 @@ export class ClientOptionsGenerator extends FileGenerator<CSharpFile> {
             name: "Clone",
             return_: this.Types.ClientOptions,
             body: this.csharp.codeblock((writer) => {
+                const unifiedFieldLines = this.unifiedFields
+                    .map((field) => `\n    ${field.name} = ${field.name},`)
+                    .join("");
                 writer.writeStatement(
                     `return new ClientOptions
 {${this.baseUrlField ? `\n    ${this.baseUrlField.name} = ${this.baseUrlField.name},` : ""}${this.environmentField ? `\n    ${this.environmentField.name} = ${this.environmentField.name},` : ""}
@@ -202,7 +358,7 @@ export class ClientOptionsGenerator extends FileGenerator<CSharpFile> {
                     `(new Dictionary<string, `,
                     this.Types.HeaderValue,
                     `>(Headers)),
-    AdditionalHeaders = AdditionalHeaders,
+    AdditionalHeaders = AdditionalHeaders,${unifiedFieldLines}
     ${this.settings.includeExceptionHandler ? "ExceptionHandler = ExceptionHandler.Clone()," : ""}
 }`
                 );
