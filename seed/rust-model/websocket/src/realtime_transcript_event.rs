73a35a10714638eb3b4dd366b6f18fd291365eab
amckinney <5760684+amckinney@users.noreply.github.com>
2026-03-19 18:07:44 +0000

    chore(rust): update rust-model seed

--- seed/rust-model/websocket/src/realtime_transcript_event.rs
+++ seed/rust-model/websocket/src/realtime_transcript_event.rs
@@ -3,5 +3,6 @@ pub use crate::prelude::*;
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub struct TranscriptEvent {
     pub r#type: String,
+    #[serde(default)]
     pub data: String,
 }
