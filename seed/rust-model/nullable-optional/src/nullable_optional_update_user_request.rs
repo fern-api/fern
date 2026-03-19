3409731dff7509f925820844b654abccae7d6e96
Alex McKinney <alexmckinney01@gmail.com>
2026-03-19 13:49:34 -0400

    fix(rust): Prevent double Option types (#13739)

--- seed/rust-model/nullable-optional/src/nullable_optional_update_user_request.rs
+++ seed/rust-model/nullable-optional/src/nullable_optional_update_user_request.rs
@@ -6,9 +6,9 @@ pub struct UpdateUserRequest {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub username: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub email: Option<Option<String>>,
+    pub email: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub phone: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub address: Option<Option<Address>>,
+    pub address: Option<Address>,
 }
