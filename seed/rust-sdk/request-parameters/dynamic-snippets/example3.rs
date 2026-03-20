7967b790690a39a5ec1850660df3db99b93e06a6
fern-support <126544928+fern-support@users.noreply.github.com>
2026-03-20 13:47:21 +0000

    chore(rust): update rust-sdk seed

--- seed/rust-sdk/request-parameters/dynamic-snippets/example3.rs
+++ seed/rust-sdk/request-parameters/dynamic-snippets/example3.rs
@@ -10,11 +10,11 @@ async fn main() {
     client
         .user
         .create_username_optional(
-            &Some(Some(CreateUsernameBodyOptionalProperties {
+            &Some(CreateUsernameBodyOptionalProperties {
                 username: Some("username".to_string()),
                 password: Some("password".to_string()),
                 name: Some("test".to_string()),
-            })),
+            }),
             None,
         )
         .await;
