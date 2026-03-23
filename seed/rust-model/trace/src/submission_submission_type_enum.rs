f5379cf3ee2a1bb1fca8a75292444fa099731999
iamnamananand996 <31537362+iamnamananand996@users.noreply.github.com>
2026-03-23 18:55:42 +0000

    chore(rust): update rust-model seed

--- seed/rust-model/trace/src/submission_submission_type_enum.rs
+++ seed/rust-model/trace/src/submission_submission_type_enum.rs
@@ -1,39 +1,16 @@
 pub use crate::prelude::*;
 
 /// Keep in sync with SubmissionType.
-#[non_exhaustive]
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub enum SubmissionTypeEnum {
+    #[serde(rename = "TEST")]
     Test,
-    /// This variant is used for forward compatibility.
-    /// If the server sends a value not recognized by the current SDK version,
-    /// it will be captured here with the raw string value.
-    __Unknown(String),
 }
-impl Serialize for SubmissionTypeEnum {
-    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        match self {
-            Self::Test => serializer.serialize_str("TEST"),
-            Self::__Unknown(val) => serializer.serialize_str(val),
-        }
-    }
-}
-
-impl<'de> Deserialize<'de> for SubmissionTypeEnum {
-    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let value = String::deserialize(deserializer)?;
-        match value.as_str() {
-            "TEST" => Ok(Self::Test),
-            _ => Ok(Self::__Unknown(value)),
-        }
-    }
-}
-
 impl fmt::Display for SubmissionTypeEnum {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Self::Test => write!(f, "TEST"),
-            Self::__Unknown(val) => write!(f, "{}", val),
-        }
+        let s = match self {
+            Self::Test => "TEST",
+        };
+        write!(f, "{}", s)
     }
 }
