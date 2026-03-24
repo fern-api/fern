c8722d155899e5c917805bf90d4b4a8cde0bea96
jsklan <jsklan.development@gmail.com>
2026-03-24 12:53:58 -0400

    Update seed config to produce wire tests in fixture output

---
+++ seed/python-sdk/server-sent-event-examples/with-wire-tests/tests/wire/test_completions.py
@@ -0,0 +1,34 @@
+from .conftest import get_client, verify_request_count
+
+
+def test_completions_stream() -> None:
+    """Test stream endpoint with WireMock"""
+    test_id = "completions.stream.0"
+    client = get_client(test_id)
+    for _ in client.completions.stream(
+        query="foo",
+    ):
+        pass
+    verify_request_count(test_id, "POST", "/stream", None, 1)
+
+
+def test_completions_stream_events() -> None:
+    """Test streamEvents endpoint with WireMock"""
+    test_id = "completions.stream_events.0"
+    client = get_client(test_id)
+    for _ in client.completions.stream_events(
+        query="query",
+    ):
+        pass
+    verify_request_count(test_id, "POST", "/stream-events", None, 1)
+
+
+def test_completions_stream_events_context_protocol() -> None:
+    """Test streamEventsContextProtocol endpoint with WireMock"""
+    test_id = "completions.stream_events_context_protocol.0"
+    client = get_client(test_id)
+    for _ in client.completions.stream_events_context_protocol(
+        query="query",
+    ):
+        pass
+    verify_request_count(test_id, "POST", "/stream-events-context-protocol", None, 1)
