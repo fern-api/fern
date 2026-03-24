2ca8c5849a10abac21f4249b13c0c8499fc35dcd
Fern Support <126544928+fern-support@users.noreply.github.com>
2026-03-24 11:20:53 -0700

    chore(php): update php-sdk seed (#13995)
    Co-authored-by: iamnamananand996 <31537362+iamnamananand996@users.noreply.github.com>

--- seed/php-sdk/no-content-response/src/Contacts/ContactsClient.php
+++ seed/php-sdk/no-content-response/src/Contacts/ContactsClient.php
@@ -129,6 +129,9 @@ class ContactsClient
             $statusCode = $response->getStatusCode();
             if ($statusCode >= 200 && $statusCode < 400) {
                 $json = $response->getBody()->getContents();
+                if (empty($json)) {
+                    throw new SeedException(message: "Expected a JSON response body, but received an empty response.");
+                }
                 return Contact::fromJson($json);
             }
         } catch (JsonException $e) {
