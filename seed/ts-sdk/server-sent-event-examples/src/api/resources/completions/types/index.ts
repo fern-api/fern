850c1a42cbab45b5bcd55ea06c728d51049b1c95
fern-support <126544928+fern-support@users.noreply.github.com>
2026-03-24 16:25:15 +0000

    chore(typescript): update ts-sdk seed

--- seed/ts-sdk/server-sent-event-examples/src/api/resources/completions/types/index.ts
+++ seed/ts-sdk/server-sent-event-examples/src/api/resources/completions/types/index.ts
@@ -1,4 +1,6 @@
 export * from "./CompletionEvent.js";
 export * from "./ErrorEvent.js";
+export * from "./EventEvent.js";
 export * from "./StreamEvent.js";
+export * from "./StreamEventContextProtocol.js";
 export * from "./StreamedCompletion.js";
