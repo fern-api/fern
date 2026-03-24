bee8e05cd2689a4f9d6af41d9669dd4ccad1e687
fern-support <126544928+fern-support@users.noreply.github.com>
2026-03-24 16:25:11 +0000

    chore(ruby): update ruby-sdk-v2 seed

---
+++ seed/ruby-sdk-v2/server-sent-event-examples/lib/seed/completions/types/stream_event_context_protocol.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Seed
+  module Completions
+    module Types
+      class StreamEventContextProtocol < Internal::Types::Model
+        extend Seed::Internal::Types::Union
+
+        discriminant :event
+
+        member -> { Seed::Completions::Types::CompletionEvent }, key: "COMPLETION"
+        member -> { Seed::Completions::Types::ErrorEvent }, key: "ERROR"
+        member -> { Seed::Completions::Types::EventEvent }, key: "NOTIFICATION"
+      end
+    end
+  end
+end
