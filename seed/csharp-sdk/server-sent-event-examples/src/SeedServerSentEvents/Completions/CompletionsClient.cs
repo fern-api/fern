4d79803cddd44d5294ea8c5ada027891331dd4aa
Fern Support <126544928+fern-support@users.noreply.github.com>
2026-03-24 09:33:48 -0700

    chore(csharp): update csharp-sdk seed (#13979)

--- seed/csharp-sdk/server-sent-event-examples/src/SeedServerSentEvents/Completions/CompletionsClient.cs
+++ seed/csharp-sdk/server-sent-event-examples/src/SeedServerSentEvents/Completions/CompletionsClient.cs
@@ -174,4 +174,87 @@ public partial class CompletionsClient : ICompletionsClient
             );
         }
     }
+
+    /// <example><code>
+    /// client.Completions.StreamEventsContextProtocolAsync(
+    ///     new StreamEventsContextProtocolRequest { Query = "query" }
+    /// );
+    /// </code></example>
+    public async IAsyncEnumerable<StreamEventContextProtocol> StreamEventsContextProtocolAsync(
+        StreamEventsContextProtocolRequest request,
+        RequestOptions? options = null,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var _headers = await new SeedServerSentEvents.Core.HeadersBuilder.Builder()
+            .Add(_client.Options.Headers)
+            .Add(_client.Options.AdditionalHeaders)
+            .Add(options?.AdditionalHeaders)
+            .BuildAsync()
+            .ConfigureAwait(false);
+        var response = await _client
+            .SendRequestAsync(
+                new JsonRequest
+                {
+                    Method = HttpMethod.Post,
+                    Path = "stream-events-context-protocol",
+                    Body = request,
+                    Headers = _headers,
+                    Options = options,
+                },
+                cancellationToken
+            )
+            .ConfigureAwait(false);
+        if (response.StatusCode is >= 200 and < 400)
+        {
+            await foreach (
+                var item in SseParser
+                    .Create(await response.Raw.Content.ReadAsStreamAsync())
+                    .EnumerateAsync(cancellationToken)
+            )
+            {
+                if (!string.IsNullOrEmpty(item.Data))
+                {
+                    if (item.Data == "[DONE]")
+                    {
+                        break;
+                    }
+                    StreamEventContextProtocol? result;
+                    try
+                    {
+                        result = JsonUtils.Deserialize<StreamEventContextProtocol>(item.Data);
+                    }
+                    catch (JsonException)
+                    {
+                        throw new SeedServerSentEventsException(
+                            $"Unable to deserialize JSON response 'item.Data'"
+                        );
+                    }
+                }
+            }
+            yield break;
+        }
+        {
+            var responseBody = await response
+                .Raw.Content.ReadAsStringAsync(cancellationToken)
+                .ConfigureAwait(false);
+            try
+            {
+                switch (response.StatusCode)
+                {
+                    case 400:
+                        throw new BadRequestError(JsonUtils.Deserialize<string>(responseBody));
+                }
+            }
+            catch (JsonException)
+            {
+                // unable to map error response, throwing generic error
+            }
+            throw new SeedServerSentEventsApiException(
+                $"Error with status code {response.StatusCode}",
+                response.StatusCode,
+                responseBody
+            );
+        }
+    }
 }
