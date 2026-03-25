ce1241b660157e07852935c91c22766c7c70dc89
jsklan <jsklan.development@gmail.com>
2026-03-25 16:57:22 -0400

    fix(cli): make registry required on GeneratorImageObjectSchema
    If specifying a custom image, the registry must also be specified.

--- packages/cli/config/src/schemas/GeneratorImageSchema.ts
+++ packages/cli/config/src/schemas/GeneratorImageSchema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const GeneratorImageObjectSchema = z.object({
     name: z.string(),
-    registry: z.string().optional()
+    registry: z.string()
 });
 
 export type GeneratorImageObjectSchema = z.infer<typeof GeneratorImageObjectSchema>;
