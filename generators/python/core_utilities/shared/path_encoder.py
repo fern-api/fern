ac84da570eaed173ebce6980aa80d5aad52961d2
HoaX7 <krhoax7@gmail.com>
2026-03-25 09:15:19 +0530

    chore: addressed review comments

--- generators/python/core_utilities/shared/path_encoder.py
+++ generators/python/core_utilities/shared/path_encoder.py
@@ -5,4 +5,8 @@ from .jsonable_encoder import jsonable_encoder
 
 
 def encode_path_parameter(value: typing.Any) -> str:
+    if value is None:
+        raise ValueError(
+            "Path parameter value cannot be 'None', this would result in a malformed URL."
+        )
     return urllib.parse.quote(str(jsonable_encoder(value)), safe="")
