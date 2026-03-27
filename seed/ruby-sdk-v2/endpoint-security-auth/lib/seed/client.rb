088183a2c06ab7b3e95d7d4baf4fa69403a0f8e6
iamnamananand996 <31537362+iamnamananand996@users.noreply.github.com>
2026-03-27 14:48:40 +0000

    chore(ruby): update ruby-sdk-v2 seed

--- seed/ruby-sdk-v2/endpoint-security-auth/lib/seed/client.rb
+++ seed/ruby-sdk-v2/endpoint-security-auth/lib/seed/client.rb
@@ -5,11 +5,13 @@ module Seed
     # @param base_url [String, nil]
     # @param token [String]
     # @param api_key [String]
+    # @param username [String]
+    # @param password [String]
     # @param client_id [String]
     # @param client_secret [String]
     #
     # @return [void]
-    def initialize(client_id:, client_secret:, base_url: nil, token: ENV.fetch("MY_TOKEN", nil), api_key: ENV.fetch("MY_API_KEY", nil))
+    def initialize(client_id:, client_secret:, base_url: nil, token: ENV.fetch("MY_TOKEN", nil), api_key: ENV.fetch("MY_API_KEY", nil), username: ENV.fetch("MY_USERNAME", nil), password: ENV.fetch("MY_PASSWORD", nil))
       # Create an unauthenticated client for the auth endpoint
       auth_raw_client = Seed::Internal::Http::RawClient.new(
         base_url: base_url,
@@ -29,14 +31,16 @@ module Seed
         options: { base_url: base_url, client_id: client_id, client_secret: client_secret }
       )
 
+      headers = {
+        "User-Agent" => "fern_endpoint-security-auth/0.0.1",
+        "X-Fern-Language" => "Ruby",
+        Authorization: "Bearer #{token}",
+        "X-API-Key" => api_key.to_s
+      }
+      headers["Authorization"] = "Basic #{Base64.strict_encode64("#{username}:#{password}")}" if !username.nil? && !password.nil?
       @raw_client = Seed::Internal::Http::RawClient.new(
         base_url: base_url,
-        headers: {
-          "User-Agent" => "fern_endpoint-security-auth/0.0.1",
-          "X-Fern-Language" => "Ruby",
-          Authorization: "Bearer #{token}",
-          "X-API-Key" => api_key.to_s
-        }.merge(@auth_provider.auth_headers)
+        headers: headers.merge(@auth_provider.auth_headers)
       )
     end
 
