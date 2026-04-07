cfa94db89016b47349cd824c8f6590c610acbe58
davidkonigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2026-04-07 12:38:28 +0000

    chore(php): update php-sdk seed

--- seed/php-sdk/pagination-custom/src/Users/Requests/ListWithCustomPagerRequest.php
+++ seed/php-sdk/pagination-custom/src/Users/Requests/ListWithCustomPagerRequest.php
@@ -4,24 +4,28 @@ namespace Seed\Users\Requests;
 
 use Seed\Core\Json\JsonSerializableType;
 
-class ListUsernamesRequestCustom extends JsonSerializableType
+class ListWithCustomPagerRequest extends JsonSerializableType
 {
     /**
-     * The cursor used for pagination in order to fetch
-     * the next page of results.
-     *
-     * @var ?string $startingAfter
+     * @var ?int $limit The maximum number of results to return.
+     */
+    public ?int $limit;
+
+    /**
+     * @var ?string $startingAfter The cursor used for pagination.
      */
     public ?string $startingAfter;
 
     /**
      * @param array{
+     *   limit?: ?int,
      *   startingAfter?: ?string,
      * } $values
      */
     public function __construct(
         array $values = [],
     ) {
+        $this->limit = $values['limit'] ?? null;
         $this->startingAfter = $values['startingAfter'] ?? null;
     }
 }
