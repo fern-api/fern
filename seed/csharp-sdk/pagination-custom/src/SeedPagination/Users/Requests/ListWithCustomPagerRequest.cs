9dd6b8abc59a74c79043b439208e7858421dfe95
davidkonigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2026-04-07 12:38:31 +0000

    chore(csharp): update csharp-sdk seed

--- seed/csharp-sdk/pagination-custom/src/SeedPagination/Users/Requests/ListWithCustomPagerRequest.cs
+++ seed/csharp-sdk/pagination-custom/src/SeedPagination/Users/Requests/ListWithCustomPagerRequest.cs
@@ -4,11 +4,16 @@ using SeedPagination.Core;
 namespace SeedPagination;
 
 [Serializable]
-public record ListUsernamesRequestCustom
+public record ListWithCustomPagerRequest
 {
     /// <summary>
-    /// The cursor used for pagination in order to fetch
-    /// the next page of results.
+    /// The maximum number of results to return.
+    /// </summary>
+    [JsonIgnore]
+    public int? Limit { get; set; }
+
+    /// <summary>
+    /// The cursor used for pagination.
     /// </summary>
     [JsonIgnore]
     public string? StartingAfter { get; set; }
