d77e3433207b9118454958dff33a37782a58e0a3
davidkonigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2026-04-07 12:38:34 +0000

    chore(swift): update swift-sdk seed

--- seed/swift-sdk/pagination-custom/Sources/Schemas/Link.swift
+++ seed/swift-sdk/pagination-custom/Sources/Schemas/Link.swift
@@ -1,32 +1,44 @@
 import Foundation
 
-public struct UsernameCursor: Codable, Hashable, Sendable {
-    public let cursor: UsernamePage
+public struct Link: Codable, Hashable, Sendable {
+    public let rel: String
+    public let method: String
+    public let href: String
     /// Additional properties that are not explicitly defined in the schema
     public let additionalProperties: [String: JSONValue]
 
     public init(
-        cursor: UsernamePage,
+        rel: String,
+        method: String,
+        href: String,
         additionalProperties: [String: JSONValue] = .init()
     ) {
-        self.cursor = cursor
+        self.rel = rel
+        self.method = method
+        self.href = href
         self.additionalProperties = additionalProperties
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.cursor = try container.decode(UsernamePage.self, forKey: .cursor)
+        self.rel = try container.decode(String.self, forKey: .rel)
+        self.method = try container.decode(String.self, forKey: .method)
+        self.href = try container.decode(String.self, forKey: .href)
         self.additionalProperties = try decoder.decodeAdditionalProperties(using: CodingKeys.self)
     }
 
     public func encode(to encoder: Encoder) throws -> Void {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try encoder.encodeAdditionalProperties(self.additionalProperties)
-        try container.encode(self.cursor, forKey: .cursor)
+        try container.encode(self.rel, forKey: .rel)
+        try container.encode(self.method, forKey: .method)
+        try container.encode(self.href, forKey: .href)
     }
 
     /// Keys for encoding/decoding struct properties.
     enum CodingKeys: String, CodingKey, CaseIterable {
-        case cursor
+        case rel
+        case method
+        case href
     }
 }
--- seed/swift-sdk/pagination-custom/Sources/Schemas/UsersListResponse.swift
+++ seed/swift-sdk/pagination-custom/Sources/Schemas/UsersListResponse.swift
@@ -1,24 +1,36 @@
 import Foundation
 
-public struct UsernamePage: Codable, Hashable, Sendable {
-    public let after: String?
+public struct UsersListResponse: Codable, Hashable, Sendable {
+    public let limit: Int?
+    public let count: Int?
+    public let hasMore: Bool?
+    public let links: [Link]
     public let data: [String]
     /// Additional properties that are not explicitly defined in the schema
     public let additionalProperties: [String: JSONValue]
 
     public init(
-        after: String? = nil,
+        limit: Int? = nil,
+        count: Int? = nil,
+        hasMore: Bool? = nil,
+        links: [Link],
         data: [String],
         additionalProperties: [String: JSONValue] = .init()
     ) {
-        self.after = after
+        self.limit = limit
+        self.count = count
+        self.hasMore = hasMore
+        self.links = links
         self.data = data
         self.additionalProperties = additionalProperties
     }
 
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.after = try container.decodeIfPresent(String.self, forKey: .after)
+        self.limit = try container.decodeIfPresent(Int.self, forKey: .limit)
+        self.count = try container.decodeIfPresent(Int.self, forKey: .count)
+        self.hasMore = try container.decodeIfPresent(Bool.self, forKey: .hasMore)
+        self.links = try container.decode([Link].self, forKey: .links)
         self.data = try container.decode([String].self, forKey: .data)
         self.additionalProperties = try decoder.decodeAdditionalProperties(using: CodingKeys.self)
     }
@@ -26,13 +38,19 @@ public struct UsernamePage: Codable, Hashable, Sendable {
     public func encode(to encoder: Encoder) throws -> Void {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try encoder.encodeAdditionalProperties(self.additionalProperties)
-        try container.encodeIfPresent(self.after, forKey: .after)
+        try container.encodeIfPresent(self.limit, forKey: .limit)
+        try container.encodeIfPresent(self.count, forKey: .count)
+        try container.encodeIfPresent(self.hasMore, forKey: .hasMore)
+        try container.encode(self.links, forKey: .links)
         try container.encode(self.data, forKey: .data)
     }
 
     /// Keys for encoding/decoding struct properties.
     enum CodingKeys: String, CodingKey, CaseIterable {
-        case after
+        case limit
+        case count
+        case hasMore = "has_more"
+        case links
         case data
     }
 }
