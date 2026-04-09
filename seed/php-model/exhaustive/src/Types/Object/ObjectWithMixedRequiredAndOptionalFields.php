67b3f2b7b9e0c381ffed33e287697203c5407e81
fern-support <126544928+fern-support@users.noreply.github.com>
2026-04-09 16:24:56 +0000

    chore(php): update php-model seed

---
+++ seed/php-model/exhaustive/src/Types/Object/ObjectWithMixedRequiredAndOptionalFields.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Seed\Types\Object;
+
+use Seed\Core\Json\JsonSerializableType;
+use Seed\Core\Json\JsonProperty;
+
+/**
+ * Tests that dynamic snippets include all required properties even when
+ * the example data only provides a subset. In C#, properties marked as
+ * `required` must be set in the object initializer.
+ */
+class ObjectWithMixedRequiredAndOptionalFields extends JsonSerializableType
+{
+    /**
+     * @var string $requiredString
+     */
+    #[JsonProperty('requiredString')]
+    public string $requiredString;
+
+    /**
+     * @var int $requiredInteger
+     */
+    #[JsonProperty('requiredInteger')]
+    public int $requiredInteger;
+
+    /**
+     * @var ?string $optionalString
+     */
+    #[JsonProperty('optionalString')]
+    public ?string $optionalString;
+
+    /**
+     * @var int $requiredLong
+     */
+    #[JsonProperty('requiredLong')]
+    public int $requiredLong;
+
+    /**
+     * @param array{
+     *   requiredString: string,
+     *   requiredInteger: int,
+     *   requiredLong: int,
+     *   optionalString?: ?string,
+     * } $values
+     */
+    public function __construct(
+        array $values,
+    ) {
+        $this->requiredString = $values['requiredString'];
+        $this->requiredInteger = $values['requiredInteger'];
+        $this->optionalString = $values['optionalString'] ?? null;
+        $this->requiredLong = $values['requiredLong'];
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->toJson();
+    }
+}
