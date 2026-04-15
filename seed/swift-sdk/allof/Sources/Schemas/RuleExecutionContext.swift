fc0680b56b56a3988c900588c694a3170e1692d5
jsklan <jsklan.development@gmail.com>
2026-04-15 10:45:17 -0400

    Revert seed output changes to reduce diff

--- seed/swift-sdk/allof/Sources/Schemas/RuleExecutionContext.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-/// Execution environment for a rule.
-public enum RuleExecutionContext: String, Codable, Hashable, CaseIterable, Sendable {
-    case prod
-    case staging
-    case dev
-}
