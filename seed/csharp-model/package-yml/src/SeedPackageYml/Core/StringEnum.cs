549acd13ebaf583d51bb03d2c26db1850cf43c8a
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2026-04-20 09:05:21 -0400

    cleanup model seed

--- seed/csharp-model/package-yml/src/SeedPackageYml/Core/StringEnum.cs
+++
@@ -1,6 +0,0 @@
-namespace SeedPackageYml.Core;
-
-public interface IStringEnum : IEquatable<string>
-{
-    public string Value { get; }
-}
--- seed/csharp-model/plain-text/src/SeedPlainText/Core/StringEnumExtensions.cs
+++
@@ -1,6 +0,0 @@
-namespace SeedPlainText.Core;
-
-internal static class StringEnumExtensions
-{
-    public static string Stringify(this IStringEnum stringEnum) => stringEnum.Value;
-}
