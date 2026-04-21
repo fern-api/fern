f9e107a28f385b4b9cdcfba9b116fc062f2d94ba
dsinghvi <10870189+dsinghvi@users.noreply.github.com>
2026-04-21 17:00:08 +0000

    chore(rust): update rust-sdk seed

--- seed/rust-sdk/examples/readme-config/src/core/mod.rs
+++ seed/rust-sdk/examples/readme-config/src/core/mod.rs
@@ -5,12 +5,14 @@ pub mod flexible_datetime;
 mod http_client;
 pub mod number_serializers;
 mod oauth_token_provider;
+pub mod pagination;
 mod query_parameter_builder;
 mod request_options;
 mod utils;
 
-pub use http_client::{ByteStream, HttpClient, OAuthConfig};
+pub use http_client::{ByteStream, HttpClient, OAuthConfig, RawResponse};
 pub use oauth_token_provider::OAuthTokenProvider;
+pub use pagination::{AsyncPaginator, PaginationResult, SyncPaginator};
 pub use query_parameter_builder::{parse_structured_query, QueryBuilder, QueryBuilderError};
 pub use request_options::RequestOptions;
 pub use utils::join_url;
