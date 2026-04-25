16f8ef5e0a0354440bd0d11e7d6aa6a4d6fbd4c8
Swimburger <3382717+Swimburger@users.noreply.github.com>
2026-04-25 03:44:16 +0000

    chore(go): update go-sdk seed

--- seed/go-sdk/undiscriminated-unions/v0/union/raw_client.go
+++ seed/go-sdk/undiscriminated-unions/v0/union/raw_client.go
@@ -275,6 +275,47 @@ func (r *RawClient) NestedUnions(
 	}, nil
 }
 
+func (r *RawClient) GetWithBaseProperties(
+	ctx context.Context,
+	request *fern.UnionWithBaseProperties,
+	opts ...option.RequestOption,
+) (*core.Response[*fern.UnionWithBaseProperties], error) {
+	options := core.NewRequestOptions(opts...)
+	baseURL := internal.ResolveBaseURL(
+		options.BaseURL,
+		r.baseURL,
+		"",
+	)
+	endpointURL := baseURL + "/with-base-properties"
+	headers := internal.MergeHeaders(
+		r.options.ToHeader(),
+		options.ToHeader(),
+	)
+	var response *fern.UnionWithBaseProperties
+	raw, err := r.caller.Call(
+		ctx,
+		&internal.CallParams{
+			URL:             endpointURL,
+			Method:          http.MethodPost,
+			Headers:         headers,
+			MaxAttempts:     options.MaxAttempts,
+			BodyProperties:  options.BodyProperties,
+			QueryParameters: options.QueryParameters,
+			Client:          options.HTTPClient,
+			Request:         request,
+			Response:        &response,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &core.Response[*fern.UnionWithBaseProperties]{
+		StatusCode: raw.StatusCode,
+		Header:     raw.Header,
+		Body:       response,
+	}, nil
+}
+
 func (r *RawClient) TestCamelCaseProperties(
 	ctx context.Context,
 	request *fern.PaymentRequest,
