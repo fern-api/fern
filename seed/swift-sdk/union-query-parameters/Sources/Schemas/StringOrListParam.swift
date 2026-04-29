3edd7cf7bcbb916ea870812aa81e2c5bc2c8763c
patrickthornton <70873350+patrickthornton@users.noreply.github.com>
2026-04-29 05:41:18 +0000

    chore(swift): update swift-sdk seed

---
+++ seed/swift-sdk/union-query-parameters/Sources/Schemas/StringOrListParam.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+/// Either a single string or a list of strings.
+public enum StringOrListParam: Codable, Hashable, Sendable {
+    case string(String)
+    case stringArray([String])
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let value = try? container.decode(String.self) {
+            self = .string(value)
+        } else if let value = try? container.decode([String].self) {
+            self = .stringArray(value)
+        } else {
+            throw DecodingError.dataCorruptedError(
+                in: container,
+                debugDescription: "Unexpected value."
+            )
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws -> Void {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .string(let value):
+            try container.encode(value)
+        case .stringArray(let value):
+            try container.encode(value)
+        }
+    }
+}
