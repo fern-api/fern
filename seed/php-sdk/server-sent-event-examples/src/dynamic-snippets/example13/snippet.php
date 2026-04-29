09d98916b1fc8ba95911a9dbaad8d939352a3b5d
patrickthornton <70873350+patrickthornton@users.noreply.github.com>
2026-04-29 16:49:27 +0000

    chore(php): update php-sdk seed

--- seed/php-sdk/server-sent-event-examples/src/dynamic-snippets/example13/snippet.php
+++ seed/php-sdk/server-sent-event-examples/src/dynamic-snippets/example13/snippet.php
@@ -3,15 +3,15 @@
 namespace Example;
 
 use Seed\SeedClient;
-use Seed\Completions\Requests\StreamEventsDiscriminantInDataRequest;
+use Seed\Completions\Requests\StreamEventsContextProtocolRequest;
 
 $client = new SeedClient(
     options: [
         'baseUrl' => 'https://api.fern.com',
     ],
 );
-$client->completions->streamEventsDiscriminantInData(
-    new StreamEventsDiscriminantInDataRequest([
+$client->completions->streamEventsContextProtocol(
+    new StreamEventsContextProtocolRequest([
         'query' => 'query',
     ]),
 );
