0f70d7406d7a2d36d25b5dc721d1721e512b66b1
patrickthornton <70873350+patrickthornton@users.noreply.github.com>
2026-04-29 16:49:36 +0000

    chore(rust): update rust-sdk seed

--- seed/rust-sdk/server-sent-event-examples/with-wire-tests/dynamic-snippets/example13.rs
+++ seed/rust-sdk/server-sent-event-examples/with-wire-tests/dynamic-snippets/example13.rs
@@ -9,8 +9,8 @@ async fn main() {
     let client = ServerSentEventsClient::new(config).expect("Failed to build client");
     client
         .completions
-        .stream_events_discriminant_in_data(
-            &StreamEventsDiscriminantInDataRequest {
+        .stream_events_context_protocol(
+            &StreamEventsContextProtocolRequest {
                 query: "query".to_string(),
             },
             None,
