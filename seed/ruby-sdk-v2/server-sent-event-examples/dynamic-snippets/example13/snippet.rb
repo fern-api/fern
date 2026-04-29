61ea01e7d947545c6f28b5bf10daa5192773ccc4
patrickthornton <70873350+patrickthornton@users.noreply.github.com>
2026-04-29 16:49:36 +0000

    chore(ruby): update ruby-sdk-v2 seed

--- seed/ruby-sdk-v2/server-sent-event-examples/dynamic-snippets/example13/snippet.rb
+++ seed/ruby-sdk-v2/server-sent-event-examples/dynamic-snippets/example13/snippet.rb
@@ -2,4 +2,4 @@ require "seed"
 
 client = Seed::Client.new(base_url: "https://api.fern.com")
 
-client.completions.stream_events_discriminant_in_data(query: "query")
+client.completions.stream_events_context_protocol(query: "query")
