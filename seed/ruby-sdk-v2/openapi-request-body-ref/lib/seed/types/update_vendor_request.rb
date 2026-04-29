0739cb2ed8cd53bb25f53178c467b52913c74fab
lifanzou <265923155+lifanzou@users.noreply.github.com>
2026-04-29 00:25:08 +0000

    chore(ruby): update ruby-sdk-v2 seed

--- seed/ruby-sdk-v2/openapi-request-body-ref/lib/seed/types/update_vendor_request.rb
+++ seed/ruby-sdk-v2/openapi-request-body-ref/lib/seed/types/update_vendor_request.rb
@@ -4,6 +4,7 @@ module Seed
   module Types
     class UpdateVendorRequest < Internal::Types::Model
       field :name, -> { String }, optional: false, nullable: false
+
       field :status, -> { Seed::Types::UpdateVendorRequestStatus }, optional: true, nullable: false
     end
   end
