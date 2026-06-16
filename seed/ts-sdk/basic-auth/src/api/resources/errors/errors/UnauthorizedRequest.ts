60aa6ca3e23e4dfb2c9ac934534c5257f63a1173
fern-support <126544928+fern-support@users.noreply.github.com>
2026-06-16 14:49:12 +0000

    chore(seed): update all seed snapshots

--- seed/ts-sdk/basic-auth/src/api/resources/errors/errors/UnauthorizedRequest.ts
+++ seed/ts-sdk/basic-auth/src/api/resources/errors/errors/UnauthorizedRequest.ts
@@ -17,6 +17,6 @@ export class UnauthorizedRequest extends errors.SeedBasicAuthError {
             Error.captureStackTrace(this, this.constructor);
         }
 
-        this.name = this.constructor.name;
+        this.name = "UnauthorizedRequest";
     }
 }
