19e5fd9309a7fdde436b4fdd516c1b8a4cdb83bc
Sandeep Dinesh <sandeep@buildwithfern.com>
2026-06-30 15:11:45 -0700

    chore(seed): update all seed snapshots (#16819)
    Co-authored-by: Swimburger <3382717+Swimburger@users.noreply.github.com>

--- seed/java-sdk/basic-auth-pw-omitted/basic-auth-pw-omitted/src/main/java/com/seed/basicAuthPwOmitted/resources/basicauth/RawBasicAuthClient.java
+++ seed/java-sdk/basic-auth-pw-omitted/basic-auth-pw-omitted/src/main/java/com/seed/basicAuthPwOmitted/resources/basicauth/RawBasicAuthClient.java
@@ -8,6 +8,7 @@ import com.seed.basicAuthPwOmitted.core.ClientOptions;
 import com.seed.basicAuthPwOmitted.core.MediaTypes;
 import com.seed.basicAuthPwOmitted.core.ObjectMappers;
 import com.seed.basicAuthPwOmitted.core.RequestOptions;
+import com.seed.basicAuthPwOmitted.core.RetryInterceptor;
 import com.seed.basicAuthPwOmitted.core.SeedBasicAuthPwOmittedApiException;
 import com.seed.basicAuthPwOmitted.core.SeedBasicAuthPwOmittedException;
 import com.seed.basicAuthPwOmitted.core.SeedBasicAuthPwOmittedHttpResponse;
@@ -58,6 +59,15 @@ public class RawBasicAuthClient {
         OkHttpClient client = clientOptions.httpClient();
         if (requestOptions != null && requestOptions.getTimeout().isPresent()) {
             client = clientOptions.httpClientWithTimeout(requestOptions);
+        }
+        if (requestOptions != null && requestOptions.getMaxRetries().isPresent()) {
+            okhttpRequest = okhttpRequest
+                    .newBuilder()
+                    .tag(
+                            RetryInterceptor.MaxRetriesOverride.class,
+                            new RetryInterceptor.MaxRetriesOverride(
+                                    requestOptions.getMaxRetries().get()))
+                    .build();
         }
         try (Response response = client.newCall(okhttpRequest).execute()) {
             ResponseBody responseBody = response.body();
@@ -120,6 +130,15 @@ public class RawBasicAuthClient {
         OkHttpClient client = clientOptions.httpClient();
         if (requestOptions != null && requestOptions.getTimeout().isPresent()) {
             client = clientOptions.httpClientWithTimeout(requestOptions);
+        }
+        if (requestOptions != null && requestOptions.getMaxRetries().isPresent()) {
+            okhttpRequest = okhttpRequest
+                    .newBuilder()
+                    .tag(
+                            RetryInterceptor.MaxRetriesOverride.class,
+                            new RetryInterceptor.MaxRetriesOverride(
+                                    requestOptions.getMaxRetries().get()))
+                    .build();
         }
         try (Response response = client.newCall(okhttpRequest).execute()) {
             ResponseBody responseBody = response.body();
