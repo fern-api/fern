a458d0fc6de29eda9134c92553433e4d9889b037
thesandlord <8902396+thesandlord@users.noreply.github.com>
2026-07-22 19:47:40 +0000

    chore(seed): update all seed snapshots

--- seed/java-sdk/java-single-property-endpoint/java-single-property-endpoint/src/main/java/com/snippets/Example0.java
+++ seed/java-sdk/java-single-property-endpoint/java-single-property-endpoint/src/main/java/com/snippets/Example0.java
@@ -5,17 +5,10 @@ import com.seed.singleProperty.resources.singleproperty.requests.GetThingRequest
 
 public class Example0 {
     public static void main(String[] args) {
-        SeedSinglePropertyClient client = SeedSinglePropertyClient
-            .builder()
-            .url("https://api.fern.com")
-            .build();
+        SeedSinglePropertyClient client =
+                SeedSinglePropertyClient.builder().url("https://api.fern.com").build();
 
-        client.singleProperty().doThing(
-            "id",
-            GetThingRequest
-                .builder()
-                .includeRemoteData(true)
-                .build()
-        );
+        client.singleProperty()
+                .doThing("id", GetThingRequest.builder().includeRemoteData(true).build());
     }
 }
