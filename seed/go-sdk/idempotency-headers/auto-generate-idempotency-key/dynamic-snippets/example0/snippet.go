8fcc61561d6a5beeb874704a325a280fad544b80
cadesark <283748896+cadesark@users.noreply.github.com>
2026-07-22 20:11:30 +0000

    chore(seed): update all seed snapshots

---
+++ seed/go-sdk/idempotency-headers/auto-generate-idempotency-key/dynamic-snippets/example0/snippet.go
@@ -0,0 +1,28 @@
+package example
+
+import (
+    context "context"
+
+    fern "github.com/idempotency-headers/fern"
+    client "github.com/idempotency-headers/fern/client"
+    option "github.com/idempotency-headers/fern/option"
+)
+
+func do() {
+    client := client.NewClient(
+        option.WithBaseURL(
+            "https://api.fern.com",
+        ),
+        option.WithToken(
+            "<token>",
+        ),
+    )
+    request := &fern.CreatePaymentRequest{
+        Amount: 1,
+        Currency: fern.CurrencyUsd,
+    }
+    client.Payment.Create(
+        context.TODO(),
+        request,
+    )
+}
