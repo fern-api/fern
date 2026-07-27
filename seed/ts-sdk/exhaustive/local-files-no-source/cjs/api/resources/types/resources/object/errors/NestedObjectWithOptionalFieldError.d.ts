e75f04e6cdc3ad7deb131cd9b1b85accdeab3e81
Naman Anand <iamnamananand996@gmail.com>
2026-07-28 01:59:05 +0530

    feat(typescript): redeclare typed body property on generated error classes (#17272)

--- seed/ts-sdk/exhaustive/local-files-no-source/cjs/api/resources/types/resources/object/errors/NestedObjectWithOptionalFieldError.d.ts
+++ seed/ts-sdk/exhaustive/local-files-no-source/cjs/api/resources/types/resources/object/errors/NestedObjectWithOptionalFieldError.d.ts
@@ -2,5 +2,6 @@ import type * as core from "../../../../../../core/index.js";
 import * as errors from "../../../../../../errors/index.js";
 import type * as SeedExhaustive from "../../../../../index.js";
 export declare class NestedObjectWithOptionalFieldError extends errors.SeedExhaustiveError {
+    readonly body: SeedExhaustive.types.NestedObjectWithOptionalField;
     constructor(body: SeedExhaustive.types.NestedObjectWithOptionalField, rawResponse?: core.RawResponse);
 }
--- seed/ts-sdk/exhaustive/local-files-no-source/esm/api/resources/types/resources/union/errors/ErrorWithUnionBody.d.mts
+++ seed/ts-sdk/exhaustive/local-files-no-source/esm/api/resources/types/resources/union/errors/ErrorWithUnionBody.d.mts
@@ -2,5 +2,6 @@ import type * as core from "../../../../../../core/index.mjs";
 import * as errors from "../../../../../../errors/index.mjs";
 import type * as SeedExhaustive from "../../../../../index.mjs";
 export declare class ErrorWithUnionBody extends errors.SeedExhaustiveError {
+    readonly body: SeedExhaustive.types.Animal;
     constructor(body: SeedExhaustive.types.Animal, rawResponse?: core.RawResponse);
 }
