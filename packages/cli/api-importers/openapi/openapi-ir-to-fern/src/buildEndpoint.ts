34475bebdbfffef7b536930a38b29176f650dbdf
naman.anand@postman.com <iamnamananand996@gmail.com>
2026-07-27 17:47:50 +0000

    fix(openapi): scope error schema consistency checks per namespace

--- packages/cli/api-importers/openapi/openapi-ir-to-fern/src/buildEndpoint.ts
+++ packages/cli/api-importers/openapi/openapi-ir-to-fern/src/buildEndpoint.ts
@@ -486,7 +486,7 @@ export function buildEndpoint({
 
         context.builder.addError(errorDeclarationFile, {
             name: errorName,
-            schema: context.isErrorUnknownSchema(parseInt(statusCode))
+            schema: context.isErrorUnknownSchema({ statusCode: parseInt(statusCode), namespace: endpoint.namespace })
                 ? { ...errorDeclaration, type: "unknown" }
                 : errorDeclaration
         });
