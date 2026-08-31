9eb14ca8b3eaf2d658663a8c9dcada35f7f21a28
thesandlord <8902396+thesandlord@users.noreply.github.com>
2026-08-31 16:28:06 +0000

    chore(seed): update all seed snapshots

---
+++ seed/go-sdk/idempotency-headers/auto-generate-idempotency-key/dynamic-snippets/example1/snippet.go
@@ -0,0 +1,23 @@
+package example
+
+import (
+    context "context"
+
+    client "github.com/idempotency-headers/fern/client"
+    option "github.com/idempotency-headers/fern/option"
+)
+
+func do() {
+    client := client.NewClient(
+        option.WithBaseURL(
+            "https://api.fern.com",
+        ),
+        option.WithToken(
+            "<token>",
+        ),
+    )
+    client.Payment.Delete(
+        context.TODO(),
+        "paymentId",
+    )
+}
